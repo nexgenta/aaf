46a6402a1e5ca53cfddb7b475956ed97004ecfda
tbingham <tbingham>
2001-04-17 04:46:44 +0000

    First round of effects checkins

--- ref-impl/src/impl/ImplEnumAAFDataDefs.cpp
+++ ref-impl/src/impl/ImplEnumAAFDataDefs.cpp
@@ -1,10 +1,26 @@
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+
 /***********************************************\
-*												*
-* Advanced Authoring Format						*
-*												*
-* Copyright (c) 1998-1999 Avid Technology, Inc. *
-*												*
+*	Stub only.   Implementation not yet added	*
 \***********************************************/
+
 
 #ifndef __ImplAAFDataDef_h__
 #include "ImplAAFDataDef.h"
@@ -20,242 +36,57 @@
 #include "ImplEnumAAFDataDefs.h"
 #endif
 
-#include "ImplAAFDictionary.h"
-#include "ImplAAFHeader.h"
-#include "ImplAAFObjectCreation.h"
-
 #include <assert.h>
 #include <string.h>
-#include "aafErr.h"
 
-extern "C" const aafClassID_t CLSID_EnumAAFDataDefs;
 
 ImplEnumAAFDataDefs::ImplEnumAAFDataDefs ()
-{
-	_current = 0;
-	_enumObj = NULL;
-	_enumProp = NULL;
-	_enumStrongProp = NULL;
-}
+{}
 
 
 ImplEnumAAFDataDefs::~ImplEnumAAFDataDefs ()
-{
-	if (_enumObj)
-	{
-		_enumObj->ReleaseReference();
-		_enumObj = NULL;
-	}
-}
+{}
 
 
 AAFRESULT STDMETHODCALLTYPE
     ImplEnumAAFDataDefs::NextOne (
-      ImplAAFDataDef **ppDataDef)
+      ImplAAFDataDef ** /*ppDataDef*/)
 {
-	aafUInt32			numElem;
-	aafUID_t			value;
-	ImplAAFHeader		*head = NULL;
-	ImplAAFDictionary	*dict = NULL;
-
-	if(_enumProp != NULL)
-		numElem = _enumProp->size() / sizeof(aafUID_t);
-	else if(_enumStrongProp != NULL)
-	{
-		size_t	siz;
-		
-		_enumStrongProp->getSize(siz);
-		numElem = siz;
-	}
-	else
-		return(AAFRESULT_INCONSISTANCY);
-
-	if(ppDataDef == NULL)
-		return(AAFRESULT_NULL_PARAM);
-	if(_current >= numElem)
-		return AAFRESULT_NO_MORE_OBJECTS;
-	XPROTECT()
-	{
-		if(_enumProp != NULL)
-		{
-			_enumProp->getValueAt(&value, _current);
-			CHECK(_enumObj->MyHeadObject(&head));
-			CHECK(head->GetDictionary (&dict));
-			CHECK(dict->LookupDataDefintion(&value, ppDataDef));
-			head->ReleaseReference();
-			head = NULL;
-			dict->ReleaseReference();
-			dict = NULL;
-		}
-		else if(_enumStrongProp != NULL)
-		{
-			_enumStrongProp->getValueAt(*ppDataDef, _current);
-			(*ppDataDef)->AcquireReference();
-		}
-		else
-			RAISE(AAFRESULT_INCONSISTANCY);
-		_current++;
-		if (head) {
-			head->ReleaseReference();
-			head = NULL;
-		}
-		if (dict) {
-			dict->ReleaseReference();
-			dict = NULL;
-		}
-	}
-	XEXCEPT
-	{
-		if(head)
-		  head->ReleaseReference();
-		head = 0;
-		if(dict)
-		  dict->ReleaseReference();
-		dict = 0;
-	}
-	XEND;
-
-	return(AAFRESULT_SUCCESS); 
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
 
 
 AAFRESULT STDMETHODCALLTYPE
     ImplEnumAAFDataDefs::Next (
-      aafUInt32  count,
-      ImplAAFDataDef ** ppDataDefs,
-      aafUInt32 *pFetched)
+      aafUInt32  /*count*/,
+      ImplAAFDataDef ** /*ppDataDef*/,
+      aafUInt32 *  /*pFetched*/)
 {
-	ImplAAFDataDef**		ppDef;
-	aafUInt32				numDefs;
-	HRESULT					hr;
-
-	if ((pFetched == NULL && count != 1) || (pFetched != NULL && count == 1))
-		return E_INVALIDARG;
-
-	// Point at the first component in the array.
-	ppDef = ppDataDefs;
-	for (numDefs = 0; numDefs < count; numDefs++)
-	{
-		hr = NextOne(ppDef);
-		if (FAILED(hr))
-			break;
-
-		// Point at the next component in the array.  This
-		// will increment off the end of the array when
-		// numComps == count-1, but the for loop should
-		// prevent access to this location.
-		ppDef++;
-	}
-	
-	if (pFetched)
-		*pFetched = numDefs;
-
-	return hr;
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
 
 
 AAFRESULT STDMETHODCALLTYPE
     ImplEnumAAFDataDefs::Skip (
-      aafUInt32  count)
+      aafUInt32  /*count*/)
 {
-	AAFRESULT	hr;
-	aafUInt32	newCurrent;
-	aafUInt32	numElem;
-
-	if(_enumProp != NULL)
-		numElem = _enumProp->size() / sizeof(aafUID_t);
-	else if(_enumStrongProp != NULL)
-	{
-		size_t	siz;
-		
-		_enumStrongProp->getSize(siz);
-		numElem = siz;
-	}
-	else
-		return(AAFRESULT_INCONSISTANCY);
-
-	newCurrent = _current + count;
-
-	if(newCurrent < numElem)
-	{
-		_current = newCurrent;
-		hr = AAFRESULT_SUCCESS;
-	}
-	else
-	{
-		hr = E_FAIL;
-	}
-
-	return hr;
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
 
 
 AAFRESULT STDMETHODCALLTYPE
     ImplEnumAAFDataDefs::Reset ()
 {
-	_current = 0;
-	return AAFRESULT_SUCCESS;
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
 
 
 AAFRESULT STDMETHODCALLTYPE
     ImplEnumAAFDataDefs::Clone (
-      ImplEnumAAFDataDefs **ppEnum)
+      ImplEnumAAFDataDefs ** /*ppEnum*/)
 {
-	ImplEnumAAFDataDefs	*result;
-	AAFRESULT					hr;
-
-	result = (ImplEnumAAFDataDefs *)CreateImpl(CLSID_EnumAAFDataDefs);
-	if (result == NULL)
-		return E_FAIL;
-
-	if(_enumProp != NULL)
-		hr = result->SetEnumProperty(_enumObj, _enumProp);
-	else if(_enumStrongProp != NULL)
-		hr = result->SetEnumStrongProperty(_enumObj, _enumStrongProp);
-	if (SUCCEEDED(hr))
-	{
-		result->_current = _current;
-		*ppEnum = result;
-	}
-	else
-	{
-	  result->ReleaseReference();
-	  result = 0;
-	  *ppEnum = NULL;
-	}
-	
-	return hr;
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
 
 
-AAFRESULT STDMETHODCALLTYPE
-    ImplEnumAAFDataDefs::SetEnumProperty( ImplAAFObject *pObj, DataDefWeakRefArrayProp_t *pProp)
-{
-	if (_enumObj)
-	  _enumObj->ReleaseReference();
-	_enumObj = 0;
-	_enumObj = pObj;
-	if (pObj)
-		pObj->AcquireReference();
-	_enumProp = pProp;				// Don't refcount, same lifetime as the object.
-	_enumStrongProp = NULL;
 
-	return AAFRESULT_SUCCESS;
-}
-
-AAFRESULT STDMETHODCALLTYPE
-    ImplEnumAAFDataDefs::SetEnumStrongProperty( ImplAAFObject *pObj, DataDefStrongRefArrayProp_t *pProp)
-{
-	if (_enumObj)
-	  _enumObj->ReleaseReference();
-	_enumObj = 0;
-	_enumObj = pObj;
-	if (pObj)
-		pObj->AcquireReference();
-	/**/
-	_enumStrongProp = pProp;		// Don't refcount, same lifetime as the object.
-	_enumProp = NULL;
-
-	return AAFRESULT_SUCCESS;
-}
