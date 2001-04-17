4879100a576b885dc830675b84dd28021c32f9bc
tbingham <tbingham>
2001-04-17 09:22:59 +0000

    Fix BoundsChecker complaints.

--- ref-impl/src/impl/ImplAAFDictionary.cpp
+++ ref-impl/src/impl/ImplAAFDictionary.cpp
@@ -143,30 +143,30 @@ ImplAAFDictionary::~ImplAAFDictionary ()
   size_t size = _codecDefinitions.getSize();
   for (i = 0; i < size; i++)
 	{
-		ImplAAFCodecDef *pPlug = _codecDefinitions.setValueAt(0, i);
-		if (pPlug)
+		ImplAAFCodecDef *pCodec = _codecDefinitions.setValueAt(0, i);
+		if (pCodec)
 		{
-		  pPlug->ReleaseReference();
+		  pCodec->ReleaseReference();
 		}
 	}
 
 	size_t containerDefSize = _containerDefinitions.getSize();
 	for (i = 0; i < containerDefSize; i++)
 	{
-		ImplAAFContainerDef *pPlug = _containerDefinitions.setValueAt(0, i);
-		if (pPlug)
+		ImplAAFContainerDef *pContainer = _containerDefinitions.setValueAt(0, i);
+		if (pContainer)
 		{
-		  pPlug->ReleaseReference();
+		  pContainer->ReleaseReference();
 		}
 	}
 
 	size_t typeDefSize = _typeDefinitions.getSize();
 	for (i = 0; i < typeDefSize; i++)
 	{
-		ImplAAFTypeDef *pPlug = _typeDefinitions.setValueAt(0, i);
-		if (pPlug)
+		ImplAAFTypeDef *pType = _typeDefinitions.setValueAt(0, i);
+		if (pType)
 		{
-		  pPlug->ReleaseReference();
+		  pType->ReleaseReference();
 		}
 	}
 
@@ -411,7 +411,9 @@ AAFRESULT STDMETHODCALLTYPE
   _classDefinitions.appendValue(pClassDef);
   pClassDef->AcquireReference();
 	
-  pClassDef->SetDict(this);
+  // BobT 6/15/99: Remove ImplAAFDefObject::Get/SetDict() in favor of
+  // ImplAAFObject::GetDictionary().
+  // pClassDef->SetDict(this);
 
   return(AAFRESULT_SUCCESS);
 }
@@ -448,12 +450,18 @@ AAFRESULT STDMETHODCALLTYPE
     _typeDefinitions.appendValue(pTypeDef);
     pTypeDef->AcquireReference();
     // Set up the (non-persistent) dictionary pointer.
-    pTypeDef->SetDict(this);
+	//
+    // BobT 6/15/99: Remove ImplAAFDefObject::Get/SetDict() in favor of
+	// ImplAAFObject::GetDictionary().
+	// pTypeDef->SetDict(this);
   } else {
     // This type is already registered, probably because it was
     // already in the persisted dictionary.
     // Set up the (non-persistent) dictionary pointer.
-    pExistingTypeDef->SetDict(this);
+	//
+    // BobT 6/15/99: Remove ImplAAFDefObject::Get/SetDict() in favor of
+	// ImplAAFObject::GetDictionary().
+	// pExistingTypeDef->SetDict(this);
     pExistingTypeDef->ReleaseReference();
   }
 
@@ -563,12 +571,18 @@ AAFRESULT STDMETHODCALLTYPE
     _dataDefinitions.appendValue(pDataDef);
     pDataDef->AcquireReference();
     // Set up the (non-persistent) dictionary pointer.
-    pDataDef->SetDict(this);
+	//
+    // BobT 6/15/99: Remove ImplAAFDefObject::Get/SetDict() in favor of
+	// ImplAAFObject::GetDictionary().
+	// pDataDef->SetDict(this);
   } else {
     // This type is already registered, probably because it was
     // already in the persisted dictionary.
     // Set up the (non-persistent) dictionary pointer.
-    pExistingDataDef->SetDict(this);
+	//
+    // BobT 6/15/99: Remove ImplAAFDefObject::Get/SetDict() in favor of
+	// ImplAAFObject::GetDictionary().
+	// pExistingDataDef->SetDict(this);
     pExistingDataDef->ReleaseReference();
   }
 
