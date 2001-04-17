2aac0dbcf46fd3c458cfa9a66efa711582a0d347
tbingham <tbingham>
2001-04-17 06:38:06 +0000

    First steps to making an AafOmf DLL

--- Utilities/AafOmf/AafOmf.h
+++ Utilities/AafOmf/AafOmf.h
@@ -1,3 +1,5 @@
+#ifndef _AAF_OMF_
+#define _AAF_OMF_	1
 /***********************************************************************
  *
  *              Copyright (c) 1998-1999 Avid Technology, Inc.
@@ -50,26 +52,19 @@
 #include "AAFTypeDefUIDs.h"
 #include "StreamLogger.h"
 
+//#ifndef _INCLUDED_OMF2_
+//#define _INCLUDED_OMF2_
+//namespace OMF2
+//{
+//#include "omPublic.h"
+//#include "omMedia.h"
+//}
+//#endif
+
 const int MAX_INDENT = 8;
 const unsigned	kLogError = 0; 	// Error level.
 const unsigned	kLogWarn 	= 1;  // Warning level.
 const unsigned	kLogInfo  = 2; 	// Informational level.
-
-// ============================================================================
-// simple helper class to initialize and cleanup COM library.
-// ============================================================================
-struct CComInitialize
-{
-	CComInitialize()
-	{
-		CoInitialize(NULL);
-	}
-
-	~CComInitialize()
-	{
-		CoUninitialize();
-	}
-};
 
 typedef struct _AafOmfGlobals
 {
@@ -109,6 +104,8 @@ typedef struct _AafOmfGlobals
 	// MC Private Properties
 	OMF2::omfProperty_t		pvtEffectIDProp;
 	OMF2::omfProperty_t		pvtAppCode;
+	OMF2::omfProperty_t		pvtAttributes;
+	OMF2::omfProperty_t		pvtDataAttribute;
 
 	// Codec Properties
 	OMF2::omfProperty_t		omCDCIComponentWidth;
@@ -118,27 +115,41 @@ typedef struct _AafOmfGlobals
 	OMF2::omfProperty_t		omCDCIWhiteReferenceLevel;
 	OMF2::omfProperty_t		omCDCIColorRange;
 	OMF2::omfProperty_t		omCDCIPaddingBits;
+	~_AafOmfGlobals( void )		{ delete pLogger; }
 } AafOmfGlobals;
+
+
+// ============================================================================
+// simple helper class to initialize and cleanup COM library.
+// ============================================================================
+struct CComInitialize
+{
+	CComInitialize()
+	{
+		CoInitialize(NULL);
+	}
+
+	~CComInitialize()
+	{
+		CoUninitialize();
+	}
+};
+
 
 int deleteFile( char* fileName );
 void IncIndentLevel( void );
 void DecIndentLevel( void );
 void AUIDtoString(aafUID_t *uid, char *buf);
+void MobIDtoString(aafMobID_t *uid, char *buf);
 AAFRESULT aafMobIDFromMajorMinor(
         aafUInt32	major,
 		aafUInt32	minor,
-		aafUID_t *mobID);     /* OUT - Newly created Mob ID */
+		aafMobID_t *mobID);     /* OUT - Newly created Mob ID */
 void RegisterCodecProperties(AafOmfGlobals *globals, OMF2::omfSessionHdl_t OMFSession);
-void RegisterOMFMCPrivate(AafOmfGlobals *globals, OMF2::omfSessionHdl_t OMFSession);
-void RegisterAAFMCPrivate(IAAFDictionary * dict);
-HRESULT SetIntegerPropOnObject(IAAFObject* pObj, aafUID_t* pClassID, aafUID_t* pPropID, const aafUID_t* pIntTypeID,
-							   aafMemPtr_t pValue, aafUInt32 ValueSize, IAAFDictionary *dict);
-
-HRESULT GetIntegerPropFromObject(IAAFObject* pObj, const aafUID_t* pClassID, aafUID_t* pPropID,
-								 const aafUID_t* pIntTypeID, aafMemPtr_t pValue, aafUInt32 ValueSize, IAAFDictionary *dict);
 
 
 #define COMMON_ERR_BASE		(AAFRESULT)0xE0000000
 #define AAF2OMF_ERR_BASE	(AAFRESULT)0xE0001000
 #define OMF2AAF_ERR_BASE	(AAFRESULT)0xE0006000
 #define AAFRESULT_FILE_NOT_OMF (OMF2AAF_ERR_BASE + 1)
+#endif
