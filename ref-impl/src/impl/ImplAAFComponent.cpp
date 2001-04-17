3f5915734437e07b36b945c166ba71f34d6033fc
tbingham <tbingham>
2001-04-17 09:06:36 +0000

    Obtain property ids via the automatically generated AAFMetaDictionary.h. Property ids are now (mostly) unique within an AAF file. Increment the stored format version number from 0.2 to 0.3.

--- ref-impl/src/impl/ImplAAFComponent.cpp
+++ ref-impl/src/impl/ImplAAFComponent.cpp
@@ -28,8 +28,8 @@
 #include "aafCvt.h"
 
 ImplAAFComponent::ImplAAFComponent ():
-	_dataDef(	PID_COMPONENT_DATADEF,	"dataDef"),
-	_length(	PID_COMPONENT_LENGTH,	"length")
+	_dataDef(	PID_Component_DataDefinition,	"Data Definition"),
+	_length(	PID_Component_Length,	"Length")
 {
 	_persistentProperties.put(   _dataDef.address());
 	_persistentProperties.put(   _length.address());
@@ -125,7 +125,7 @@ AAFRESULT STDMETHODCALLTYPE
  * Private Function: SetNewProps()
  *
  *      This is an internal function that is called by functions that
- *      create new components.  It sets the length and datakind property
+ *      create new components.  It sets the length and data definition property
  *      values on the input component object.
  *
  *      This function supports both 1.x and 2.x files.
