7d09b1f22edf155fb834cc7494d1e3b3e2c85ad0
tbingham <tbingham>
2001-04-17 02:57:35 +0000

    Change DataDef internals & split of dataDef defines

---
+++ ref-impl/src/impl/ImplAAFTimecodeStream12M.h
@@ -0,0 +1,91 @@
+//@doc
+//@class    AAFTimecodeStream12M | Implementation class for AAFTimecodeStream12M
+#ifndef __ImplAAFTimecodeStream12M_h__
+#define __ImplAAFTimecodeStream12M_h__
+
+
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+
+/***********************************************\
+*	Stub only.   Implementation not yet added	*
+\***********************************************/
+
+
+#ifndef __AAFTypes_h__
+#include "AAFTypes.h"
+#endif
+
+
+
+
+
+
+#ifndef __ImplAAFTimecodeStream_h__
+#include "ImplAAFTimecodeStream.h"
+#endif
+
+
+class ImplAAFTimecodeStream12M : public ImplAAFTimecodeStream
+{
+public:
+  //
+  // Constructor/destructor
+  //
+  //********
+  ImplAAFTimecodeStream12M ();
+
+protected:
+  virtual ~ImplAAFTimecodeStream12M ();
+
+public:
+  /****/
+  // Override from AAFTimecodeStream
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetSampleSize (/*[out]*/ aafUInt32 *  pSampleSize);
+
+
+public:
+	// SDK-internal calls
+  virtual AAFRESULT STDMETHODCALLTYPE
+	UnpackTimecode(aafUInt8 *buffer, aafUInt32 buflen, aafUInt32 fps, aafTimecode_t *tc);
+  virtual AAFRESULT STDMETHODCALLTYPE
+	PackTimecode(aafTimecode_t *tc, aafUInt8 *buffer, aafUInt32 buflen);
+  virtual AAFRESULT STDMETHODCALLTYPE
+	UnpackUserBits(aafUInt8 *packedBuffer, aafUInt32 packedBuflen,
+					aafUInt8 *unpackedBuffer, aafUInt32 unpackedBuflen);
+  virtual AAFRESULT STDMETHODCALLTYPE
+	PackUserBits(aafUInt8 *unpackedBuffer, aafUInt32 unpackedBuflen,
+					aafUInt8 *packedBuffer, aafUInt32 packedBuflen);
+
+
+
+public:
+  // Declare this class to be storable.
+  //
+  OMDECLARE_STORABLE(ImplAAFTimecodeStream12M)
+
+  // Declare the module test method. The implementation of the will be be
+  // in /test/ImplAAFTimecodeStream12MTest.cpp.
+  static AAFRESULT test();
+};
+
+#endif // ! __ImplAAFTimecodeStream12M_h__
+
+
