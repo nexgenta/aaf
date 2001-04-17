f52fbb5b586d2283a33de7dca9158721570b2342
tbingham <tbingham>
2001-04-17 05:16:06 +0000

    COM CoClass that ansi file streams through the implementation of the IAAFEssenceContainer interface. This is the CoClass created to read and write essence to external files.

--- ref-impl/plugins/CAAFEssenceFileContainer.cpp
+++ ref-impl/plugins/CAAFEssenceFileContainer.cpp
@@ -1,52 +1,32 @@
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- * prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+#include <stdio.h>
 
 #include "CAAFEssenceFileContainer.h"
 #include "CAAFEssenceFileStream.h"
 
 #include <assert.h>
 #include "AAFResult.h"
-#include "AAFDefUIDs.h"
-#include "aafErr.h"
-#include "AAFStoredObjectIDs.h"
-#include "AAFContainerDefs.h"
 
 #include <errno.h>
-
-#if defined(_MAC) || defined(macintosh)
-#include <wstring.h>
-#endif
-
-const aafUID_t  EXAMPLE_FILE_PLUGIN =	{ 0x914B3AD1, 0xEDE7, 0x11d2, { 0x80, 0x9F, 0x00, 0x60, 0x08, 0x14, 0x3e, 0x6f } };
 
 
 // CLSID for AAFEssenceFileStream 
 // {a7337030-c103-11d2-8089-006008143e6f}
 const CLSID CLSID_AAFEssenceFileContainer = { 0xa7337030, 0xc103, 0x11d2, { 0x80, 0x89, 0x00, 0x60, 0x08, 0x14, 0x3e, 0x6f } };
+
+
+// Default Interface for AAFEssenceFileContainer 
+// {a7337031-c103-11d2-808a-006008143e6f}
+const IID IID_IAAFEssenceContainer = { 0xa7337031, 0xc103, 0x11d2, { 0x80, 0x8a, 0x00, 0x60, 0x08, 0x14, 0x3e, 0x6f } };
+
+
 
 // convenient error handlers.
 inline void checkResult(HRESULT r)
@@ -89,19 +69,6 @@ void CAAFEssenceFileContainer::SetLastFileStream(CAAFEssenceFileStream *pLastFil
   _pLastFileStream = pLastFileStream; 
 }
 
-static bool PathsAreEqual(const wchar_t *path1, const wchar_t *path2)
-{
-	assert(NULL != path1 && NULL != path2);
-	
-	if (NULL == path1 || NULL == path2)
-		return false;
-	
-	int i = 0;
-	while ((path1[i] && path2[i]) && (path1[i] == path2[i]))
-		++i;
-			
-	return (path1[i] == path2[i]);
-}
 
 HRESULT CAAFEssenceFileContainer::CheckExistingStreams(
   const wchar_t *pwcPath,
@@ -113,7 +80,7 @@ HRESULT CAAFEssenceFileContainer::CheckExistingStreams(
   {
     // Is the file stream already open?
     const wchar_t *existingFilePath = pCurrentFileStream->FilePath();
-    if (PathsAreEqual(existingFilePath, pwcPath))
+    if (0 == wcscmp(existingFilePath, pwcPath))
     {
       // Check for invalid mode combinations.
       FileStreamMode existingFileMode = pCurrentFileStream->StreamMode();
@@ -154,165 +121,11 @@ HRESULT STDMETHODCALLTYPE
   return S_OK;
 }
 
-HRESULT STDMETHODCALLTYPE
-    CAAFEssenceFileContainer::GetNumDefinitions (aafInt32 *pDefCount)
-{
-	if(pDefCount == NULL)
-		return AAFRESULT_NULL_PARAM;
-	
-	*pDefCount = 1;
-	return AAFRESULT_SUCCESS;
-}
-
-HRESULT STDMETHODCALLTYPE
-    CAAFEssenceFileContainer::GetIndexedDefinitionID (aafInt32 index, aafUID_t *uid)
-{
-	if(uid == NULL)
-		return AAFRESULT_NULL_PARAM;
-
-	*uid = ContainerFile;		// UID of the DefObject
-	return AAFRESULT_SUCCESS;
-}
-
-HRESULT STDMETHODCALLTYPE
-    CAAFEssenceFileContainer::GetPluginDescriptorID (aafUID_t *uid)
-{
-	*uid = EXAMPLE_FILE_PLUGIN;		// UID of the PluginDescriptor
-	return AAFRESULT_SUCCESS;
-}
 
 
 HRESULT STDMETHODCALLTYPE
-    CAAFEssenceFileContainer::GetIndexedDefinitionObject (aafInt32 index, IAAFDictionary *dict, IAAFDefObject **def)
-{
-	aafUID_t			uid;
-	IAAFContainerDef	*container = NULL;
-    IAAFClassDef        *pcd = 0;
-
-	if((dict == NULL) || (def == NULL))
-		return AAFRESULT_NULL_PARAM;
-
-	XPROTECT()
-	{
-		CHECK(dict->LookupClassDef(AUID_AAFContainerDef, &pcd));
-		CHECK(pcd->CreateInstance(IID_IAAFContainerDef, 
-								  (IUnknown **)&container));
-		pcd->Release();
-		pcd = 0;
-		uid = ContainerFile;
-		CHECK(container->SetEssenceIsIdentified(kAAFFalse));
-		CHECK(container->Initialize(uid, L"Raw file Container", L"Essence is in a non-container file."));
-		CHECK(container->QueryInterface(IID_IAAFDefObject, (void **)def));
-		container->Release();
-		container = NULL;
-	}
-	XEXCEPT
-	{
-		if(container != NULL)
-		  {
-			container->Release();
-			container = 0;
-		  }
-		if (pcd)
-		  {
-			pcd->Release();
-			pcd = 0;
-		  }
-	}
-	XEND
-
-	return AAFRESULT_SUCCESS;
-}
-
-
-static wchar_t *manufURL = L"http://www.avid.com";
-static wchar_t *downloadURL = L"ftp://ftp.avid.com/pub/";
-const aafUID_t MANUF_JEFFS_PLUGINS = { 0xA6487F21, 0xE78F, 0x11d2, { 0x80, 0x9E, 0x00, 0x60, 0x08, 0x14, 0x3E, 0x6F } };
-static aafVersionType_t samplePluginVersion = { 0, 1 };
-
-static wchar_t *manufName = L"Avid Technology, Inc.";
-static wchar_t *manufRev = L"Rev 0.1";
-
-HRESULT STDMETHODCALLTYPE
-    CAAFEssenceFileContainer::CreateDescriptor (IAAFDictionary *dict, IAAFPluginDescriptor **descPtr)
-{
-	IAAFPluginDescriptor	*desc = NULL;
-	IAAFLocator				*pLoc = NULL;
-	IAAFNetworkLocator		*pNetLoc = NULL;
-	IAAFClassDef            *pcd = 0;
-	
-	XPROTECT()
-	{
-	    CHECK(dict->LookupClassDef(AUID_AAFPluginDescriptor, &pcd));
-		CHECK(pcd->CreateInstance(IID_IAAFPluginDescriptor, 
-								  (IUnknown **)&desc));
-		pcd->Release();
-		pcd = 0;
-		*descPtr = desc;
-		CHECK(desc->Initialize(EXAMPLE_FILE_PLUGIN, L"Essence File Container", L"Handles non-container files."));
-
-		CHECK(desc->SetCategoryClass(AUID_AAFDefObject));
-		CHECK(desc->SetPluginVersionString(manufRev));
-		CHECK(dict->LookupClassDef(AUID_AAFNetworkLocator, &pcd));
-		CHECK(pcd->CreateInstance(IID_IAAFLocator, 
-								  (IUnknown **)&pLoc));
-		CHECK(pLoc->SetPath (manufURL));
-		CHECK(pLoc->QueryInterface(IID_IAAFNetworkLocator, (void **)&pNetLoc));
-		CHECK(desc->SetManufacturerInfo(pNetLoc));
-		pNetLoc->Release();
-		pNetLoc = NULL;
-		pLoc->Release();
-		pLoc = NULL;
-
-		CHECK(desc->SetManufacturerID(MANUF_JEFFS_PLUGINS));
-		CHECK(desc->SetPluginManufacturerName(manufName));
-		CHECK(desc->SetIsSoftwareOnly(kAAFTrue));
-		CHECK(desc->SetIsAccelerated(kAAFFalse));
-		CHECK(desc->SetSupportsAuthentication(kAAFFalse));
-		
-		/**/
-		CHECK(pcd->CreateInstance(IID_IAAFLocator, 
-								  (IUnknown **)&pLoc));
-		pcd->Release ();
-		pcd = 0;
-		CHECK(pLoc->SetPath (downloadURL));
-		CHECK(desc->AppendLocator(pLoc));
-		desc->Release();
-		desc = NULL;
-		pLoc->Release();
-		pLoc = NULL;
-	}
-	XEXCEPT
-	{
-		if(desc != NULL)
-		  {
-			desc->Release();
-			desc = 0;
-		  }
-		if(pLoc != NULL)
-		  {
-			pLoc->Release();
-			pLoc = 0;
-		  }
-		if(pNetLoc != NULL)
-		  {
-			pNetLoc->Release();
-			pNetLoc = 0;
-		  }
-		if (pcd)
-		  {
-			pcd->Release ();
-			pcd = 0;
-		  }
-	}
-	XEND
-
-	return AAFRESULT_SUCCESS;
-}
-
-HRESULT STDMETHODCALLTYPE
-    CAAFEssenceFileContainer::CreateEssenceStream (const aafCharacter * pName,
-		aafMobID_constptr pMobID,
+    CAAFEssenceFileContainer::CreateEssenceStream (wchar_t *  pName,
+        aafUID_t *  pMobID,
         IAAFEssenceStream ** ppEssenceStream)
 {
   HRESULT hr = S_OK;
@@ -354,8 +167,8 @@ HRESULT STDMETHODCALLTYPE
 
 
 HRESULT STDMETHODCALLTYPE
-    CAAFEssenceFileContainer::CreateEssenceStreamWriteOnly (const aafCharacter * pName,
-        aafMobID_constptr pMobID,
+    CAAFEssenceFileContainer::CreateEssenceStreamWriteOnly (wchar_t *  pName,
+        aafUID_t *  pMobID,
         IAAFEssenceStream ** ppEssenceStream)
 {
   return HRESULT_NOT_IMPLEMENTED;
@@ -363,8 +176,8 @@ HRESULT STDMETHODCALLTYPE
 
 
 HRESULT STDMETHODCALLTYPE
-    CAAFEssenceFileContainer::OpenEssenceStreamReadOnly (const aafCharacter * pName,
-        aafMobID_constptr pMobID,
+    CAAFEssenceFileContainer::OpenEssenceStreamReadOnly (wchar_t *  pName,
+        aafUID_t *  pMobID,
         IAAFEssenceStream ** ppEssenceStream)
 {
   HRESULT hr = S_OK;
@@ -405,8 +218,8 @@ HRESULT STDMETHODCALLTYPE
 
 
 HRESULT STDMETHODCALLTYPE
-    CAAFEssenceFileContainer::OpenEssenceStreamAppend (const aafCharacter * pName,
-        aafMobID_constptr pMobID,
+    CAAFEssenceFileContainer::OpenEssenceStreamAppend (wchar_t *  pName,
+        aafUID_t *  pMobID,
         IAAFEssenceStream ** ppEssenceStream)
 {
   HRESULT hr = S_OK;
@@ -478,7 +291,17 @@ HRESULT CAAFEssenceFileContainer::InternalQueryInterface
     return CAAFUnknown::InternalQueryInterface(riid, ppvObj);
 }
 
+
 //
 // Define the contrete object support implementation.
 // 
-AAF_DEFINE_FACTORY(AAFEssenceFileContainer)
+HRESULT CAAFEssenceFileContainer::COMCreate(IUnknown *pUnkOuter, void **ppvObjOut)
+{
+	*ppvObjOut = NULL;
+ 	CAAFEssenceFileContainer *pAAFEssenceFileContainer = new CAAFEssenceFileContainer(pUnkOuter);
+ 	if (NULL == pAAFEssenceFileContainer)
+ 		return E_OUTOFMEMORY;
+ 	*ppvObjOut = static_cast<IAAFEssenceContainer *>(pAAFEssenceFileContainer);
+ 	((IUnknown *)(*ppvObjOut))->AddRef();
+ 	return S_OK;
+}
