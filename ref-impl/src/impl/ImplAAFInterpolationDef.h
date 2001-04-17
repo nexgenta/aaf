9ac12dd48a7321391ccea140fc36420014d7c889
tbingham <tbingham>
2001-04-17 03:33:10 +0000

    .

---
+++ ref-impl/src/impl/ImplAAFInterpolationDef.h
@@ -0,0 +1,62 @@
+//@doc
+//@class    AAFInterpolationDef | Implementation class for AAFInterpolationDef
+#ifndef __ImplAAFInterpolationDef_h__
+#define __ImplAAFInterpolationDef_h__
+
+
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+*                                          *
+\******************************************/
+
+
+
+class ImplAAFClassDef;
+
+
+/***********************************************\
+*												*
+* Advanced Authoring Format						*
+*												*
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+*												*
+\***********************************************/
+
+
+
+
+
+#ifndef __ImplAAFDefObject_h__
+#include "ImplAAFDefObject.h"
+#endif
+
+
+class ImplAAFInterpolationDef : public ImplAAFDefObject
+{
+public:
+  //
+  // Constructor/destructor
+  //
+  //********
+  ImplAAFInterpolationDef ();
+
+protected:
+  virtual ~ImplAAFInterpolationDef();
+
+public:
+
+  
+
+
+public:
+  // Declare this class to be storable.
+  //
+  OMDECLARE_STORABLE(ImplAAFInterpolationDef)
+};
+
+#endif // ! __ImplAAFInterpolationDef_h__
+
+
---
+++ ref-impl/src/impl/ImplAAFParameter.h
@@ -0,0 +1,70 @@
+//@doc
+//@class    AAFParameter | Implementation class for AAFParameter
+#ifndef __ImplAAFParameter_h__
+#define __ImplAAFParameter_h__
+
+/***********************************************\
+*												*
+* Advanced Authoring Format						*
+*												*
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+*												*
+\***********************************************/
+
+#ifndef __AAFTypes_h__
+#include "AAFTypes.h"
+#endif
+
+class ImplAAFParameterDef;
+class ImplAAFTypeDef;
+
+#ifndef __ImplAAFObject_h__
+#include "ImplAAFObject.h"
+#endif
+
+
+class ImplAAFParameter : public ImplAAFObject
+{
+public:
+  //
+  // Constructor/destructor
+  //
+  //********
+  ImplAAFParameter ();
+
+protected:
+  virtual ~ImplAAFParameter ();
+
+public:
+  virtual AAFRESULT STDMETHODCALLTYPE
+    SetParameterDefinition (
+      ImplAAFParameterDef *pParmDef);
+
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetParameterDefinition (
+      ImplAAFParameterDef **ppParmDef);	
+
+  virtual AAFRESULT STDMETHODCALLTYPE
+    SetTypeDefinition (
+      ImplAAFTypeDef*  pTypeDef);
+
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetTypeDefinition (
+      ImplAAFTypeDef **ppTypeDef);
+
+public:
+  // Declare this class to be storable.
+  //
+  OMDECLARE_STORABLE(ImplAAFParameter)
+
+private:
+
+	OMFixedSizeProperty<aafUID_t>				_parmDef;
+	OMFixedSizeProperty<aafUID_t>				_typeDef;
+};
+
+#endif // ! __ImplAAFParameter_h__
+
+
+
+
