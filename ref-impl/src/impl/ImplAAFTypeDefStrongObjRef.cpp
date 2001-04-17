ac340b742a03752db8e0165f8021a66414a56bd0
tbingham <tbingham>
2001-04-17 06:30:20 +0000

    Bug fix: Use PID_MetaDefinition_Identification instead of PID_DefinitionObject_Identification for weak references to class and type definitions.

--- ref-impl/src/impl/ImplAAFTypeDefStrongObjRef.cpp
+++ ref-impl/src/impl/ImplAAFTypeDefStrongObjRef.cpp
@@ -1,11 +1,29 @@
-/******************************************\
-*                                          *
-* Advanced Authoring Format                *
-*                                          *
-* Copyright (c) 1998 Avid Technology, Inc. *
-* Copyright (c) 1998 Microsoft Corporation *
-*                                          *
-\******************************************/
+/***********************************************************************
+ *
+ *              Copyright (c) 1998-1999 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and accompanying 
+ * documentation, and to distribute and sublicense application software
+ * incorporating this software for any purpose is hereby granted, 
+ * provided that (i) the above copyright notice and this permission
+ * notice appear in all copies of the software and related documentation,
+ * and (ii) the name Avid Technology, Inc. may not be used in any
+ * advertising or publicity relating to the software without the specific,
+ * prior written permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+ * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+ * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+ * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+ * LIABILITY.
+ *
+ ************************************************************************/
 
 
 #ifndef __ImplAAFPropValData_h__
@@ -20,23 +38,23 @@
 #include "ImplAAFTypeDefStrongObjRef.h"
 #endif
 
-#ifndef __ImplAAFHeader_h_
-#include "ImplAAFHeader.h"
-#endif
-
 #include "AAFStoredObjectIDs.h"
 #include "AAFPropertyIDs.h"
+#include "ImplAAFObjectCreation.h"
+#include "ImplAAFDictionary.h"
+
 
 #include <assert.h>
 #include <string.h>
 
 
-#define RELEASE_IF_SET(obj) \
-    if (obj) { obj->ReleaseReference(); obj = NULL; }
-
+extern "C" const aafClassID_t CLSID_AAFPropValData;
 
 ImplAAFTypeDefStrongObjRef::ImplAAFTypeDefStrongObjRef ()
-  : _referencedType (PID_TypeDefinitionStrongObjectReference_ReferencedType, "ReferencedType")
+  : _referencedType ( PID_TypeDefinitionStrongObjectReference_ReferencedType,
+                      "ReferencedType",
+                      "/Dictionary/ClassDefinitions",
+                      PID_MetaDefinition_Identification)
 {
   _persistentProperties.put(_referencedType.address());
 }
@@ -45,26 +63,36 @@ ImplAAFTypeDefStrongObjRef::ImplAAFTypeDefStrongObjRef ()
 ImplAAFTypeDefStrongObjRef::~ImplAAFTypeDefStrongObjRef ()
 {}
 
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFTypeDefStrongObjRef::Initialize (
+      const aafUID_t & id,
+      ImplAAFClassDef * pObjType,
+      const aafCharacter * pTypeName)
+{
+  if (! pObjType)  return AAFRESULT_NULL_PARAM;
+  if (! pTypeName)  return AAFRESULT_NULL_PARAM;
+
+  return pvtInitialize (id, pObjType, pTypeName);
+}
+
+
 // Override from AAFTypeDefObjectRef
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFTypeDefStrongObjRef::pvtInitialize (
-      const aafUID_t *  pID,
-      const aafUID_t * pRefdObjID,
-      wchar_t *  pTypeName)
+      const aafUID_t & id,
+      const ImplAAFClassDef * pClassDef,
+      const aafCharacter * pTypeName)
 {
-  if (! pID)       return AAFRESULT_NULL_PARAM;
-  if (! pRefdObjID)  return AAFRESULT_NULL_PARAM;
   if (! pTypeName) return AAFRESULT_NULL_PARAM;
 
   AAFRESULT hr;
 
-  hr = SetName (pTypeName);
-  if (! AAFRESULT_SUCCEEDED (hr)) return hr;
+  hr = ImplAAFMetaDefinition::Initialize(id, pTypeName, NULL);
+	if (AAFRESULT_FAILED (hr))
+    return hr;
 
-  _referencedType = *pRefdObjID;
-
-  hr = SetAUID (pID);
-  if (! AAFRESULT_SUCCEEDED (hr)) return hr;
+  _referencedType = pClassDef;
 
   return AAFRESULT_SUCCESS;
 }
@@ -81,14 +109,25 @@ AAFRESULT STDMETHODCALLTYPE
   OMStorable ** ppStorable = NULL;
   aafUInt32 bitsSize = 0;
   AAFRESULT hr;
-  ImplAAFPropValData * pvd = dynamic_cast<ImplAAFPropValData*>(pPropVal);
+  ImplAAFPropValDataSP pvd;
+  pvd = dynamic_cast<ImplAAFPropValData*>(pPropVal);
   assert (pvd);
 
   hr = pvd->AllocateBits (sizeof (OMStorable*), (aafMemPtr_t*) &ppStorable);
   if (AAFRESULT_FAILED(hr)) return hr;
-  assert (*ppStorable);
+  assert (ppStorable);
+  if (*ppStorable)
+	{
+	  // An object was already here.  Release it before we trash the
+	  // reference to it.
+	  ImplAAFObject * tmp = dynamic_cast<ImplAAFObject*>(*ppStorable);
+	  assert (tmp);
+	  tmp->ReleaseReference ();
+	  tmp = 0;
+	  *ppStorable = 0;
+	}
 
-  assert (*ppStorable);
+  assert (! *ppStorable);
   *ppStorable = pObject;
   pObject->AcquireReference ();
 
@@ -106,7 +145,8 @@ ImplAAFTypeDefStrongObjRef::GetObject (ImplAAFPropertyValue * pPropVal,
   OMStorable ** ppStorable = NULL;
   aafUInt32 bitsSize = 0;
   AAFRESULT hr;
-  ImplAAFPropValData * pvd = dynamic_cast<ImplAAFPropValData*>(pPropVal);
+  ImplAAFPropValDataSP pvd;
+  pvd = dynamic_cast<ImplAAFPropValData*>(pPropVal);
   assert (pvd);
 
   hr = pvd->GetBitsSize (&bitsSize);
@@ -116,7 +156,8 @@ ImplAAFTypeDefStrongObjRef::GetObject (ImplAAFPropertyValue * pPropVal,
   if (AAFRESULT_FAILED(hr)) return hr;
   assert (*ppStorable);
   assert (ppObject);
-  ImplAAFObject * pObj = dynamic_cast<ImplAAFObject*>(*ppStorable);
+  ImplAAFObject * pObj;
+  pObj = dynamic_cast<ImplAAFObject*>(*ppStorable);
   assert (pObj);
   *ppObject = pObj;
   (*ppObject)->AcquireReference ();
@@ -128,48 +169,54 @@ ImplAAFTypeDefStrongObjRef::GetObject (ImplAAFPropertyValue * pPropVal,
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFTypeDefStrongObjRef::GetObjectType (ImplAAFClassDef ** ppObjType)
 {
-  if (! ppObjType) return AAFRESULT_NULL_PARAM;
+  if (! ppObjType)
+	return AAFRESULT_NULL_PARAM;
 
-  ImplAAFHeader * pHead = NULL;
-  ImplAAFDictionary * pDict = NULL;
-  AAFRESULT rReturned = AAFRESULT_SUCCESS;
-  try
-	{
-	  AAFRESULT hr;
-	  hr = MyHeadObject(&pHead);
-	  if (AAFRESULT_FAILED(hr))
-		throw hr;
-	  assert (pHead);
-	  hr = (pHead->GetDictionary(&pDict));
-	  if (AAFRESULT_FAILED(hr))
-		throw hr;
-	  assert (pDict);
+   if(_referencedType.isVoid())
+		return AAFRESULT_OBJECT_NOT_FOUND;
+  ImplAAFClassDef *pClassDef = _referencedType;
 
-	  ImplAAFClassDef * pcd = NULL;
-	  aafUID_t id = _referencedType;
-	  hr = pDict->LookupClass (&id, &pcd);
-	  if (AAFRESULT_FAILED(hr))
-		throw hr;
+  *ppObjType = pClassDef;
+  assert (*ppObjType);
+  (*ppObjType)->AcquireReference ();
+  return AAFRESULT_SUCCESS;
 
-	  *ppObjType = pcd;
-	  (*ppObjType)->AcquireReference ();
-	}
-  catch (AAFRESULT &rCaught)
-	{
-	  rReturned = rCaught;
-	}
-  RELEASE_IF_SET (pHead);
-  RELEASE_IF_SET (pDict);
-
-  return rReturned;
 }
 
 // Override from AAFTypeDefObjectRef
 AAFRESULT STDMETHODCALLTYPE
-    ImplAAFTypeDefStrongObjRef::CreateValue (/*[in]*/ ImplAAFObject * /*pObj*/,
-      /*[out]*/ ImplAAFPropertyValue ** /*ppPropVal*/)
+    ImplAAFTypeDefStrongObjRef::CreateValue (/*[in]*/ ImplAAFObject * pObj,
+      /*[out]*/ ImplAAFPropertyValue ** ppPropVal)
 {
-  return AAFRESULT_NOT_IMPLEMENTED;
+  if (! pObj)
+	return AAFRESULT_NULL_PARAM;
+  if (! ppPropVal)
+	return AAFRESULT_NULL_PARAM;
+
+  ImplAAFPropValData * pvd = 0;
+  pvd = (ImplAAFPropValData*) CreateImpl (CLSID_AAFPropValData);
+  if (!pvd) return AAFRESULT_NOMEMORY;
+
+  ImplAAFPropValDataSP spPvd;
+  spPvd = pvd;
+  // SmartPointer operator= will automatically
+  // AddRef; CreateImpl *also* will addref, so we've got one too
+  // many.  Put us back to normal.
+  pvd->ReleaseReference ();
+  pvd = 0;
+
+  AAFRESULT hr;
+  hr = spPvd->Initialize (this);
+  if (AAFRESULT_FAILED (hr)) return hr;
+
+  hr = SetObject (spPvd, pObj);
+  if (AAFRESULT_FAILED (hr))
+	return hr;
+  assert (ppPropVal);
+  *ppPropVal = spPvd;
+  assert (*ppPropVal);
+  (*ppPropVal)->AcquireReference ();
+  return AAFRESULT_SUCCESS;
 }
 
 
@@ -186,7 +233,7 @@ AAFRESULT STDMETHODCALLTYPE
 
 aafBool ImplAAFTypeDefStrongObjRef::IsFixedSize (void) const
 {
-  return AAFTrue;
+  return kAAFTrue;
 }
 
 
@@ -196,4 +243,25 @@ size_t ImplAAFTypeDefStrongObjRef::PropValSize (void) const
 }
 
 
-OMDEFINE_STORABLE(ImplAAFTypeDefStrongObjRef, AUID_AAFTypeDefStrongObjRef);
+aafBool ImplAAFTypeDefStrongObjRef::IsRegistered (void) const
+{
+  return kAAFTrue;
+}
+
+
+size_t ImplAAFTypeDefStrongObjRef::NativeSize (void) const
+{
+  return sizeof (ImplAAFObject*);
+}
+
+
+OMProperty * ImplAAFTypeDefStrongObjRef::pvtCreateOMPropertyMBS
+  (OMPropertyId pid,
+   const char * name) const
+{
+  assert (name);
+  OMProperty * result =
+	new OMStrongReferenceProperty<ImplAAFObject> (pid, name);
+  assert (result);
+  return result;
+}
