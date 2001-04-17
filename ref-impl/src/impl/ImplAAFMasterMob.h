d89ad2e1afd1fb50a1f1e221f7668c8c93cf1ff0
tbingham <tbingham>
2001-04-17 03:49:43 +0000

    Where appropriate: change pointers to references, ref/ptr to const ref/ptr.

--- ref-impl/src/impl/ImplAAFMasterMob.h
+++ ref-impl/src/impl/ImplAAFMasterMob.h
@@ -4,13 +4,32 @@
 #define __ImplAAFMasterMob_h__
 
 
-/******************************************\
-*                                          *
-* Advanced Authoring Format                *
-*                                          *
-* Copyright (c) 1998 Avid Technology, Inc. *
-*                                          *
-\******************************************/
+/***********************************************************************
+ *
+ *              Copyright (c) 1998-1999 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and accompanying 
+ * documentation, and to distribute and sublicense application software
+ * incorporating this software for any purpose is hereby granted, 
+ * provided that (i) the above copyright notice and this permission
+ * notice appear in all copies of the software and related documentation,
+ * and (ii) the name Avid Technology, Inc. may not be used in any
+ * advertising or publicity relating to the software without the specific,
+ *  prior written permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+ * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+ * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+ * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+ * LIABILITY.
+ *
+ ************************************************************************/
 
 class ImplAAFSourceClip;
 
@@ -54,7 +73,7 @@ public:
   virtual AAFRESULT STDMETHODCALLTYPE
     AddMasterSlot
         (// @parm [in] Data kind of new Master Mob slot
-		 aafUID_t * pDataDef,
+		 const aafUID_t & dataDef,
 
 		 // @parm [in] Slot ID of the Source Mob slot to be added to the Master Mob
          aafSlotID_t  sourceSlotID,
@@ -66,7 +85,7 @@ public:
 		 aafSlotID_t  masterSlotID,
 
 		 // @parm [in, string] Name to assign to new slot in Master Mob
-		 aafWChar *  pSlotName);
+		 const aafWChar *  pSlotName);
 
 
 
@@ -118,7 +137,7 @@ public:
 		 aafSlotID_t  slotID,
 
 		 // @parm [in] Index of requested representation
-		 aafInt32  index,
+		 aafUInt32  index,
 
 		 // @parm [out] Requested Source Clip
          ImplAAFSourceClip ** ppSourceClip);
@@ -230,15 +249,15 @@ public:
   virtual AAFRESULT STDMETHODCALLTYPE
     NewPhysSourceRef
         (// @parm [in] Edit rate of slot to contain reference
-		 aafRational_t  editrate,
+		 const aafRational_t & editrate,
 
 		 // @parm [in] SlotID of slot to contain reference
 		 aafSlotID_t  aMobSlot,
 
 		 // @parm [in] Data kind of slot to contain reference
-         aafUID_t * pEssenceKind,
+         const aafUID_t & essenceKind,
 
-		aafSourceRef_t  ref,
+		 aafSourceRef_t  ref,
 
 		 // @parm [in] Length of the Source Clip
          aafLength_t  srcRefLength);
@@ -249,15 +268,15 @@ public:
   virtual AAFRESULT STDMETHODCALLTYPE
     AppendPhysSourceRef
         (// @parm [in] Edit rate of slot to contain reference
-		 aafRational_t  editrate,
+		 const aafRational_t & editrate,
 
 		 // @parm [in] SlotID of slot to contain reference
 		 aafSlotID_t  aMobSlot,
 
 		 // @parm [in] Data kind of slot to contain reference
-         aafUID_t * pEssenceKind,
+         const aafUID_t & essenceKind,
 
-		aafSourceRef_t  ref,
+		 aafSourceRef_t  ref,
 
 		 // @parm [in] Length of the Source Clip
          aafLength_t  srcRefLength);
@@ -404,9 +423,6 @@ public:
 	//@comm Replaces omfmGetNumChannels*/
 
 public:
-  // Declare this class to be storable.
-  //
-  OMDECLARE_STORABLE(ImplAAFMasterMob)
 
   // Internal to the SDK
   virtual AAFRESULT ReconcileMobLength(void);
