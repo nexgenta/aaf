5ae895d5d4fb5197b06d77c1f8d7f15f9c6d0932
tbingham <tbingham>
2001-04-17 04:10:33 +0000

    Initial Implementation.

--- ref-impl/src/impl/ImplAAFTaggedValue.h
+++ ref-impl/src/impl/ImplAAFTaggedValue.h
@@ -4,32 +4,14 @@
 #define __ImplAAFTaggedValue_h__
 
 
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- * prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
 
 #ifndef __ImplAAFTypeDef_h__
 #include "ImplAAFTypeDef.h"
@@ -39,7 +21,6 @@
 #include "ImplAAFObject.h"
 #endif
 
-class ImplAAFTypeDef;
 
 class ImplAAFTaggedValue : public ImplAAFObject
 {
@@ -62,16 +43,10 @@ public:
   virtual AAFRESULT STDMETHODCALLTYPE
     Initialize
         (// @parm [in, string] User defined name of this tagged value object
-         const aafCharacter * pName,
+         wchar_t *  pName,
 
-         // @parm [in] Type Definition of the following value data
-         ImplAAFTypeDef * pType,
-         
-         // @parm [in] Size of preallocated buffer
-         aafUInt32  valueSize,
-
-         // @parm [in, size_is(valueSize)] buffer containing value
-         aafDataBuffer_t  pValue);
+         // @parm [in] Type definition of this tagged value object
+         ImplAAFTypeDef * pTypeDef);
 
   //****************
   // GetName()
@@ -90,7 +65,7 @@ public:
   virtual AAFRESULT STDMETHODCALLTYPE
     GetNameBufLen
         // @parm [out] Name buffer length
-        (aafUInt32 *  pLen);
+        (aafInt32 *  pLen);
 
   //****************
   // GetTypeDefinition()
@@ -159,13 +134,19 @@ public:
 
 
 public:
+  // Declare this class to be storable.
+  //
+  OMDECLARE_STORABLE(ImplAAFTaggedValue)
+
+  // Declare the module test method. The implementation of the will be be
+  // in /test/ImplAAFTaggedValueTest.cpp.
+  static AAFRESULT test();
 
 	// persistent data
 	OMWideStringProperty				_name;
+	OMFixedSizeProperty<aafUID_t>		_type;
 	OMVariableSizeProperty<aafUInt8>	_value;
 
-  bool _initialized;
-  ImplAAFTypeDef * _cachedTypeDef;
 };
 
 #endif // ! __ImplAAFTaggedValue_h__
