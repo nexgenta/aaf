a295f109cdb9bcab1f8a13c037b1292df53d928b
tbingham <tbingham>
2001-04-17 10:02:42 +0000

    Fix some property name and size bugs.

--- ref-impl/src/impl/ImplAAFTypeDefExtEnum.cpp
+++ ref-impl/src/impl/ImplAAFTypeDefExtEnum.cpp
@@ -1,10 +1,29 @@
-/******************************************\
-*                                          *
-* Advanced Authoring Format                *
-*                                          *
-* Copyright (c) 1998 Avid Technology, Inc. *
-*                                          *
-\******************************************/
+/***********************************************************************
+ *
+ *              Copyright (c) 1998-1999 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and accompanying 
+ * documentation, and to distribute and sublicense application software
+ * incorporating this software for any purpose is hereby granted, 
+ * provided that (i) the above copyright notice and this permission
+ * notice appear in all copies of the software and related documentation,
+ * and (ii) the name Avid Technology, Inc. may not be used in any
+ * advertising or publicity relating to the software without the specific,
+ *  prior written permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+ * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+ * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+ * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+ * LIABILITY.
+ *
+ ************************************************************************/
 
 #include "AAFStoredObjectIDs.h"
 #include "AAFPropertyIDs.h"
@@ -38,8 +57,8 @@
 extern "C" const aafClassID_t CLSID_AAFPropValData;
 
 ImplAAFTypeDefExtEnum::ImplAAFTypeDefExtEnum ()
-  :	_ElementNames  ( PID_TypeDefinitionExtendibleEnumeration_ElementNames,  "Element Names"),
-	_ElementValues ( PID_TypeDefinitionExtendibleEnumeration_ElementValues, "Element Values")
+  :	_ElementNames  ( PID_TypeDefinitionExtendibleEnumeration_ElementNames,  "ElementNames"),
+	_ElementValues ( PID_TypeDefinitionExtendibleEnumeration_ElementValues, "ElementValues")
 {
   _persistentProperties.put(_ElementNames.address());
   _persistentProperties.put(_ElementValues.address());
@@ -269,6 +288,12 @@ AAFRESULT STDMETHODCALLTYPE
 
   AAFRESULT hr;
 
+  // Call this method to find out if this is a legal value
+  aafUInt32 tmp; // unused
+  hr = GetNameBufLenFromAUID (pValueIn, &tmp);
+  if (AAFRESULT_FAILED (hr))
+	return hr;
+
   ptd = BaseType ();
   assert (ptd);
 
@@ -321,7 +346,8 @@ AAFRESULT STDMETHODCALLTYPE
 	  // character for new name's trailing null
 	  newNameCharCount = origNameCharCount + wcslen (pName) + 1;
 	  namesBuf = new aafWChar[newNameCharCount];
-	  _ElementNames.getValue (namesBuf, origNameCharCount);
+	  if (origNameCharCount)
+		_ElementNames.getValue (namesBuf, origNameCharCount*sizeof(aafWChar));
 
 	  // Append new name to end of buffer.  Don't forget that original
 	  // buffer may have embedded nulls, so start copying at desired
@@ -334,7 +360,8 @@ AAFRESULT STDMETHODCALLTYPE
 
 	  // add one for new element to be appended
 	  valsBuf = new aafUID_t[origNumElems+1];
-	  _ElementValues.getValue (valsBuf, origNumElems);
+	  if (origNumElems)
+		_ElementValues.getValue (valsBuf, origNumElems*sizeof(aafUID_t));
 	  assert (pValue);
 	  valsBuf[origNumElems] = *pValue;
 
@@ -342,8 +369,8 @@ AAFRESULT STDMETHODCALLTYPE
 	  // Copy newly-appended name and value buffers out.  Do these
 	  // together, last so that if any errors occurred during the
 	  // calculations we won't leave the property half-appended.
-	  _ElementNames.setValue (namesBuf, newNameCharCount);
-	  _ElementValues.setValue (valsBuf, origNumElems+1);
+	  _ElementNames.setValue (namesBuf, newNameCharCount*sizeof(aafWChar));
+	  _ElementValues.setValue (valsBuf, (origNumElems+1)*sizeof (aafUID_t));
 	}
   catch (AAFRESULT &rCaught)
 	{
