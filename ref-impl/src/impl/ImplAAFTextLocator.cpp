b27f7f4827251772480e8f8825f5dde6c468baa5
tbingham <tbingham>
2001-04-17 04:23:23 +0000

    Initial Implementation.

--- ref-impl/src/impl/ImplAAFTextLocator.cpp
+++ ref-impl/src/impl/ImplAAFTextLocator.cpp
@@ -1,29 +1,11 @@
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- * prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+/***********************************************\
+*                                               *
+* Advanced Authoring Format                     *
+*                                               *
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+* Copyright (c) 1998-1999 Microsoft Corporation *
+*                                               *
+\***********************************************/
 
 #ifndef __ImplAAFTextLocator_h__
 #include "ImplAAFTextLocator.h"
@@ -38,7 +20,7 @@
 
 
 ImplAAFTextLocator::ImplAAFTextLocator () :
-	_name(PID_TextLocator_Name,		L"Name")
+	_name(PID_TextLocator_Name,		"Text Locator")
 {
 	_persistentProperties.put(_name.address());
 	_name = L"";
@@ -56,7 +38,7 @@ AAFRESULT STDMETHODCALLTYPE
 
 
 AAFRESULT STDMETHODCALLTYPE
-    ImplAAFTextLocator::GetName (aafCharacter* pNameBuf, aafUInt32  bufSize)
+    ImplAAFTextLocator::GetName (wchar_t* pNameBuf, aafInt32  bufSize)
 {
 	bool stat;
 	if (! pNameBuf)
@@ -75,7 +57,7 @@ AAFRESULT STDMETHODCALLTYPE
 
 
 AAFRESULT STDMETHODCALLTYPE
-    ImplAAFTextLocator::GetNameBufLen (aafUInt32* pLen)
+    ImplAAFTextLocator::GetNameBufLen (aafInt32* pLen)
 {
 	if (! pLen)
 	{
@@ -89,7 +71,7 @@ AAFRESULT STDMETHODCALLTYPE
 
 
 AAFRESULT STDMETHODCALLTYPE
-    ImplAAFTextLocator::SetName (const aafCharacter* pNameBuf)
+    ImplAAFTextLocator::SetName (wchar_t* pNameBuf)
 {
 	if (! pNameBuf)
 	{
@@ -102,5 +84,6 @@ AAFRESULT STDMETHODCALLTYPE
 
 
 
+OMDEFINE_STORABLE(ImplAAFTextLocator, AUID_AAFTextLocator);
 
 
