922dbdb5400659b6b6b14ad9b3144f957d449822
tbingham <tbingham>
2001-04-17 03:27:01 +0000

    Smart pointers for COM interfaces.

--- ref-impl/include/AAFSmartPointer.h
+++ ref-impl/include/AAFSmartPointer.h
@@ -1,37 +1,13 @@
-#ifndef __AAFSmartPointer_h__
-#define __AAFSmartPointer_h__
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- *  prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
-
-// Define smart pointer assertions before including the base header.
-#ifndef AAF_SMART_POINTER_ASSERT
-#define AAF_SMART_POINTER_ASSERT(condition) \
-  if (! (condition)) throw #condition
-#endif // ! AAF_SMART_POINTER_ASSERT
+#ifndef __IAAFSmartPointer_h__
+#define __IAAFSmartPointer_h__
+/***********************************************\
+*                                               *
+* Advanced Authoring Format                     *
+*                                               *
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+* Copyright (c) 1998-1999 Microsoft Corporation *
+*                                               *
+\***********************************************/
 
 #ifndef __AAFSmartPointerBase_h__
 #include "AAFSmartPointerBase.h"
@@ -82,16 +58,16 @@
 struct AAFRefCountedCOMReference : public AAFCountedReference<IUnknown>
 {
 protected:
-  aafUInt32 acquire (IUnknown * pObj)
+  void acquire (IUnknown * pObj)
   {
 	AAF_SMART_POINTER_ASSERT (pObj);
-	return pObj->AddRef ();
+	pObj->AddRef ();
   }
 
-  aafUInt32 release (IUnknown * pObj)
+  void release (IUnknown * pObj)
   {
 	AAF_SMART_POINTER_ASSERT (pObj);
-	return pObj->Release ();
+	pObj->Release ();
   }
 };
 
@@ -106,4 +82,4 @@ struct IAAFSmartPointer
 };
 
 
-#endif // ! __AAFSmartPointer_h__
+#endif // ! __IAAFSmartPointer_h__
