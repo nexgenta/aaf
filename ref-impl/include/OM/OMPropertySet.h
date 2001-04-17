1ed99316644d89287700b65a907d679fb991ab95
tbingham <tbingham>
2001-04-17 07:07:54 +0000

    Portability improvements.

--- ref-impl/include/OM/OMPropertySet.h
+++ ref-impl/include/OM/OMPropertySet.h
@@ -1,25 +1,92 @@
+// @doc OMEXTERNAL
 #ifndef OMPROPERTYSET_H
 #define OMPROPERTYSET_H
 
 #include "OMPortability.h"
+#include "OMTypes.h"
 
-#define MAXIMUMPROPERTIES (10)
+#include <stddef.h>
 
 class OMProperty;
 class OMStorable;
 
+  // @class Container class for <c OMProperty> objects.
 class OMPropertySet {
 public:
+  // @access Public members.
+
+    // @cmember Constructor.
   OMPropertySet(void);
 
-  OMProperty* get(const int pid) const;
-  void put(OMProperty* p);
+    // @cmember Destructor.
+  ~OMPropertySet(void);
+
+    // @cmember Get the <c OMProperty> associated with the property id
+    //          <p propertyId>.
+  OMProperty* get(const OMPropertyId propertyId) const;
+
+    // @cmember Insert the <c OMProperty> <p property> into this
+    //          <c OMPropertySet>.
+  void put(OMProperty* property);
+
+    // @cmember Iterate over the <c OMProperty> objects in this
+    //          <c OMPropertySet>.
+  void iterate(size_t& context, OMProperty*& property) const;
+
+    // @cmember Is an <c OMProperty> with property id <p propertyId>
+    //          present in this <c OMPropertySet> ?
+  bool isPresent(const OMPropertyId propertyId) const;
+
+    // @cmember Is an <c OMProperty> with property id <p propertyId>
+    //          allowed in this <c OMPropertySet> ?
+  bool isAllowed(const OMPropertyId propertyId) const;
+
+    // @cmember Is an <c OMProperty> with property id <p propertyId>
+    //          a required member of this <c OMPropertySet> ?
+  bool isRequired(const OMPropertyId propertyId) const;
+
+    // @cmember The number of <c OMProperty> objects in this
+    //          <c OMPropertySet>.
   size_t count(void) const;
-  void setContainingObject(const OMStorable* containingObject);
+
+    // @cmember This <c OMPropertySet> is contained by the given
+    //          <c OMStorable> object <p container>. The <c OMProperty>
+    //          objects in this <c OMPropertySet> are the properties of
+    //          the given <c OMStorable> object <p container>.
+  void setContainer(const OMStorable* container);
+
+    // @cmember The <c OMStorable> object that contains this
+    //          <c OMPropertySet>.
+  OMStorable* container(void) const;
+
+protected:
+  static bool equal(const OMPropertyId& propertyIda,
+                    const OMPropertyId& propertyIdb);
+
 private:
-  OMProperty* _propertySet[MAXIMUMPROPERTIES];
-  size_t _count;
-  const OMStorable* _containingObject;
+
+  struct OMPropertySetElement;
+
+  // OMPropertySetElement for 'propertyId' or null if not found.
+  //
+  OMPropertySetElement* find(const OMPropertyId propertyId) const;
+
+  // First free entry or null if full.
+  //
+  OMPropertySetElement* find(void) const;
+
+  void grow(const size_t additionalElements);
+
+  struct OMPropertySetElement {
+    OMPropertyId _propertyId;
+    OMProperty* _property;
+    bool _valid;
+  };
+  OMPropertySetElement* _propertySet;  // Representation
+  size_t _capacity;                    // Number of potential elements
+  size_t _count;                       // Number of usable elements
+
+  const OMStorable* _container;
 };
 
 #endif
--- ref-impl/src/OM/OMStoredPropertySetIndex.h
+++ ref-impl/src/OM/OMStoredPropertySetIndex.h
@@ -1,46 +1,80 @@
+// @doc OMINTERNAL
 #ifndef OMSTOREDPROPERTYSETINDEX_H
 #define OMSTOREDPROPERTYSETINDEX_H
 
 #include "OMPortability.h"
+#include "OMTypes.h"
 
+#include <stddef.h>
+
+  // @class The in-memory representation of the on-disk index for a
+  //        stored property set.
 class OMStoredPropertySetIndex {
+private:
+  struct IndexEntry;  
 public:
-  
-  OMStoredPropertySetIndex(size_t capacity);
-  void insert(int pid, int type, size_t offset, size_t length);
-  
-  struct IndexEntry;
-  
-  IndexEntry* find(int pid) const;
+  // @access Public members.
 
+    // @cmember Constructor.  
+  OMStoredPropertySetIndex(size_t capacity);
+
+    // @cmember Destructor.
+  ~OMStoredPropertySetIndex(void);
+
+    // @cmember Insert a new property into this <c OMStoredPropertySetIndex>.
+    //          The new property has id <p propertyId>. The property
+    //          representation is of type <p type>. The property value
+    //          occupies <p length> bytes starting at offset <p offset>.
+  void insert(OMPropertyId propertyId,
+              OMUInt32 type,
+              OMUInt32 offset,
+              OMUInt32 length);
+  
+    // @cmember The number of properties in this <c OMStoredPropertySetIndex>.
+    // @this const
   size_t entries(void) const;
 
-  void iterate(size_t& context, int& pid, int& type, size_t& offset, size_t& length) const;
+    // @cmember Iterate over the properties in this
+    //          <c OMStoredPropertySetIndex>.
+    // @this const
+  void iterate(size_t& context,
+               OMPropertyId& propertyId,
+               OMUInt32& type,
+               OMUInt32& offset,
+               OMUInt32& length) const;
 
-  bool isDirty(void);
-  void setDirty(void);
-  void clearDirty(void);
+    // @cmember Find the property with property id <p propertyId> in this
+    //          <c OMStoredPropertySetIndex>. If found the <p type>,
+    //          <p offset> and <p length> of the property are returned.
+    // @this const  
+  bool find(const OMPropertyId& propertyId,
+            OMUInt32& type,
+            OMUInt32& offset,
+            OMUInt32& length) const;
 
-  bool isSorted(void);
+    // @cmember Is this <c OMStoredPropertySetIndex> valid ?
+    // @this const
+  bool isValid(void) const;
 
 protected:
-    
+
   OMStoredPropertySetIndex::IndexEntry* find(void) const;
+
+  OMStoredPropertySetIndex::IndexEntry* find(OMPropertyId propertyId) const;
     
 private:
     
   struct IndexEntry {
-    int _pid;
-    int _type;
-    size_t _offset;
-  size_t _length;
+    OMPropertyId _propertyId;
+    OMUInt32 _type;
+    OMUInt32 _offset;
+    OMUInt32 _length;
     bool _valid;
   };
     
   size_t _capacity;
   IndexEntry* _table;
   size_t _entries;
-  bool _dirty;
 };
 
 #endif
