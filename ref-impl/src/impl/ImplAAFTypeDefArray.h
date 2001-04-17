7ce6aecef882cb77c73878ba2b00739462fc1d4d
tbingham <tbingham>
2001-04-17 08:58:03 +0000

    adding liability comments

--- ref-impl/src/impl/ImplAAFTypeDefArray.h
+++ ref-impl/src/impl/ImplAAFTypeDefArray.h
@@ -1,33 +1,36 @@
 #ifndef __ImplAAFTypeDefArray_h__
 #define __ImplAAFTypeDefArray_h__
 
-//=---------------------------------------------------------------------=
-//
-// The contents of this file are subject to the AAF SDK Public
-// Source License Agreement (the "License"); You may not use this file
-// except in compliance with the License.  The License is available in
-// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
-// Association or its successor.
-// 
-// Software distributed under the License is distributed on an "AS IS"
-// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
-// the License for the specific language governing rights and limitations
-// under the License.
-// 
-// The Original Code of this file is Copyright 1998-2001, Licensor of the
-// AAF Association.
-// 
-// The Initial Developer of the Original Code of this file and the
-// Licensor of the AAF Association is Avid Technology.
-// All rights reserved.
-//
-//=---------------------------------------------------------------------=
-
+/***********************************************************************
+ *
+ *              Copyright (c) 1998-1999 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and accompanying 
+ * documentation, and to distribute and sublicense application software
+ * incorporating this software for any purpose is hereby granted, 
+ * provided that (i) the above copyright notice and this permission
+ * notice appear in all copies of the software and related documentation,
+ * and (ii) the name Avid Technology, Inc. may not be used in any
+ * advertising or publicity relating to the software without the specific,
+ *  prior written permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+ * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+ * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+ * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+ * LIABILITY.
+ *
+ ************************************************************************/
 #ifndef __ImplAAFTypeDef_h__
 #include "ImplAAFTypeDef.h"
 #endif
 
-class ImplAAFPropertyValue;
 
 class ImplAAFTypeDefArray : public ImplAAFTypeDef
 {
@@ -53,15 +56,6 @@ public:
 
 
   //****************
-  // CreateEmptyValue()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    CreateEmptyValue
-        (// @parm [out] newly-created property empty value,
-         ImplAAFPropertyValue ** ppPropVal);
-
-
-  //****************
   // CreateValueFromValues()
   //
   virtual AAFRESULT STDMETHODCALLTYPE
@@ -74,7 +68,7 @@ public:
          aafUInt32  numElements,
 
          // @parm [out] newly-created property value
-         ImplAAFPropertyValue ** ppPropVal) ; //= 0;   //--cf make this a pure virtual method
+         ImplAAFPropertyValue ** ppPropVal);
 
 
   //****************
@@ -152,39 +146,10 @@ public:
          aafUInt32  dataSize);
 
 
-  // Override callbacks from OMStorable
-  virtual void onSave(void* clientContext) const;
-  virtual void onRestore(void* clientContext) const;
-
-  // Method is called after class has been added to MetaDictionary.
-  // If this method fails the class is removed from the MetaDictionary and the
-  // registration method will fail.
-  virtual HRESULT CompleteClassRegistration(void);
-
 protected:
-	
-	//returns number of elements in this array
   virtual aafUInt32 pvtCount (ImplAAFPropertyValue * pInPropVal) const = 0;
-
-  //Validates the input source 
-  virtual AAFRESULT STDMETHODCALLTYPE
-	  ValidateInputParams (ImplAAFPropertyValue ** ppElementValues,
-						aafUInt32  numElements);
-
-  //Creates a new PV
-  virtual AAFRESULT STDMETHODCALLTYPE
-	  CreateValue(ImplAAFPropertyValue ** ppPropVal, 
-								 aafUInt32 dataSize = 0); //overloaded function
-
-  //Copies individual element-values into the new PV
-  virtual AAFRESULT STDMETHODCALLTYPE
-	  CopyValuesIntoValue (ImplAAFPropertyValue ** ppElementValues,
-										  aafUInt32  numElements,
-										  aafUInt32  sizeElem,
-										  ImplAAFPropertyValue ** ppPropVal) ;
-
-  //Function to test appropriate array-ability;  pure-virtual
-  virtual bool IsArrayable(ImplAAFTypeDef * pSourceTypeDef) const = 0;
+  //
+  // returns number of elements in this array
 };
 
 //
