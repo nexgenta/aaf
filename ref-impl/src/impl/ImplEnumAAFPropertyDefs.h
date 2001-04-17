eb067ff0d301a41daa8edffdf52f65060d44c921
tbingham <tbingham>
2001-04-17 04:53:09 +0000

    Add support to initialize properties from the DM class definition.

--- ref-impl/src/impl/ImplEnumAAFPropertyDefs.h
+++ ref-impl/src/impl/ImplEnumAAFPropertyDefs.h
@@ -3,41 +3,25 @@
 #ifndef __ImplEnumAAFPropertyDefs_h__
 #define __ImplEnumAAFPropertyDefs_h__
 
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- *  prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+/***********************************************\
+*                                               *
+* Advanced Authoring Format                     *
+*                                               *
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+* Copyright (c) 1998-1999 Microsoft Corporation *
+*                                               *
+\***********************************************/
 
 class ImplAAFPropertyDef;
-class ImplAAFClassDef;
 
-#ifndef __ImplAAFRoot_h__
-#include "ImplAAFRoot.h"
+#ifndef __ImplAAFObject_h__
+#include "ImplAAFObject.h"
 #endif
 
-#include "OMReferenceContainerIter.h"
+
+typedef OMStrongReferenceVectorProperty<ImplAAFPropertyDef>
+  propertyDefStrongRefArrayProp_t; 
+
 
 class ImplEnumAAFPropertyDefs : public ImplAAFRoot
 {
@@ -107,12 +91,13 @@ public:
 public:
   // SDK Internal 
   virtual AAFRESULT STDMETHODCALLTYPE
-	  SetIterator(ImplAAFClassDef *pObj,
-				OMReferenceContainerIterator<ImplAAFPropertyDef>* iterator);
+    SetEnumStrongProperty (ImplAAFObject *pObj,
+						   propertyDefStrongRefArrayProp_t *pProp);
 
 private:
-	ImplAAFClassDef                   * _enumObj;
-	OMReferenceContainerIterator<ImplAAFPropertyDef>*	_iterator;
+	aafUInt32                         _current;
+	ImplAAFObject                   * _enumObj;
+	propertyDefStrongRefArrayProp_t * _enumStrongProp;
 };
 
 //
