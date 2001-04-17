a4d4e3c21faf5f7cf57facee21a481c0497401fa
tbingham <tbingham>
2001-04-17 09:35:56 +0000

    Add read-only module test capability

--- test/com/ComModTestAAF/ComModTestAAF.cpp
+++ test/com/ComModTestAAF/ComModTestAAF.cpp
@@ -31,6 +31,7 @@
 
 //
 #include <iostream.h>
+#include <fstream.h>
 #include <stdlib.h>
 #include <time.h>
 
@@ -46,7 +47,7 @@
 // Make sure we have defined IID_IUnknown and IID_IClassFactory.
 #include <initguid.h>
 #include <coguid.h>	
-
+#include "DataInput.h"
 
 #if !defined(CDECL) && defined(_MSC_VER)
 #define CDECL	_cdecl
@@ -63,20 +64,17 @@
 #endif
 
 
-#ifndef __AAFTypes_h__
-#include "AAFTypes.h"
+#ifndef __AAF_h__
+#include "AAF.h"
 #endif
 
 
 #include "CAAFModuleTest.h"
-
-#ifdef __powerc
-#include <console.h> /* Mac command line window */
-#endif
+#include "ModuleTest.h"
 
 #ifdef WIN32
 #include <winbase.h>
-#include <tchar.h>
+//#include <tchar.h>
 #endif
 
 typedef AAFRESULT (*AAFModuleTestProc)();
@@ -85,6 +83,42 @@ typedef AAFRESULT (*AAFModuleTestProc)();
 #define SUCCESS (0)
 #define FAILURE (-1)
 
+// routine copied from Tim Bingham's test program...
+static void formatError(DWORD errorCode)
+{
+  cerr << "RESULT = " << (long)errorCode << " (0x" << hex << errorCode << dec << ")" << endl;
+
+#ifdef WIN32
+  CHAR buffer[256];
+
+  int status = FormatMessageA(
+    FORMAT_MESSAGE_FROM_SYSTEM,
+    NULL,
+    errorCode,
+    LANG_SYSTEM_DEFAULT,
+    buffer, sizeof(buffer)/sizeof(buffer[0]),
+    NULL);
+
+  if (status != 0) {
+    int length = strlen(buffer);
+    if (length >= 1) {
+      buffer[length - 1] = '\0';
+    }
+    cerr << buffer << endl;
+  }
+#endif
+
+	cerr << endl;
+}
+
+static void throwIfError(HRESULT hr)
+{
+  if (FAILED(hr))
+  {
+    formatError(hr);
+    throw hr;
+	}
+}
 
 // helper class
 struct CComInitialize
@@ -94,35 +128,55 @@ struct CComInitialize
 };
 
 
+// simple helper class to initialize and cleanup AAF library.
+struct CAAFInitialize
+{
+  CAAFInitialize(const char *dllname = NULL)
+  {
+	  cout << "Attempting to load the AAF dll...";
+	  cout.flush();
+    HRESULT hr = AAFLoad(dllname);
+    if (S_OK != hr)
+  	{
+      cerr << "FAILED! ";
+      throwIfError(hr);
+		}
+    cout << "DONE" << endl;
+  }
+
+  ~CAAFInitialize()
+  {
+    AAFUnload();
+  }
+};
+
+
+// simple helper class to initialize and cleanup AAF library.
+class CAAFInitializePlugins
+{
+public:
+  CAAFInitializePlugins() :
+    pPluginMgr(NULL)
+  {
+    throwIfError(AAFGetPluginManager(&pPluginMgr));
+    throwIfError(pPluginMgr->RegisterSharedPlugins());
+    pPluginMgr->Release();
+    pPluginMgr = NULL;
+  }
+
+  ~CAAFInitializePlugins()
+  {
+    if (pPluginMgr)
+      pPluginMgr->Release();
+  }
+
+  // cached for error cleanup.
+  IAAFPluginManager *pPluginMgr;
+};
 
 
 // forward declarations.
 
-#ifdef WIN32
-// routine copied from Tim Bingham's test program...
-void formatError(DWORD errorCode)
-{
-  TCHAR buffer[256];
-
-  int status = FormatMessage(
-    FORMAT_MESSAGE_FROM_SYSTEM,
-    NULL,
-    errorCode,
-    LANG_SYSTEM_DEFAULT,
-    buffer, sizeof(buffer)/sizeof(buffer[0]),
-    NULL);
-
-  if (status != 0) {
-    int length = _tcslen(buffer);
-    if (length >= 2) {
-      buffer[length - 2] = '\0';
-    }
-    cerr << buffer << endl;
-  } else {
-    cerr << hex << errorCode << dec << endl;
-  }
-}
-#endif
 
 
 
@@ -136,6 +190,9 @@ void formatError(DWORD errorCode)
 int main(int argc, char* argv[])
 {
 	int result = SUCCESS;
+	int	startArg = 1;
+	testMode_t	testMode = kAAFUnitTestReadWrite;
+
 
 	// Initialize com library for this process.
 	CComInitialize comInit;
@@ -146,12 +203,11 @@ int main(int argc, char* argv[])
 	{
 		HRESULT hr = S_OK;
 
-		/* console window for mac */
+		// Make sure the dll can be loaded and initialized.
+		CAAFInitialize aafInit;
 
-		#ifdef __powerc
-		argc = ccommand(&argv);
-		#endif
-
+		// Make sure the shared plugins can be loaded and registered.
+   		CAAFInitializePlugins aafInitPlugins;
 
 		/* Check arguments to see if help was requested */
 
@@ -163,9 +219,19 @@ int main(int argc, char* argv[])
 			cout<< "No arguments --> To run all tests\n";
 			cout<< "Otherwise any AAF object class name can be typed\n";
 			cout<< "and that objects test method will be executed.\n";
-			cout<< "ex AAFSegment AAFTransition etc\n"<< endl;
+			cout<< "ex AAFSegment AAFTransition etc\n\n";
+			cout<< "Input can also be read in from a text.\n";
+			cout<< "Just name it \"COMMODAAF (PPC)\"" << endl;
 
 			return(0);
+		}
+
+		if ( argc > 1 &&
+			(0 == strncmp(argv[1],"-r",2) ||
+			 0 == strncmp(argv[1],"-r",2) )	)
+		{
+			testMode = kAAFUnitTestReadOnly;
+			startArg++;
 		}
 
 		/* Print Header */
@@ -181,18 +247,18 @@ int main(int argc, char* argv[])
 		time(&s_time);
 		cout<< ctime(&s_time)<< endl<< endl;
 
-		if (1 >= argc)
+		if (startArg >= argc)
 			// Call all Module tests...
-			hr = AAFModuleTest.Test(NULL);
+			hr = AAFModuleTest.Test(testMode, NULL);
 		else
 		{
 			// Call only the modules that are named in the command line.
 			int module;
 
-			for (module = 1; module < argc; module++)
-				hr = AAFModuleTest.Test(reinterpret_cast<unsigned char *>(argv[module]));
+			for (module = startArg; module < argc; module++)
+				hr = AAFModuleTest.Test(testMode, reinterpret_cast<unsigned char *>(argv[module]));
 		}
-		
+
 		/* Get and Print finish time	*/
 		time_t e_time;
 		time(&e_time);
@@ -207,6 +273,10 @@ int main(int argc, char* argv[])
 
 
 		result = (int)hr;
+	}
+	catch (HRESULT& rhr)
+	{
+		result = rhr;
 	}
 	catch (...)
 	{
