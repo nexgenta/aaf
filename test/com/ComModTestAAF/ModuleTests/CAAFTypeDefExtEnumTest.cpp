1ced5027132be5f3a6c6127b6c1d54e30a063861
tbingham <tbingham>
2001-04-17 05:51:30 +0000

    Checkin a version that actually builds.

--- test/com/ComModTestAAF/ModuleTests/CAAFTypeDefExtEnumTest.cpp
+++ test/com/ComModTestAAF/ModuleTests/CAAFTypeDefExtEnumTest.cpp
@@ -1,5 +1,5 @@
 // @doc INTERNAL
-// @com This file implements the module test for CAAFDefinitionObject
+// @com This file implements the module test for CAAFTypeDefExtEnum
 /******************************************\
 *                                          *
 * Advanced Authoring Format                *
@@ -20,87 +20,10 @@
 #error - improperly defined include guard
 #endif
 
-
-#include <iostream.h>
-
-// Temporarily necessary global declarations.
-extern "C" const CLSID CLSID_AAFTypeDefExtEnum; // generated
+#include "AAFResult.h"
 
 
 HRESULT CAAFTypeDefExtEnum::test()
 {
-  HRESULT hr = AAFRESULT_NOT_IMPLEMENTED;
-  IAAFTypeDefExtEnum *pObject = NULL;
-
-  try
-    {
-      // Attempt to create an AAFTypeDefExtEnum.
-      hr =  CoCreateInstance(
-                             CLSID_AAFTypeDefExtEnum,
-                             NULL, 
-                             CLSCTX_INPROC_SERVER, 
-                             IID_IAAFTypeDefExtEnum, (void **)&pObject);
-      if (FAILED(hr))
-        {
-          cerr << "CAAFTypeDefExtEnum::test...FAILED!";
-          cerr << hr;
-          cerr << "\tCoCreateInstance(&CLSID_AAFTypeDefExtEnum, NULL,"
-            " CLSCTX_INPROC_SERVER, &IID_IAAFTypeDefExtEnum, ...);" <<
-              endl;
-          return hr;
-        }
-
-      // module-specific tests go here
-
-      if (pObject)
-        pObject->Release();
-      return AAFRESULT_NOT_IMPLEMENTED;
-
-    }
-  catch (...)
-    {
-      cerr << "CAAFTypeDefExtEnum::test...Caught general C++"
-        " exception!" << endl; 
-    }
-
-  // Cleanup our object if it exists.
-  if (pObject)
-    pObject->Release();
-
-  return hr;
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
