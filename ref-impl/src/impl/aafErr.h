b5eac96996a0641a0dd8629912e0e705b3623368
tbingham <tbingham>
2001-04-17 03:39:22 +0000

    restoring bob's changes

--- ref-impl/src/impl/aafErr.h
+++ ref-impl/src/impl/aafErr.h
@@ -1,25 +1,28 @@
-//=---------------------------------------------------------------------=
-//
-// The contents of this file are subject to the AAF SDK Public
-// Source License Agreement (the "License"); You may not use this file
-// except in compliance with the License.  The License is available in
-// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
-// Association or its successor.
-// 
-// Software distributed under the License is distributed on an "AS IS"
-// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
-// the License for the specific language governing rights and limitations
-// under the License.
-// 
-// The Original Code of this file is Copyright 1998-2001, Licensor of the
-// AAF Association.
-// 
-// The Initial Developer of the Original Code of this file and the
-// Licensor of the AAF Association is Avid Technology.
-// All rights reserved.
-//
-//=---------------------------------------------------------------------=
-
+/***********************************************************************
+ *
+ *              Copyright (c) 1996 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and to distribute
+ * and sublicense application software incorporating this software for
+ * any purpose is hereby granted, provided that (i) the above
+ * copyright notice and this permission notice appear in all copies of
+ * the software and related documentation, and (ii) the name Avid
+ * Technology, Inc. may not be used in any advertising or publicity
+ * relating to the software without the specific, prior written
+ * permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, INDIRECT, CONSEQUENTIAL OR OTHER DAMAGES OF
+ * ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE, INCLUDING, 
+ * WITHOUT  LIMITATION, DAMAGES RESULTING FROM LOSS OF USE,
+ * DATA OR PROFITS, AND WHETHER OR NOT ADVISED OF THE POSSIBILITY OF
+ * DAMAGE, REGARDLESS OF THE THEORY OF LIABILITY.
+ *
+ ************************************************************************/
 
 #ifndef OMError_h_
 #define OMError_h_ 1
@@ -138,7 +141,7 @@ AAF_EXPORT void aafPrintStackTrace(AAFFile * file);
 	 *
 	 *************************************************************/
 #define XPROTECT()	{ aafErr_t zzOmfEcode = AAFRESULT_SUCCESS; \
-						aafBool zzOmfPropagate = kAAFTrue;
+						aafBool zzOmfPropagate = AAFTrue;
 
 #define CHECK(a) { if((zzOmfEcode = (a)) != AAFRESULT_SUCCESS) goto zzOmfCleanup; }
 
@@ -162,7 +165,7 @@ AAF_EXPORT void aafPrintStackTrace(AAFFile * file);
 						}
 						
 #define RERAISE(a)		zzOmfEcode = (a)
-#define NO_PROPAGATE()	zzOmfPropagate = kAAFFalse
+#define NO_PROPAGATE()	zzOmfPropagate = AAFFalse
 #define XCODE()			zzOmfEcode
 #define XEXCEPT			goto zzOmfExit; \
 						zzOmfCleanup:
