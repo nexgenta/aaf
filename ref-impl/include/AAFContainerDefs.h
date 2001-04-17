d6a4f2b965cd8a15bfe1e59f7e174d6ac1aa8bb8
tbingham <tbingham>
2001-04-17 03:57:18 +0000

    Add missing definitions

--- ref-impl/include/AAFContainerDefs.h
+++ ref-impl/include/AAFContainerDefs.h
@@ -1,14 +1,33 @@
-/***********************************************\
-*												*
-* Advanced Authoring Format						*
-*												*
-* Copyright (c) 1998-1999 Avid Technology, Inc. *
-* Copyright (c) 1998-1999 Microsoft Corporation *
-*												*
-\***********************************************/
+/***********************************************************************
+ *
+ *              Copyright (c) 1998-1999 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and accompanying 
+ * documentation, and to distribute and sublicense application software
+ * incorporating this software for any purpose is hereby granted, 
+ * provided that (i) the above copyright notice and this permission
+ * notice appear in all copies of the software and related documentation,
+ * and (ii) the name Avid Technology, Inc. may not be used in any
+ * advertising or publicity relating to the software without the specific,
+ *  prior written permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+ * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+ * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+ * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+ * LIABILITY.
+ *
+ ************************************************************************/
 
 // Following are the UIDs of the well-known essence container definition objects in AAF.
 //
 
 const aafUID_t ContainerAAF = { 0x4313B571, 0xD8BA, 0x11d2, { 0x80, 0x9B, 0x00, 0x60, 0x08, 0x14, 0x3E, 0x6F } };
 const aafUID_t ContainerFile = { 0x4313B572, 0xD8BA, 0x11d2, { 0x80, 0x9B, 0x00, 0x60, 0x08, 0x14, 0x3E, 0x6F } };
+const aafUID_t ContainerOMF = { 0x4b1c1a46, 0x3f2, 0x11d4, { 0x80, 0xfb, 0x0, 0x60, 0x8, 0x14, 0x3e, 0x6f } };
