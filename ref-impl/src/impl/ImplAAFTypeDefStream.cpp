f43182e59fe73d158a95bd493d73f8814718d34a
tbingham <tbingham>
2001-04-17 06:30:02 +0000

    Initial dodo-generated checkin

--- ref-impl/src/impl/ImplAAFTypeDefStream.cpp
+++ ref-impl/src/impl/ImplAAFTypeDefStream.cpp
@@ -1,126 +1,58 @@
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- * prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
 
 
 
 
 
+#include "AAFStoredObjectIDs.h"
 
 #ifndef __ImplAAFTypeDefStream_h__
 #include "ImplAAFTypeDefStream.h"
 #endif
-
-#include "AAFStoredObjectIDs.h"
-#include "AAFPropertyIDs.h"
-
 
 #include <assert.h>
 #include <string.h>
 
 
 ImplAAFTypeDefStream::ImplAAFTypeDefStream ()
-  : _elementType  (PID_TypeDefinitionStream_ElementType,
-                   L"ElementType", 
-                   L"/Dictionary/TypeDefinitions", 
-                   PID_MetaDefinition_Identification)
-{
-  _persistentProperties.put(_elementType.address());
-}
+{}
 
 
 ImplAAFTypeDefStream::~ImplAAFTypeDefStream ()
 {}
 
-
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFTypeDefStream::Initialize (
-      const aafUID_t & id,
-      ImplAAFTypeDef * pTypeDef,
-      const aafCharacter * pTypeName)
+      aafUID_t *  /*pID*/,
+      ImplAAFTypeDef * /*pTypeDef*/,
+      wchar_t *  /*pTypeName*/)
 {
-  if (! pTypeDef)
-	return AAFRESULT_NULL_PARAM;
-  if (! pTypeName)
-	return AAFRESULT_NULL_PARAM;
-  if (!pTypeDef->attached())
-    return AAFRESULT_OBJECT_ALREADY_ATTACHED;
-
-  return pvtInitialize(id, pTypeDef, pTypeName);
-}
-
-
-AAFRESULT STDMETHODCALLTYPE
-    ImplAAFTypeDefStream::pvtInitialize (
-      const aafUID_t & id,
-      ImplAAFTypeDef * pTypeDef,
-      const aafCharacter * pTypeName)
-{
-  if (! pTypeDef)
-	return AAFRESULT_NULL_PARAM;
-  if (! pTypeName)
-	return AAFRESULT_NULL_PARAM;
-
-  assert (pTypeDef);
-  if (! pTypeDef->IsStreamable() || !pTypeDef->IsFixedSize())
-    return AAFRESULT_BAD_TYPE;
-
-  _elementType = pTypeDef;
-
-  return AAFRESULT_SUCCESS;
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
 
 
 
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFTypeDefStream::GetType (
-      ImplAAFTypeDef ** ppTypeDef)
+      ImplAAFTypeDef ** /*ppTypeDef*/)
 {
-  if (! ppTypeDef)
-	return AAFRESULT_NULL_PARAM;
-
-  *ppTypeDef = _elementType;
-  if (*ppTypeDef)
-    (*ppTypeDef)->AcquireReference();
-
-  return AAFRESULT_SUCCESS;
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
 
 
 
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFTypeDefStream::GetCount (
-      ImplAAFPropertyValue * pPropVal,
-      aafInt64 * pCount)
+      ImplAAFPropertyValue * /*pPropVal*/,
+      aafInt64 *  /*pCount*/)
 {
-  if (! pPropVal)
-	return AAFRESULT_NULL_PARAM;
-  if (! pCount)
-	return AAFRESULT_NULL_PARAM;
-
   return AAFRESULT_NOT_IMPLEMENTED;
 }
 
@@ -128,15 +60,10 @@ AAFRESULT STDMETHODCALLTYPE
 
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFTypeDefStream::AppendElements (
-      ImplAAFPropertyValue * pInPropVal,
-      ImplAAFPropertyValue ** ppMemberPropVals,
+      ImplAAFPropertyValue * /*pInPropVal*/,
+      ImplAAFPropertyValue * /*pMemberPropVals*/,
       aafUInt32  /*numElements*/)
 {
-  if (! pInPropVal)
-	return AAFRESULT_NULL_PARAM;
-  if (! ppMemberPropVals)
-	return AAFRESULT_NULL_PARAM;
-
   return AAFRESULT_NOT_IMPLEMENTED;
 }
 
@@ -144,11 +71,8 @@ AAFRESULT STDMETHODCALLTYPE
 
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFTypeDefStream::CreateEmpty (
-      ImplAAFPropertyValue ** ppPropVal)
+      ImplAAFPropertyValue ** /*ppPropVal*/)
 {
-  if (! ppPropVal)
-	return AAFRESULT_NULL_PARAM;
-
   return AAFRESULT_NOT_IMPLEMENTED;
 }
 
@@ -156,16 +80,11 @@ AAFRESULT STDMETHODCALLTYPE
 
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFTypeDefStream::GetElementValues (
-      ImplAAFPropertyValue * pInPropVal,
+      ImplAAFPropertyValue * /*pInPropVal*/,
       aafInt64  /*startElement*/,
-      ImplAAFPropertyValue ** pOutPropVals,
+      ImplAAFPropertyValue ** /*pOutPropVals*/,
       aafUInt32  /*numElements*/)
 {
-  if (! pInPropVal)
-	return AAFRESULT_NULL_PARAM;
-  if (! pOutPropVals)
-	return AAFRESULT_NULL_PARAM;
-
   return AAFRESULT_NOT_IMPLEMENTED;
 }
 
@@ -173,53 +92,15 @@ AAFRESULT STDMETHODCALLTYPE
 
  AAFRESULT STDMETHODCALLTYPE
    ImplAAFTypeDefStream::GetElements (
-      ImplAAFPropertyValue * pPropVal,
+      ImplAAFPropertyValue * /*pPropVal*/,
       aafInt64  /*startElement*/,
-      aafMemPtr_t pData,
+      aafMemPtr_t  /*pData*/,
       aafUInt32  /*numElements*/)
 {
-  if (! pPropVal)
-	return AAFRESULT_NULL_PARAM;
-  if (! pData)
-	return AAFRESULT_NULL_PARAM;
-
   return AAFRESULT_NOT_IMPLEMENTED;
 }
 
 
-bool ImplAAFTypeDefStream::IsAggregatable () const
-{ return false; }
-
-bool ImplAAFTypeDefStream::IsStreamable () const
-{ return false; }
-
-bool ImplAAFTypeDefStream::IsFixedArrayable () const
-{ return false; }
-
-bool ImplAAFTypeDefStream::IsVariableArrayable () const
-{ return false; }
-
-bool ImplAAFTypeDefStream::IsStringable () const
-{ return false; }
+OMDEFINE_STORABLE(ImplAAFTypeDefStream, AUID_AAFTypeDefStream);
 
 
-
-
-
-
-// override from OMStorable.
-const OMClassId& ImplAAFTypeDefStream::classId(void) const
-{
-  return (*reinterpret_cast<const OMClassId *>(&AUID_AAFTypeDefStream));
-}
-
-// Override callbacks from OMStorable
-void ImplAAFTypeDefStream::onSave(void* clientContext) const
-{
-  ImplAAFTypeDef::onSave(clientContext);
-}
-
-void ImplAAFTypeDefStream::onRestore(void* clientContext) const
-{
-  ImplAAFTypeDef::onRestore(clientContext);
-}
