378181126f0734999d8619e22acd70f402fcb723
tbingham <tbingham>
2001-04-17 04:54:35 +0000

    Change over some classes to use Sets

--- ref-impl/src/impl/ImplEnumAAFTypeDefs.h
+++ ref-impl/src/impl/ImplEnumAAFTypeDefs.h
@@ -3,21 +3,39 @@
 #ifndef __ImplEnumAAFTypeDefs_h__
 #define __ImplEnumAAFTypeDefs_h__
 
-/******************************************\
-*                                          *
-* Advanced Authoring Format                *
-*                                          *
-* Copyright (c) 1998 Avid Technology, Inc. *
-* Copyright (c) 1998 Microsoft Corporation *
-*                                          *
-\******************************************/
+/***********************************************************************
+ *
+ *              Copyright (c) 1998-1999 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and accompanying 
+ * documentation, and to distribute and sublicense application software
+ * incorporating this software for any purpose is hereby granted, 
+ * provided that (i) the above copyright notice and this permission
+ * notice appear in all copies of the software and related documentation,
+ * and (ii) the name Avid Technology, Inc. may not be used in any
+ * advertising or publicity relating to the software without the specific,
+ *  prior written permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+ * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+ * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+ * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+ * LIABILITY.
+ *
+ ************************************************************************/
 
 class ImplAAFTypeDef;
 
 #ifndef __ImplAAFObject_h__
 #include "ImplAAFObject.h"
 #endif
-
+#include "OMReferenceContainerIter.h"
 
 typedef OMVariableSizeProperty<aafUID_t> typeDefWeakRefArrayProp_t;
 typedef OMStrongReferenceVectorProperty<ImplAAFTypeDef> typeDefStrongRefArrayProp_t;
@@ -88,15 +106,11 @@ public:
 public:
   // SDK Internal 
   virtual AAFRESULT STDMETHODCALLTYPE
-    SetEnumProperty( ImplAAFObject *pObj, typeDefWeakRefArrayProp_t *pProp);
-  virtual AAFRESULT STDMETHODCALLTYPE
-    SetEnumStrongProperty( ImplAAFObject *pObj, typeDefStrongRefArrayProp_t *pProp);
-
+	  SetIterator(ImplAAFObject *pObj,
+				OMReferenceContainerIterator<ImplAAFTypeDef>* iterator);
 private:
-	aafUInt32						_current;
-	ImplAAFObject					*_enumObj;
-	typeDefWeakRefArrayProp_t		*_enumProp;
-	typeDefStrongRefArrayProp_t		*_enumStrongProp;
+	ImplAAFObject*	_enumObj;
+	OMReferenceContainerIterator<ImplAAFTypeDef>*	_iterator;
 };
 
 #endif // ! __ImplEnumAAFTypeDefs_h__
