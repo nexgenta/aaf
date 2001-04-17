b9c9385acfbcd92ffbad2b4aa2a3dba8477f9692
tbingham <tbingham>
2001-04-17 05:02:58 +0000

    Add support to initialize properties from the DM class definition.

--- ref-impl/src/impl/ImplAAFTypeDefRename.h
+++ ref-impl/src/impl/ImplAAFTypeDefRename.h
@@ -9,12 +9,8 @@
 * Advanced Authoring Format                *
 *                                          *
 * Copyright (c) 1998 Avid Technology, Inc. *
-* Copyright (c) 1998 Microsoft Corporation *
 *                                          *
 \******************************************/
-
-
-
 
 
 
@@ -43,7 +39,7 @@ public:
   virtual AAFRESULT STDMETHODCALLTYPE
     Initialize
         (// @parm [in] auid to be used to identify this type
-         aafUID_t *  pID,
+         const aafUID_t *  pID,
 
          // @parm [in] type to which this is an alias
          ImplAAFTypeDef * pBaseType,
@@ -58,28 +54,7 @@ public:
   virtual AAFRESULT STDMETHODCALLTYPE
     GetBaseType
         // @parm [out] type definition for which this is an alias
-        (ImplAAFTypeDef ** ppBaseType);
-
-
-  //****************
-  // GetName()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    GetName
-        (// @parm [out, size_is(bufSize), string] buffer into which the name is written
-         wchar_t *  pName,
-
-         // @parm [in] The size of the pName buffer, in bytes
-         aafInt32  bufSize);
-
-
-  //****************
-  // GetNameBufLen()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    GetNameBufLen
-        // @parm [out] required buffer length, in bytes
-        (aafInt32 *  pLen);
+        (ImplAAFTypeDef ** ppBaseType) const;
 
 
   //****************
@@ -100,11 +75,11 @@ public:
   //
   //*************************************************************
 
-  /*
-  virtual void reorder(OMByte* bytes,
-                       size_t bytesSize) const;
+  virtual void reorder(OMByte* externalBytes,
+                       size_t externalBytesSize) const;
 
-  virtual size_t externalSize(void) const;
+  virtual size_t externalSize(OMByte* internalBytes,
+							  size_t internalBytesSize) const;
 
   virtual void externalize(OMByte* internalBytes,
                            size_t internalBytesSize,
@@ -112,25 +87,51 @@ public:
                            size_t externalBytesSize,
                            OMByteOrder byteOrder) const;
 
-  virtual size_t internalSize(void) const;
+  virtual size_t internalSize(OMByte* externalBytes,
+							  size_t externalBytesSize) const;
 
   virtual void internalize(OMByte* externalBytes,
                            size_t externalBytesSize,
                            OMByte* internalBytes,
                            size_t internalBytesSize,
                            OMByteOrder byteOrder) const;
-						   */
+
+
+  // overrides from ImplAAFTypeDef
+  //
+  virtual aafBool IsFixedSize (void) const;
+  virtual size_t PropValSize (void) const;
+  aafBool IsRegistered (void) const;
+  size_t NativeSize (void) const;
+
+  // Override this one, not the ...MBS() one...
+  OMProperty * 
+    pvtCreateOMProperty (OMPropertyId pid,
+						 const aafCharacter * name) const;
 
 public:
   // Declare this class to be storable.
   //
   OMDECLARE_STORABLE(ImplAAFTypeDefRename)
 
-  // Declare the module test method. The implementation of the will be be
-  // in /test/ImplAAFTypeDefRenameTest.cpp.
-  static AAFRESULT test();
+private:
+  ImplAAFTypeDefSP BaseType () const;
+
+  // OMWeakReferenceProperty<ImplAAFTypeDef> _RenamedType;
+  OMFixedSizeProperty<aafUID_t>           _RenamedType;
+
+  ImplAAFTypeDefSP _cachedBaseType;
 };
 
+//
+// smart pointer
+//
+
+#ifndef __ImplAAFSmartPointer_h__
+// caution! includes assert.h
+#include "ImplAAFSmartPointer.h"
+#endif
+
+typedef ImplAAFSmartPointer<ImplAAFTypeDefRename> ImplAAFTypeDefRenameSP;
+
 #endif // ! __ImplAAFTypeDefRename_h__
-
-
