3fc7057d3564941ee04e88a1eef630726bcf5611
tbingham <tbingham>
2001-04-17 07:21:19 +0000

    Remove use of <iostream.h>.

--- ref-impl/src/OM/OMUtilities.h
+++ ref-impl/src/OM/OMUtilities.h
@@ -34,6 +34,12 @@
 #include <stdio.h>
 #include <stddef.h>
 
+// @module OMUtilities | Utility functions including error handling,
+//         obtaining information about the host computer, wide character
+//         string manipulation, property path manipulation and accessing
+//         disk files with wide character names.
+//   @mauthor Tim Bingham | tjb | Avid Technology, Inc.
+
   // @func Set the name of the program to be use in error messages.
   //   @parm The program name.
 void setProgramName(const char* name);
@@ -126,8 +132,6 @@ size_t squeezeWideString(const wchar_t* clearName,
                          size_t clearNameSize,
                          wchar_t* squeezedName,
                          size_t squeezedNameSize);
-
-void printWideString(const wchar_t* string);
 
   // @func Save a character string.
   //   @parm The character string to save.
