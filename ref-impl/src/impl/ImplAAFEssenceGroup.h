89d99c6f8515742b68a0ed327310ed5d0b233525
tbingham <tbingham>
2001-04-17 05:11:26 +0000

    Changes for Andy

--- ref-impl/src/impl/ImplAAFEssenceGroup.h
+++ ref-impl/src/impl/ImplAAFEssenceGroup.h
@@ -4,23 +4,33 @@
 #define __ImplAAFEssenceGroup_h__
 
 
-/******************************************\
-*                                          *
-* Advanced Authoring Format                *
-*                                          *
-* Copyright (c) 1998 Avid Technology, Inc. *
-* Copyright (c) 1998 Microsoft Corporation *
-*                                          *
-\******************************************/
+/***********************************************************************
+ *
+ *              Copyright (c) 1998-1999 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and accompanying 
+ * documentation, and to distribute and sublicense application software
+ * incorporating this software for any purpose is hereby granted, 
+ * provided that (i) the above copyright notice and this permission
+ * notice appear in all copies of the software and related documentation,
+ * and (ii) the name Avid Technology, Inc. may not be used in any
+ * advertising or publicity relating to the software without the specific,
+ *  prior written permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+ * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+ * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+ * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+ * LIABILITY.
+ *
+ ************************************************************************/
 
-/******************************************\
-*                                          *
-* Advanced Authoring Format                *
-*                                          *
-* Copyright (c) 1998 Avid Technology, Inc. *
-* Copyright (c) 1998 Microsoft Corporation *
-*                                          *
-\******************************************/
 
 /***********************************************\
 *	Stub only.   Implementation not yet added	*
@@ -54,7 +64,7 @@ protected:
 public:
 
 
-/****/
+
   //****************
   // SetStillFrame()
   //
@@ -64,7 +74,7 @@ public:
         (ImplAAFSourceClip * stillFrame);
     //@comm Essence group choices should be added with the AddChoice() function.
     
-/****/
+
   //****************
   // GetStillFrame()
   //
@@ -74,33 +84,64 @@ public:
         (ImplAAFSourceClip **stillFrame);
     //@comm Essence group choices should be added with the AddChoice() function.
     
-/****/
+
   //****************
-  // AddChoice()
+  // AppendChoice()
   //
   virtual AAFRESULT STDMETHODCALLTYPE
     AppendChoice
         // @parm [in] Source clip to add as a choice 
-        (ImplAAFSourceClip * choice);
+        (ImplAAFSegment * choice);
 
-/****/
+
   //****************
-  // GetNumChoices()
+  // PrependChoice()
   //
   virtual AAFRESULT STDMETHODCALLTYPE
-    GetNumChoices
+    PrependChoice
+        // @parm [in] Source clip to add as a choice 
+        (ImplAAFSegment * choice);
+
+
+  //****************
+  // InsertChoiceAt()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    InsertChoiceAt
+        // @parm [in] place to put new choice
+        (aafUInt32 index,
+
+		 // @parm [in] Source clip to add as a choice
+		 ImplAAFSegment * choice);
+
+
+  //****************
+  // CountChoices()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    CountChoices
         (aafUInt32  *result);
 
+
   //****************
-  // GetIndexedChoice()
+  // GetChoiceAt()
   //
   virtual AAFRESULT STDMETHODCALLTYPE
-    GetIndexedChoice
+    GetChoiceAt
         (// @parm [in] The 0-based index into the array
          aafUInt32  index,
 
          // @parm [out] The representation at that index
-         ImplAAFSourceClip  ** result);
+         ImplAAFSegment  ** result);
+
+
+  //****************
+  // RemoveChoiceAt()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    RemoveChoiceAt
+        (// @parm [in] The 0-based index into the array
+         aafUInt32  index);
 
 public:
 	//SDK-private methods
@@ -116,20 +157,13 @@ public:
 										ImplAAFOperationGroup **effeObject, aafInt32	*nestDepth,
 										ImplAAFComponent **foundObj, aafBool *foundTransition);
 
-	AAFRESULT GetCriteriaSourceClip(
+	AAFRESULT GetCriteriaSegment(
 			aafMediaCriteria_t *criteria,
-			ImplAAFSourceClip		**retSrcClip);
-
-public:
-  // Declare this class to be storable.
-  //
-  OMDECLARE_STORABLE(ImplAAFEssenceGroup)
+			ImplAAFSegment		**retSrcClip);
 
 private:
-	OMStrongReferenceVectorProperty<ImplAAFSourceClip>	_choices;
+	OMStrongReferenceVectorProperty<ImplAAFSegment>	_choices;
 	OMStrongReferenceProperty<ImplAAFSourceClip>		_stillFrame;
 };	
 
 #endif // ! __ImplAAFEssenceGroup_h__
-
-
