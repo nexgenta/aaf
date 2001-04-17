977293148b29b926b5d15ed4448008e88984013e
tbingham <tbingham>
2001-04-17 05:03:06 +0000

    Add GetTypeCategory() method (override from ImplAAFTypeDef).

--- ref-impl/src/impl/ImplAAFTypeDefRename.h
+++ ref-impl/src/impl/ImplAAFTypeDefRename.h
@@ -4,13 +4,32 @@
 #define __ImplAAFTypeDefRename_h__
 
 
-/******************************************\
-*                                          *
-* Advanced Authoring Format                *
-*                                          *
-* Copyright (c) 1998 Avid Technology, Inc. *
-*                                          *
-\******************************************/
+/***********************************************************************
+ *
+ *              Copyright (c) 1998-1999 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and accompanying 
+ * documentation, and to distribute and sublicense application software
+ * incorporating this software for any purpose is hereby granted, 
+ * provided that (i) the above copyright notice and this permission
+ * notice appear in all copies of the software and related documentation,
+ * and (ii) the name Avid Technology, Inc. may not be used in any
+ * advertising or publicity relating to the software without the specific,
+ *  prior written permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+ * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+ * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+ * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+ * LIABILITY.
+ *
+ ************************************************************************/
 
 
 
@@ -109,10 +128,16 @@ public:
     pvtCreateOMProperty (OMPropertyId pid,
 						 const aafCharacter * name) const;
 
-public:
-  // Declare this class to be storable.
+  //****************
+  // GetTypeCategory()
   //
-  OMDECLARE_STORABLE(ImplAAFTypeDefRename)
+  // (override from ImplAAFTypeDef)
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetTypeCategory
+        // @parm [out] Returned type category
+        (eAAFTypeCategory_t *  pTid);
+
 
 private:
   ImplAAFTypeDefSP BaseType () const;
