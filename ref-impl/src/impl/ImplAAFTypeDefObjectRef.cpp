14cda371b7eb088d6a0fd85afb655dc511c8b47e
tbingham <tbingham>
2001-04-17 08:19:03 +0000

    Add methods to assist in aggregation error checking: IsAggregatable(), IsStreamable(), IsFixedArrayable(), IsVariableArrayable(), and IsStringable().

--- ref-impl/src/impl/ImplAAFTypeDefObjectRef.cpp
+++ ref-impl/src/impl/ImplAAFTypeDefObjectRef.cpp
@@ -1,11 +1,29 @@
-/******************************************\
-*                                          *
-* Advanced Authoring Format                *
-*                                          *
-* Copyright (c) 1998 Avid Technology, Inc. *
-* Copyright (c) 1998 Microsoft Corporation *
-*                                          *
-\******************************************/
+/***********************************************************************
+ *
+ *              Copyright (c) 1998-1999 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and accompanying 
+ * documentation, and to distribute and sublicense application software
+ * incorporating this software for any purpose is hereby granted, 
+ * provided that (i) the above copyright notice and this permission
+ * notice appear in all copies of the software and related documentation,
+ * and (ii) the name Avid Technology, Inc. may not be used in any
+ * advertising or publicity relating to the software without the specific,
+ *  prior written permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+ * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+ * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+ * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+ * LIABILITY.
+ *
+ ************************************************************************/
 
 
 #ifndef __ImplAAFPropertyValue_h__
@@ -37,8 +55,26 @@ ImplAAFTypeDefObjectRef::~ImplAAFTypeDefObjectRef ()
 
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFTypeDefObjectRef::Initialize (
-      aafUID_t *  pID,
+      const aafUID_t *  pID,
       ImplAAFClassDef * pObjType,
+      wchar_t *  pTypeName)
+{
+  AAFRESULT hr;
+  aafUID_t id;
+
+  if (! pObjType)  return AAFRESULT_NULL_PARAM;
+  hr = pObjType->GetAUID(&id);
+  if (! AAFRESULT_SUCCEEDED (hr)) return hr;
+
+  return pvtInitialize (pID, &id, pTypeName);
+}
+
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFTypeDefObjectRef::pvtInitialize (
+      const aafUID_t *  pID,
+      const aafUID_t * pRefdObjID,
       wchar_t *  pTypeName)
 {
   return AAFRESULT_NOT_IMPLEMENTED;
@@ -125,3 +161,19 @@ void ImplAAFTypeDefObjectRef::internalize(OMByte* externalBytes,
   assert (internalBytesSize >= externalBytesSize);
   copy (externalBytes, internalBytes, externalBytesSize);
 }
+
+
+bool ImplAAFTypeDefObjectRef::IsAggregatable () const
+{ return false; }
+
+bool ImplAAFTypeDefObjectRef::IsStreamable () const
+{ return false; }
+
+bool ImplAAFTypeDefObjectRef::IsFixedArrayable () const
+{ return false; }
+
+bool ImplAAFTypeDefObjectRef::IsVariableArrayable () const
+{ return true; }
+
+bool ImplAAFTypeDefObjectRef::IsStringable () const
+{ return false; }
