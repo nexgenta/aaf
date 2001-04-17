f5bccdebb4a125145e296bbf40cc6a7c0498b263
tbingham <tbingham>
2001-04-17 12:23:12 +0000

    This is the first version of a perl script that searches dod files for enums and automically generates code to validate each type associated with the corresponding enum.

--- dodo/GenEnumValidation.pl
+++ dodo/GenEnumValidation.pl
@@ -1,102 +1,12 @@
-###############################################################################
-#
-# The contents of this file are subject to the AAF SDK Public
-# Source License Agreement (the "License"); You may not use this file
-# except in compliance with the License.  The License is available in
-# AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
-# Association or its successor.
-# 
-# Software distributed under the License is distributed on an "AS IS"
-# basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
-# the License for the specific language governing rights and limitations
-# under the License.
-# 
-# The Original Code of this file is Copyright 1998-2001, Licensor of the
-# AAF Association.
-# 
-# The Initial Developer of the Original Code of this file and the
-# Licensor of the AAF Association is Avid Technology.
-# All rights reserved.
-#
-###############################################################################
-
-###############################################################################
 #
 # File: GenEnumValidation.pl
-#
-# Created: 16-JAN-2000 by Tom Ransdell.
 # 
-# Purpose:
-#   Automatically generate runtime validation code for each enum recognized
-# from dod the files passed on the command line. NOTE: If there are any 
-# dependencies between files then files must be passed in the correct
-# dependency order or the C/C++ compiler will fail to compile the generated
-# validate files, CAAFEnumValidation.h and CAAFEnumValidation.cpp.
-#
-# Typical usage:
-# perl GenEnumValidation.pl AAFTypes.dod AAFPluginTypes.dod > enumValidationLog.txt
-# will generate three files:
-# CAAFEnumValidation.cpp,
-# CAAFEnumValidation.h and
-# enumValidationLog.txt
-#
-# perl GenEnumValidation.pl --source AAFTypes.dod AAFPluginTypes.dod > enumValidationLog.txt
-# will generate two files:
-# CAAFEnumValidation.cpp and
-# enumValidationLog.txt
-#
-#
-# perl GenEnumValidation.pl --include AAFTypes.dod AAFPluginTypes.dod > enumValidationLog.txt
-# will generate two files:
-# CAAFEnumValidation.h and
-# enumValidationLog.txt
-#
-###############################################################################
 
 require 5.002;
 
-use Getopt::Long;
 use Class::Struct;
 
-
-#
-# Define a structure to represent an enum.
-#
-struct(EnumStruct => [tag => '$', name => '$', num => '$', member => '@']);
-
-#
-# Define a structure to represent an enum member.
-#
-struct(EnumMemberStruct => [name => '$', value => '$']);
-
-
-
 printf "Running GenEnumValidation.pl...\n";
-
-
-# Command line options that control the type of the output.
-my $include = 0; # 'CAAFEnumValidation.h';
-my $source = 0; #'CAAFEnumValidation.cpp';
-
-# Setup option tags and local variable assignment.
-my %optctl = (
-  "debug" => \$debug,
-  "include" => \$include,
-  "source" => \$source
-);
-
-# Get the options
-my $result = GetOptions (\%optctl, "debug", "include", "source");
-exit 1 if (!$result);
-
-# Default to creating both the source and the include files.
-if (!$include && !$source) {
- $include = 1;
- $source = 1;
-}
-
-# Variable to hold the list of processed files.
-my @fileList = ();
 
 # Variable to hold a copy of the current line that we are processing.
 my $line = '';
@@ -115,11 +25,20 @@ my $enumString = '';
 # during processing.
 my %enums = ();
 
-# State variable that holds a reference to the current line
-# matching subroutine.
-my $processLine;
+# Initially we want to start out by searching for the first line 
+# that contains an enum declaration.
+my $processLine = \&find_start_of_enum;
 
 
+#
+# Define a structure to represent an enum.
+#
+struct(EnumStruct => [tag => '$', name => '$', num => '$', member => '@']);
+
+#
+# Define a structure to represent an enum member.
+#
+struct(EnumMemberStruct => [name => '$', value => '$']);
 
 #
 # process all of the input files...
@@ -128,25 +47,14 @@ foreach $file (@ARGV) {
   printf "Processing: %s\n", $file;
 
   open (DODOFILE , "$file") or die "Could not open \"$file\".\n";
-
-  # Save the file name.
-  push @fileList, $file;
-
-  # Initially we want to start out by searching for the first line 
-  # that contains an enum declaration.
-  $processLine = \&find_start_of_enum;
-
 LINE: while (<DODOFILE>) {
 
     # Skip any lines that contain a dodo macro.
     next LINE if (/AD_/ || /\#c/);
 	
-    # special extra enum that we don't need to validate (only exists
-    # to keep Microsoft's MIDL compiler happy.
-    next LINE if (/aafMAX_NUM_RGBA_COMPS/);
-
-    s/\s*\/\/.*$//; # remove c++ comments.
-    s/\/\*.*\*\///; # remove one line c comments. 
+	# special extra enum that we don't need to validate (only exists
+	# to keep Microsoft's MIDL compiler happy.
+	next LINE if (/aafMAX_NUM_RGBA_COMPS/);
 
     $line = $_;
 
@@ -158,8 +66,8 @@ LINE: while (<DODOFILE>) {
 
 
 # Write out a function prototype to validate each enum.
-&make_validateHeader if ($include);
-&make_validateSource if ($source);
+&make_validateHeader;
+&make_validateSource;
 
 
 
@@ -173,7 +81,7 @@ exit;
 sub find_start_of_enum 
 {
   # Find the starting line that defines the enum.
-  if ($line =~ /^\s*(typedef)?\s*enum\s+(\w*)\s*(\})?/) {
+  if ($line =~ /(typedef)?\s*enum\s+(\w*)\s*(\})?/) {
     $enumCount++;
 
     printf "%d: %s: %s", $enumCount, $2, $line;
@@ -205,44 +113,10 @@ sub find_start_of_enum
 # starting line of an enum.
 sub find_end_of_enum 
 {
-  my $lastLine = '';
-  
-  # There are many substitution operations on this line.
-  $_ = $line;
-
   # Find the last line that defines the enum.
-  if (/}/) {
-    $lastLine = $_;
-    s/}.*$//;     # remove the '}' and any trailing characters.
-                  # continue and see if there was any member on this line.
-  } 
-
-  # Filter out any unnecessary text.
-  s/{//;          # remove any '{'.
-  s/\,.*$//;      # remove any commas and trailing characters.
-  s/^\s*//;       # remove all leading whitespace.
-  s/^\n$//;       # remove any blank lines.
-  $line = $_;
-
-  if ($line ne '') { # skip any blank lines.
-    # At this point the line should be either:
-    # memberName or
-    # memberName = value
-    /^(\w+)\s*|$/ or die "Unexpected or malformed enum member.\n";
-    $enumMember = new EnumMemberStruct;
-    $enumMember->name($1);
-
-    # Save this enum member inside the current enum.
-    $enum->member($enum->num, $enumMember);
-    $enum->num($enum->num + 1);
-
-    # Grow the current expression string.
-    $enumString = join ('', $enumString, $line);
-  }
-
-  if ($lastLine ne '') {
+  if ($line =~ /}/) {
     # Save the enum name if one exists.
-    if ($lastLine =~ /}\s*(\w+)\s*;/) {
+    if ($line =~ /}\s*(\w+)\s*;/) {
       if ($1 ne '') {
         $enum->name($1);
         printf "%s\n", $enum->name;
@@ -263,9 +137,53 @@ sub find_end_of_enum
 
     # Start over looking for the start of the enum.
     $processLine = \&find_start_of_enum;
+
+  } else {
+    # Filter out any unnecessary text.
+    $_ = $line;
+    s/\s*\/\/.*$//; # remove c++ comments.
+    s/\/\*.*\*\///; # remove one line c comments. 
+    s/{//;          # remove any '{'.
+    s/\,.*$//;      # remove any commas and trailing characters.
+    s/^\s*//;       # remove all leading whitespace.
+    s/^\n$//;       # remove any blank lines.
+    $line = $_;
+
+    if ($line ne '') { # skip any blank lines.
+      # At this point the line should be either:
+      # memberName or
+      # memberName = value
+      /^(\w+)\s*|$/ or die "Unexpected or malformed enum member.\n";
+      $enumMember = new EnumMemberStruct;
+      $enumMember->name($1);
+
+      # Save this enum member inside the current enum.
+      $enum->member($enum->num, $enumMember);
+      $enum->num($enum->num + 1);
+
+      # Grow the current expression string.
+      $enumString = join ('', $enumString, $line);
+    }
   }
 }
 
+
+# Subroutine to emit the AAF copywrite notice
+sub printCopywrite
+{
+  local (*THEFILE) = @_;  # Filehandle
+  my $theTime = gmtime;
+
+  printf THEFILE "/**************************************************\\\n";
+  printf THEFILE "*                                                  *\n";
+  printf THEFILE "* Advanced Authoring Format                        *\n";
+  printf THEFILE "*                                                  *\n";
+  printf THEFILE "* Copyright (c) 1998-2000 Avid Technology, Inc.    *\n";
+  printf THEFILE "*                                                  *\n";
+  printf THEFILE "* Automatically generated:%s *\n", $theTime;
+  printf THEFILE "*                                                  *\n";
+  printf THEFILE "\\**************************************************/\n\n";
+}
 
 # Subroutine to print the validation function declaration.
 # This is shared by both make_validateHeader and 
@@ -275,38 +193,27 @@ sub printFunctionDeclaration
   local (*THEFILE, $name) = @_;  # Filehandle, string
 
   # Enums that end in AAF are special. Strip off the
-  # the _e so that we can use the rest of the name as 
-  # base and append the '_t' to create the corresponding 
-  # type name.
+  # the _e (or _t for compatibility) so that we can use the 
+  # rest of the name as base and append the '_t' to create 
+  # the corresponding type name.
   $_ = $name;
-  s/(_[et])$/_t/;
+  s/(_[et])$//;
   $name = $_;
 
-  print THEFILE 'bool Is_', $name, '_Valid(', $name, ' param)';
+  print THEFILE 'bool Is_', $name, '_Valid(', $name;  
+  print THEFILE '_t' if ($1 ne ''); # only add if one was removed.
+  print THEFILE ' param)';
 }
 
 # Subroutine to write out the validateEnums.h header file
 # with a function prototype for each enum.
 sub make_validateHeader
 {
-  open(HEADER, ">CAAFEnumValidation.h") or die "Count not create enum validation header file.\n";
+  open(HEADER, ">CAAFEnumValidation.h") || die "Count not create validateEnums.h\n";
 
-  print HEADER "\n";
-  
-  # Include order is important. For now we assume that the files
-  # are being processing in dependency order. For example:
-  # the file list had better have AAFTypes.dod before AAFPluginsTypes.dod.
-  foreach $file (@fileList) {
-    $_ = $file;
-    s/(\.dod)$//;
-    
-    print HEADER '#include "', $_;
-    print HEADER '.h' if ($1 ne ''); # only add if one was removed.
-    print HEADER '"', "\n";
-  }
-
-#  print HEADER '#include "AAFTypes.h"', "\n";
-#  print HEADER '#include "AAFPluginTypes.h"', "\n\n";
+  printCopywrite(*HEADER);
+  print HEADER '#include "AAFTypes.h"', "\n";
+  print HEADER '#include "AAFPluginTypes.h"', "\n\n";
 
   # Write out a function prototype to validate each enum.
   print HEADER "\n\n// Function prototypes for all AAF enums.\n\n";
@@ -332,9 +239,9 @@ sub make_validateHeader
 # with a function prototype for each enum.
 sub make_validateSource
 {
-  open(SOURCE, ">CAAFEnumValidation.cpp") or die "Count not create enum validation source file.\n";
+  open(SOURCE, ">CAAFEnumValidation.cpp") || die "Count not create validateEnums.cpp\n";
 
-  print SOURCE "\n";
+  printCopywrite(*SOURCE);
   print SOURCE '#include "CAAFEnumValidation.h"', "\n\n";
 
   foreach $enumName (sort keys %enums) {
@@ -356,3 +263,5 @@ sub make_validateSource
 
   close(SOURCE);
 }
+
+
