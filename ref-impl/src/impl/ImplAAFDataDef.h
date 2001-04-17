39b09c97829a30bcb7daa5d07026508316451578
tbingham <tbingham>
2001-04-17 03:38:07 +0000

    Remove Initialize() from IAAFDefObject() and put it on primary interfaces of concrete derived classes.

--- ref-impl/src/impl/ImplAAFDataDef.h
+++ ref-impl/src/impl/ImplAAFDataDef.h
@@ -4,20 +4,37 @@
 #define __ImplAAFDataDef_h__
 
 
-/******************************************\
-*                                          *
-* Advanced Authoring Format                *
-*                                          *
-* Copyright (c) 1998 Avid Technology, Inc. *
-* Copyright (c) 1998 Microsoft Corporation *
-*                                          *
-\******************************************/
+/***********************************************************************
+ *
+ *              Copyright (c) 1998-1999 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and accompanying 
+ * documentation, and to distribute and sublicense application software
+ * incorporating this software for any purpose is hereby granted, 
+ * provided that (i) the above copyright notice and this permission
+ * notice appear in all copies of the software and related documentation,
+ * and (ii) the name Avid Technology, Inc. may not be used in any
+ * advertising or publicity relating to the software without the specific,
+ * prior written permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+ * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+ * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+ * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+ * LIABILITY.
+ *
+ ************************************************************************/
 
 
 #ifndef __ImplAAFDefObject_h__
 #include "ImplAAFDefObject.h"
 #endif
-
 
 class ImplAAFDataDef : public ImplAAFDefObject
 {
@@ -32,16 +49,15 @@ protected:
   virtual ~ImplAAFDataDef ();
 
 public:
-
-
-
   //****************
-  // GetName()
+  // Initialize()
   //
   virtual AAFRESULT STDMETHODCALLTYPE
-    GetName
-        // @parm [retval,out] Pointer to name
-        (aafString_t *  pstrName);
+    Initialize
+        // @parm [in] Pointer to an AUID reference
+        (const aafUID_t & id,
+		 const aafCharacter *name,
+		 const aafCharacter *description);
 
   //****************
   // IsPictureKind()
@@ -80,8 +96,8 @@ public:
   //
   virtual AAFRESULT STDMETHODCALLTYPE
     DoesDataDefConvertTo
-        (// @parm [in,ref] Auid to compare against
-         aafUID_t *  pAuid,
+        (// @parm [in,ref] def to compare against
+         ImplAAFDataDef * pDataDef,
 
          // @parm [retval,out] pointer to result
          aafBool *  bDoesConvertTo);
@@ -91,8 +107,8 @@ public:
   //
   virtual AAFRESULT STDMETHODCALLTYPE
     IsDataDefOf
-        (// @parm [in,ref] Auid to compare against
-         aafUID_t *  pAuid,
+        (// @parm [in,ref] data def to compare against
+         ImplAAFDataDef * pDataDef,
 
          // @parm [retval,out] pointer to result
          aafBool *  bIsDataDefOf);
@@ -102,24 +118,29 @@ public:
   //
   virtual AAFRESULT STDMETHODCALLTYPE
     DoesDataDefConvertFrom
-        (// @parm [in,ref] Auid to compare against
-         aafUID_t *  pAuid,
+        (// @parm [in,ref] data def to compare against
+         ImplAAFDataDef * pDataDef,
 
          // @parm [retval,out] pointer to result
          aafBool *  bDoesConvertFrom);
 
+private:
+  // *NOT* reference counted!
+  ImplAAFDictionary * _pCachedDict;
 
-
-public:
-  // Declare this class to be storable.
-  //
-  OMDECLARE_STORABLE(ImplAAFDataDef)
-
-  // Declare the module test method. The implementation of the will be be
-  // in /test/ImplAAFDataDefTest.cpp.
-  static AAFRESULT test();
+  ImplAAFDictionary * GetDict();
 };
 
+//
+// smart pointer
+//
+
+#ifndef __ImplAAFSmartPointer_h__
+// caution! includes assert.h
+#include "ImplAAFSmartPointer.h"
+#endif
+
+typedef ImplAAFSmartPointer<ImplAAFDataDef> ImplAAFDataDefSP;
+
+
 #endif // ! __ImplAAFDataDef_h__
-
-
