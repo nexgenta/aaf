ecf05b2e0b90be3045ed6a911b46e2df4152783a
tbingham <tbingham>
2001-04-17 06:34:48 +0000

    Further codec integration

--- ref-impl/plugins/CAAFWAVECodec.h
+++ ref-impl/plugins/CAAFWAVECodec.h
@@ -3,54 +3,36 @@
 #ifndef __CAAFWAVECodec_h__
 #define __CAAFWAVECodec_h__
 
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- * prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
 
-
-#include "AAFUtils.h"
+#ifndef __AAF_h__
 #include "AAF.h"
+#endif
+
 #include "AAFPlugin.h"
+
+#ifndef __AAFDefaultCodec_h__
+#include "CAAFDefaultCodec.h"
+#endif
+
+#ifndef __CAAFUnknown_h__
 #include "CAAFUnknown.h"
+#endif
 
 EXTERN_C const CLSID CLSID_AAFWaveCodec;
 
-typedef struct
-{
-	void			*buf;
-	aafInt32		buflen;
-	aafInt32		samplesLeft;
-	aafInt32		bytesXfered;
-	aafmMultiXfer_t	*xfer;
-} interleaveBuf_t;
-
+#if 0
+// A Codec has a generic "plugin" interface, and a more specific "codec"
+// interface.  Both of which are described in this .h file.
 class CAAFWaveCodec
-  : public IAAFEssenceCodec,
-	public IAAFPlugin,
-    public CAAFUnknown
+  : public CAAFDefaultCodec
 {
 protected:
 
@@ -66,106 +48,123 @@ public:
 
   // Set up a codec.
   STDMETHOD (Start)
-     (void);
+     ();
 
   // Tear down a codec.
   STDMETHOD (Finish)
-     (void);
+     ();
 
-  STDMETHOD (GetNumDefinitions)(aafInt32 *pDefCount);
-  STDMETHOD (GetIndexedDefinitionID)(aafInt32 index, aafUID_t *result);
-  STDMETHOD (GetPluginDescriptorID)(aafUID_t *result);
-  STDMETHOD (GetEssenceDescriptorID)(aafUID_t *result);
-  STDMETHOD (GetEssenceDataID)(aafUID_t *result);
-  STDMETHOD (GetIndexedDefinitionObject)(aafInt32 index, IAAFDictionary *dict, IAAFDefObject **def);
-  STDMETHOD (CreateDescriptor)(IAAFDictionary *dict, IAAFPluginDescriptor **desc);
 
-  STDMETHOD (SetEssenceAccess)
-    (/*[in]*/ IAAFEssenceAccess *access); // Set the AAFEssenceAccess used as a factory for AAFEssenceFormat
 
- STDMETHOD (GetFlavourCount)
-    (/*[in]*/ aafInt32  *pCount);
+protected:
+  // 
+  // Declare the QI that implements for the interfaces
+  // for this module. This will be called by CAAFUnknown::QueryInterface().
+  // 
+  virtual HRESULT InternalQueryInterface(REFIID riid, void **ppvObjOut);
 
-	// Some codecs have variants handled by a single codec.
+
+public:
+  //
+  // This class as concrete. All AAF objects can be constructed from
+  // a CLSID. This will allow subclassing all "base-classes" by
+  // aggreggation.
+  // 
+  AAF_DECLARE_CONCRETE();
+  //
+  //********
+
+  // Declare the module test method. The implementation of the will be be
+  // in /test/CAAFPluginTest.cpp.
+  static HRESULT test();
+};
+#endif
+
+#if 1 
+class CAAFWaveCodec
+  : public CAAFDefaultCodec
+{
+protected:
+
+  //********
+  //
+  // Constructor/destructor
+  //
+  CAAFWaveCodec (IUnknown * pControllingUnknown, aafBool doInit = AAFTrue);
+  virtual ~CAAFWaveCodec ();
+
+public:
+
+
+
+
+  // Some codecs have variants handled by a single codec.
 	// (For example, the Avid AVR codec handles multiple AVRs.)
 	// The number of variants is returned by GetMetaInfo, and cached
 	// by the AAFPluginManager.
 	//
-  STDMETHOD (GetIndexedFlavourID)
+  STDMETHOD (GetIndexedVariantID)
     (/*[in]*/ aafInt32  index, // Which variant to get the ID for
      /*[out]*/ aafUID_t *  pVariant); // The returned variant ID 
 
 	
   // Sets a value indicating whether the SDK is handling the compression.
-  STDMETHOD (SetCompressionEnabled)
-    (/*[in]*/ aafBool  enable); // isCompressionEnabled 
+  STDMETHOD (SetCompression)
+    (/*[in]*/ aafCompressEnable_t  enable); // isCompressionEnabled 
 
-  STDMETHOD (GetDataDefinitionCount)
-        (/* [out] */ aafInt32 *pCount);
-
-			// // All codecs handle at least one kind of media (picture, sound, control)
+  // // All codecs handle at least one kind of media (picture, sound, control)
 	// but some handle more than one.  The kind of media is specified by an
 	// AAFDataDefinition.  The numnber of data definitions is returned by
 	// GetMetaInfo, and cached by the AAFPluginManager.
   STDMETHOD (GetIndexedDataDefinition)
     (/*[in]*/ aafInt32  index, // Which data definition to get the ID for
-     /*[out]*/ aafUID_t * pVariant); // The returned dataDefinition 
+     /*[out]*/ aafUID_t *  pVariant); // The returned dataDefinition 
 
-  STDMETHOD (GetMaxCodecDisplayNameLength)
-    (aafUInt32  *bufSize);
+  // This function is called at startup by the plugin manager
+  STDMETHOD (GetMetaInfo)
+    (/*[out]*/ aafCodecMetaInfo_t *  pInfo); // The returned meta information about this codec 
 
-	// Given a variant ID, return the human readable name
+  // Given a variant ID, return the human readable name
   STDMETHOD (GetCodecDisplayName)
-    (/*[in]*/ aafUID_constref variant, // which variant of the codec to use
-     /*[in,string]*/ aafCharacter *  pName, // Human-readable name of the variant
-     /*[in]*/ aafUInt32  bufSize); // length of the buffer to hold variant Name 
+    (/*[in]*/ aafUID_t  variant, // which variant of the codec to use
+     /*[in,string]*/ wchar_t *  pName, // Human-readable name of the variant
+     /*[in]*/ aafInt32  bufSize); // length of the buffer to hold variant Name 
+
+  // This function is called when opening a media stream on a file mob.
+  STDMETHOD (GetSelectInfo)
+    (/*[in]*/ aafUID_t  fileMobID, // See if the current codec can process data from this file mob
+     /*[out]*/ aafCodecSelectInfo_t *  pInfo); // The returned meta information about this codec 
+	
+
 	
   // Returns the number of channels which this codec can handle
 			// of the given essence kind
   STDMETHOD (GetNumChannels)
-    (/*[in]*/ IAAFSourceMob *fileMob, // Get the number of processable channels on this file mob
-     /*[in]*/ aafUID_constref essenceKind, // This is the type of essence to open
-	 IAAFEssenceStream *stream,
+    (/*[in]*/ aafUID_t  fileMobID, // Get the number of processable channels on this file mob
+     /*[in]*/ aafUID_t  essenceKind, // This is the type of essence to open
      /*[out]*/ aafInt16 *  pNumChannels); // The number of channels present 
 
-  STDMETHOD (GetSelectInfo)
-   (IAAFSourceMob *fileMob,
-        IAAFEssenceStream *stream,
-        aafSelectInfo_t *  pSelectInfo);
-
-  // Returns the number of samples which this codec can find on the
-			// given slot.
-  STDMETHOD (GetNumSamples)
-     (/*[in]*/ aafUID_constref essenceKind, // This is the type of essence to check
-	/*[out]*/ aafLength_t *  pNumSamples); // The number of samples present of that type
-
-	 
   // Returns a (possibly zero-length) string listing any problems
 			//found with the essence, or the relationship between variables
 			// in the essence descriptor, and any such values contained within
 			// the essence data
-  STDMETHOD (ValidateEssence)
-    (/*[in]*/ IAAFSourceMob *fileMob, // Run a check on this file mob
-         IAAFEssenceStream *stream,
-    /*[in]*/ aafCheckVerbose_t  verbose, // This is the verbosity level of the output
-     /*[out]*/ aafCheckWarnings_t warning, // This determines whether the output contains warnings
-         aafInt32  bufSize,
-		wchar_t *  pName,
-        aafInt32  *bytesWritten);
+  STDMETHOD (SemanticCheck)
+    (/*[in]*/ aafUID_t  fileMobID, // Run a check on this file mob
+     /*[in]*/ aafCheckVerbose_t  verbose, // This is the verbosity level of the output
+     /*[out]*/ aafCheckWarnings_t *  warning, // This determines whether the output contains warnings
+     /*[in,string]*/ wchar_t *  pName, // Human-readable text describing problems (or lack therof) with the media
+     /*[in]*/ aafInt32  bufSize); // length of the buffer to hold variant Name 
 		
   // Create a media data object, and attach the correct type of
 			//EssenceDescriptor to the fileMob
   STDMETHOD (Create)
-    (/*[in]*/ IAAFSourceMob *fileMob, // Create the essence attached to this file mob
-     /*[in]*/ aafUID_constref variant, // which variant of the codec to use
-        IAAFEssenceStream * stream,
-        aafInt32 numParms,
-        aafmMultiCreate_t *createParms);
+    (/*[in]*/ aafUID_t  fileMobID, // Create the essence attached to this file mob
+     /*[in]*/ aafUID_t  variant, // which variant of the codec to use
+     /*[in]*/ IAAFEssenceStream * stream); // Here is an essence stream with the raw data 
 
   // Open a media data object.
   STDMETHOD (Open)
-    (/*[in]*/ IAAFSourceMob *fileMob, // Open the essence attached to this file mob
-        aafSlotID_t	slotID,
+    (/*[in]*/ aafUID_t  fileMobID, // Open the essence attached to this file mob
      /*[in]*/ aafMediaOpenMode_t  openMode, // In this mode
      /*[in]*/ IAAFEssenceStream * stream); // Here is an essence stream with the raw data 
 	
@@ -175,15 +174,13 @@ public:
   STDMETHOD (WriteBlocks)
     (/*[in]*/ aafDeinterleave_t  inter, // Whether the material will be de-interleaved on read
      /*[in]*/ aafInt16  xferBlockCount, // How many aafMultiXfer blocks follow
-     /*[in]*/ aafmMultiXfer_t *  xferBlock, // One or more blocks containing buffer pointer and length 
-     /*[in]*/ aafmMultiResult_t *  resultBlock);
+     /*[in]*/ aafmMultiXfer_t *  xferBlock); // One or more blocks containing buffer pointer and length 
 
   // Read blocks into one or more buffers, de-interleaving if needed.
   STDMETHOD (ReadBlocks)
     (/*[in]*/ aafDeinterleave_t  inter, // Whether the material will be de-interleaved on read
      /*[in]*/ aafInt16  xferBlockCount, // How many aafmMultiXfer blocks follow
-     /*[in]*/ aafmMultiXfer_t *  xferBlock, // One or more blocks containing buffer pointer and length 
-     /*[in]*/ aafmMultiResult_t *  resultBlock);
+     /*[in]*/ aafmMultiXfer_t *  xferBlock); // One or more blocks containing buffer pointer and length 
 
   // Seek to a particular sample frame on the media.  The
 			// sample frame is one frame for picture, and one sample
@@ -192,8 +189,8 @@ public:
     (/*[in]*/ aafPosition_t  sampleFrame); // The sample frame to seek to. 
 
   // Close the media stream, ready to open another or Finish.
-  STDMETHOD (CompleteWrite)
-     (IAAFSourceMob *desc);
+  STDMETHOD (Close)
+     ();
 
 		
 
@@ -221,19 +218,16 @@ public:
 
   // Write some number of bytes to the stream exactly and with no formatting or compression.
   STDMETHOD (WriteRawData)
-    (/*[in]*/ aafUInt32 nSamples,	//number of samples to read
-	/*[in,size_is(buflen)]*/ aafDataBuffer_t  buffer, // to a buffer
-     /*[in]*/ aafUInt32  buflen); // of this size 
+    (/*[in,size_is(buflen)]*/ aafDataBuffer_t  buffer, // to a buffer
+     /*[in]*/ aafInt32  buflen); // of this size 
 
 
 
   // Read some number of bytes from the stream exactly and with no formatting or compression.
   STDMETHOD (ReadRawData)
-    (/*[in]*/ aafUInt32 nSamples,	//number of samples to read
-	 /*[in]*/ aafUInt32  buflen, // to a buffer of this size
+    (/*[in]*/ aafUInt32  buflen, // to a buffer of this size
      /*[out, size_is(buflen), length_is(*bytesRead)]*/ aafDataBuffer_t  buffer, // here is the buffer
-     /*[out,ref]*/ aafUInt32 *  bytesRead,	// Return bytes actually read 
-     /*[out,ref]*/ aafUInt32 *  samplesRead); // Return samples actually read 
+     /*[out,ref]*/ aafUInt32 *  bytesRead); // Return bytes actually read 
 
 
 	
@@ -241,7 +235,7 @@ public:
 			// an AAFEssenceDescriptor to match, with all fields filled in.
   STDMETHOD (CreateDescriptorFromStream)
     (/*[in]*/ IAAFEssenceStream * pStream, // A raw file stream
-     /*[in]*/ IAAFSourceMob *fileMob); // Put the finished descriptor on this source mob 
+     /*[in]*/ aafUID_t  pSourceMobID); // Put the finished descriptor on this source mob 
 
 
 
@@ -256,23 +250,7 @@ public:
   // Supply an essence format object specifying what data is
 			// required, and it is filled in with the data values.
   STDMETHOD (GetEssenceFormat)
-    (/*[in]*/ IAAFEssenceFormat *pTemplate, // An essence format template object 
-    /*[out]*/ IAAFEssenceFormat **pResult); // An essence format result object 
-
-  STDMETHOD (GetDefaultEssenceFormat)
-    (/*[out]*/ IAAFEssenceFormat **pFormat); // An essence format result object 
-
-  STDMETHOD (GetIndexedSampleSize)
-    (aafUID_constref dataDefID,
-	 aafPosition_t pos,
-	 aafLength_t *pResult);
-
-  STDMETHOD (GetLargestSampleSize)
-    (aafUID_constref dataDefID,
-	 aafLength_t *pResult);
-
-  STDMETHOD (AddSampleIndexEntry)
-    (aafPosition_t pos);
+    (/*[in,out]*/ IAAFEssenceFormat ** ppFormat); // An essence format object 
 
 
 protected:
@@ -280,7 +258,7 @@ protected:
   // Declare the QI that implements for the interfaces
   // for this module. This will be called by CAAFUnknown::QueryInterface().
   // 
-  STDMETHOD(InternalQueryInterface)(REFIID riid, void **ppvObjOut);
+  virtual HRESULT InternalQueryInterface(REFIID riid, void **ppvObjOut);
 
 
 public:
@@ -296,34 +274,8 @@ public:
   // Declare the module test method. The implementation of the will be be
   // in /test/CAAFEssenceCodecTest.cpp.
   static HRESULT test();
-
-private:
-	IAAFEssenceStream	*_stream;
-	IAAFWAVEDescriptor	*_mdes;
-	IAAFEssenceAccess	*_access;
-	AAFByteOrder		_nativeByteOrder;
-	aafRational_t		_sampleRate;
-	aafUInt16			_bitsPerSample;
-	aafUInt16			_numCh;
-	aafUInt16			_bytesPerFrame;
-	aafUInt32			_sampleFrames;
-	aafBool				_headerLoaded;
-	interleaveBuf_t		*_interleaveBuf;
-	aafInt64			_dataStartOffset;
-	aafInt64			_dataSizeOffset;
-	aafBool				_readOnly;
-	aafBool				_sampleDataHeaderWritten;
-	aafBool				_initialSeekPerformed;
-
-	AAFRESULT fillSwappedWAVEData(aafUInt8 **destBufHdl, aafInt32 maxsize, void *data);
-	AAFRESULT scanSwappedWAVEData(aafUInt8 **srcBufHdl, aafInt32 maxsize, void *data);
-	AAFRESULT CreateWAVEheader(aafUInt8 *buffer, aafInt32 bufsize, aafInt16 numCh);
-	AAFRESULT loadWAVEHeader(void);
-	AAFRESULT GetWAVEData(aafUInt32 len, void *buf);
-
-	AAFRESULT ComputeWriteChunkSize(aafInt64 sizeOff, aafInt64 end);
-	AAFRESULT CreateAudioDataEnd(void);
 };
+#endif
 
 #endif // ! __CAAFEssenceCodec_h__
 
