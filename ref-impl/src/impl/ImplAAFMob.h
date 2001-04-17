21867d00ccf1dbf273da21aa0eca653e5a11733e
tbingham <tbingham>
2001-04-17 04:31:37 +0000

    restoring bob's changes

--- ref-impl/src/impl/ImplAAFMob.h
+++ ref-impl/src/impl/ImplAAFMob.h
@@ -3,32 +3,13 @@
 #ifndef __ImplAAFMob_h__
 #define __ImplAAFMob_h__
 
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- *  prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+*                                          *
+\******************************************/
 
 #ifndef __ImplAAFMobSlot_h__
 #include "ImplAAFMobSlot.h"
@@ -161,7 +142,7 @@ public:
   //
   virtual AAFRESULT STDMETHODCALLTYPE
     SetMobID
-        (const aafUID_t &  mobID);  //@parm [in, ref] New Mob ID
+        (aafUID_t *  mobID);  //@parm [in, ref] New Mob ID
 
 
   //****************
@@ -193,10 +174,10 @@ public:
   //
   virtual AAFRESULT STDMETHODCALLTYPE
     AppendNewTimelineSlot
-        (const aafRational_t &editRate,   //@parm [in] Edit rate property value
+        (aafRational_t  editRate,   //@parm [in] Edit rate property value
 		 ImplAAFSegment * segment,   //@parm [in] Segment to append as slot component
 		 aafSlotID_t  slotID,   //@parm [in] The Slot ID
-         const aafWChar *  slotName,   //@parm [in] Slot Name (optional)
+         aafWChar *  slotName,   //@parm [in] Slot Name (optional)
 		 aafPosition_t  origin,
 		 ImplAAFTimelineMobSlot ** newSlot);  //@parm [out] Newly created slot
 
@@ -302,8 +283,8 @@ public:
   //
   virtual AAFRESULT STDMETHODCALLTYPE
     ChangeRef
-        (const aafUID_t & oldMobID,   //@parm [in,ref] Old Mob ID reference in source clip
-		 const aafUID_t & newMobID);  //@parm [in,ref] New Mob ID reference in source clip
+        (aafUID_t *  oldMobID,   //@parm [in,ref] Old Mob ID reference in source clip
+		 aafUID_t *  newMobID);  //@parm [in,ref] New Mob ID reference in source clip
 
 
 
@@ -332,9 +313,9 @@ AAFRESULT
         (aafAppendOption_t  addType,
 		 aafRational_t  editrate,
 		 aafSlotID_t  aMobSlot,
-         const aafUID_t & essenceKind,
-		 aafSourceRef_t  ref,
-		 aafLength_t  srcRefLength);
+         aafUID_t * pEssenceKind,
+		aafSourceRef_t  ref,
+        aafLength_t  srcRefLength);
 
 	virtual AAFRESULT InternalSearchSource(	
     aafSlotID_t trackID,             /* IN */
