55a9fffe1040e8af74291c037cdb3c9b72bbbb47
tbingham <tbingham>
2001-04-17 10:15:33 +0000

    Initial dodo-generated checkin

--- ref-impl/src/impl/ImplAAFTypeDef.cpp
+++ ref-impl/src/impl/ImplAAFTypeDef.cpp
@@ -1,42 +1,29 @@
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- *  prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
 
 
-#include "ImplAAFTypeDef.h"
-#include "ImplAAFDictionary.h"
+#ifndef __ImplEnumAAFReferenceValues_h__
+#include "ImplEnumAAFReferenceValues.h"
+#endif
+
+
+
+
 #include "AAFStoredObjectIDs.h"
-#include "AAFTypeDefUIDs.h"
+
+#ifndef __ImplAAFTypeDef_h__
+#include "ImplAAFTypeDef.h"
+#endif
 
 #include <assert.h>
 #include <string.h>
 
-#if defined(macintosh) || defined(_MAC)
-#include <wstring.h>
-#endif
 
 ImplAAFTypeDef::ImplAAFTypeDef ()
 {}
@@ -49,8 +36,7 @@ AAFRESULT STDMETHODCALLTYPE
     ImplAAFTypeDef::GetTypeCategory (
       eAAFTypeCategory_t *  /*pTid*/)
 {
-  // Should be implemented in derived class.
-  return AAFRESULT_INTERNAL_ERROR;
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
 
 
@@ -59,149 +45,72 @@ AAFRESULT STDMETHODCALLTYPE
     ImplAAFTypeDef::RawAccessType (
       ImplAAFTypeDef ** /*ppRawTypeDef*/)
 {
-  // Should be implemented in derived class.
-  return AAFRESULT_INTERNAL_ERROR;
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
 
-
-
-void ImplAAFTypeDef::reorder(OMByte* /*bytes*/,
-							 size_t /*bytesSize*/) const
-{
-  // Should be implemented in derived class.
-  assert (0);
-}
-
-
-size_t ImplAAFTypeDef::externalSize(OMByte* /*internalBytes*/,
-									size_t /*internalBytesSize*/) const
-{
-  // Should be implemented in derived class.
-  assert (0);
-  return 0; // Not reached!
-}
-
-
-void ImplAAFTypeDef::externalize(OMByte* /*internalBytes*/,
-								 size_t /*internalBytesSize*/,
-								 OMByte* /*externalBytes*/,
-								 size_t /*externalBytesSize*/,
-								 OMByteOrder /*byteOrder*/) const
-{
-  // Should be implemented in derived class.
-  assert (0);
-}
-
-
-size_t ImplAAFTypeDef::internalSize(OMByte* /*externalBytes*/,
-									size_t /*externalSize*/) const
-{
-  // Should be implemented in derived class.
-  assert (0);
-  return 0; // Not reached!
-}
-
-
-void ImplAAFTypeDef::internalize(OMByte* /*externalBytes*/,
-								 size_t /*externalBytesSize*/,
-								 OMByte* /*internalBytes*/,
-								 size_t /*internalBytesSize*/,
-								 OMByteOrder /*byteOrder*/) const
-{
-  // Should be implemented in derived class.
-  assert (0);
-}
-
-
-aafBool ImplAAFTypeDef::IsFixedSize (void) const
-{
-  // Should be implemented in derived class.
-  assert (0);
-  return kAAFFalse; // not reached!
-}
-
-
-size_t ImplAAFTypeDef::PropValSize (void) const
-{
-  // Should be implemented in derived class.
-  assert (0);
-  return 0; // not reached!
-}
-
-
-aafBool ImplAAFTypeDef::IsRegistered (void) const
-{
-  // Should be implemented in derived class.
-  assert (0);
-  return kAAFFalse; // not reached!
-}
-
-
-size_t ImplAAFTypeDef::NativeSize (void) const
-{
-  // Should be implemented in derived class.
-  assert (0);
-  return 0; // not reached!
-}
-
-
-/*
-OMProperty * ImplAAFTypeDef::pvtCreateOMProperty
-  (OMPropertyId pid,
-   const aafCharacter * name) const
-{
-  assert (name);
-  size_t wNameLen = wcslen (name) + 1;
-
-  // Convert the prop name
-  char * nameBuf = new char [wNameLen];
-  assert (nameBuf);
-  wcstombs (nameBuf, name, wNameLen);
-
-  OMProperty * result = pvtCreateOMPropertyMBS (pid, nameBuf);
-  delete[] nameBuf;
-  return result;
-}
-*/
-
-OMProperty * ImplAAFTypeDef::pvtCreateOMPropertyMBS
-  (OMPropertyId /*pid*/,
-   const char * /*name*/) const
-{
-  // Should be implemented in derived class.
-  assert (0);
-  return 0; // not reached!
-}
 
 
 AAFRESULT STDMETHODCALLTYPE
-    ImplAAFTypeDef::pvtGetUInt8Array8Type (
-      ImplAAFTypeDef ** ppRawTypeDef)
+    ImplAAFTypeDef::GetMinVersion (
+      aafVersionType_t *  /*pMinVersion*/)
 {
-  if (! ppRawTypeDef)
-	return AAFRESULT_NULL_PARAM;
-  
-  ImplAAFDictionarySP pDict;
-  AAFRESULT hr = GetDictionary(&pDict);
-  if (AAFRESULT_FAILED (hr)) return hr;
-  
-  return pDict->LookupTypeDef (kAAFTypeID_UInt8Array, ppRawTypeDef);
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
 
 
-// These all should be pure virtual, but if we allow client extension
-// of behavior, clients may have to instantiate these.
-bool ImplAAFTypeDef::IsAggregatable () const
-{ assert (0); return false; }
 
-bool ImplAAFTypeDef::IsStreamable () const
-{ assert (0); return false; }
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFTypeDef::SetMinVersion (
+      aafVersionType_t *  /*pMinVersion*/)
+{
+  return AAFRESULT_NOT_IMPLEMENTED;
+}
 
-bool ImplAAFTypeDef::IsFixedArrayable () const
-{ assert (0); return false; }
 
-bool ImplAAFTypeDef::IsVariableArrayable () const
-{ assert (0); return false; }
 
-bool ImplAAFTypeDef::IsStringable () const
-{ assert (0); return false; }
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFTypeDef::GetMaxVersion (
+      aafVersionType_t *  /*pMaxVersion*/)
+{
+  return AAFRESULT_NOT_IMPLEMENTED;
+}
+
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFTypeDef::SetMaxVersion (
+      aafVersionType_t  /*MaxVersion*/)
+{
+  return AAFRESULT_NOT_IMPLEMENTED;
+}
+
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFTypeDef::GetSwapNeeded (
+      aafSwapNeeded_t *  /*pSwapNeeded*/)
+{
+  return AAFRESULT_NOT_IMPLEMENTED;
+}
+
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFTypeDef::SetSwapNeeded (
+      aafSwapNeeded_t  /*SwapNeeded*/)
+{
+  return AAFRESULT_NOT_IMPLEMENTED;
+}
+
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFTypeDef::GetRefValues (
+      ImplEnumAAFReferenceValues ** /*ppEnum*/)
+{
+  return AAFRESULT_NOT_IMPLEMENTED;
+}
+
+OMDEFINE_STORABLE(ImplAAFTypeDef, AUID_AAFTypeDef);
+
+
--- ref-impl/src/impl/ImplAAFTypeDefFixedArray.cpp
+++ ref-impl/src/impl/ImplAAFTypeDefFixedArray.cpp
@@ -1,29 +1,15 @@
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- *  prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+
+
+
 
 #include "AAFStoredObjectIDs.h"
 
@@ -31,377 +17,112 @@
 #include "ImplAAFTypeDefFixedArray.h"
 #endif
 
-#ifndef __ImplAAFTypeDefStrongObjRef_h__
-#include "ImplAAFTypeDefStrongObjRef.h"
-#endif
-
-#ifndef __ImplAAFTypeDefWeakObjRef_h__
-#include "ImplAAFTypeDefWeakObjRef.h"
-#endif
-
-#ifndef __ImplAAFPropertyValue_h__
-#include "ImplAAFPropertyValue.h"
-#endif
-
-#ifndef __ImplAAFHeader_h_
-#include "ImplAAFHeader.h"
-#endif
-
-#include "AAFPropertyIDs.h"
-#include "ImplAAFObjectCreation.h"
-
 #include <assert.h>
 #include <string.h>
 
-extern "C" const aafClassID_t CLSID_AAFPropertyValue;
-
 
 ImplAAFTypeDefFixedArray::ImplAAFTypeDefFixedArray ()
-  : _ElementType  ( PID_TypeDefinitionFixedArray_ElementType,  "ElementType"),
-    _ElementCount ( PID_TypeDefinitionFixedArray_ElementCount, "ElementCount")
-{
-  _persistentProperties.put(_ElementType.address());
-  _persistentProperties.put(_ElementCount.address());
-}
+{}
 
 
 ImplAAFTypeDefFixedArray::~ImplAAFTypeDefFixedArray ()
 {}
 
+ AAFRESULT STDMETHODCALLTYPE
+   ImplAAFTypeDefFixedArray::Initialize (
+      aafUID_t *  /*pID*/,
+      ImplAAFTypeDef * /*pTypeDef*/,
+      aafUInt32  /*nElements*/,
+      wchar_t *  /*pTypeName*/)
+{
+  return AAFRESULT_NOT_IMPLEMENTED;
+}
+
+
 
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFTypeDefFixedArray::GetType (
-      ImplAAFTypeDef ** ppTypeDef) const
+      ImplAAFTypeDef ** /*ppTypeDef*/)
 {
-  if (! ppTypeDef) return AAFRESULT_NULL_PARAM;
-
-  if (!_cachedBaseType)
-	{
-	  ImplAAFTypeDefFixedArray * pNonConstThis =
-		  (ImplAAFTypeDefFixedArray *) this;
-
-	  ImplAAFDictionarySP pDict;
-
-	  AAFRESULT hr;
-	  hr = GetDictionary(&pDict);
-	  if (AAFRESULT_FAILED(hr))
-		return hr;
-	  assert (pDict);
-
-	  hr = pDict->LookupTypeDef (_ElementType, &pNonConstThis->_cachedBaseType);
-	  if (AAFRESULT_FAILED(hr))
-		return hr;
-	  assert (_cachedBaseType);
-	}
-  assert (ppTypeDef);
-  *ppTypeDef = _cachedBaseType;
-  assert (*ppTypeDef);
-  (*ppTypeDef)->AcquireReference ();
-
-  return AAFRESULT_SUCCESS;
-}
-
-
-AAFRESULT STDMETHODCALLTYPE
-   ImplAAFTypeDefFixedArray::Initialize (
-      const aafUID_t & id,
-      ImplAAFTypeDef * pTypeDef,
-      aafUInt32  nElements,
-      const aafCharacter * pTypeName)
-{
-  if (! pTypeDef)  return AAFRESULT_NULL_PARAM;
-
-  assert (pTypeDef);
-  if (! pTypeDef->IsFixedArrayable())
-	return AAFRESULT_BAD_TYPE;
-
-  aafUID_t typeId;
-  AAFRESULT hr = pTypeDef->GetAUID(&typeId);
-  if (! AAFRESULT_SUCCEEDED (hr)) return hr;
-
-  return pvtInitialize (id, typeId, nElements, pTypeName);
-}
-
-
-
-AAFRESULT STDMETHODCALLTYPE
-   ImplAAFTypeDefFixedArray::pvtInitialize (
-      const aafUID_t & id,
-      const aafUID_t & typeId,
-      aafUInt32  nElements,
-      const aafCharacter * pTypeName)
-{
-  if (! pTypeName) return AAFRESULT_NULL_PARAM;
-
-  HRESULT hr;
-
-  hr = SetName (pTypeName);
-  if (! AAFRESULT_SUCCEEDED (hr)) return hr;
-
-  hr = SetAUID (id);
-  if (! AAFRESULT_SUCCEEDED (hr)) return hr;
-
-  _ElementType = typeId;
-  _ElementCount = nElements;
-
-  return AAFRESULT_SUCCESS;
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
 
 
 
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFTypeDefFixedArray::GetCount (
-      aafUInt32 *  pCount) const
+      aafUInt32 *  /*pCount*/)
 {
-  if (! pCount) return AAFRESULT_NULL_PARAM;
-  *pCount = _ElementCount;
-  return AAFRESULT_SUCCESS;
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
 
-
-// Override from AAFTypeDef
-AAFRESULT STDMETHODCALLTYPE
-ImplAAFTypeDefFixedArray::GetTypeCategory (eAAFTypeCategory_t *  pTid)
-{
-  if (!pTid) return AAFRESULT_NULL_PARAM;
-  *pTid = kAAFTypeCatFixedArray;
-  return AAFRESULT_SUCCESS;
-}
-
-
-aafUInt32 ImplAAFTypeDefFixedArray::pvtCount
-(
- ImplAAFPropertyValue * /*pInPropVal*/ /*arg unused*/
-) const
-{
-  return _ElementCount;
-}
-
-
-ImplAAFTypeDefSP ImplAAFTypeDefFixedArray::BaseType (void) const
-{
-  ImplAAFTypeDefSP result;
-  AAFRESULT hr = GetType (&result);
-  assert (AAFRESULT_SUCCEEDED (hr));
-  assert (result);
-  return result;
-}
-
-
-void ImplAAFTypeDefFixedArray::reorder(OMByte* externalBytes,
-									   size_t externalBytesSize) const
-{
-  aafUInt32 numElems = _ElementCount;
-  aafUInt32 elem = 0;
-
-  ImplAAFTypeDefSP ptd = BaseType ();
-  aafUInt32 elemSize = ptd->NativeSize ();
-  aafInt32 numBytesLeft = externalBytesSize;
-
-  for (elem = 0; elem < numElems; elem++)
-	{
-	  ptd->reorder (externalBytes, elemSize);
-	  externalBytes += elemSize;
-	  numBytesLeft -= elemSize;
-	  assert (numBytesLeft >= 0);
-	}
-}
-
-
-size_t ImplAAFTypeDefFixedArray::externalSize(OMByte* /*internalBytes*/,
-											  size_t /*internalBytesSize*/) const
-{
-  ImplAAFTypeDefSP ptd = BaseType ();
-  assert (ptd->IsFixedSize ());
-  // size_t result = _ElementCount * ptd->externalSize (0, 0);
-  size_t result = _ElementCount * ptd->PropValSize ();
-  return result;
-}
-
-
-void ImplAAFTypeDefFixedArray::externalize(OMByte* internalBytes,
-										   size_t internalBytesSize,
-										   OMByte* externalBytes,
-										   size_t externalBytesSize,
-										   OMByteOrder byteOrder) const
-{
-  aafUInt32 numElems = _ElementCount;
-  aafUInt32 elem = 0;
-
-  ImplAAFTypeDefSP ptd = BaseType ();
-  aafUInt32 internalSize = ptd->NativeSize ();
-  aafUInt32 externalSize = ptd->PropValSize ();
-  if (internalSize == externalSize)
-	{
-	  copy (internalBytes,
-			externalBytes,
-			externalBytesSize);
-	}
-  else
-	{
-	  aafInt32 internalBytesLeft = internalBytesSize;
-	  aafInt32 externalBytesLeft = externalBytesSize;
-
-	  for (elem = 0; elem < numElems; elem++)
-		{
-		  ptd->externalize (internalBytes,
-							internalSize,
-							externalBytes,
-							externalSize,
-							byteOrder);
-		  internalBytes += internalSize;
-		  externalBytes += externalSize;
-		  internalBytesLeft -= internalSize;
-		  externalBytesLeft -= externalSize;
-		  assert (internalBytesLeft >= 0);
-		  assert (externalBytesLeft >= 0);
-		}
-	}
-}
-
-
-size_t ImplAAFTypeDefFixedArray::internalSize(OMByte* /*externalBytes*/,
-											  size_t /*externalBytesSize*/) const
-{
-  ImplAAFTypeDefSP ptd = BaseType ();
-  assert (ptd->IsFixedSize ());
-  // size_t result = _ElementCount * ptd->internalSize (0, 0);
-  size_t result = _ElementCount * ptd->NativeSize ();
-  return result;
-}
-
-
-void ImplAAFTypeDefFixedArray::internalize(OMByte* externalBytes,
-										   size_t externalBytesSize,
-										   OMByte* internalBytes,
-										   size_t internalBytesSize,
-										   OMByteOrder byteOrder) const
-{
-  aafUInt32 numElems = _ElementCount;
-  aafUInt32 elem = 0;
-
-  ImplAAFTypeDefSP ptd = BaseType ();
-  assert (ptd->IsFixedSize ());
-  aafUInt32 internalElemSize = ptd->NativeSize ();
-  aafUInt32 externalElemSize = ptd->PropValSize ();
-  if (internalElemSize == externalElemSize)
-	{
-	  copy (externalBytes,
-			internalBytes,
-			internalBytesSize);
-	}
-  else
-	{
-	  aafInt32 internalBytesLeft = internalBytesSize;
-	  aafInt32 externalBytesLeft = externalBytesSize;
-
-	  for (elem = 0; elem < numElems; elem++)
-		{
-		  ptd->internalize (externalBytes,
-							externalElemSize,
-							internalBytes,
-							internalElemSize,
-							byteOrder);
-		  internalBytes += internalElemSize;
-		  externalBytes += externalElemSize;
-		  internalBytesLeft -= internalElemSize;
-		  externalBytesLeft -= externalElemSize;
-		  assert (internalBytesLeft >= 0);
-		  assert (externalBytesLeft >= 0);
-		}
-	}
-}
-
-
-aafBool ImplAAFTypeDefFixedArray::IsFixedSize (void) const
-{
-  return BaseType()->IsFixedSize();
-}
-
-size_t ImplAAFTypeDefFixedArray::PropValSize (void) const
-{
-  return (BaseType()->PropValSize()) * _ElementCount;
-}
-
-
-aafBool ImplAAFTypeDefFixedArray::IsRegistered (void) const
-{
-  return BaseType()->IsRegistered();
-}
-
-
-size_t ImplAAFTypeDefFixedArray::NativeSize (void) const
-{
-  assert (IsRegistered());
-
-  size_t result;
-  ImplAAFTypeDefSP elemType;
-  elemType = BaseType ();
-  assert (elemType);
-  result = elemType->NativeSize() * _ElementCount;
-  return result;
-}
-
-
-OMProperty * ImplAAFTypeDefFixedArray::pvtCreateOMPropertyMBS
-  (OMPropertyId pid,
-   const char * name) const
-{
-  assert (name);
-
-  ImplAAFTypeDefSP ptd = BaseType ();
-  assert (ptd);
-
-  OMProperty * result = 0;
-
-  if (dynamic_cast<ImplAAFTypeDefStrongObjRef*>((ImplAAFTypeDef*) ptd))
-	{
-	  // element is strong ref
-	  result = new OMStrongReferenceVectorProperty<ImplAAFObject> (pid, name);
-	}
-  else if (dynamic_cast<ImplAAFTypeDefWeakObjRef*>((ImplAAFTypeDef*) ptd))
-	{
-	  // element is weak ref, hence implemented as AUID array.
-	  // Specify a size of one element.
-	  result = new OMSimpleProperty (pid, name, sizeof (aafUID_t));
-	}
-
-  else
-	{
-	  // We don't support variable arrays of variably-sized properties.
-	  assert (ptd->IsFixedSize());
-	  aafUInt32 elemSize = ptd->NativeSize ();
-
-	  // But even though elems are fixed size, the variable array is
-	  // of variable size.  Specify a size of one element.
-	  result = new OMSimpleProperty (pid, name, elemSize);
-	}
-
-  assert (result);
-  return result;
-}
 
 
 AAFRESULT STDMETHODCALLTYPE
-    ImplAAFTypeDefFixedArray::RawAccessType (
-      ImplAAFTypeDef ** ppRawTypeDef)
+    ImplAAFTypeDefFixedArray::CreateValueFromValues (
+      ImplAAFPropertyValue * /*pElementValues*/,
+      aafUInt32  /*numElements*/,
+      ImplAAFPropertyValue ** /*ppPropVal*/)
 {
-  // Return variable array of unsigned char
-  return pvtGetUInt8Array8Type (ppRawTypeDef);
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
 
 
-bool ImplAAFTypeDefFixedArray::IsAggregatable () const
-{ return true; }
 
-bool ImplAAFTypeDefFixedArray::IsStreamable () const
-{ return true; }
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFTypeDefFixedArray::CreateValueFromCArray (
+      aafMemPtr_t  /*pInitData*/,
+      aafUInt32  /*initDataSize*/,
+      ImplAAFPropertyValue ** /*ppPropVal*/)
+{
+  return AAFRESULT_NOT_IMPLEMENTED;
+}
 
-bool ImplAAFTypeDefFixedArray::IsFixedArrayable () const
-{ return true; }
 
-bool ImplAAFTypeDefFixedArray::IsVariableArrayable () const
-{ return true; }
 
-bool ImplAAFTypeDefFixedArray::IsStringable () const
-{ return false; }
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFTypeDefFixedArray::GetElementValue (
+      ImplAAFPropertyValue * /*pInPropVal*/,
+      aafUInt32  /*index*/,
+      ImplAAFPropertyValue ** /*ppOutPropVal*/)
+{
+  return AAFRESULT_NOT_IMPLEMENTED;
+}
+
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFTypeDefFixedArray::GetCArray (
+      ImplAAFPropertyValue * /*pPropVal*/,
+      aafMemPtr_t  /*pData*/,
+      aafUInt32  /*dataSize*/)
+{
+  return AAFRESULT_NOT_IMPLEMENTED;
+}
+
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFTypeDefFixedArray::SetElementValue (
+      ImplAAFPropertyValue * /*pPropVal*/,
+      aafUInt32  /*index*/,
+      ImplAAFPropertyValue * /*pMemberPropVal*/)
+{
+  return AAFRESULT_NOT_IMPLEMENTED;
+}
+
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFTypeDefFixedArray::SetCArray (
+      ImplAAFPropertyValue * /*pPropVal*/,
+      aafMemPtr_t  /*pData*/,
+      aafUInt32  /*dataSize*/)
+{
+  return AAFRESULT_NOT_IMPLEMENTED;
+}
+
+
+OMDEFINE_STORABLE(ImplAAFTypeDefFixedArray, AUID_AAFTypeDefFixedArray);
+
+
