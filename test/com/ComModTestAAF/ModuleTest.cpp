75f014b077f4f9031b1dfaf4b7e9f0906621bf01
tbingham <tbingham>
2001-04-17 10:13:54 +0000

    Implementation for types and utilities shared by two or more module tests.

--- test/com/ComModTestAAF/ModuleTest.cpp
+++ test/com/ComModTestAAF/ModuleTest.cpp
@@ -1,24 +1,29 @@
-//=---------------------------------------------------------------------=
-//
-// The contents of this file are subject to the AAF SDK Public
-// Source License Agreement (the "License"); You may not use this file
-// except in compliance with the License.  The License is available in
-// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
-// Association or its successor.
-// 
-// Software distributed under the License is distributed on an "AS IS"
-// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
-// the License for the specific language governing rights and limitations
-// under the License.
-// 
-// The Original Code of this file is Copyright 1998-2001, Licensor of the
-// AAF Association.
-// 
-// The Initial Developer of the Original Code of this file and the
-// Licensor of the AAF Association is Avid Technology.
-// All rights reserved.
-//
-//=---------------------------------------------------------------------=
+/***********************************************************************
+ *
+ *              Copyright (c) 2001 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and accompanying 
+ * documentation, and to distribute and sublicense application software
+ * incorporating this software for any purpose is hereby granted, 
+ * provided that (i) the above copyright notice and this permission
+ * notice appear in all copies of the software and related documentation,
+ * and (ii) the name Avid Technology, Inc. may not be used in any
+ * advertising or publicity relating to the software without the specific,
+ *  prior written permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+ * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+ * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+ * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+ * LIABILITY.
+ *
+ ************************************************************************/
 //
 // File: ModuleTest.cpp
 // 
@@ -39,11 +44,12 @@
 // DR3 -> {1, 0, 0, 3, kAAFVersionBeta};
 // DR4 -> {1, 0, 0, 4, kAAFVersionBeta};
 
-
-bool DR4TestSupported(aafProductVersion_constref toolkitVersion)
+// Support for Sets (strong reference sets) added in DR4
+bool StrongReferenceSetsSupported(aafProductVersion_constref toolkitVersion)
 {
   bool result = true;
-   
+  
+  
   if (kAAFVersionBeta == toolkitVersion.type)
   {
     if (1 == toolkitVersion.major && 
@@ -56,14 +62,15 @@ bool DR4TestSupported(aafProductVersion_constref toolkitVersion)
   }
   
   return result;
+} 
 
-}
 
-
-bool RC1TestSupported(aafProductVersion_constref toolkitVersion)
+// Support for weak references was added after DR4
+bool WeakReferencesSupported(aafProductVersion_constref toolkitVersion)
 {
   bool result = true;
-   
+  
+  
   if (kAAFVersionBeta == toolkitVersion.type)
   {
     if (1 == toolkitVersion.major && 
@@ -76,22 +83,6 @@ bool RC1TestSupported(aafProductVersion_constref toolkitVersion)
   }
   
   return result;
-
-}
-
-
-
-// Support for Sets (strong reference sets) added in DR4
-bool StrongReferenceSetsSupported(aafProductVersion_constref toolkitVersion)
-{
-  return DR4TestSupported(toolkitVersion);
-} 
-
-
-// Support for weak references was added after DR4
-bool WeakReferencesSupported(aafProductVersion_constref toolkitVersion)
-{
-  return RC1TestSupported(toolkitVersion);
 } 
 
 
@@ -105,7 +96,21 @@ bool WeakReferenceSetsSupported(aafProductVersion_constref toolkitVersion)
 // Support for extending AAFObject was added after DR4
 bool ExtendingAAFObjectSupported(aafProductVersion_constref toolkitVersion)
 {
-  return RC1TestSupported(toolkitVersion);
+  bool result = true;
+  
+  
+  if (kAAFVersionBeta == toolkitVersion.type)
+  {
+    if (1 == toolkitVersion.major && 
+        0 == toolkitVersion.minor && 
+        0 == toolkitVersion.tertiary && 
+        4 >= toolkitVersion.patchLevel)
+    {
+      result = false;
+    }
+  }
+  
+  return result;
 } 
 
 
