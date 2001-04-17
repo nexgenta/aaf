e075c821d3a16c8528d901acefe396518fb25357
tbingham <tbingham>
2001-04-17 03:14:43 +0000

    Hash table code

--- ref-impl/src/impl/aafTable.h
+++ ref-impl/src/impl/aafTable.h
@@ -1,30 +1,28 @@
 /***********************************************************************
  *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
+ *              Copyright (c) 1996 Avid Technology, Inc.
  *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- *  prior written permission of Avid Technology, Inc.
+ * Permission to use, copy and modify this software and to distribute
+ * and sublicense application software incorporating this software for
+ * any purpose is hereby granted, provided that (i) the above
+ * copyright notice and this permission notice appear in all copies of
+ * the software and related documentation, and (ii) the name Avid
+ * Technology, Inc. may not be used in any advertising or publicity
+ * relating to the software without the specific, prior written
+ * permission of Avid Technology, Inc.
  *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
  * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
  * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
+ * SPECIAL, INCIDENTAL, INDIRECT, CONSEQUENTIAL OR OTHER DAMAGES OF
+ * ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE, INCLUDING, 
+ * WITHOUT  LIMITATION, DAMAGES RESULTING FROM LOSS OF USE,
+ * DATA OR PROFITS, AND WHETHER OR NOT ADVISED OF THE POSSIBILITY OF
+ * DAMAGE, REGARDLESS OF THE THEORY OF LIABILITY.
  *
  ************************************************************************/
-
 
 #ifndef _AAF_TABLE_API_
 #define _AAF_TABLE_API_ 1
@@ -32,7 +30,7 @@
 #include "aafErr.h"
 #include "AAFTypes.h"
 
-#ifdef __cplusplus
+#if PORT_LANG_CPLUSPLUS
 extern          "C"
 {
 #endif
@@ -41,39 +39,39 @@ extern          "C"
 
 typedef enum
 {
-	kAafTableDupError,
-	kAafTableDupReplace,
-	kAafTableDupAddDup
-} aafTableDuplicate_t;
+	kOmTableDupError,
+	kOmTableDupReplace,
+	kOmTableDupAddDup
+} omTableDuplicate_t;
 
 typedef enum
 {
 	kTableSrchAny,
 	kTableSrchMatch,
 	kTableSrchUnique
-} aafTableSearch_t;
+} omTableSearch_t;
 
-typedef aafInt32	(*aafTblMapProc)( void *key);
-typedef aafBool	(*aafTblCompareProc)( void *key1, void *key2);
-typedef void	(*aafTblDisposeProc)(void *valuePtr);
+typedef aafInt32	(*omTblMapProc)( void *key);
+typedef aafBool	(*omTblCompareProc)( void *key1, void *key2);
+typedef void	(*omTblDisposeProc)(void *valuePtr);
 
-typedef struct aafTable aafTable_t;
+typedef struct omTable omTable_t;
 typedef struct aafTableLink tableLink_t;
 
 typedef struct
 {
 	aafInt32				cookie;		/* Private */
-	aafTable_t			*table;		/* Private */
+	omTable_t			*table;		/* Private */
 	aafInt32				curHash;	/* Private */
 	tableLink_t			*nextEntry;	/* Private */
-	aafTableSearch_t		srch;		/* Private */
+	omTableSearch_t		srch;		/* Private */
 	void				*srchKey;	/* Private */
 	/* */
 	void				*key;		/* OUT */
 	aafInt32				keylen;		/* OUT */
 	void				*valuePtr;	/* OUT */
 	aafInt32				valueLen;	/* OUT */
-} aafTableIterate_t;
+} omTableIterate_t;
 
 /************************************************************************
  *
@@ -89,75 +87,75 @@ typedef struct
 aafErr_t NewTable(
 			AAFFile * file,
 			aafInt16 initKeySize,
-			aafTblMapProc myMap,
-			aafTblCompareProc myCompare,
+			omTblMapProc myMap,
+			omTblCompareProc myCompare,
 			aafInt32 numBuckets,
-			aafTable_t **resultPtr);
+			omTable_t **resultPtr);
 			
 aafErr_t SetTableDispose(
-			aafTable_t *table,
-			aafTblDisposeProc proc);
+			omTable_t *table,
+			omTblDisposeProc proc);
 			
 aafErr_t TableAddValuePtr(
-			aafTable_t *table,
+			omTable_t *table,
 			void *key,
 			aafInt16 keyLen,
 			void *value,
-			aafTableDuplicate_t dup);
+			omTableDuplicate_t dup);
 			
 aafErr_t TableAddValueBlock(
-			aafTable_t *table,
+			omTable_t *table,
 			void *key,
 			short keyLen,
 			void *value,
 			aafInt32 valueLen,
-			aafTableDuplicate_t dup);
+			omTableDuplicate_t dup);
 			
 aafErr_t TableRemove(
-			aafTable_t *table,
+			omTable_t *table,
 			void *key);
 			
 aafBool TableIncludesKey(
-			aafTable_t *table,
+			omTable_t *table,
 			void *key);
 
 void *TableLookupPtr(
-			aafTable_t *table,
+			omTable_t *table,
 			void *key);
 			
 aafErr_t TableLookupBlock(
-			aafTable_t *table,
+			omTable_t *table,
 			void *key,
 			aafInt32 valueLen,
 			void *valuePtr,
 			aafBool *found);
 			
 aafErr_t TableFirstEntry(
-			aafTable_t *table,
-			aafTableIterate_t *iter,
+			omTable_t *table,
+			omTableIterate_t *iter,
 			aafBool *found);
 			
 aafErr_t TableFirstEntryMatching(
-			aafTable_t *table,
-			aafTableIterate_t *iter,
+			omTable_t *table,
+			omTableIterate_t *iter,
 			void *key,
 			aafBool *found);
 			
 aafErr_t TableFirstEntryUnique(
-			aafTable_t *table,
-			aafTableIterate_t *iter,
+			omTable_t *table,
+			omTableIterate_t *iter,
 			aafBool *found);
 			
 aafErr_t TableNextEntry(
-			aafTableIterate_t *iter,
+			omTableIterate_t *iter,
 			aafBool *found);
 
 aafInt32 TableNumEntriesMatching(
-			aafTable_t *table,
+			omTable_t *table,
 			void *key);
 			
 aafErr_t TableSearchDataValue(
-			aafTable_t *table,
+			omTable_t *table,
 			aafInt32 valueLen,
 			void *value,
 			aafInt32 keyLen,
@@ -165,13 +163,13 @@ aafErr_t TableSearchDataValue(
 			aafBool *found);
 			
 aafErr_t TableDispose(
-			aafTable_t *table);
+			omTable_t *table);
 			
 aafErr_t TableDisposeAll(
-			aafTable_t *table);
+			omTable_t *table);
 			
 aafErr_t TableDisposeItems(
-			aafTable_t *table);
+			omTable_t *table);
 
 
 /************************************************************************
@@ -183,22 +181,22 @@ aafErr_t TableDisposeItems(
 aafErr_t NewStringTable(
 			AAFFile * file,
 			aafBool caseSensistive,
-			aafTblCompareProc myCompare,
+			omTblCompareProc myCompare,
 			aafInt32 numBuckets,
-			aafTable_t **resultPtr);
+			omTable_t **resultPtr);
 			
 aafErr_t TableAddString(
-			aafTable_t *table,
+			omTable_t *table,
 			char *key,
 			void *value,
-			aafTableDuplicate_t dup);
+			omTableDuplicate_t dup);
 			
 aafErr_t TableAddStringBlock(
-			aafTable_t *table,
+			omTable_t *table,
 			char *key,
 			void *value,
 			aafInt32 valueLen,
-			aafTableDuplicate_t dup);
+			omTableDuplicate_t dup);
 
 /************************************************************************
  *
@@ -209,111 +207,155 @@ aafErr_t TableAddStringBlock(
 aafErr_t NewUIDTable(
 			AAFFile * file,
 			aafInt32 numBuckets,
-			aafTable_t **result);
+			omTable_t **result);
 			
 aafErr_t TableAddUID(
-			aafTable_t *table,
+			omTable_t *table,
 			aafUID_t key,
 			void *value,
-			aafTableDuplicate_t dup);
-			
-aafErr_t TableAddUIDBlock(
-			aafTable_t *table,
-			aafUID_t key,
-			void *value,
-			aafInt32 valueLen,
-			aafTableDuplicate_t dup);
+			omTableDuplicate_t dup);
 			
 aafErr_t TableRemoveUID(
-			aafTable_t *table,
+			omTable_t *table,
 			aafUID_t key);
 			
 aafBool TableIncludesUID(
-			aafTable_t *table,
+			omTable_t *table,
 			aafUID_t key);
 			
 void *TableUIDLookupPtr(
-			aafTable_t *table,
+			omTable_t *table,
 			aafUID_t key);
 
-void TableUIDLookupBlock(
-			aafTable_t *table,
-			aafUID_t key,
-			aafInt32 valueLen,
-			void *valuePtr,
-			aafBool *found);
-
 /************************************************************************
  *
- * MobID Table Functions
+ * ClassID Table Functions
  *
  ************************************************************************/
 
-aafErr_t NewMobIDTable(
-			aafInt32 numBuckets,
-			aafTable_t **result);
-			
-aafErr_t TableAddMobID(
-			aafTable_t *table,
-			aafMobID_constref key,
-			void *value,
-			aafTableDuplicate_t dup);
-			
-aafErr_t TableAddMobIDBlock(
-			aafTable_t *table,
-			aafMobID_constref key,
-			void *value,
-			aafInt32 valueLen,
-			aafTableDuplicate_t dup);
-			
-aafErr_t TableRemoveMobID(
-			aafTable_t *table,
-			aafMobID_constref key);
-			
-aafBool TableIncludesMobID(
-			aafTable_t *table,
-			aafMobID_constref key);
-			
-void *TableMobIDLookupPtr(
-			aafTable_t *table,
-			aafMobID_constref key);
-
-void TableMobIDLookupBlock(
-			aafTable_t *table,
-			aafMobID_constref key,
-			aafInt32 valueLen,
-			void *valuePtr,
-			aafBool *found);
-
-/************************************************************************
- *
- * SlotID Table Functions
- *
- ************************************************************************/
-
-aafErr_t NewSlotIDTable(
+#if FULL_TOOLKIT
+aafErr_t NewClassIDTable(
 			AAFFile * file,
 			aafInt32 numBuckets,
-			aafTable_t **result);
-			
-aafErr_t TableAddSlotID(
-			aafTable_t *table,
-			aafSlotID_t key,
+			omTable_t **result);
+
+aafErr_t TableAddClassID(
+			omTable_t *table,
+			aafClassIDPtr_t key,
 			void *value,
 			aafInt32 valueLen);
 			
-aafErr_t TableSlotIDLookup(
-			aafTable_t *table,
-			aafSlotID_t key,
+aafErr_t TableClassIDLookup(
+			omTable_t *table,
+			aafClassIDPtr_t key,
+			aafInt32 valueLen,
+			void *valuePtr,
+			aafBool *found);
+#endif
+/************************************************************************
+ *
+ * TrackID Table Functions
+ *
+ ************************************************************************/
+
+aafErr_t NewTrackIDTable(
+			AAFFile * file,
+			aafInt32 numBuckets,
+			omTable_t **result);
+			
+aafErr_t TableAddTrackID(
+			omTable_t *table,
+			aafTrackID_t key,
+			void *value,
+			aafInt32 valueLen);
+			
+aafErr_t TableTrackIDLookup(
+			omTable_t *table,
+			aafTrackID_t key,
 			void *value,
 			aafInt32 valueLen,
 			aafBool *found);
 
-#ifdef AAF_SELF_TEST
-void testaafTable(void);
+/************************************************************************
+ *
+ * aafProperty_t Table Functions
+ *
+ ************************************************************************/
+
+#if FULL_TOOLKIT
+aafErr_t NewPropertyTable(
+			AAFFile * file,
+			aafInt32 numBuckets,
+			omTable_t **result);
+			
+aafErr_t TableAddProperty(
+			omTable_t *table,
+			aafProperty_t key,
+			void *value, 
+			aafInt32 valueLen,
+			omTableDuplicate_t dup);
+			
+aafErr_t TablePropertyLookup(
+			omTable_t *table,
+			aafProperty_t key,
+			aafInt32 valueLen,
+			void *valuePtr,
+			aafBool *found);
 #endif
 
-#ifdef __cplusplus
+/************************************************************************
+ *
+ * aafType_t Table Functions
+ *
+ ************************************************************************/
+
+#if FULL_TOOLKIT
+aafErr_t NewTypeTable(
+			AAFFile * file,
+			aafInt32 numBuckets,
+			omTable_t **result);
+			
+aafErr_t TableAddType(
+			omTable_t *table,
+			aafType_t key,
+			void *value,
+			aafInt32 valueLen);
+			
+aafErr_t TableTypeLookup(
+			omTable_t *table,
+			aafType_t key,
+			aafInt32 valueLen,
+			void *valuePtr,
+			aafBool *found);
+#endif
+
+/************************************************************************
+ *
+ * Definition (Datakind and EffectDef) Table Functions
+ *
+ ************************************************************************/
+
+#if FULL_TOOLKIT
+aafErr_t NewDefTable(
+			AAFFile * file,
+			aafInt32 numBuckets,
+			omTable_t **result);
+			
+aafErr_t TableAddDef(
+			omTable_t *table,
+			aafUniqueName_t key,
+			void *value);
+			
+void *TableDefLookup(
+			omTable_t *table,
+			aafUniqueName_t key);
+#endif
+
+#ifdef AAF_SELF_TEST
+void testOmTable(void);
+#endif
+
+#if PORT_LANG_CPLUSPLUS
 }
 #endif
 #endif				/* _AAF_TABLE_API_ */
