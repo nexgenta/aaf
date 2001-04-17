caf21caf268b23a9d2a24ca641da8e4b561355a0
tbingham <tbingham>
2001-04-17 02:42:34 +0000

    Macros to configure builds

--- ref-impl/bld_cfg/bld_cfg.h
+++ ref-impl/bld_cfg/bld_cfg.h
@@ -1,9 +1,7 @@
-#ifndef _aaf_tk_bld_cfg_bld_cfg_h_
-#define _aaf_tk_bld_cfg_bld_cfg_h_
+#ifndef _bld_cfg_AAFBuildConfig_h_
+#define _bld_cfg_AAFBuildConfig_h_
 
 /******************************************\
-*                                          *
-* Advanced Authoring Format                *
 *                                          *
 * Copyright (c) 1998 Avid Technology, Inc. *
 *                                          *
@@ -19,6 +17,10 @@
 /*
 
 Macros defined here:
+
+AAF_BUILD_CONFIG_BUILTIN_BOOL
+	- is defined to be non-zero if this platform/compiler supports
+	  a built-in bool type.
 
 AAF_BUILD_CONFIG_ASSY_POWERPC
 	- is defined to be non-zero if this platform supports PowerPC
@@ -40,52 +42,11 @@ AAF_BUILD_CONFIG_ASSY_386
 	- is defined to be non-zero if this platform supports Intel
 	  386 assembly code.
 
-AAF_BUILD_CONFIG_EXTERN_C_HEADER
-	- Evaluates to the appropriate "extern C" lead statement for
-	  shared C and C++ header files.  Should be put at the top of
-	  such a header file.
-
-AAF_BUILD_CONFIG_EXTERN_C_TRAILER
-	- Evaluates to the appropriate "extern C" trailing statement
-	  for shared C and C++ header files.  Should be put at the
-	  bottom of such a header file.
-
-AAF_BUILD_CONFIG_EXPLICIT_TEMPLATES
-	- Evaluates to true if this platform requires explicit template
-	  instantiation.
-
  */
 
 
-#if __cplusplus
-  #define AAF_BUILD_CONFIG_CPLUSPLUS 1
+#if (__GNUC__ >= 2)
+  #define AAF_BUILD_CONFIG_BUILTIN_BOOL	1
 #endif
 
-#if AAF_BUILD_CONFIG_CPLUSPLUS
-  #define AAF_BUILD_CONFIG_EXTERN_C_HEADER	extern "C" {
-  #define AAF_BUILD_CONFIG_EXTERN_C_TRAILER	}
-#else /* ! AAF_BUILD_CONFIG_CPLUSPLUS */
-  #define AAF_BUILD_CONFIG_EXTERN_C_HEADER
-  #define AAF_BUILD_CONFIG_EXTERN_C_TRAILER
-#endif /* ! AAF_BUILD_CONFIG_CPLUSPLUS */
-
-
-#if __GNUC__
-  #define AAF_BUILD_CONFIG_EXPLICIT_TEMPLATES 1
-#endif
-
-#endif /* ! _aaf_tk_bld_cfg_bld_cfg_h_ */
-
-
-
-///////////////////////////
-// Emacs formatting info //
-///////////////////////////
-
-
-/* INDENT OFF */
-/*
-;;; Local Variables: ***
-;;; tab-width:4 ***
-;;; End: ***
-*/
+#endif /* ! _bld_cfg_AAFBuildConfig_h_ */
