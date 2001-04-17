cf363892eda030d287e692ee8dfc8124f63a0f4b
tbingham <tbingham>
2001-04-17 06:37:19 +0000

    Split source code into cpp and h file for better maintenance

--- Utilities/AafOmf/AafOmf.h
+++ Utilities/AafOmf/AafOmf.h
@@ -1,123 +1,78 @@
-#ifndef _AAF_OMF_
-#define _AAF_OMF_	1
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
 /***********************************************************************
  *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
+ *              Copyright (c) 1996 Avid Technology, Inc.
  *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- *  prior written permission of Avid Technology, Inc.
+ * Permission to use, copy and modify this software and to distribute
+ * and sublicense application software incorporating this software for
+ * any purpose is hereby granted, provided that (i) the above
+ * copyright notice and this permission notice appear in all copies of
+ * the software and related documentation, and (ii) the name Avid
+ * Technology, Inc. may not be used in any advertising or publicity
+ * relating to the software without the specific, prior written
+ * permission of Avid Technology, Inc.
  *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
  * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
  * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
+ * SPECIAL, INCIDENTAL, INDIRECT, CONSEQUENTIAL OR OTHER DAMAGES OF
+ * ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE, INCLUDING, 
+ * WITHOUT  LIMITATION, DAMAGES RESULTING FROM LOSS OF USE,
+ * DATA OR PROFITS, AND WHETHER OR NOT ADVISED OF THE POSSIBILITY OF
+ * DAMAGE, REGARDLESS OF THE THEORY OF LIABILITY.
  *
  ************************************************************************/
-
 /************************************************************************
  *
- * Omf2Aaf.h Describes structures and prototypes for the OMF to AAF 
+ * AafOmf.h Describes structures and prototypes for the OMF to AAF 
  *          conversion utility.
  *
  ************************************************************************/
 
-  
+// OMF Includes
+namespace OMF2
+{
+#include "omPublic.h"
+#include "omMedia.h"
+}
+
+#include "AAFTypes.h"
+#include "AAFResult.h"
+#include "AAFDefUIDs.h"
+
 #if defined(_MAC) || defined(macintosh)
 #include <initguid.h> // define all of the AAF guids.
 #include "AAF.h"
 #else
 #include "AAF.h"
+// TODO: This should not be here, I added them for now to get a good link.
+const CLSID CLSID_AAFFile = { 0x9346ACD2, 0x2713, 0x11d2, { 0x80, 0x35, 0x00, 0x60, 0x08, 0x14, 0x3E, 0x6F } };
 
 #endif
 
-#include "AAFTypes.h"
-#include "AAFResult.h"
-#include "AAFDataDefs.h"
-#include "AAFOperationDefs.h"
-#include "AAFParameterDefs.h"
-#include "AAFInterpolatorDefs.h"
-#include "AAFTypeDefUIDs.h"
-#include "StreamLogger.h"
+// Include the defintions for the AAF Stored Object identifiers.
+#define INIT_AUID
+#include "AAFStoredObjectIDs.h"
 
-//#ifndef _INCLUDED_OMF2_
-//#define _INCLUDED_OMF2_
-//namespace OMF2
-//{
-//#include "omPublic.h"
-//#include "omMedia.h"
-//}
-//#endif
 
-const int MAX_INDENT = 8;
-const unsigned	kLogError = 0; 	// Error level.
-const unsigned	kLogWarn 	= 1;  // Warning level.
-const unsigned	kLogInfo  = 2; 	// Informational level.
+#define FAILURE (2)
 
-typedef struct _AafOmfGlobals
-{
-	aafInt16		numIndents;
-	char			indentLeader[MAX_INDENT+1];	
-
-	aafBool			bVerboseMode;
-	aafBool			bCreateTOCFile;
-	aafBool			bConvertAllObjects;
-	aafBool			bOMFFileOpen;
-	aafBool			bAAFFileOpen;
-	aafBool			bLogFile;
-	aafBool			bDefFile;
-	aafBool			bDeleteOutput;
-	aafBool			bConvertAAFFile;
-
-	// For Statistical summary
-	aafInt32		nNumOMFMobs;
-	aafInt32		nNumAAFMobs;
-	aafInt32		nNumOMFObjects;
-	aafInt32		nNumAAFObjects;
-	aafInt32		nNumOMFProperties;
-	aafInt32		nNumAAFProperties;
-	aafInt32		nNumUndefinedOMFObjects;
-	aafInt32		nNumUndefinedOMFProperties;
-	aafInt32		nNumUndefinedAAFObjects;
-	aafInt32		nNumUndefinedAAFProperties;
-
-	char			sInFileName[256];
-	char			sTOCFileName[256];
-	char			sDefinitionFileName[256];
-	char			sOutFileName[256];
-	char			sLogFileName[256];
-	char*			pProgramName;
-	StreamLogger*		pLogger;
-
-	// MC Private Properties
-	OMF2::omfProperty_t		pvtEffectIDProp;
-	OMF2::omfProperty_t		pvtAppCode;
-	OMF2::omfProperty_t		pvtAttributes;
-	OMF2::omfProperty_t		pvtDataAttribute;
-
-	// Codec Properties
-	OMF2::omfProperty_t		omCDCIComponentWidth;
-	OMF2::omfProperty_t		omCDCIHorizontalSubsampling;
-	OMF2::omfProperty_t		omCDCIColorSiting;
-	OMF2::omfProperty_t		omCDCIBlackReferenceLevel;
-	OMF2::omfProperty_t		omCDCIWhiteReferenceLevel;
-	OMF2::omfProperty_t		omCDCIColorRange;
-	OMF2::omfProperty_t		omCDCIPaddingBits;
-	~_AafOmfGlobals( void )		{ delete pLogger; }
-} AafOmfGlobals;
-
+// ============================================================================
+// char to wide char conversion function prototypes
+// ============================================================================
+static void convert(wchar_t* wcName, size_t length, const char* name);
+static void convert(char* cName, size_t length, const wchar_t* name);
+static void convert(char* cName, size_t length, const char* name);
+static void convert(wchar_t* wName, size_t length, const wchar_t* name);
 
 // ============================================================================
 // simple helper class to initialize and cleanup COM library.
@@ -135,27 +90,68 @@ struct CComInitialize
 	}
 };
 
+// ============================================================================
+// simple helper class to run the main application.
+// ============================================================================
+class AafOmf
+{
+public:
+	AafOmf();
+	~AafOmf();
 
-int deleteFile( char* fileName );
-void IncIndentLevel( void );
-void DecIndentLevel( void );
-void AUIDtoString(aafUID_t *uid, char *buf);
-void MobIDtoString(aafMobID_constref uid, char *buf);
-AAFRESULT aafMobIDFromMajorMinor(
-        aafUInt32	prefix,
-        aafUInt32	major,
-		aafUInt32	minor,
-		aafUInt8	UMIDType,
-		aafMobID_t *mobID);     /* OUT - Newly created Mob ID */
-void RegisterCodecProperties(AafOmfGlobals *globals, OMF2::omfSessionHdl_t OMFSession);
-HRESULT InitGlobalVars( void );
-HRESULT IsOMFFile (char * pFileName );
+public:
+	void Usage( void );
+	HRESULT GetUserInput(int argc, char* argv[]);
+	HRESULT OpenInputFile( void );
+	HRESULT OpenOutputFile( void );
+	aafBool IsOMFFile(char* pFileName);
+	HRESULT OMFFileOpen( char* pFileName );
+	HRESULT	TOCFileCreate( void ); 
+	void OMFFileClose( void );
+	HRESULT AAFFileOpen( char* pFileName );
+	HRESULT AAFDefinitionFileOpen( void );
+	void AAFFileClose(void );
+	HRESULT OMFFileRead( void );
+	HRESULT ConvertOMFHeader( void );
+	HRESULT ConvertOMFDataDefinitionObject( OMF2::omfObject_t obj);
+	HRESULT ConvertOMFClassDictionaryObject( OMF2::omfObject_t obj);
+	HRESULT ParseOMFMOBObject( OMF2::omfObject_t obj );
+	HRESULT ConvertOMFMOBObject( OMF2::omfObject_t obj, IAAFMob* pMob );
+	HRESULT ConvertOMFCompositionObject( OMF2::omfObject_t obj );
+	HRESULT ConvertOMFMasterMob( OMF2::omfObject_t obj );
+	HRESULT ConvertOMFSourceMob( OMF2::omfObject_t obj );
+	HRESULT ConvertOMFMobSlots( OMF2::omfObject_t obj, IAAFMob* pMob );
+	HRESULT ConvertOMFSequence( OMF2::omfObject_t obj, IAAFSequence* pSequence );
 
-extern AafOmfGlobals*	gpGlobals;
+	char*					pProgramName;
 
+private:
+	aafBool					bVerboseMode;
+	aafBool					bCreateTOCFile;
+	aafBool					bConvertAllObjects;
+	aafBool					bOMFFileOpen;
+	aafBool					bAAFFileOpen;
+	aafBool					bLogFile;
+	aafBool					bDefFile;
+	char					sInFileName[256];
+	char					sTOCFileName[256];
+	char					sDefinitionFileName[256];
+	char					sOutFileName[256];
+	char					sLogFileName[256];
 
-#define COMMON_ERR_BASE		(AAFRESULT)0xE0000000
-#define AAF2OMF_ERR_BASE	(AAFRESULT)0xE0001000
-#define OMF2AAF_ERR_BASE	(AAFRESULT)0xE0006000
-#define AAFRESULT_FILE_NOT_OMF (OMF2AAF_ERR_BASE + 1)
-#endif
+	// For Statistical summary
+	aafInt32				nNumOMFObjects;
+	aafInt32				nNumAAFObjects;
+	aafInt32				nNumOMFProperties;
+	aafInt32				nNumAAFProperties;
+	aafInt32				nNumUndefinedOMFObjects;
+	aafInt32				nNumUndefinedOMFProperties;
+
+    OMF2::omfSessionHdl_t	OMFSession;
+	OMF2::omfHdl_t			OMFFileHdl;
+	OMF2::omfFileRev_t		OMFFileRev;
+
+	IAAFFile*				pFile;
+	IAAFHeader*				pHeader;
+	IAAFDictionary*			pDictionary;
+};
