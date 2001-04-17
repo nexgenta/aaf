361b248d0da38da0b1d31374647d38b9c16c17fd
tbingham <tbingham>
2001-04-17 09:47:36 +0000

    Add setBits to OMReferenceProperty, OMStrongReferenceVectorProperty and OMDataStreamProperty.

--- ref-impl/src/OM/OMDataStreamProperty.cpp
+++ ref-impl/src/OM/OMDataStreamProperty.cpp
@@ -1,3 +1,30 @@
+/***********************************************************************
+*
+*              Copyright (c) 1998-1999 Avid Technology, Inc.
+*
+* Permission to use, copy and modify this software and accompanying
+* documentation, and to distribute and sublicense application software
+* incorporating this software for any purpose is hereby granted,
+* provided that (i) the above copyright notice and this permission
+* notice appear in all copies of the software and related documentation,
+* and (ii) the name Avid Technology, Inc. may not be used in any
+* advertising or publicity relating to the software without the specific,
+* prior written permission of Avid Technology, Inc.
+*
+* THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+* WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+* IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+* SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+* OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+* OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+* ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+* RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+* ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+* LIABILITY.
+*
+************************************************************************/
+
 // @doc OMEXTERNAL
 #include "OMDataStreamProperty.h"
 
@@ -259,6 +286,20 @@ void OMDataStreamProperty::getBits(OMByte* bits, size_t size) const
   PRECONDITION("Valid size", size >= bitsSize());
 
   memcpy(bits, &_stream, bitsSize());
+}
+
+  // @mfunc Set the raw bits of this <c OMDataStreamProperty>. The raw
+  //         bits are copied from the buffer at address <p bits> which
+  //         is <p size> bytes in size.
+  //   @parm The address of the buffer from which the raw bits are copied.
+  //   @parm The size of the buffer.
+void OMDataStreamProperty::setBits(const OMByte* bits, size_t size)
+{
+  TRACE("OMDataStreamProperty::setBits");
+  PRECONDITION("Valid bits", bits != 0);
+  PRECONDITION("Valid size", size >= bitsSize());
+
+  ASSERT("Unimplemented code not reached", false);
 }
 
 
