e3661b40d8c5fead37fc63a21f5af6452a879c6d
tbingham <tbingham>
2001-04-17 05:11:43 +0000

    Initial checkin of dodo generated impl and module test sources.

--- ref-impl/src/impl/ImplAAFEventMobSlot.cpp
+++ ref-impl/src/impl/ImplAAFEventMobSlot.cpp
@@ -7,39 +7,24 @@
 *                                          *
 \******************************************/
 
+
+
+
+
+
+
+#include "AAFStoredObjectIDs.h"
+
 #ifndef __ImplAAFEventMobSlot_h__
 #include "ImplAAFEventMobSlot.h"
 #endif
-
-#ifndef __ImplAAFSequence_h__
-#include "ImplAAFSequence.h"
-#endif
-
-#ifndef __ImplAAFEvent_h__
-#include "ImplAAFEvent.h"
-#endif
-
-#include "ImplAAFDictionary.h"
-#include "ImplAAFDataDef.h"
-
-#include "AAFStoredObjectIDs.h"
-#include "AAFPropertyIDs.h"
-#include "AAFResult.h"
-#include "aafErr.h"
-#include "aafCvt.h"
-#include "aafUtils.h"
-
-
 
 #include <assert.h>
 #include <string.h>
 
 
-ImplAAFEventMobSlot::ImplAAFEventMobSlot ():
-  _editRate(PID_EventMobSlot_EditRate,	"EditRate")
-{
-  _persistentProperties.put(_editRate.address());
-}
+ImplAAFEventMobSlot::ImplAAFEventMobSlot ()
+{}
 
 
 ImplAAFEventMobSlot::~ImplAAFEventMobSlot ()
@@ -48,25 +33,17 @@ ImplAAFEventMobSlot::~ImplAAFEventMobSlot ()
 
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFEventMobSlot::GetEditRate (
-      aafRational_t *  pEditRate)
+      aafRational_t *  /*pEditRate*/)
 {
-  if (NULL == pEditRate)
-    return AAFRESULT_NULL_PARAM;
-
-  *pEditRate = _editRate;
-  return AAFRESULT_SUCCESS;
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
 
 
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFEventMobSlot::SetEditRate (
-      aafRational_t *  pEditRate)
+      aafRational_t *  /*pEditRate*/)
 {
-  if (NULL == pEditRate)
-    return AAFRESULT_NULL_PARAM;
-
-  _editRate = *pEditRate;
-  return AAFRESULT_SUCCESS;
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
 
 
@@ -74,156 +51,12 @@ AAFRESULT STDMETHODCALLTYPE
 
 
 
-// Override from AAFMobSlot
-AAFRESULT STDMETHODCALLTYPE
-ImplAAFEventMobSlot::SetSegment (/*[in]*/ ImplAAFSegment * pSegment)
-{
-  ImplAAFSequence *pSequence = NULL;
-  ImplAAFEvent *pEvent = NULL;
-  ImplAAFComponent *pComponent = NULL;
-  ImplAAFDictionary *pDict = NULL;
-  ImplAAFDataDef *pDef = NULL;
-  aafBool	willConvert;
-
-  if (NULL == pSegment)
-    return AAFRESULT_NULL_PARAM;
-
-  // Do even attempt to set the given segment if has already been
-  // attached to another persistent object.
-  if (pSegment->attached())
-    return AAFRESULT_OBJECT_ALREADY_ATTACHED;
-
-
-  XPROTECT()
+  // Override from AAFMobSlot
+  AAFRESULT STDMETHODCALLTYPE
+    ImplAAFEventMobSlot::SetSegment (/*[in]*/ ImplAAFSegment * /*pSegment*/)
   {
-    // Validate the following constraints if the segment is a sequence:
-    // A) all events must have the same Event subclass and 
-    // B) all events must have the same data kind.
-    // C) datakind of the each event must be the same as the sequence.
-    // D) the positions must be increasing.
-    //
-    // Otherwise the segment must be an event.
-    //
-    pSequence = dynamic_cast<ImplAAFSequence *>(pSegment);
-    if (NULL != pSequence)
-    {
-      aafUID_t sequDataDef, componentDataDef;
-      aafInt32 i, numberOfComponents = 0;
-      aafPosition_t previousPosition;
-      
-      
-      // Save the sequences data definition guid.
-      CHECK(pSequence->GetDataDef(&sequDataDef));
-
-      // There must be at least one component in the sequence.
-      CHECK(pSequence->GetNumComponents(&numberOfComponents));
-      if (0 >= numberOfComponents)
-        RAISE(AAFRESULT_OBJECT_SEMANTIC);
-
-      // Get the first component so that we can extract information
-      // to compare to any remaining elements.
-      CHECK(pSequence->GetNthComponent(0, &pComponent));
-      
-      // The component must have the same data definition [id] as the sequence.
-      CHECK(pComponent->GetDataDef(&componentDataDef));
- 
-	  CHECK(GetDictionary(&pDict));
-	  CHECK(pDict->LookupDataDefintion(&componentDataDef, &pDef));
-	  pDict->ReleaseReference();
-	  pDict = NULL;
-	  CHECK(pDef->DoesDataDefConvertTo(&sequDataDef, &willConvert));
-	  pDef->ReleaseReference();
-	  pDef = NULL;
-
-	  if (willConvert == AAFFalse)
-		  RAISE(AAFRESULT_OBJECT_SEMANTIC);
-
-      // Get the runtime type info for validation.
-      aafUID_t firstComponentType;
-      CHECK(pComponent->GetObjectClass(&firstComponentType));
-
-      // The component MUST be an event.
-      pEvent = dynamic_cast<ImplAAFEvent *>(pComponent);
-      if (NULL == pEvent)
-        RAISE(AAFRESULT_OBJECT_SEMANTIC);
-
-      // Save the position of the first event so that we can use this 
-      // as our starting value...
-      CHECK(pEvent->GetPosition(&previousPosition));
-      
-      // Release reference before reusing variable.
-      pComponent->ReleaseReference();
-      pComponent = NULL;
-
-      // Make sure that all components in the sequence have the same
-      // data definition and represent the same event class.
-      for (i = 1; i < numberOfComponents; ++i)
-      {
-        CHECK(pSequence->GetNthComponent(i, &pComponent));
-
-        // The component must have the same data definition [id] as the
-        // sequence.
-		CHECK(GetDictionary(&pDict));
-		CHECK(pDict->LookupDataDefintion(&componentDataDef, &pDef));
-		pDict->ReleaseReference();
-		pDict = NULL;
-		CHECK(pDef->DoesDataDefConvertTo(&sequDataDef, &willConvert));
-		pDef->ReleaseReference();
-		pDef = NULL;
-
-		if (willConvert == AAFFalse)
-			RAISE(AAFRESULT_INVALID_DATADEF);
-
-        // Validate that this event is the "same" type of event as the
-        // first component of the sequence.
-        aafUID_t  componentType;
-        CHECK(pComponent->GetObjectClass(&componentType));
-        if (0 != memcmp(&firstComponentType, &componentType, sizeof(firstComponentType)))
-          RAISE(AAFRESULT_OBJECT_SEMANTIC);
-
-        // The component MUST be an event.
-        pEvent = dynamic_cast<ImplAAFEvent *>(pComponent);
-        if (NULL == pEvent)
-          RAISE(AAFRESULT_OBJECT_SEMANTIC);
-
-        // Make sure the position of this event is no earlier then the 
-        // previous event.
-        aafPosition_t currentPosition;
-        CHECK(pEvent->GetPosition(&currentPosition));
-        if (Int64Less(currentPosition, previousPosition))
-          RAISE(AAFRESULT_OBJECT_SEMANTIC);
-
-        // Save the current position to compare to the next event.
-        previousPosition = currentPosition;
-
-        pComponent->ReleaseReference();
-        pComponent = NULL;
-      }
-    }
-    else
-    {
-      // The segment must be an event.
-      pEvent = dynamic_cast<ImplAAFEvent *>(pSegment);
-      if (NULL == pEvent)
-        RAISE(AAFRESULT_OBJECT_SEMANTIC);
-    }
-
-    // Call the parent class to set the segment...
-    CHECK(ImplAAFMobSlot::SetSegment(pSegment));
+    return AAFRESULT_NOT_IMPLEMENTED;
   }
-  XEXCEPT
-  {
-    if (NULL != pComponent)
-      pComponent->ReleaseReference();
-    if (NULL != pDict)
-      pDict->ReleaseReference();
-    if (NULL != pDef)
-      pDef->ReleaseReference();
-  }
-  XEND;
-
-  return(AAFRESULT_SUCCESS);
-}
 
 
 
--- test/com/ComModTestAAF/ModuleTests/CAAFTextClipTest.cpp
+++ test/com/ComModTestAAF/ModuleTests/CAAFTextClipTest.cpp
@@ -1,478 +1,77 @@
 // @doc INTERNAL
-// @com This file implements the module test for CAAFTextClip
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- * prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+// @com This file implements the module test for CAAFDefinitionObject
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
 
-#include "AAF.h"
+
+
+
+
+
+
+#include "CAAFTextClip.h"
+#include "CAAFTextClip.h"
+#ifndef __CAAFTextClip_h__
+#error - improperly defined include guard
+#endif
 
 
 #include <iostream.h>
-#include <stdio.h>
-#include <assert.h>
-#include <memory.h>
 
-#include "AAFStoredObjectIDs.h"
-#include "AAFResult.h"
-#include "AAFDefUIDs.h"
-
-#include "CAAFBuiltinDefs.h"
+// Temporarily necessary global declarations.
+extern "C" const CLSID CLSID_AAFTextClip; // generated
 
 
-// Cross-platform utility to delete a file.
-static void RemoveTestFile(const wchar_t* pFileName)
+HRESULT CAAFTextClip::test()
 {
-  const size_t kMaxFileName = 512;
-  char cFileName[kMaxFileName];
-
-  size_t status = wcstombs(cFileName, pFileName, kMaxFileName);
-  if (status != (size_t)-1)
-  { // delete the file.
-    remove(cFileName);
-  }
-}
-
-
-// convenient error handlers.
-inline void checkResult(HRESULT r)
-{
-  if (FAILED(r))
-    throw r;
-}
-inline void checkExpression(bool expression, HRESULT r)
-{
-  if (!expression)
-    throw r;
-}
-
-
-class TextClipTest
-{
-public:
-  TextClipTest();
-  ~TextClipTest();
-
-  void Create(wchar_t *pFileName, aafProductIdentification_t* pinfo);
-  void Open(wchar_t *pFileName);
-  void Close();
-
-  void CreateTextClip();
-  void OpenTextClip();
-
-private:
-  IAAFFile *_pFile;
-  bool _bWritableFile;
-  IAAFHeader *_pHeader;
-  IAAFDictionary *_pDictionary;
-  aafMobID_t _referencedMobID; // save id for validation in the open test...
-};
-
-
-extern "C" HRESULT CAAFTextClip_test()
-{
-  HRESULT hr = S_OK;
-  aafProductIdentification_t	ProductInfo = {0};
-  aafWChar * pFileName = L"AAFTextClipTest.aaf";
-
-  // Initialize the product info for this module test
-  ProductInfo.companyName = L"AAF Developers Desk";
-  ProductInfo.productName = L"AAFTextClip Test";
-  ProductInfo.productVersion.major = 1;
-  ProductInfo.productVersion.minor = 0;
-  ProductInfo.productVersion.tertiary = 0;
-  ProductInfo.productVersion.patchLevel = 0;
-  ProductInfo.productVersion.type = kAAFVersionUnknown;
-  ProductInfo.productVersionString = NULL;
-  ProductInfo.productID = UnitTestProductID;
-  ProductInfo.platform = NULL;
-
-  // Create an instance of our text clip test class and run the
-  // tests...
-  TextClipTest test;
+  HRESULT hr = AAFRESULT_NOT_IMPLEMENTED;
+  IAAFTextClip *pObject = NULL;
 
   try
-  {
-    // Attempt to create a test file
-    test.Create(pFileName, &ProductInfo);
+    {
+      // Attempt to create an AAFTextClip.
+      hr =  CoCreateInstance(
+                             CLSID_AAFTextClip,
+                             NULL, 
+                             CLSCTX_INPROC_SERVER, 
+                             IID_IAAFTextClip, (void **)&pObject);
+      if (FAILED(hr))
+        {
+          cerr << "CAAFTextClip::test...FAILED!";
+          cerr << hr;
+          cerr << "\tCoCreateInstance(&CLSID_AAFTextClip, NULL,"
+            " CLSCTX_INPROC_SERVER, &IID_IAAFTextClip, ...);" <<
+              endl;
+          return hr;
+        }
 
-    // Attempt to read the test file.
-    test.Open(pFileName);
-  }
-  catch (HRESULT& rHR)
-  {
-    hr = rHR;
-  }
+      // module-specific tests go here
+
+      if (pObject)
+        pObject->Release();
+      return AAFRESULT_NOT_IMPLEMENTED;
+
+    }
   catch (...)
-  {
-    cerr << "CAAFTextClip_test...Caught general C++ exception!" << endl;
-    hr = AAFRESULT_TEST_FAILED;
-  }
+    {
+      cerr << "CAAFTextClip::test...Caught general C++"
+        " exception!" << endl; 
+    }
+
+  // Cleanup our object if it exists.
+  if (pObject)
+    pObject->Release();
 
   return hr;
 }
 
 
-TextClipTest::TextClipTest() :
-  _pFile(NULL),
-  _bWritableFile(false),
-  _pHeader(NULL),
-  _pDictionary(NULL)
-{
-  memset(&_referencedMobID, 0, sizeof(_referencedMobID));
-}
-
-TextClipTest::~TextClipTest()
-{
-  Close();
-}
 
 
-void TextClipTest::Create(wchar_t *pFileName, aafProductIdentification_t* pinfo)
-{
-  assert(NULL == _pFile);
 
-  // Remove the previous test file if any.
-  RemoveTestFile(pFileName);
-    
-  // Create the file
-  checkResult(AAFFileOpenNewModify(pFileName, 0, pinfo, &_pFile));
-  _bWritableFile = true;
-
-  // We can't really do anthing in AAF without the header.
-  checkResult(_pFile->GetHeader(&_pHeader));
-
-  // Get the AAF Dictionary so that we can create valid AAF objects.
-  checkResult(_pHeader->GetDictionary(&_pDictionary));
-
-  // Make a text clip.
-  CreateTextClip();
-
-  // cleanup
-  Close();
-}
-
-
-void TextClipTest::Open(wchar_t *pFileName)
-{
-  assert(NULL == _pFile);
-    
-  // Create the file
-  checkResult(AAFFileOpenExistingRead(pFileName, 0, &_pFile));
-  _bWritableFile = false;
-
-  // We can't really do anthing in AAF without the header.
-  checkResult(_pFile->GetHeader(&_pHeader));
-
-  // Open and validate the text clip.
-  OpenTextClip();
-
-  // cleanup
-  Close();
-}
-
-
-void TextClipTest::Close()
-{
-  if (_pDictionary)
-  {
-    _pDictionary->Release();
-    _pDictionary = NULL;
-  }
-
-  if (_pHeader)
-  {
-    _pHeader->Release();
-    _pHeader = NULL;
-  }
-  
-  if (_pFile)
-  {
-    if (_bWritableFile)
-      checkResult(_pFile->Save());
-
-    checkResult(_pFile->Close());
-
-    _pFile->Release();
-    _pFile = NULL;
-  }  
-}
-
-
-void TextClipTest::CreateTextClip()
-{
-  assert(_pHeader && _pDictionary);
-
-  HRESULT hr = S_OK;
-  IAAFMasterMob *pMasterMob = NULL;
-  IAAFMob *pReferencedMob = NULL;
-  IAAFTextClip *pTextClip = NULL;
-  IAAFSourceReference *pSourceReference = NULL;
-  IAAFCompositionMob *pCompositionMob = NULL;
-  IAAFMob *pReferencingMob = NULL;
-  IAAFSegment *pSegment = NULL;
-  IAAFTimelineMobSlot *pMobSlot = NULL;
-
-
-  try
-  {
-	CAAFBuiltinDefs defs (_pDictionary);
-
-	//Make the MOB to be referenced
-	checkResult(defs.cdMasterMob()->
-				CreateInstance(IID_IAAFMasterMob, 
-							   (IUnknown **)&pMasterMob));
-  
-  checkResult(pMasterMob->QueryInterface(IID_IAAFMob, (void **)&pReferencedMob));
-  checkResult(pReferencedMob->GetMobID(&_referencedMobID));
-  checkResult(pReferencedMob->SetName(L"TextClipTest::ReferencedMob"));
-
-  // Save the master mob.
-  checkResult(_pHeader->AddMob(pReferencedMob));
-
-  // Use EssenceAccess to write some text essence
-    // Create a file mob for the text essence.
-    // Create the corresponding text essence.
-    // Write some text essence.
-
-
-  // Create a TextClip
-  checkResult(defs.cdTextClip()->
-			  CreateInstance(IID_IAAFTextClip, 
-							 (IUnknown **)&pTextClip));
-
-  // Initialize the source reference data.
-  checkResult(pTextClip->QueryInterface(IID_IAAFSourceReference, (void **)&pSourceReference));
-  checkResult(pSourceReference->SetSourceID(_referencedMobID));
-  checkResult(pSourceReference->SetSourceMobSlotID(0));
-
-  // Create a composition mob to hold the text clip.
-  checkResult(defs.cdCompositionMob()->
-			  CreateInstance(IID_IAAFCompositionMob, 
-							 (IUnknown **)&pCompositionMob));
-  checkResult(pCompositionMob->QueryInterface(IID_IAAFMob, (void **)&pReferencingMob));
-  checkResult(pReferencingMob->SetName(L"CompositionMob_TextClipTest"));
- 
-  
-  checkResult(pTextClip->QueryInterface(IID_IAAFSegment, (void **)&pSegment));
-  IAAFMobSlot *pSlot = NULL;
-  aafRational_t editRate = { 0, 1};
-  checkResult(pReferencingMob->AppendNewTimelineSlot(editRate,
-													 pSegment,
-													 1,
-													 L"TextClipTest",
-													 0,
-													 &pMobSlot));
-
-
-  // Save the referencing mob.
-  checkResult(_pHeader->AddMob(pReferencingMob));
-  }
-  catch (HRESULT& rHR)
-  {
-    hr = rHR;
-    // fall through and handle cleanup
-  }
-
-  // Cleanup local references
-  if (pMobSlot)
-  {
-    pMobSlot->Release();
-    pMobSlot = NULL;
-  }
-
-  if (pSegment)
-  {
-    pSegment->Release();
-    pSegment = NULL;
-  }
-
-  if (pReferencingMob)
-  {
-    pReferencingMob->Release();
-    pReferencingMob = NULL;
-  }
-
-  if (pCompositionMob)
-  {
-    pCompositionMob->Release();
-    pCompositionMob = NULL;
-  }
-
-  if (pSourceReference)
-  {
-    pSourceReference->Release();
-    pSourceReference = NULL;
-  }
-
-  if (pTextClip)
-  {
-    pTextClip->Release();
-    pTextClip = NULL;
-  }
-
-  if (pReferencedMob)
-  {
-    pReferencedMob->Release();
-    pReferencedMob = NULL;
-  }
-
-  if (pMasterMob)
-  {
-    pMasterMob->Release();
-    pMasterMob = NULL;
-  }
-
-
-  // Propogate the error if necessary.
-  checkResult(hr);
-}
-
-
-void TextClipTest::OpenTextClip()
-{
-  assert(_pHeader);
-
-  HRESULT hr = S_OK;
-  aafNumSlots_t compositionMobs = 0;
-  IEnumAAFMobs *pEnumMobs = NULL;
-  IAAFMob *pReferencingMob = NULL;
-  IAAFCompositionMob *pCompositionMob = NULL;
-  aafNumSlots_t mobSlots = 0;
-  IEnumAAFMobSlots *pEnumSlots = NULL;
-  IAAFMobSlot *pMobSlot = NULL;
-  IAAFSegment *pSegment = NULL;
-  IAAFTextClip *pTextClip = NULL;
-  IAAFSourceReference *pSourceReference = NULL;
-  aafMobID_t masterMobID = {0};
-  IAAFMasterMob *pMasterMob = NULL;
-  IAAFMob *pReferencedMob = NULL;
-
-
-  try
-  {
-    // Get the number of composition mobs in the file (should be one)
-    checkResult(_pHeader->CountMobs(kAAFCompMob, &compositionMobs));
-    checkExpression(1 == compositionMobs, AAFRESULT_TEST_FAILED);
-
-    // Get the composition mob. There should only be one.
-    aafSearchCrit_t criteria;
-    criteria.searchTag = kAAFByMobKind;
-    criteria.tags.mobKind = kAAFCompMob;
-    checkResult(_pHeader->GetMobs(&criteria, &pEnumMobs));
-    checkResult(pEnumMobs->NextOne(&pReferencingMob));
-    checkResult(pReferencingMob->QueryInterface(IID_IAAFCompositionMob, (void **)&pCompositionMob));
-
-    // Get the text clip in the slot. There should be only one.
-    checkResult(pReferencingMob->CountSlots(&mobSlots));
-    checkExpression(1 == mobSlots, AAFRESULT_TEST_FAILED);
-    checkResult(pReferencingMob->GetSlots(&pEnumSlots));
-    checkResult(pEnumSlots->NextOne(&pMobSlot));
-    checkResult(pMobSlot->GetSegment(&pSegment));
-    checkResult(pSegment->QueryInterface(IID_IAAFTextClip, (void **)&pTextClip));
-
-    // Attempt to "resolve" the reference and find the master mob.
-    checkResult(pTextClip->QueryInterface(IID_IAAFSourceReference, (void **)&pSourceReference));  
-    checkResult(pSourceReference->GetSourceID(&masterMobID));
-    checkExpression(0 == memcmp(&masterMobID, &_referencedMobID, sizeof(masterMobID)), AAFRESULT_TEST_FAILED);
-    checkResult(_pHeader->LookupMob(masterMobID, &pReferencedMob));
-    checkResult(pReferencedMob->QueryInterface(IID_IAAFMasterMob, (void **)&pMasterMob));
-
-    
-    // Use EssenceAccess to read some text essence
-      // Find the file mob for the text essence.
-      // Find the corresponding text essence.
-      // Read some text essence.
-  }
-  catch (HRESULT& rHR)
-  {
-    hr = rHR;
-    // fall through and handle cleanup
-  }
-
-  // Cleanup local references
-  if (pMasterMob)
-  {
-    pMasterMob->Release();
-    pMasterMob = NULL;
-  }
-
-  if (pReferencedMob)
-  {
-    pReferencedMob->Release();
-    pReferencedMob = NULL;
-  }
-
-  if (pSourceReference)
-  {
-    pSourceReference->Release();
-    pSourceReference = NULL;
-  }
-
-  if (pTextClip)
-  {
-    pTextClip->Release();
-    pTextClip = NULL;
-  }
-
-  if (pSegment)
-  {
-    pSegment->Release();
-    pSegment = NULL;
-  }
-
-  if (pMobSlot)
-  {
-    pMobSlot->Release();
-    pMobSlot = NULL;
-  }
-
-  if (pEnumSlots)
-  {
-    pEnumSlots->Release();
-    pEnumSlots = NULL;
-  }
-
-  if (pCompositionMob)
-  {
-    pCompositionMob->Release();
-    pCompositionMob = NULL;
-  }
-
-  if (pReferencingMob)
-  {
-    pReferencingMob->Release();
-    pReferencingMob = NULL;
-  }
-
-  if (pEnumMobs)
-  {
-    pEnumMobs->Release();
-    pEnumMobs = NULL;
-  }
-
-  // Propogate the error if necessary.
-  checkResult(hr);
-}
