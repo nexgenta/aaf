209e10e82d04a40fb33e72fee4a437ca883fe8ba
tbingham <tbingham>
2001-04-17 04:25:00 +0000

    Implement OMType virtual functions.

--- ref-impl/src/impl/ImplAAFTypeDefString.h
+++ ref-impl/src/impl/ImplAAFTypeDefString.h
@@ -102,8 +102,36 @@ public:
 
 
 
+  //*************************************************************
+  //
+  // Overrides from OMType, via inheritace through ImplAAFTypeDef
+  //
+  //*************************************************************
+
+  virtual void reorder(OMByte* externalBytes,
+                       size_t externalBytesSize) const;
+
+  virtual size_t externalSize(OMByte* internalBytes,
+							  size_t internalBytesSize) const;
+
+  virtual void externalize(OMByte* internalBytes,
+                           size_t internalBytesSize,
+                           OMByte* externalBytes,
+                           size_t externalBytesSize,
+                           OMByteOrder byteOrder) const;
+
+  virtual size_t internalSize(OMByte* externalBytes,
+							  size_t externalBytesSize) const;
+
+  virtual void internalize(OMByte* externalBytes,
+                           size_t externalBytesSize,
+                           OMByte* internalBytes,
+                           size_t internalBytesSize,
+                           OMByteOrder byteOrder) const;
+
 private:
-  OMWeakReferenceProperty<ImplAAFTypeDef> _ElementType;
+  // OMWeakReferenceProperty<ImplAAFTypeDef> _ElementType;
+  OMFixedSizeProperty<aafUID_t>           _ElementType;
 
 
 public:
