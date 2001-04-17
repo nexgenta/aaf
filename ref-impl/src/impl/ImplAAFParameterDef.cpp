bffce1fa3f0b90910b1c4ed9dde97270086d8bcb
tbingham <tbingham>
2001-04-17 09:14:59 +0000

    Added TypeDefIndirect: enabled for ConstantValue, ControlPoint and TaggedValue. The AAFTypeDefIndirect module test is incomplete...

--- ref-impl/src/impl/ImplAAFParameterDef.cpp
+++ ref-impl/src/impl/ImplAAFParameterDef.cpp
@@ -1,11 +1,29 @@
-/***********************************************\
-*												*
-* Advanced Authoring Format						*
-*												*
-* Copyright (c) 1998-1999 Avid Technology, Inc. *
-* Copyright (c) 1998-1999 Microsoft Corporation *
-*												*
-\***********************************************/
+/***********************************************************************
+ *
+ *              Copyright (c) 1998-1999 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and accompanying 
+ * documentation, and to distribute and sublicense application software
+ * incorporating this software for any purpose is hereby granted, 
+ * provided that (i) the above copyright notice and this permission
+ * notice appear in all copies of the software and related documentation,
+ * and (ii) the name Avid Technology, Inc. may not be used in any
+ * advertising or publicity relating to the software without the specific,
+ * prior written permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+ * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+ * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+ * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+ * LIABILITY.
+ *
+ ************************************************************************/
 
 
 #include "AAFStoredObjectIDs.h"
@@ -35,7 +53,29 @@ ImplAAFParameterDef::~ImplAAFParameterDef ()
 
 
 AAFRESULT STDMETHODCALLTYPE
-    ImplAAFParameterDef::GetTypeDef (
+    ImplAAFParameterDef::Initialize (
+      const aafUID_t & id,
+	  const aafWChar * pName,
+	  const aafWChar * pDesc,
+    ImplAAFTypeDef * pType)
+{
+  AAFRESULT result = AAFRESULT_SUCCESS;
+	if (pName == NULL || pDesc == NULL || pType == NULL)
+	{
+	  return AAFRESULT_NULL_PARAM;
+	}
+	else
+	{
+    AAFRESULT result = pvtInitialize(id, pName, pDesc);
+	  if (AAFRESULT_SUCCEEDED (result))
+      result = SetTypeDef (pType);
+	}
+	return result;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFParameterDef::GetTypeDefinition (
       ImplAAFTypeDef **ppTypeDef)
 {
 	aafUID_t			uid;
@@ -48,14 +88,15 @@ AAFRESULT STDMETHODCALLTYPE
 	{
 		uid = _typeDef;
 		CHECK(GetDictionary(&dict));
-		CHECK(dict->LookupType (&uid, ppTypeDef));
+		CHECK(dict->LookupTypeDef (_typeDef, ppTypeDef));
 		dict->ReleaseReference();
 		dict = NULL;
 	}
 	XEXCEPT
 	{
 		if(dict != NULL)
-			dict->ReleaseReference();
+		  dict->ReleaseReference();
+		dict = 0;
 	}
 	XEND;
 	
@@ -83,14 +124,17 @@ AAFRESULT STDMETHODCALLTYPE
 
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFParameterDef::GetDisplayUnits (
-      wchar_t *pDisplayUnits,
-      aafInt32  bufSize)
+      aafCharacter *pDisplayUnits,
+      aafUInt32  bufSize)
 {
-	bool stat;
-
 	if(pDisplayUnits == NULL)
 		return(AAFRESULT_NULL_PARAM);
 
+	if(!_displayUnits.isPresent())
+		return AAFRESULT_PROP_NOT_PRESENT;
+	
+	bool stat;
+	
 	stat = _displayUnits.copyToBuffer(pDisplayUnits, bufSize);
 	if (! stat)
 	{
@@ -103,10 +147,14 @@ AAFRESULT STDMETHODCALLTYPE
 
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFParameterDef::GetDisplayUnitsBufLen (
-      aafInt32 * pLen)
+      aafUInt32 * pLen)
 {
 	if(pLen == NULL)
 		return(AAFRESULT_NULL_PARAM);
+
+	if(!_displayUnits.isPresent())
+		return AAFRESULT_PROP_NOT_PRESENT;
+
 	*pLen = _displayUnits.size();
 	return(AAFRESULT_SUCCESS); 
 }
@@ -116,7 +164,7 @@ AAFRESULT STDMETHODCALLTYPE
 
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFParameterDef::SetDisplayUnits (
-      wchar_t *pDisplayUnits)
+      const aafCharacter *pDisplayUnits)
 {
 	if(pDisplayUnits == NULL)
 		return(AAFRESULT_NULL_PARAM);
@@ -126,6 +174,5 @@ AAFRESULT STDMETHODCALLTYPE
 	return(AAFRESULT_SUCCESS); 
 }
 
-OMDEFINE_STORABLE(ImplAAFParameterDef, AUID_AAFParameterDef);
 
 
