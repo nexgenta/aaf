74b02309e8bf6e20cdd550e7a4d826153928d7b2
tbingham <tbingham>
2001-04-17 05:28:29 +0000

    Cleaning out memory leaks

--- ref-impl/src/impl/ImplAAFEssenceData.cpp
+++ ref-impl/src/impl/ImplAAFEssenceData.cpp
@@ -32,8 +32,8 @@
 const aafUID_t kNullID = {0};
 
 ImplAAFEssenceData::ImplAAFEssenceData () :
-  _fileMobID(PID_EssenceData_MobID, "MobID"),
-  _mediaData(PID_ESSENCEDATA_MEDIADATA, "Essence Data")
+  _fileMobID(PID_EssenceData_MobID,	"MobID"),
+  _mediaData(PID_EssenceData_Data,	"Data")
 {
   // Add the properties into the property set.
   _persistentProperties.put(_fileMobID.address());
@@ -61,7 +61,7 @@ ImplAAFEssenceData::~ImplAAFEssenceData ()
     return AAFRESULT_NULL_PARAM;
   // Cannot access the data property if it is NOT associated with a file.
   if (!persistent())
-    return AAFRESULT_NOT_IN_FILE;
+    return AAFRESULT_OBJECT_NOT_PERSISTENT;
   
   try
   {
@@ -92,7 +92,7 @@ ImplAAFEssenceData::~ImplAAFEssenceData ()
     return AAFRESULT_NULL_PARAM;
   // Cannot access the data property if it is NOT associated with a file.
   if (!persistent())
-    return AAFRESULT_NOT_IN_FILE;
+    return AAFRESULT_OBJECT_NOT_PERSISTENT;
   
   try
   {
@@ -118,7 +118,7 @@ AAFRESULT STDMETHODCALLTYPE
   AAFRESULT result = AAFRESULT_SUCCESS;
   // Cannot access the data property if it is NOT associated with a file.
   if (!persistent())
-    return AAFRESULT_NOT_IN_FILE;
+    return AAFRESULT_OBJECT_NOT_PERSISTENT;
 
   try
   {
@@ -147,7 +147,7 @@ AAFRESULT STDMETHODCALLTYPE
     return AAFRESULT_NULL_PARAM;
   // Cannot access the data property if it is NOT associated with a file.
   if (!persistent())
-    return AAFRESULT_NOT_IN_FILE;
+    return AAFRESULT_OBJECT_NOT_PERSISTENT;
 
   try
   {
@@ -175,7 +175,7 @@ AAFRESULT STDMETHODCALLTYPE
     return AAFRESULT_NULL_PARAM;
   // Cannot access the data property if it is NOT associated with a file.
   if (!persistent())
-    return AAFRESULT_NOT_IN_FILE;
+    return AAFRESULT_OBJECT_NOT_PERSISTENT;
 
 
   AAFRESULT result = AAFRESULT_SUCCESS;
@@ -241,6 +241,7 @@ AAFRESULT STDMETHODCALLTYPE
   XEXCEPT
   {  // save the error code.
     result = (XCODE());
+	NO_PROPAGATE();
   }
   XEND;
 
@@ -283,7 +284,7 @@ AAFRESULT STDMETHODCALLTYPE
     CHECK(GetFileMobID(&mobID));
 
     // Does a mob with the ID already exist?  If not, return error.
-    // NOTE: Will return AAFRESULT_NOT_IN_FILE if this object has
+    // NOTE: Will return AAFRESULT_OBJECT_NOT_PERSISTENT if this object has
     // not been appended to to the file.
     CHECK(MyHeadObject(&pHeader));
     CHECK(pHeader->LookupMob(&mobID, &pMob));
@@ -305,6 +306,7 @@ AAFRESULT STDMETHODCALLTYPE
   XEXCEPT
   {  // save the error code.
     result = (XCODE());
+	NO_PROPAGATE();
   }
   XEND;
 
