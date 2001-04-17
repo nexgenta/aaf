fedd20e7f57dd58f5b9d514ce9879d0e572f7d7e
tbingham <tbingham>
2001-04-17 05:09:50 +0000

    third round of vector to set changes

--- ref-impl/src/impl/ImplEnumAAFOperationDefs.h
+++ ref-impl/src/impl/ImplEnumAAFOperationDefs.h
@@ -31,11 +31,96 @@
  *
  ************************************************************************/
 
-#include "ImplAAFEnumerator.h"
+class ImplAAFOperationDef;
 
-#include "ImplAAFOperationDef.h"
 
-typedef ImplAAFEnumerator<ImplAAFOperationDef> ImplEnumAAFOperationDefs;
+
+
+
+
+
+#ifndef __ImplAAFObject_h__
+#include "ImplAAFObject.h"
+#endif
+#include "OMReferenceContainerIter.h"
+
+typedef OMVariableSizeProperty<aafUID_t> OperationDefWeakRefArrayProp_t;
+
+class ImplEnumAAFOperationDefs : public ImplAAFRoot
+{
+public:
+  //
+  // Constructor/destructor
+  //
+  //********
+  ImplEnumAAFOperationDefs ();
+
+protected:
+  virtual ~ImplEnumAAFOperationDefs ();
+
+public:
+
+
+  //****************
+  // NextOne()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    NextOne
+        // @parm [out,retval] The Next OperationDefinition
+        (ImplAAFOperationDef ** ppOperationDef);
+
+  //****************
+  // Next()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    Next
+        (// @parm [in] number of effect definitions requested
+         aafUInt32  count,
+
+         // @parm [out, size_is(count), length_is(*pFetched)] array to receive operation definitions
+         ImplAAFOperationDef ** ppOperationDefs,
+
+         // @parm [out,ref] number of actual OperationDefs fetched into ppOperationDefs array
+         aafUInt32 *  pFetched);
+
+  //****************
+  // Skip()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    Skip
+        // @parm [in] Number of elements to skip
+        (aafUInt32  count);
+
+  //****************
+  // Reset()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    Reset ();
+
+
+  //****************
+  // Clone()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    Clone
+        // @parm [out,retval] new enumeration
+        (ImplEnumAAFOperationDefs ** ppEnum);
+
+
+public:
+  // SDK Internal 
+  virtual AAFRESULT STDMETHODCALLTYPE
+    SetEnumProperty( ImplAAFObject *pObj, OperationDefWeakRefArrayProp_t *pProp);// !!!TEmp until weak reference code
+  virtual AAFRESULT STDMETHODCALLTYPE
+	  SetIterator(ImplAAFObject *pObj,
+				OMReferenceContainerIterator<ImplAAFOperationDef>* iterator);
+
+private:
+	aafUInt32							_current;
+	ImplAAFObject						*_enumObj;
+	OperationDefWeakRefArrayProp_t		*_enumProp;
+	OMReferenceContainerIterator<ImplAAFOperationDef>*	_iterator;
+};
 
 #endif // ! __ImplEnumAAFOperationDefs_h__
 
