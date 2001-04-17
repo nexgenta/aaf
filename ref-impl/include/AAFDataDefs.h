14f9222a834f48e445ae308523e41db3a058d2f1
tbingham <tbingham>
2001-04-17 03:24:40 +0000

    Change DataDef internals & split of dataDef defines

--- ref-impl/include/AAFDataDefs.h
+++ ref-impl/include/AAFDataDefs.h
@@ -1,26 +1,11 @@
-#ifndef __AAFDataDefs_h__
-#define __AAFDataDefs_h__
-//=---------------------------------------------------------------------=
-//
-// The contents of this file are subject to the AAF SDK Public
-// Source License Agreement (the "License"); You may not use this file
-// except in compliance with the License.  The License is available in
-// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
-// Association or its successor.
-// 
-// Software distributed under the License is distributed on an "AS IS"
-// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
-// the License for the specific language governing rights and limitations
-// under the License.
-// 
-// The Original Code of this file is Copyright 1998-2001, Licensor of the
-// AAF Association.
-// 
-// The Initial Developer of the Original Code of this file and the
-// Licensor of the AAF Association is Avid Technology.
-// All rights reserved.
-//
-//=---------------------------------------------------------------------=
+/***********************************************\
+*												*
+* Advanced Authoring Format						*
+*												*
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+* Copyright (c) 1998-1999 Microsoft Corporation *
+*												*
+\***********************************************/
 
 // Following are the UIDs of the well-known data definition objects in AAF.
 //
@@ -44,7 +29,3 @@ const aafUID_t DDEF_PictureWithMatte = { 0x05CBA732, 0x1DAA, 0x11d3, { 0x80, 0xA
 const aafUID_t DDEF_Sound = { 0x78E1EBE1, 0x6CEF, 0x11D2, { 0x80, 0x7D, 0x00, 0x60, 0x08, 0x14, 0x3E, 0x6F } };
 const aafUID_t DDEF_Timecode = { 0x7F275E81, 0x77E5, 0x11D2, { 0x80, 0x7F, 0x00, 0x60, 0x08, 0x14, 0x3E, 0x6F } };
 const aafUID_t DDEF_Edgecode = { 0xd2bb2af0, 0xd234, 0x11d2, { 0x89, 0xee, 0x0, 0x60, 0x97, 0x11, 0x62, 0x12 } };
-const aafUID_t DDEF_Unknown = { 0x851419d0, 0x2e4f, 0x11d3, { 0x8a, 0x5b, 0x0, 0x50, 0x4, 0xe, 0xf7, 0xd2 } };
-
-
-#endif // !  __AAFDataDefs_h__
