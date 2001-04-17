0807b0c5ec3f6acfedfefdc47be6ab0f03567ba0
tbingham <tbingham>
2001-04-17 03:36:05 +0000

    Change DataDef internals & split of dataDef defines

--- ref-impl/src/impl/ImplAAFDataDef.h
+++ ref-impl/src/impl/ImplAAFDataDef.h
@@ -4,32 +4,14 @@
 #define __ImplAAFDataDef_h__
 
 
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- *  prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
 
 
 #ifndef __ImplAAFDefObject_h__
@@ -50,6 +32,17 @@ protected:
   virtual ~ImplAAFDataDef ();
 
 public:
+
+
+
+  //****************
+  // GetName()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetName
+        // @parm [retval,out] Pointer to name
+        (aafString_t *  pstrName);
+
   //****************
   // IsPictureKind()
   //
@@ -88,7 +81,7 @@ public:
   virtual AAFRESULT STDMETHODCALLTYPE
     DoesDataDefConvertTo
         (// @parm [in,ref] Auid to compare against
-         const aafUID_t & id,
+         aafUID_t *  pAuid,
 
          // @parm [retval,out] pointer to result
          aafBool *  bDoesConvertTo);
@@ -99,7 +92,7 @@ public:
   virtual AAFRESULT STDMETHODCALLTYPE
     IsDataDefOf
         (// @parm [in,ref] Auid to compare against
-         const aafUID_t & id,
+         aafUID_t *  pAuid,
 
          // @parm [retval,out] pointer to result
          aafBool *  bIsDataDefOf);
@@ -110,10 +103,21 @@ public:
   virtual AAFRESULT STDMETHODCALLTYPE
     DoesDataDefConvertFrom
         (// @parm [in,ref] Auid to compare against
-         const aafUID_t & id,
+         aafUID_t *  pAuid,
 
          // @parm [retval,out] pointer to result
          aafBool *  bDoesConvertFrom);
+
+
+
+public:
+  // Declare this class to be storable.
+  //
+  OMDECLARE_STORABLE(ImplAAFDataDef)
+
+  // Declare the module test method. The implementation of the will be be
+  // in /test/ImplAAFDataDefTest.cpp.
+  static AAFRESULT test();
 };
 
 #endif // ! __ImplAAFDataDef_h__
