f6a8b1afca71deef263085fcd1f19e6f6be86c78
tbingham <tbingham>
2001-04-17 05:02:08 +0000

    1. Added many new methods to support more flexible plugin registration: RegisterClassFactory, RegisterPluginFile, RegisterPluginDirectory and RegisterSharedPlugins. 2. No plugins are registered by default. The client must call one of the public registration methods if they need access to any external plugin objects.

--- ref-impl/src/impl/ImplAAFPluginManager.h
+++ ref-impl/src/impl/ImplAAFPluginManager.h
@@ -4,14 +4,32 @@
 #define __ImplAAFPluginManager_h__
 
 
-/************************************************\
-*												*
-* Advanced Authoring Format						*
-*												*
-* Copyright (c) 1998-1999 Avid Technology, Inc. *
-* Copyright (c) 1998-1999 Microsoft Corporation *
-*												*
-\************************************************/
+/***********************************************************************
+ *
+ *              Copyright (c) 1998-1999 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and accompanying 
+ * documentation, and to distribute and sublicense application software
+ * incorporating this software for any purpose is hereby granted, 
+ * provided that (i) the above copyright notice and this permission
+ * notice appear in all copies of the software and related documentation,
+ * and (ii) the name Avid Technology, Inc. may not be used in any
+ * advertising or publicity relating to the software without the specific,
+ *  prior written permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+ * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+ * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+ * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+ * LIABILITY.
+ *
+ ************************************************************************/
 
 
 
@@ -26,6 +44,15 @@ class ImplAAFDefObject;
 
 #include "AAFPlugin.h"
 #include "aafTable.h"
+
+// Implemenent a visitor pattern for plugins with a given definition id.
+// Used with ForEachPluginWithDefinitionDo.
+typedef aafBool	(*AAFDOTOPLUGINWITHDEFINTIONPROC)(
+  aafUID_constref defID, 
+  REFCLSID clsid, 
+  void *refCon);
+
+
 
 class ImplAAFDictionary;
 
@@ -58,13 +85,23 @@ public:
 
 
 public:
-  // Declare the module test method. The implementation of the will be be
-  // in /test/ImplAAFPluginManagerTest.cpp.
-  static AAFRESULT test();
-
-public:
 	//Toolkit internal only
 static ImplAAFPluginManager *GetPluginManager(void);
+
+// Implemenent a visitor pattern for plugins with a given definition id.
+AAFRESULT ForEachPluginWithDefinitionDo(
+  aafUID_constref defID, 
+  AAFDOTOPLUGINWITHDEFINTIONPROC proc, 
+  void *refCon);
+
+bool FindPluginFromDefintion(
+  aafUID_constref	pluginID,
+  CLSID& clsid);
+
+bool FindPluginFromEssenceDesc(
+  aafUID_constref	essenceDesc,
+  CLSID& clsid);
+
 
 // Create an object contained within one of the loaded plugin files.
 AAFRESULT CreateInstance(
@@ -73,6 +110,11 @@ AAFRESULT CreateInstance(
 			REFIID riid, 
 			void ** result);
 
+AAFRESULT CreateInstanceFromDefinition(
+			aafUID_constref		pluginID,
+			IUnknown* pUnkOuter, 
+			REFIID riid, 
+			void ** result);
 
 AAFRESULT GetPluginInstance(
 			aafUID_t		pluginID,
@@ -82,8 +124,30 @@ AAFRESULT MakeCodecFromEssenceDesc(
 			aafUID_t		essenceDesc,	// Stored class ID
 			IAAFEssenceCodec **codec);
 
+// Attempt to register all of the plugin files in the installation directory.
+AAFRESULT RegisterSharedPlugins(void);
+
+// Attempt to register all of the plugin files in the given directory.
+AAFRESULT RegisterPluginDirectory(const char *directoryName);
+AAFRESULT RegisterPluginDirectory(aafCharacter_constptr wdirectoryName);
+
+// Attempt to register all of the plugins in the given file.
+AAFRESULT RegisterPluginFile(const char *fileName);
+AAFRESULT RegisterPluginFile(aafCharacter_constptr wfileName);
+
+// Associate a factory object with a code class id.
+AAFRESULT RegisterPluginFactory(
+			REFCLSID rclsid,
+      IClassFactory *pFactory);
+
 AAFRESULT RegisterPlugin(
 			CLSID		plugin);
+
+// Attempt to unregister all plugins. This may fail if any plugins
+// are still loaded. This method should only be called after all
+// files have been closed, all client created plugins have been
+// released but before AAFUnload() has been called.
+AAFRESULT UnregisterAllPlugins(void);
 
 AAFRESULT Init(void);
 
@@ -97,6 +161,7 @@ AAFRESULT CreatePluginDefinition (aafUID_t  pluginDefID,
 
 private:
   aafTable_t	*_pluginFiles;
+  aafTable_t  *_pluginFactories;
 	aafTable_t	*_plugins;
 	aafTable_t	*_codecDesc;
 };
