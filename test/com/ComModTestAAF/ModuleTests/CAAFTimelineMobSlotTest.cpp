e28d3443bb43f160b8040ed621ea121fc035af06
tbingham <tbingham>
2001-04-17 05:50:06 +0000

    Get rid of duman-typed-comtest in aafobjects.mk

--- test/com/ComModTestAAF/ModuleTests/CAAFTimelineMobSlotTest.cpp
+++ test/com/ComModTestAAF/ModuleTests/CAAFTimelineMobSlotTest.cpp
@@ -39,10 +39,6 @@
 
 #include <iostream.h>
 
-// Temporarily necessary global declarations.
-extern "C" const CLSID CLSID_AAFTimelineMobSlot;
-
-
 HRESULT CAAFTimelineMobSlot::test()
 {
   HRESULT hr = AAFRESULT_NOT_IMPLEMENTED;
