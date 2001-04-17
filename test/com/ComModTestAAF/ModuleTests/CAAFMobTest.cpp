fb521852794aac20561dbbe106173a04dbcb7675
tbingham <tbingham>
2001-04-17 09:43:00 +0000

    Added SetCreateTime method to Mob (task#24)

--- test/com/ComModTestAAF/ModuleTests/CAAFMobTest.cpp
+++ test/com/ComModTestAAF/ModuleTests/CAAFMobTest.cpp
@@ -1,13 +1,31 @@
 // @doc INTERNAL
 // @com This file implements the module test for CAAFMob
-/******************************************\
-*                                          *
-* Advanced Authoring Format                *
-*                                          *
-* Copyright (c) 1998 Avid Technology, Inc. *
-* Copyright (c) 1998 Microsoft Corporation *
-*                                          *
-\******************************************/
+/***********************************************************************
+ *
+ *              Copyright (c) 1998-1999 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and accompanying 
+ * documentation, and to distribute and sublicense application software
+ * incorporating this software for any purpose is hereby granted, 
+ * provided that (i) the above copyright notice and this permission
+ * notice appear in all copies of the software and related documentation,
+ * and (ii) the name Avid Technology, Inc. may not be used in any
+ * advertising or publicity relating to the software without the specific,
+ * prior written permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+ * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+ * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+ * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+ * LIABILITY.
+ *
+ ************************************************************************/
 
 
 #include "AAF.h"
@@ -49,86 +67,104 @@ inline void checkExpression(bool expression, HRESULT r)
 
 static HRESULT CreateAAFFile(aafWChar * pFileName)
 {
-	IAAFFile *					pFile = NULL;
+  IAAFFile *					pFile = NULL;
   bool bFileOpen = false;
   IAAFHeader *        pHeader = NULL;
   IAAFDictionary*  pDictionary = NULL;
-	IAAFMob						*pMob = NULL;
-	IAAFMobSlot		*newSlot = NULL;
-	IAAFSegment		*seg = NULL;
-	IAAFSourceClip	*sclp = NULL;
-	aafProductIdentification_t	ProductInfo;
-	aafUID_t					newUID;
-	HRESULT						hr = S_OK;
+  IAAFMob						*pMob = NULL;
+  IAAFTimelineMobSlot *newSlot = NULL;
+  IAAFSegment		*seg = NULL;
+  IAAFSourceClip	*sclp = NULL;
+  aafProductIdentification_t	ProductInfo;
+  aafUID_t					newUID;
+  HRESULT						hr = S_OK;
 
-	ProductInfo.companyName = L"AAF Developers Desk";
-	ProductInfo.productName = L"AAFMob Test";
-	ProductInfo.productVersion.major = 1;
-	ProductInfo.productVersion.minor = 0;
-	ProductInfo.productVersion.tertiary = 0;
-	ProductInfo.productVersion.patchLevel = 0;
-	ProductInfo.productVersion.type = kVersionUnknown;
-	ProductInfo.productVersionString = NULL;
-	ProductInfo.productID = UnitTestProductID;
-	ProductInfo.platform = NULL;
+  ProductInfo.companyName = L"AAF Developers Desk";
+  ProductInfo.productName = L"AAFMob Test";
+  ProductInfo.productVersion.major = 1;
+  ProductInfo.productVersion.minor = 0;
+  ProductInfo.productVersion.tertiary = 0;
+  ProductInfo.productVersion.patchLevel = 0;
+  ProductInfo.productVersion.type = kVersionUnknown;
+  ProductInfo.productVersionString = NULL;
+  ProductInfo.productID = UnitTestProductID;
+  ProductInfo.platform = NULL;
 
 
   try
-  {
-    // Remove the previous test file if any.
-    RemoveTestFile(pFileName);
+	{
+	  // Remove the previous test file if any.
+	  RemoveTestFile(pFileName);
 
-    // Create the file.
-		checkResult(AAFFileOpenNewModify(pFileName, 0, &ProductInfo, &pFile));
-		bFileOpen = true;
+	  // Create the file.
+	  checkResult(AAFFileOpenNewModify(pFileName, 0, &ProductInfo, &pFile));
+	  bFileOpen = true;
  
-    // We can't really do anthing in AAF without the header.
-		checkResult(pFile->GetHeader(&pHeader));
+	  // We can't really do anthing in AAF without the header.
+	  checkResult(pFile->GetHeader(&pHeader));
 
-    // Get the AAF Dictionary so that we can create valid AAF objects.
-    checkResult(pHeader->GetDictionary(&pDictionary));
+	  // Get the AAF Dictionary so that we can create valid AAF objects.
+	  checkResult(pHeader->GetDictionary(&pDictionary));
  		
-  //Make the first mob
+	  //Make the first mob
 	  long	test;
 	  aafRational_t	audioRate = { 44100, 1 };
 
 	  // Create a Mob
-	  checkResult(pDictionary->CreateInstance(&AUID_AAFMob,
-							  IID_IAAFMob, 
-							  (IUnknown **)&pMob));
+	  checkResult(pDictionary->CreateInstance(AUID_AAFMob,
+											  IID_IAAFMob, 
+											  (IUnknown **)&pMob));
 
-		checkResult(CoCreateGuid((GUID *)&newUID));
-	  checkResult(pMob->SetMobID(&newUID));
+	  checkResult(CoCreateGuid((GUID *)&newUID));
+	  checkResult(pMob->SetMobID(newUID));
 	  checkResult(pMob->SetName(L"MOBTest"));
 	  
+	  // Set a modification time stamp: 7 December, 1941 at 05:13:12.01
+	  aafTimeStamp_t ts;
+	  ts.date.year = 1941;
+	  ts.date.month = 12;
+	  ts.date.day = 7;
+	  ts.time.hour = 5;
+	  ts.time.minute = 31;
+	  ts.time.second = 12;
+	  ts.time.fraction = 1;
+	  checkResult(pMob->SetCreateTime(ts));
+	  checkResult(pMob->SetModTime(ts));
+
 	  // Add some slots
 	  for(test = 0; test < 5; test++)
-	  {
- 		  checkResult(pDictionary->CreateInstance(&AUID_AAFSourceClip,
-							     IID_IAAFSourceClip, 
-							     (IUnknown **)&sclp));		
+		{
+ 		  checkResult(pDictionary->CreateInstance(AUID_AAFSourceClip,
+												  IID_IAAFSourceClip, 
+												  (IUnknown **)&sclp));		
 
 		  checkResult(sclp->QueryInterface (IID_IAAFSegment, (void **)&seg));
 
-		  checkResult(pMob->AppendNewSlot (seg, test+1, slotNames[test], &newSlot));
+		  aafRational_t editRate = { 0, 1};
+		  checkResult(pMob->AppendNewTimelineSlot (editRate,
+												   seg,
+												   test+1,
+												   slotNames[test],
+												   0,
+												   &newSlot));
 
 		  newSlot->Release();
-      newSlot = NULL;
+		  newSlot = NULL;
 
 		  seg->Release();
-      seg = NULL;
+		  seg = NULL;
 
-      sclp->Release();
-      sclp = NULL;
-	  }
+		  sclp->Release();
+		  sclp = NULL;
+		}
 
-    // Add the mob to the file.
-	  checkResult(pHeader->AppendMob(pMob));
-  }
+	  // Add the mob to the file.
+	  checkResult(pHeader->AddMob(pMob));
+	}
   catch (HRESULT& rResult)
-  {
-    hr = rResult;
-  }
+	{
+	  hr = rResult;
+	}
 
 
   // Cleanup and return
@@ -151,61 +187,61 @@ static HRESULT CreateAAFFile(aafWChar * pFileName)
     pHeader->Release();
       
   if (pFile)
-  {  // Close file
-    if (bFileOpen)
-	  {
+	{  // Close file
+	  if (bFileOpen)
+		{
 		  pFile->Save();
 		  pFile->Close();
-	  }
-    pFile->Release();
-  }
+		}
+	  pFile->Release();
+	}
 
   return hr;
 }
 
 static HRESULT ReadAAFFile(aafWChar * pFileName)
 {
-	IAAFFile *					pFile = NULL;
+  IAAFFile *					pFile = NULL;
   bool bFileOpen = false;
-	IAAFHeader *				pHeader = NULL;
-	IEnumAAFMobs *mobIter = NULL;
-	IAAFMob			*aMob = NULL;
-	IEnumAAFMobSlots	*slotIter = NULL;
-	IAAFMobSlot		*slot = NULL;
-	aafProductIdentification_t	ProductInfo;
-	aafNumSlots_t	numMobs, n, s;
-	HRESULT						hr = S_OK;
+  IAAFHeader *				pHeader = NULL;
+  IEnumAAFMobs *mobIter = NULL;
+  IAAFMob			*aMob = NULL;
+  IEnumAAFMobSlots	*slotIter = NULL;
+  IAAFMobSlot		*slot = NULL;
+  aafProductIdentification_t	ProductInfo;
+  aafNumSlots_t	numMobs, n, s;
+  HRESULT						hr = S_OK;
 
-	ProductInfo.companyName = L"AAF Developers Desk";
-	ProductInfo.productName = L"AAFMob Test";
-	ProductInfo.productVersion.major = 1;
-	ProductInfo.productVersion.minor = 0;
-	ProductInfo.productVersion.tertiary = 0;
-	ProductInfo.productVersion.patchLevel = 0;
-	ProductInfo.productVersion.type = kVersionUnknown;
-	ProductInfo.productVersionString = NULL;
-	ProductInfo.platform = NULL;
+  ProductInfo.companyName = L"AAF Developers Desk";
+  ProductInfo.productName = L"AAFMob Test";
+  ProductInfo.productVersion.major = 1;
+  ProductInfo.productVersion.minor = 0;
+  ProductInfo.productVersion.tertiary = 0;
+  ProductInfo.productVersion.patchLevel = 0;
+  ProductInfo.productVersion.type = kVersionUnknown;
+  ProductInfo.productVersionString = NULL;
+  ProductInfo.platform = NULL;
 
   try
-  {
-    // Open the file
-		checkResult(AAFFileOpenExistingRead(pFileName, 0, &pFile));
-		bFileOpen = true;
+	{
+	  // Open the file
+	  checkResult(AAFFileOpenExistingRead(pFileName, 0, &pFile));
+	  bFileOpen = true;
 
-    // We can't really do anthing in AAF without the header.
-  	checkResult(pFile->GetHeader(&pHeader));
+	  // We can't really do anthing in AAF without the header.
+	  checkResult(pFile->GetHeader(&pHeader));
 
 
-	  checkResult(pHeader->GetNumMobs(kAllMob, &numMobs));
+	  checkResult(pHeader->CountMobs(kAllMob, &numMobs));
 	  checkExpression(1 == numMobs, AAFRESULT_TEST_FAILED);
 
 
 	  aafSearchCrit_t		criteria;
 	  criteria.searchTag = kNoSearch;
-    checkResult(pHeader->EnumAAFAllMobs (&criteria, &mobIter));
+	  checkResult(pHeader->GetMobs (&criteria, &mobIter));
 
-    for(n = 0; n < numMobs; n++)
-	  {
+	  for(n = 0; n < numMobs; n++)
+		{
 		  aafWChar		name[500], slotName[500];
 		  aafNumSlots_t	numSlots;
 		  aafUID_t		mobID;
@@ -215,32 +251,52 @@ static HRESULT ReadAAFFile(aafWChar * pFileName)
 		  checkResult(aMob->GetName (name, sizeof(name)));
 		  checkResult(aMob->GetMobID (&mobID));
 
-		  checkResult(aMob->GetNumSlots (&numSlots));
+		  // Check the modification time stamp: should be 7 December, 1941 at 05:13:12.01
+		  aafTimeStamp_t tsc = { 0 };
+		  checkResult(aMob->GetCreateTime(&tsc));
+		  checkExpression (1941 == tsc.date.year, AAFRESULT_TEST_FAILED);
+		  checkExpression (12 == tsc.date.month, AAFRESULT_TEST_FAILED);
+		  checkExpression (7 == tsc.date.day, AAFRESULT_TEST_FAILED);
+		  checkExpression (5 == tsc.time.hour, AAFRESULT_TEST_FAILED);
+		  checkExpression (31 == tsc.time.minute, AAFRESULT_TEST_FAILED);
+		  checkExpression (12 == tsc.time.second, AAFRESULT_TEST_FAILED);
+		  checkExpression (1 == tsc.time.fraction, AAFRESULT_TEST_FAILED);
+		  aafTimeStamp_t ts = { 0 };
+		  checkResult(aMob->GetModTime(&ts));
+		  checkExpression (1941 == ts.date.year, AAFRESULT_TEST_FAILED);
+		  checkExpression (12 == ts.date.month, AAFRESULT_TEST_FAILED);
+		  checkExpression (7 == ts.date.day, AAFRESULT_TEST_FAILED);
+		  checkExpression (5 == ts.time.hour, AAFRESULT_TEST_FAILED);
+		  checkExpression (31 == ts.time.minute, AAFRESULT_TEST_FAILED);
+		  checkExpression (12 == ts.time.second, AAFRESULT_TEST_FAILED);
+		  checkExpression (1 == ts.time.fraction, AAFRESULT_TEST_FAILED);
+
+		  checkResult(aMob->CountSlots (&numSlots));
 		  checkExpression(5 == numSlots, AAFRESULT_TEST_FAILED);
 
-			checkResult(aMob->EnumAAFAllMobSlots(&slotIter));
+		  checkResult(aMob->GetSlots(&slotIter));
 
-			for(s = 0; s < numSlots; s++)
+		  for(s = 0; s < numSlots; s++)
 			{
-				checkResult(slotIter->NextOne (&slot));
-				checkResult(slot->GetName (slotName, sizeof(slotName)));
-				checkResult(slot->GetSlotID(&trackID));
-				checkExpression (wcscmp(slotName, slotNames[s]) == 0, AAFRESULT_TEST_FAILED);
+			  checkResult(slotIter->NextOne (&slot));
+			  checkResult(slot->GetName (slotName, sizeof(slotName)));
+			  checkResult(slot->GetSlotID(&trackID));
+			  checkExpression (wcscmp(slotName, slotNames[s]) == 0, AAFRESULT_TEST_FAILED);
 
-				slot->Release();
-				slot = NULL;
+			  slot->Release();
+			  slot = NULL;
 			}
 
-			aMob->Release();
-			aMob = NULL;
+		  aMob->Release();
+		  aMob = NULL;
 		}
 	}
-	catch (HRESULT& rResult)
+  catch (HRESULT& rResult)
 	{
-    hr = rResult;
+	  hr = rResult;
 	}
 
-	// Cleanup object references
+  // Cleanup object references
   if (slot)
     slot->Release();
 
@@ -257,11 +313,11 @@ static HRESULT ReadAAFFile(aafWChar * pFileName)
     pHeader->Release();
       
   if (pFile)
-  {  // Close file
-    if (bFileOpen)
-      pFile->Close();
-     pFile->Release();
-  }
+	{  // Close file
+	  if (bFileOpen)
+		pFile->Close();
+	  pFile->Release();
+	}
 
   return hr;
 }
@@ -295,9 +351,6 @@ extern "C" HRESULT CAAFMob_test()
 		cout << "     GetNameBufLen" << endl; 
 		cout << "     AppendSlot" << endl; 
 		cout << "     EnumAAFAllMobSlots - needs unit test" << endl; 
-		cout << "     GetModTime - needs unit test" << endl; 
-		cout << "     SetModTime" << endl; 
-		cout << "     GetCreateTime - needs unit test" << endl; 
 		cout << "     AppendComment - needs unit test" << endl; 
 		cout << "     GetNumComments" << endl; 
 		cout << "     EnumAAFAllMobComments - needs unit test" << endl; 
