caecdbc8141c60bd58c13d9bbd2bdc165303ac36
tbingham <tbingham>
2001-04-17 09:51:02 +0000

    Clean up autoduck tags.

--- ref-impl/src/OM/OMFile.h
+++ ref-impl/src/OM/OMFile.h
@@ -1,4 +1,31 @@
-// @doc OMINTERNAL
+/***********************************************************************
+*
+*              Copyright (c) 1998-1999 Avid Technology, Inc.
+*
+* Permission to use, copy and modify this software and accompanying
+* documentation, and to distribute and sublicense application software
+* incorporating this software for any purpose is hereby granted,
+* provided that (i) the above copyright notice and this permission
+* notice appear in all copies of the software and related documentation,
+* and (ii) the name Avid Technology, Inc. may not be used in any
+* advertising or publicity relating to the software without the specific,
+* prior written permission of Avid Technology, Inc.
+*
+* THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+* WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+* IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+* SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+* OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+* OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+* ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+* RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+* ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+* LIABILITY.
+*
+************************************************************************/
+
+// @doc OMEXTERNAL
 #ifndef OMFILE_H
 #define OMFILE_H
 
@@ -125,18 +152,15 @@ public:
   OMStoredObject* rootStoredObject(void);
 
     // @cmember Retrieve the <c OMClassFactory> from this <c OMFile>.
-    //   @this const
   const OMClassFactory* classFactory(void) const;
 
     // @cmember Retrieve the <c OMObjectDirectory> from this <c OMFile>.
   OMObjectDirectory* objectDirectory(void);
 
     // @cmember The byte order of this <c OMFile>.
-    //   @this const
   OMByteOrder byteOrder(void) const;
 
     // @cmember The loading mode (eager or lazy) of this <c OMFile>.
-    //   @this const
   OMLoadMode loadMode(void) const;
 
   // OMStorable overrides.
