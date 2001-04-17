cea863a0f38043107c69f544a73bbbb2bb6d1059
tbingham <tbingham>
2001-04-17 03:24:46 +0000

    Change AUIDs to object pointers in API calls; general API cleanup.

--- ref-impl/include/AAFDataDefs.h
+++ ref-impl/include/AAFDataDefs.h
@@ -1,11 +1,31 @@
-/***********************************************\
-*												*
-* Advanced Authoring Format						*
-*												*
-* Copyright (c) 1998-1999 Avid Technology, Inc. *
-* Copyright (c) 1998-1999 Microsoft Corporation *
-*												*
-\***********************************************/
+#ifndef __AAFDataDefs_h__
+#define __AAFDataDefs_h__
+/***********************************************************************
+ *
+ *              Copyright (c) 1998-1999 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and accompanying 
+ * documentation, and to distribute and sublicense application software
+ * incorporating this software for any purpose is hereby granted, 
+ * provided that (i) the above copyright notice and this permission
+ * notice appear in all copies of the software and related documentation,
+ * and (ii) the name Avid Technology, Inc. may not be used in any
+ * advertising or publicity relating to the software without the specific,
+ * prior written permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+ * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+ * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+ * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+ * LIABILITY.
+ *
+ ************************************************************************/
 
 // Following are the UIDs of the well-known data definition objects in AAF.
 //
@@ -29,3 +49,7 @@ const aafUID_t DDEF_PictureWithMatte = { 0x05CBA732, 0x1DAA, 0x11d3, { 0x80, 0xA
 const aafUID_t DDEF_Sound = { 0x78E1EBE1, 0x6CEF, 0x11D2, { 0x80, 0x7D, 0x00, 0x60, 0x08, 0x14, 0x3E, 0x6F } };
 const aafUID_t DDEF_Timecode = { 0x7F275E81, 0x77E5, 0x11D2, { 0x80, 0x7F, 0x00, 0x60, 0x08, 0x14, 0x3E, 0x6F } };
 const aafUID_t DDEF_Edgecode = { 0xd2bb2af0, 0xd234, 0x11d2, { 0x89, 0xee, 0x0, 0x60, 0x97, 0x11, 0x62, 0x12 } };
+const aafUID_t DDEF_Unknown = { 0x851419d0, 0x2e4f, 0x11d3, { 0x8a, 0x5b, 0x0, 0x50, 0x4, 0xe, 0xf7, 0xd2 } };
+
+
+#endif // !  __AAFDataDefs_h__
---
+++ ref-impl/src/impl/ImplAAFDataDef.h
@@ -0,0 +1,136 @@
+//@doc
+//@class    AAFDataDef | Implementation class for AAFDataDef
+#ifndef __ImplAAFDataDef_h__
+#define __ImplAAFDataDef_h__
+
+
+/***********************************************************************
+ *
+ *              Copyright (c) 1998-1999 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and accompanying 
+ * documentation, and to distribute and sublicense application software
+ * incorporating this software for any purpose is hereby granted, 
+ * provided that (i) the above copyright notice and this permission
+ * notice appear in all copies of the software and related documentation,
+ * and (ii) the name Avid Technology, Inc. may not be used in any
+ * advertising or publicity relating to the software without the specific,
+ * prior written permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+ * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+ * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+ * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+ * LIABILITY.
+ *
+ ************************************************************************/
+
+
+#ifndef __ImplAAFDefObject_h__
+#include "ImplAAFDefObject.h"
+#endif
+
+class ImplAAFDataDef : public ImplAAFDefObject
+{
+public:
+  //
+  // Constructor/destructor
+  //
+  //********
+  ImplAAFDataDef ();
+
+protected:
+  virtual ~ImplAAFDataDef ();
+
+public:
+  //****************
+  // IsPictureKind()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    IsPictureKind
+        // @parm [retval,out] pointer to the return value
+        (aafBool *  bIsPictureKind);
+
+  //****************
+  // IsMatteKind()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    IsMatteKind
+        // @parm [retval,out] pointer to the return value
+        (aafBool *  bIsMatteKind);
+
+  //****************
+  // IsPictureWithMatteKind()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    IsPictureWithMatteKind
+        // @parm [retval,out] pointer to the return value
+        (aafBool *  bIsPictureWithMatteKind);
+
+  //****************
+  // IsSoundKind()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    IsSoundKind
+        // @parm [retval,out] pointer to the return value
+        (aafBool *  bIsSoundKind);
+
+  //****************
+  // DoesDataDefConvertTo()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    DoesDataDefConvertTo
+        (// @parm [in,ref] def to compare against
+         ImplAAFDataDef * pDataDef,
+
+         // @parm [retval,out] pointer to result
+         aafBool *  bDoesConvertTo);
+		   
+  //****************
+  // IsDataDefOf()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    IsDataDefOf
+        (// @parm [in,ref] data def to compare against
+         ImplAAFDataDef * pDataDef,
+
+         // @parm [retval,out] pointer to result
+         aafBool *  bIsDataDefOf);
+
+  //****************
+  // DoesDataDefConvertFrom()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    DoesDataDefConvertFrom
+        (// @parm [in,ref] data def to compare against
+         ImplAAFDataDef * pDataDef,
+
+         // @parm [retval,out] pointer to result
+         aafBool *  bDoesConvertFrom);
+
+private:
+  // *NOT* reference counted!
+  ImplAAFDictionary * _pCachedDict;
+
+  ImplAAFDictionary * GetDict();
+};
+
+//
+// smart pointer
+//
+
+#ifndef __ImplAAFSmartPointer_h__
+// caution! includes assert.h
+#include "ImplAAFSmartPointer.h"
+#endif
+
+typedef ImplAAFSmartPointer<ImplAAFDataDef> ImplAAFDataDefSP;
+
+
+#endif // ! __ImplAAFDataDef_h__
