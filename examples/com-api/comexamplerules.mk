a5c5c0f0cd61feb0990113115fe1b5fe112b10bf
tbingham <tbingham>
2001-04-17 11:48:26 +0000

    Irix CC makefile

--- examples/com-api/comexamplerules.mk
+++ examples/com-api/comexamplerules.mk
@@ -1,39 +1,24 @@
+
 PACKAGE = Examples
 
-AAFBASE ?= ../../..
+include $(AAFBASE)/build/defs.mk
 
-
-# Common definitions
-include $(AAFBASE)/build/common.mk
-
-
-# Include directories
 INCLUDES = -I../../../ref-impl/include \
 	   -I../../../ref-impl/include/ref-api \
 	   -I../../../ref-impl/include/com-api \
 	   -I../../../Utilities/Include
 
-
 .PHONY : all
-all : $(BINDIR)/$(EXAMPLE)$(EXE)
+all : $(BINDIR)/$(EXAMPLE)$(EXE_SUFFIX)
 
-
-$(BINDIR)/$(EXAMPLE)$(EXE) : $(BINDIR) $(OBJDIR) $(CXXOBJS)
-	$(LD) $(CXXOBJS) $(RPATH_OPT) \
-	-L$(LIBDIR) -laaflib -laafiid -o $@
-
+$(BINDIR)/$(EXAMPLE)$(EXE_SUFFIX) : $(BINDIR) $(OBJDIR) $(CXXOBJS)
+	$(CC) -n32 -Wl,-wall $(CXXOBJS) $(CC2LDOPT) \
+	$(LD_DYN_LIB_PATH_FLAG) $(CC2LDOPT) $(LIBDIR) \
+	-L$(LIBDIR) $(CC2LDOPT) -laaflib -laafiid -o $@ -lC -laafutils
 
 .PHONY : clean
 clean :
-	@$(RM) $(CXXOBJS)
-	@$(RM) $(DEPS)
-	@$(RM) $(BINDIR)/$(EXAMPLE)$(EXE)
-	@$(RMDIR) $(OBJDIR)/ii_files
-
-
-.PHONY : realclean
-realclean : clean
-
+	@rm -f $(CXXOBJS)
+	@rm -f $(BINDIR)/$(EXAMPLE)$(EXE_SUFFIX)
 
 include $(AAFBASE)/build/rules.mk
-
