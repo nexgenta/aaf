f2fab99454934b9d58aedafa9e7eac8ea75093a6
tbingham <tbingham>
2001-04-17 06:28:19 +0000

    Add Initialize method.

--- ref-impl/src/impl/ImplAAFTimelineMobSlot.cpp
+++ ref-impl/src/impl/ImplAAFTimelineMobSlot.cpp
@@ -25,7 +25,7 @@
 #include <assert.h>
 #include <AAFResult.h>
 #include "aafCvt.h" 
-
+#include "AAFUtils.h"
 
 ImplAAFTimelineMobSlot::ImplAAFTimelineMobSlot ():
 	_editRate(	PID_TIMELINEMOBSLOT_EDITRATE,	"EditRate"),
@@ -40,6 +40,13 @@ ImplAAFTimelineMobSlot::ImplAAFTimelineMobSlot ():
 
 ImplAAFTimelineMobSlot::~ImplAAFTimelineMobSlot ()
 {}
+
+
+AAFRESULT STDMETHODCALLTYPE
+ImplAAFTimelineMobSlot::Initialize ()
+{
+  return AAFRESULT_SUCCESS;
+}
 
 
 AAFRESULT STDMETHODCALLTYPE
@@ -82,6 +89,97 @@ AAFRESULT STDMETHODCALLTYPE
 	return AAFRESULT_SUCCESS;
 }
 
+AAFRESULT ImplAAFTimelineMobSlot::FindSegment(aafPosition_t offset,
+										  ImplAAFSegment **segment,
+										  aafRational_t *srcRate,
+										  aafPosition_t *diffPos)
+{
+	aafBool					foundClip = AAFFalse;
+	ImplAAFMobSlot			*tmpTrack = NULL;
+	aafPosition_t begPos = CvtInt32toPosition(0, begPos);
+	aafRational_t tmpSrcRate;
+	aafPosition_t origin = 0;
+	aafSlotID_t tmpTrackID;
+	ImplAAFSegment	*tmpSegment = NULL;
+
+	if(diffPos == NULL || segment == NULL || srcRate == NULL)
+		return(AAFRESULT_NULL_PARAM);
+
+	XPROTECT()
+	{
+		/* Initialize return parameters */
+		*segment = NULL;
+				
+		CHECK(GetEditRate(&tmpSrcRate));
+		CHECK(GetOrigin(&origin));
+		CHECK(GetSlotID(&tmpTrackID));
+		CHECK(GetSegment(&tmpSegment));
+		*srcRate = tmpSrcRate;
+		
+		/* Normalize the requested position on the track by adding
+		* the StartPosition (1.x) or Origin (2.x) to it.  The origin
+		* was acquired above with omfiTrackGetInfo().
+		* The StartPosition/Origin will usually be 0.  It may be 
+		* negative, if more data before the original "origin" was 
+		* digitized later.
+		*/
+		if (!IsInt64Positive(origin))
+			MultInt32byInt64(-1, origin, &origin);
+		AddInt64toInt64(origin, &offset);
+		
+		CHECK(tmpSegment->FindSubSegment(offset, &begPos, segment, &foundClip));
+		if(!foundClip)
+			RAISE(AAFRESULT_TRAVERSAL_NOT_POSS);
+
+		/* Calculate diffPos - difference between requested offset and
+		* the beginning of clip that contains it. 
+		*/
+		(*diffPos) = offset;
+		CHECK(SubInt64fromInt64(begPos, diffPos));
+		
+	} /* XPROTECT */
+	XEXCEPT
+	{
+	}
+	XEND;
+	return(AAFRESULT_SUCCESS);
+}
+
+AAFRESULT ImplAAFTimelineMobSlot::ConvertToEditRate(aafPosition_t srcPos,
+										aafRational_t destRate,
+										aafPosition_t *convertPos)
+{
+	aafRational_t	srcRate;
+
+	XPROTECT()
+	{
+		CHECK(GetEditRate(&srcRate));
+		CHECK(AAFConvertEditRate(srcRate, srcPos,
+						destRate, kRoundFloor, convertPos));
+	}
+	XEXCEPT
+	XEND;
+
+	return AAFRESULT_SUCCESS;
+}
+
+AAFRESULT ImplAAFTimelineMobSlot::ConvertToMyRate(aafPosition_t srcPos,
+										  ImplAAFMobSlot *srcSlot,
+										aafPosition_t *convertPos)
+{
+	aafRational_t	destRate;
+	
+	XPROTECT()
+	{
+		CHECK(GetEditRate(&destRate));
+		CHECK(srcSlot->ConvertToEditRate(srcPos, destRate, convertPos));
+	}
+	XEXCEPT
+	XEND;
+
+	return AAFRESULT_SUCCESS;
+}
+
 extern "C" const aafClassID_t CLSID_AAFTimelineMobSlot;
 
 OMDEFINE_STORABLE(ImplAAFTimelineMobSlot, CLSID_AAFTimelineMobSlot);
@@ -94,7 +192,7 @@ ImplAAFTimelineMobSlot::GetObjectClass(aafUID_t * pClass)
 	{
 	  return AAFRESULT_NULL_PARAM;
 	}
-  memcpy (pClass, &CLSID_AAFTimelineMobSlot, sizeof aafClassID_t);
+  memcpy (pClass, &CLSID_AAFTimelineMobSlot, sizeof (aafClassID_t));
   return AAFRESULT_SUCCESS;
 }
 
