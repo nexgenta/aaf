6eddb3a5972753f836c148673795463518753941
tbingham <tbingham>
2001-04-17 07:30:49 +0000

    Adding stub implementation files for private subclasses of AAFPropertyValue to support object reference, both singletons and collections (sets and vectors).

--- ref-impl/src/impl/ImplAAFRefContainerValue.cpp
+++ ref-impl/src/impl/ImplAAFRefContainerValue.cpp
@@ -1,24 +1,29 @@
-//=---------------------------------------------------------------------=
-//
-// The contents of this file are subject to the AAF SDK Public
-// Source License Agreement (the "License"); You may not use this file
-// except in compliance with the License.  The License is available in
-// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
-// Association or its successor.
-// 
-// Software distributed under the License is distributed on an "AS IS"
-// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
-// the License for the specific language governing rights and limitations
-// under the License.
-// 
-// The Original Code of this file is Copyright 1998-2001, Licensor of the
-// AAF Association.
-// 
-// The Initial Developer of the Original Code of this file and the
-// Licensor of the AAF Association is Avid Technology.
-// All rights reserved.
-//
-//=---------------------------------------------------------------------=
+/***********************************************************************
+ *
+ *              Copyright (c) 2000 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and accompanying 
+ * documentation, and to distribute and sublicense application software
+ * incorporating this software for any purpose is hereby granted, 
+ * provided that (i) the above copyright notice and this permission
+ * notice appear in all copies of the software and related documentation,
+ * and (ii) the name Avid Technology, Inc. may not be used in any
+ * advertising or publicity relating to the software without the specific,
+ *  prior written permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+ * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+ * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+ * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+ * LIABILITY.
+ *
+ ************************************************************************/
 
 #include "ImplAAFRefContainerValue.h"
 
@@ -26,88 +31,92 @@
 #include "ImplAAFRoot.h"
 #endif
 
-#ifndef __ImplAAFStorable_h__
-#include "ImplAAFStorable.h"
-#endif
-
 #ifndef __ImplAAFTypeDefObjectRef_h__
 #include "ImplAAFTypeDefObjectRef.h"
 #endif
 
-#ifndef __ImplEnumAAFPropertyValues_h__
-#include "ImplEnumAAFPropertyValues.h"
-#endif
-
-#ifndef __ImplAAFRefValue_h__
-#include "ImplAAFRefValue.h"
-#endif
-
-#ifndef __ImplEnumAAFStorablePropVals_h__
-#include "ImplEnumAAFStorablePropVals.h"
-#endif
-
 
 #include "OMProperty.h"
-#include "OMContainerProperty.h" // TBD: include header for base class for singleton references.
-#include "OMReferenceContainer.h"
-#include "OMRefSetProperty.h"
-#include "OMRefVectorProperty.h"
+//#include "OMRefProperty.h" // TBD: include header for base class for singleton references.
 #include "OMPropertyDefinition.h"
-#include "OMObject.h"
 
 #include <assert.h>
 #include <string.h>
 
-extern "C" const aafClassID_t CLSID_EnumAAFStorablePropVals;
 
-
-ImplAAFRefContainerValue::ImplAAFRefContainerValue ()
+ImplAAFRefContainerValue::ImplAAFRefContainerValue () :
+  _containerProperty(NULL),
+  _propertyContainer(NULL)
 {}
 
 
 ImplAAFRefContainerValue::~ImplAAFRefContainerValue ()
 {
+  _containerProperty = NULL;
+  
+#if defined(REFERENCE_PROPERTY_CONTAINER)
+  if (_propertyContainer)
+  {
+    _propertyContainer->ReleaseReference();
+    _propertyContainer = NULL;
+  }
+#endif // #if defined(REFERENCE_PROPERTY_CONTAINER)
 }
 
 
 
 
 
-AAFRESULT ImplAAFRefContainerValue::Initialize (
-	  const ImplAAFTypeDef *propertyType)
-{
-  return ImplAAFPropertyValue::Initialize(propertyType);
-}
 
 
 AAFRESULT ImplAAFRefContainerValue::Initialize (
   const ImplAAFTypeDef *containerType,    
   OMProperty *property)
 {
-  // Concrete class' Initialize method should have already
-  // validated the parameters.
-  assert (NULL != containerType && NULL != property);
+  AAFRESULT result = AAFRESULT_SUCCESS;
   
-  if (NULL == dynamic_cast<OMContainerProperty *>(property))
+  assert (!isInitialized());
+  if (isInitialized())
+    return AAFRESULT_ALREADY_INITIALIZED;
+  assert (property);
+  if (NULL == containerType || NULL == property)
+    return AAFRESULT_NULL_PARAM;
+
+  // Get the type definition. This must be a stream type.
+  assert (property->definition());
+  if (NULL == property->definition())
     return AAFRESULT_INVALID_PARAM;
-    
-  AAFRESULT result = ImplAAFPropertyValue::Initialize(containerType, property);
-  return result;
-}
-
+  const OMType *type = property->definition()->type();
+  assert (type);
   
-// Retrieve the property as an OMReferenceContainer.
-OMReferenceContainer* ImplAAFRefContainerValue::referenceContainer(void) const
-{
-  assert (isInitialized());
-  OMProperty* p = property();
-  OMContainerProperty* cp = dynamic_cast<OMContainerProperty*>(p);
-  assert(cp != 0);
-  OMReferenceContainer* result = cp->referenceContainer();
-  assert(result != 0);
-  return result;
-}
+// TBD: The given property must be an OM reference property.  
+// Until the OMReferenceProperty is checked in just use the given
+// property as the containerProperty
+  OMProperty *containerProperty = property;
+//  OMReferenceProperty *containerProperty = dynamic_cast<OMReferenceProperty *>(property);
+//  assert (containerProperty);
+//  if (NULL == containerProperty)
+//    return AAFRESULT_INVALID_PARAM;
 
+  // Get the storable container for this property. Since this is a "direct 
+  // access" interface we need to hold onto a reference so tha the container
+  // is not deleted.
+  ImplAAFRoot * propertyContainer = dynamic_cast<ImplAAFRoot *>
+                                      (property->propertySet()->container());
+  assert (propertyContainer);
+  if (NULL == propertyContainer)
+    return AAFRESULT_INVALID_PARAM;
+  
+  // Save our initialized member data.
+  SetType(const_cast<ImplAAFTypeDef *>(containerType));
+  _containerProperty = containerProperty;
+  _propertyContainer = propertyContainer;
+#if defined(REFERENCE_PROPERTY_CONTAINER)
+  _propertyContainer->AcquireReference();
+#endif // #if defined(REFERENCE_PROPERTY_CONTAINER)
+    
+  return AAFRESULT_SUCCESS;
+}
 
 //
 // WriteTo
@@ -115,310 +124,13 @@ OMReferenceContainer* ImplAAFRefContainerValue::referenceContainer(void) const
 AAFRESULT STDMETHODCALLTYPE ImplAAFRefContainerValue::WriteTo(
   OMProperty* pOmProp)
 {
-  assert (isInitialized());
-  if (!isInitialized())
-    return AAFRESULT_NOT_INITIALIZED;
-
-  if (NULL != property())
-  {
-    // Make sure that the given property is the same one that was used to 
-    // initialize this property value. NOTE: Copying an object reference to a 
-    // different OMProperty should be handled through another interface.
-    if (pOmProp != property())
-      return AAFRESULT_INVALID_PARAM;
-  }
+  // Make sure that the given property is the same one that was used to 
+  // initialize this property value. NOTE: Copying an object reference to a 
+  // different OMProperty should be handled through another interface.
+  if (pOmProp != _containerProperty)
+    return AAFRESULT_INVALID_PARAM;
   
-  return AAFRESULT_SUCCESS;
+  // The first version of this class defers to the older   
+  return ImplAAFPropValData::WriteTo(pOmProp);
 //  return AAFRESULT_SUCCESS;
-}
-
-
-/* static */ ImplAAFStorable * ImplAAFRefContainerValue::GetStorableFromPropertyValue(
-  ImplAAFPropertyValue* pPropertyValue,
-  AAFRESULT & result)
-{
-  result = AAFRESULT_SUCCESS;
-  assert(NULL != pPropertyValue);
-  
-  ImplAAFStorable* storable = NULL; // initialize the returned parameter
-  
-  // Make sure that the type definition can contain a single object
-  // reference.
-  ImplAAFTypeDefSP pPropertyValueType;
-  result = pPropertyValue->GetType(&pPropertyValueType);
-  if (AAFRESULT_FAILED(result))
-    return NULL;
-  ImplAAFTypeDefObjectRef* pObjectRefType = NULL;
-  pObjectRefType = dynamic_cast<ImplAAFTypeDefObjectRef *>((ImplAAFTypeDef *)pPropertyValueType);
-  if (NULL == pObjectRefType)
-  {
-    result = AAFRESULT_INVALID_PARAM;
-    return NULL;
-  }
-  
-  // Use the type to extract the object from the input property
-  // value.
-  ImplAAFRoot *pObject = NULL;
-  result = pObjectRefType->GetObject(pPropertyValue, &pObject); // returns reference counted object!
-  if (AAFRESULT_FAILED(result))
-    return NULL;
-    
-  // the returned object is NOT reference counted.
-  pObject->ReleaseReference();
-
-  storable = ImplAAFRefValue::ConvertRootToOMStorable(pObject);
-  assert(NULL != storable);
-  if (NULL == storable)
-    result = AAFRESULT_INVALID_OBJ; 
-
-  
-  return storable;  
-}
-
-void ImplAAFRefContainerValue::ReleaseOldObject(OMObject *object)
-{
-  assert(object && usesReferenceCounting());
-  ImplAAFStorable *pOldObject = ImplAAFRefValue::ConvertOMObjectToRoot(object);
-  assert(NULL != pOldObject);
-  if (NULL == pOldObject)
-    throw AAFRESULT_INVALID_OBJ; // ???
-  pOldObject->ReleaseReference();
-}
-
-  
-// Utility to release all old OMObjects from the given container.
-AAFRESULT ImplAAFRefContainerValue::ReleaseAllObjects(OMReferenceContainer *pContainerProperty)
-{
-  assert(pContainerProperty && usesReferenceCounting());
-
-  OMReferenceContainerIterator* containerIter = pContainerProperty->createIterator();
-  if (NULL == containerIter)
-    return AAFRESULT_NOMEMORY;
-    
-  AAFRESULT result = AAFRESULT_SUCCESS;
-  while (AAFRESULT_SUCCEEDED(result) && (containerIter->before() || containerIter->valid()))
-  {
-    if (++(*containerIter))
-    {
-      OMObject *object = containerIter->currentObject();
-      ImplAAFStorable *obj = dynamic_cast<ImplAAFStorable*>(object);
-      assert(NULL != obj);
-      if (NULL == obj)
-      {
-        result = AAFRESULT_INVALID_OBJ;
-      }
-      else
-      {
-        obj->ReleaseReference();
-      }
-    }
-  }
-
-  delete containerIter;
-  containerIter = NULL;
-  
-  return AAFRESULT_SUCCESS;
-}
-
-
-// Insert the given object into this contain property.
-AAFRESULT STDMETHODCALLTYPE ImplAAFRefContainerValue::InsertObject(
-  ImplAAFStorable* pObject)
-{
-  AAFRESULT result = AAFRESULT_SUCCESS;
-  if (NULL == pObject)
-    return AAFRESULT_NULL_PARAM;
-		
-	result = ValidateNewObject(pObject);
-	if (AAFRESULT_FAILED(result))
-		return result;
-
-  assert (isInitialized());
-  if (!isInitialized())
-    return AAFRESULT_NOT_INITIALIZED;
- 
-  // Hand off to the OMReferenceContainer
-  OMReferenceContainer* pReferenceContainer = referenceContainer();
-
-  // Object refernce containers should only contain a single reference
-  // to an object.
-  if (!pReferenceContainer->containsObject(pObject))
-  {
-    pReferenceContainer->insertObject(pObject);
-    if (usesReferenceCounting())
-    {
-      pObject->AcquireReference();
-    }
-    
-  }
-    
-  return result;
-}
-
-
-// Is the given object in the container property.
-AAFRESULT STDMETHODCALLTYPE ImplAAFRefContainerValue::ContainsObject(
-  ImplAAFStorable* pObject,
-  aafBoolean_t* pResult)
-{
-  AAFRESULT result = AAFRESULT_SUCCESS;
-  if (NULL == pObject || NULL == pResult)
-    return AAFRESULT_NULL_PARAM;
-  
-  *pResult = kAAFFalse;  
-
-  assert (isInitialized());
-  if (!isInitialized())
-    return AAFRESULT_NOT_INITIALIZED;
- 
-  // Hand off to the OMReferenceContainer
-  OMReferenceContainer* pReferenceContainer = referenceContainer();
-  if (pReferenceContainer->containsObject(pObject))
-  {
-    *pResult = kAAFTrue;
-  }
-    
-  return result;
-}
-
-
-// Remove the given object from the container property.
-AAFRESULT STDMETHODCALLTYPE ImplAAFRefContainerValue::RemoveObject(
-  ImplAAFStorable* pObject)
-{
-  AAFRESULT result = AAFRESULT_SUCCESS;
-  if (NULL == pObject)
-    return AAFRESULT_NULL_PARAM;
-
-  assert (isInitialized());
-  if (!isInitialized())
-    return AAFRESULT_NOT_INITIALIZED;
- 
-  // Hand off to the OMeferenceContainer
-  OMReferenceContainer* pReferenceContainer = referenceContainer();
-  
-  // Object refernce containers should only contain a single reference
-  // to an object.
-  if (!pReferenceContainer->containsObject(pObject))
-    result = AAFRESULT_OBJECT_NOT_FOUND;
-
-  pReferenceContainer->removeObject(pObject);
-  if (usesReferenceCounting())
-    pObject->ReleaseReference();
-    
-  return result;
-}
-
-
-  
-// Insert the given object into this contain property.
-AAFRESULT STDMETHODCALLTYPE ImplAAFRefContainerValue::InsertElement(
-  ImplAAFPropertyValue* pPropertyValue)
-{
-  AAFRESULT result = AAFRESULT_SUCCESS;
-  if (NULL == pPropertyValue)
-    return AAFRESULT_NULL_PARAM;
-    
-  ImplAAFStorable * pObject = GetStorableFromPropertyValue(pPropertyValue, result);
-  if (AAFRESULT_FAILED(result))
-    return result;
-  
-  result = InsertObject(pObject); 
-  return result;
-}
-
-// Is the given object in the container property.
-AAFRESULT STDMETHODCALLTYPE ImplAAFRefContainerValue::ContainsElement(
-  ImplAAFPropertyValue* pPropertyValue,
-  aafBoolean_t* pResult)
-{
-  AAFRESULT result = AAFRESULT_SUCCESS;
-  if (NULL == pPropertyValue || NULL == pResult)
-    return AAFRESULT_NULL_PARAM;
-  
-  *pResult = kAAFFalse;  
-    
-  ImplAAFStorable * pObject = GetStorableFromPropertyValue(pPropertyValue, result);
-  if (AAFRESULT_FAILED(result))
-    return result;
-  
-  result = ContainsObject(pObject, pResult); 
-  return result;
-}
-
-
-// The number of objects in the container property
-AAFRESULT STDMETHODCALLTYPE ImplAAFRefContainerValue::Count(
-  aafUInt32* pCount)
-{
-  AAFRESULT result = AAFRESULT_SUCCESS;
-  if (NULL == pCount)
-    return AAFRESULT_NULL_PARAM;
- 
-  *pCount = referenceContainer()->count(); 
-  return result;
-}
-  
-
-// Remove the given object from the container property.
-AAFRESULT STDMETHODCALLTYPE ImplAAFRefContainerValue::RemoveElement(
-  ImplAAFPropertyValue* pPropertyValue)
-{
-  AAFRESULT result = AAFRESULT_SUCCESS;
-  if (NULL == pPropertyValue)
-    return AAFRESULT_NULL_PARAM;
-    
-  ImplAAFStorable * pObject = GetStorableFromPropertyValue(pPropertyValue, result);
-  if (AAFRESULT_FAILED(result))
-    return result;
-  
-  result = RemoveObject(pObject); 
-  return result;
-}
-
-  
-// Get an enumerator for the given container property
-AAFRESULT STDMETHODCALLTYPE ImplAAFRefContainerValue::GetElements(
-  ImplEnumAAFPropertyValues** ppEnum)
-{
-  AAFRESULT result = AAFRESULT_SUCCESS;
-  if (NULL == ppEnum)
-    return AAFRESULT_NULL_PARAM;
-  
-  *ppEnum = NULL; // initialize the return parameter.
-
-  assert (isInitialized());
-  if (!isInitialized())
-    return AAFRESULT_NOT_INITIALIZED;
-
-
-  ImplAAFRoot* pRoot = CreateImpl(CLSID_EnumAAFStorablePropVals);
-  if (NULL == pRoot)
-    return AAFRESULT_NOMEMORY;
-    
-  ImplEnumAAFStorablePropVals* pNewEnum = dynamic_cast<ImplEnumAAFStorablePropVals*>(pRoot);
-  if (NULL != pNewEnum)
-  {
-    OMReferenceContainerIterator* newIterator = referenceContainer()->createIterator();
-    if (NULL != newIterator)
-    {
-      result = pNewEnum->Initialize(this, newIterator);
-      if (AAFRESULT_SUCCEEDED(result))
-      {
-        *ppEnum = pNewEnum;
-        pNewEnum->AcquireReference();
-      }
-    }
-    else
-    {
-      // _containerIterator->copy() failed...
-      result = AAFRESULT_NOMEMORY;
-    } 
-  }
-
-  // This will free the newly created object if the initialzation has failed.
-  pRoot->ReleaseReference();
-
-
-  return result;
 }
--- ref-impl/src/impl/ImplAAFStrongRefArrayValue.cpp
+++ ref-impl/src/impl/ImplAAFStrongRefArrayValue.cpp
@@ -1,35 +1,34 @@
-//=---------------------------------------------------------------------=
-//
-// The contents of this file are subject to the AAF SDK Public
-// Source License Agreement (the "License"); You may not use this file
-// except in compliance with the License.  The License is available in
-// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
-// Association or its successor.
-// 
-// Software distributed under the License is distributed on an "AS IS"
-// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
-// the License for the specific language governing rights and limitations
-// under the License.
-// 
-// The Original Code of this file is Copyright 1998-2001, Licensor of the
-// AAF Association.
-// 
-// The Initial Developer of the Original Code of this file and the
-// Licensor of the AAF Association is Avid Technology.
-// All rights reserved.
-//
-//=---------------------------------------------------------------------=
+/***********************************************************************
+ *
+ *              Copyright (c) 2000 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and accompanying 
+ * documentation, and to distribute and sublicense application software
+ * incorporating this software for any purpose is hereby granted, 
+ * provided that (i) the above copyright notice and this permission
+ * notice appear in all copies of the software and related documentation,
+ * and (ii) the name Avid Technology, Inc. may not be used in any
+ * advertising or publicity relating to the software without the specific,
+ *  prior written permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+ * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+ * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+ * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+ * LIABILITY.
+ *
+ ************************************************************************/
 
 #include "ImplAAFStrongRefArrayValue.h"
 
 #ifndef __ImplAAFRoot_h__
 #include "ImplAAFRoot.h"
-#endif
-
-// TBD: Note the following include for ImplAAFClassDef.h really belongs
-// in ImplAAFTypeDefStrongObjRef.h!
-#ifndef __ImplAAFClassDef_h__
-#include "ImplAAFClassDef.h"
 #endif
 
 #ifndef __ImplAAFTypeDefStrongObjRef_h__
@@ -60,23 +59,6 @@ ImplAAFStrongRefArrayValue::~ImplAAFStrongRefArrayValue ()
 
 
 
-AAFRESULT ImplAAFStrongRefArrayValue::Initialize (
-	  const ImplAAFTypeDefArray *containerType,
-	  bool fixed)
-{
-  AAFRESULT result = AAFRESULT_SUCCESS;
-
-  result = ImplAAFRefArrayValue::Initialize(containerType, fixed);
-  
-  if (AAFRESULT_SUCCEEDED(result))
-  {
-    // This instance is now fully initialized.
-    setInitialized();
-  }
-  
-  return result;
-}
-
 
 AAFRESULT ImplAAFStrongRefArrayValue::Initialize (
   const ImplAAFTypeDefArray *containerType,    
@@ -104,61 +86,3 @@ AAFRESULT STDMETHODCALLTYPE ImplAAFStrongRefArrayValue::WriteTo(
 {
   return (ImplAAFRefArrayValue::WriteTo(pOmProp));
 }
-
-
-
-
-  
-//
-// ImplAAFRefContainer overrides
-//
-
-// Return the type of object references in the container.
-ImplAAFTypeDefObjectRef * ImplAAFStrongRefArrayValue::GetElementType(void) const // the result is NOT reference counted.
-{
-  ImplAAFTypeDefObjectRef * pContainerElementType = NULL;
-  ImplAAFTypeDefArray *pContainerType = NULL;
-  AAFRESULT result = AAFRESULT_SUCCESS;
-  ImplAAFTypeDefSP pType, pElementType;
-  
-  result = GetType(&pType);
-  assert(AAFRESULT_SUCCEEDED(result));
-  if (AAFRESULT_SUCCEEDED(result))
-  {
-    pContainerType = dynamic_cast<ImplAAFTypeDefArray *>((ImplAAFTypeDef *)pType); // extract obj from smartptr
-    assert(NULL != pContainerType);
-    if (NULL != pContainerType)
-    {
-      result = pContainerType->GetType(&pElementType);
-      assert(AAFRESULT_SUCCEEDED(result));
-      if (AAFRESULT_SUCCEEDED(result))
-      {
-        pContainerElementType = dynamic_cast<ImplAAFTypeDefStrongObjRef *>((ImplAAFTypeDef *)pElementType); // extract obj from smartptr
-      }
-    }
-  }
-  
-  assert(pContainerElementType);
-  return pContainerElementType;
-}
-
-
-// Perform specialized validation for an object before it is added
-// to a container.
-AAFRESULT ImplAAFStrongRefArrayValue::ValidateNewObject(ImplAAFStorable *pNewObject) const
-{
-  assert(NULL != pNewObject);
-  if (pNewObject->attached())
-    return AAFRESULT_OBJECT_ALREADY_ATTACHED;
-  else
-    return AAFRESULT_SUCCESS;
-}
-
-
-// Perform any specialized cleanup of any object after it has been removed
-// from the container.
-bool ImplAAFStrongRefArrayValue::usesReferenceCounting(void) const
-{
-  return true;
-}
-  
