286a6d2dac013a807d03bae36d22351ff6067cf8
tbingham <tbingham>
2001-04-17 04:34:29 +0000

    Rename Seek to SetPosition, Add GetPosition and remove extra buflen parameter from Read and Write. Added corresponding calls to new data stream property.

--- ref-impl/src/impl/ImplAAFEssenceData.h
+++ ref-impl/src/impl/ImplAAFEssenceData.h
@@ -3,34 +3,18 @@
 #ifndef __ImplAAFEssenceData_h__
 #define __ImplAAFEssenceData_h__
 
+#include "OMStorable.h"
+#include "OMProperty.h"
 #include "OMDataStreamProperty.h"
 
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- *  prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
 
 class ImplAAFSourceMob;
 
@@ -136,10 +120,25 @@ public:
   virtual AAFRESULT STDMETHODCALLTYPE
     GetFileMobID
 		// @parm [out] the file mob id associated with essence
-        (aafMobID_t *  pFileMobID);
+        (aafUID_t *  pFileMobID);
+
+
+
+public:
+  // Declare this class to be storable.
+  //
+  OMDECLARE_STORABLE(ImplAAFEssenceData)
+
+  // Declare the module test method. The implementation of the will be be
+  // in /test/ImplAAFEssenceDataTest.cpp.
+  static AAFRESULT test();
+
+  // Return this objects stored object class.
+  virtual AAFRESULT STDMETHODCALLTYPE
+	GetObjectClass(aafUID_t * pClass);
 
 private:
-  OMFixedSizeProperty<aafMobID_t> _fileMobID;
+  OMFixedSizeProperty<aafUID_t> _fileMobID;
   OMDataStreamProperty _mediaData;
 };
 
