93516813ad9eceafd0c7efeb9bbe6fa4ced0a17a
tbingham <tbingham>
2001-04-17 06:46:27 +0000

    Persistense support added

--- ref-impl/src/impl/ImplAAFSourceClip.cpp
+++ ref-impl/src/impl/ImplAAFSourceClip.cpp
@@ -1,29 +1,11 @@
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- *  prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
 
 
 #ifndef __ImplAAFDataDef_h__
@@ -34,31 +16,39 @@
 #include "ImplAAFMob.h"
 #endif
 
+
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+
 #ifndef __ImplAAFSourceClip_h__
 #include "ImplAAFSourceClip.h"
 #endif
 
-#include "AAFStoredObjectIDs.h"
-#include "AAFPropertyIDs.h"
-
 #include <assert.h>
-#include "aafCvt.h" 
-#include "AAFResult.h"
-#include "AAFDefUIDs.h"
-#include "ImplAAFHeader.h"
+#include <AAFResult.h>
+
 
 ImplAAFSourceClip::ImplAAFSourceClip ():
-	_fadeInLength(		PID_SourceClip_FadeInLength,		"FadeInLength"),
-	_fadeInType(	PID_SourceClip_FadeInType,		"FadeInType"),
-	_fadeOutLength(	PID_SourceClip_FadeOutLength,		"FadeOutLength"),
-	_fadeOutType(	PID_SourceClip_FadeOutType,		"FadeOutType"),
-	_startTime(		PID_SourceClip_StartTime,		"StartTime")
+	_fadeInLen(		PID_SOURCECLIP_FADEINLEN,		"fadeInLen"),
+	_fadeInType(	PID_SOURCECLIP_FADEINTYPE,		"fadeInType"),
+	_fadeInPresent( PID_SOURCECLIP_FADEINPRESENT,	"fadeInPresent"),
+	_fadeOutLen(	PID_SOURCECLIP_FADEOUTLEN,		"fadeOutLen"),
+	_fadeOutType(	PID_SOURCECLIP_FADEOUTTYPE,		"fadeOutType"),
+	_fadeOutPresent(PID_SOURCECLIP_FADEOUTPRESENT,	"fadeOutPresent")
 {
-	_persistentProperties.put(		_fadeInLength.address());
+	_persistentProperties.put(		_fadeInLen.address());
 	_persistentProperties.put(		_fadeInType.address());
-	_persistentProperties.put(		_fadeOutLength.address());
+	_persistentProperties.put(		_fadeInPresent.address());
+	_persistentProperties.put(		_fadeOutLen.address());
 	_persistentProperties.put(		_fadeOutType.address());
-	_persistentProperties.put(		_startTime.address());
+	_persistentProperties.put(		_fadeOutPresent.address());
 
 }
 
@@ -68,284 +58,90 @@ ImplAAFSourceClip::~ImplAAFSourceClip ()
 
 
 AAFRESULT STDMETHODCALLTYPE
-    ImplAAFSourceClip::Initialize(aafUID_t*		pDatadef,
-								  aafLength_t*	pLength,
-								  aafSourceRef_t	sourceRef)
+    ImplAAFSourceClip::InitializeSourceClip (ImplAAFDataDef * /*datadef  */,
+                           aafLength_t      /*length   */,
+                           aafSourceRef_t   /*sourceRef*/)
 {
-    AAFRESULT aafError = AAFRESULT_SUCCESS;
-	if (pDatadef == NULL ||
-		pLength == NULL)
-	{
-		aafError = AAFRESULT_NULL_PARAM;
-	}
-	else
-	{
-		SetDataDef( pDatadef );
-		SetLength( pLength );
-		SetSourceID( sourceRef.sourceID );
-		SetSourceMobSlotID( sourceRef.sourceSlotID );
-		_startTime = sourceRef.startTime;
-
-		_fadeInLength		= 0;
-		_fadeInType		= kFadeNone;
-	
-		_fadeOutLength		= 0;
-		_fadeOutType	= kFadeNone;
-}
-
-	return aafError;
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
 
 
 AAFRESULT STDMETHODCALLTYPE
-   ImplAAFSourceClip::GetFade (aafLength_t	*fadeInLen,
-                           aafFadeType_t	*fadeInType,
-                           aafBool			*fadeInPresent,
-                           aafLength_t		*fadeOutLen,
-                           aafFadeType_t	*fadeOutType,
-                           aafBool			*fadeOutPresent)
+   ImplAAFSourceClip::GetFade (aafInt32 *       /*fadeInLen     */,
+                           aafFadeType_t *  /*fadeInType    */,
+                           aafBool *        /*fadeInPresent */,
+                           aafInt32 *       /*fadeOutLen    */,
+                           aafFadeType_t *  /*fadeOutType   */,
+                           aafBool *        /*fadeOutPresent*/)
 {
-    AAFRESULT aafError = AAFRESULT_SUCCESS;
-
-	if (fadeInLen      == NULL ||
-		fadeInType     == NULL ||
-		fadeInPresent  == NULL ||
-		fadeOutLen     == NULL ||
-		fadeOutType    == NULL ||
-		fadeOutPresent == NULL )
-	{
-		aafError = 	AAFRESULT_NULL_PARAM;
-	}
-	else
-
-
-	if (!_fadeInLength.isPresent()   ||
-		!_fadeInType.isPresent()  ||
-		!_fadeOutLength.isPresent()  ||
-		!_fadeOutType.isPresent() )
-	{
-		aafError =  AAFRESULT_PROP_NOT_PRESENT;
-	}
-
-	else
-	{
-	
-        *fadeInLen      = _fadeInLength;
-		*fadeInType		= _fadeInType;
-		if (_fadeInLength > 0)
-		{
-			*fadeInPresent	= AAFTrue;
-		}
-		else
-		{
-			*fadeInPresent = AAFFalse;
-		}
-
-		*fadeOutLen		= _fadeOutLength;
-		*fadeOutType	= _fadeOutType;
-		if (_fadeOutLength > 0)
-		{
-			*fadeOutPresent	= AAFTrue;
-		}
-		else
-		{
-			*fadeOutPresent = AAFFalse;
-		}
-	}
-
-	return aafError;
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
 
 
-/*************************************************************************
- * Function: ResolveRef()
- *
- * 		Given a source clip object, this function returns a pointer to
- *      the mob that it references.  If this mob does not exist, the
- *      error AAFRESULT_MOB_NOT_FOUND is returned.
- *
- * Argument Notes:
- *
- * ReturnValue:
- *		Error code .
- *
- * Possible Errors:
- *		Standard errors .
- *************************************************************************/
 AAFRESULT STDMETHODCALLTYPE
-    ImplAAFSourceClip::ResolveRef (ImplAAFMob ** mob)
+    ImplAAFSourceClip::ResolveRef (ImplAAFMob ** /*mob*/)
 {
-    aafSourceRef_t sourceRef;
-	ImplAAFMob * tmpMob = NULL;
-	ImplAAFHeader *head = NULL;
-	aafInt32 index = 0;
-    AAFRESULT aafError = AAFRESULT_SUCCESS;
-
-	*mob = NULL;
-
-	XPROTECT()
-	  {
-		if (!mob)
-		  {
-			RAISE(AAFRESULT_NULL_PARAM);
-		  }
-
-		/* NOTE: This function should really be doing more checking,
-		 * i.e., does track exist in mob, does position (adjusted by
-		 * editrate) exist in track, etc.
-		 */
-
-		CHECK(GetSourceReference(&sourceRef));
-		CHECK(MyHeadObject(&head));
-		CHECK(head->LookupMob(&sourceRef.sourceID, mob));
-
-		head->ReleaseReference();
-		head = NULL;
-	  } /* XPROTECT */
-
-	XEXCEPT
-	  {
-		if(head)
-		  head->ReleaseReference();
-		head = 0;
-		return(XCODE());
-	  }
-	XEND;
-
-	return(aafError);
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
 
 
-/*************************************************************************
- * Function: GetSourceReference()
- *
- * 		This function returns the 3 properties on a source Clip that
- *      make of the "source reference" (sourceID, sourceTrackID, and
- *      startTime).
- *
- * Argument Notes:
- *
- * ReturnValue:
- *		Error code.
- *
- * Possible Errors:
- *		Standard errors.
- *************************************************************************/
 AAFRESULT STDMETHODCALLTYPE
-    ImplAAFSourceClip::GetSourceReference (aafSourceRef_t*	pSourceRef)
+    ImplAAFSourceClip::GetRef (aafSourceRef_t *  /*sourceRef*/)
 {
-    AAFRESULT aafError = AAFRESULT_SUCCESS;
-	aafUID_t	sourceID;
-	aafSlotID_t slotID;
-
-	XPROTECT()
-	{	
-		if (pSourceRef)
-		{
-			GetSourceID( &sourceID );
-			GetSourceMobSlotID( &slotID );
-			pSourceRef->sourceID = sourceID;
-			pSourceRef->sourceSlotID = slotID;
-			pSourceRef->startTime = _startTime;
-		} /* if sourceRef */
-		else
-		{
-			RAISE(AAFRESULT_NULL_PARAM);
-		}
-	 } /* XPROTECT */
-
-	XEXCEPT
-	  {
-		return(XCODE());
-	  }
-	XEND;
-
-	return(aafError);
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
 
 
  AAFRESULT STDMETHODCALLTYPE
-   ImplAAFSourceClip::SetFade (aafInt32		fadeInLen,
-                           aafFadeType_t	fadeInType,
-                           aafInt32			fadeOutLen,
-                           aafFadeType_t	fadeOutType)
+   ImplAAFSourceClip::SetFade (aafInt32       /*fadeInLen  */,
+                           aafFadeType_t  /*fadeInType */,
+                           aafInt32       /*fadeOutLen */,
+                           aafFadeType_t  /*fadeOutType*/)
 {
-    AAFRESULT aafError = AAFRESULT_SUCCESS;
-
-	if (fadeInLen > 0)
-	{
-		_fadeInLength	= fadeInLen;
-		_fadeInType	= fadeInType;
-	}
-
-	if (fadeOutLen > 0)
-	{
-		_fadeOutLength		= fadeOutLen;
-		_fadeOutType	= fadeOutType;
-	}
-
-	return aafError;
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
 
 
 AAFRESULT STDMETHODCALLTYPE
-    ImplAAFSourceClip::SetSourceReference (aafSourceRef_t  sourceRef)
+    ImplAAFSourceClip::SetRef (aafSourceRef_t  /*sourceRef*/)
 {
-    aafInt32	tmp1xSourcePosition = 0;
-	aafInt16	tmp1xTrackNum = 0;
-	AAFRESULT   aafError = AAFRESULT_SUCCESS;
-	
-	/* If UID is NUL - make the rest of the fields 0 too. */
-	if( (sourceRef.sourceID.Data1 == NilMOBID.Data1) && 
-		(sourceRef.sourceID.Data2 == NilMOBID.Data2) &&
-		(sourceRef.sourceID.Data3 == NilMOBID.Data3) &&
-		(sourceRef.sourceID.Data4[0] == NilMOBID.Data4[0]) &&
-		(sourceRef.sourceID.Data4[1] == NilMOBID.Data4[1]) &&
-		(sourceRef.sourceID.Data4[2] == NilMOBID.Data4[2]) &&
-		(sourceRef.sourceID.Data4[3] == NilMOBID.Data4[3]) &&
-		(sourceRef.sourceID.Data4[4] == NilMOBID.Data4[4]) &&
-		(sourceRef.sourceID.Data4[5] == NilMOBID.Data4[5]) &&
-		(sourceRef.sourceID.Data4[6] == NilMOBID.Data4[6]) &&
-		(sourceRef.sourceID.Data4[7] == NilMOBID.Data4[7]) 	)
-	{
-		sourceRef.sourceSlotID = 0;
-		CvtInt32toPosition(0, sourceRef.startTime);	
-	}
-	SetSourceID( sourceRef.sourceID );
-	SetSourceMobSlotID( sourceRef.sourceSlotID) ;
-	_startTime = sourceRef.startTime;
-
-	return(aafError);
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
 
 
 
-AAFRESULT ImplAAFSourceClip::TraverseToClip(aafLength_t length,
-											ImplAAFSegment **sclp,
-											ImplAAFPulldown **pulldownObj,
-											aafInt32 *pulldownPhase,
-											aafLength_t *sclpLen,
-											aafBool *isMask)
-{
-	XPROTECT()
-	{
-		*sclp = this;
-		// We are returning a reference to this object so bump the ref count
-		AcquireReference();
-		CHECK((*sclp)->GetLength(sclpLen));
-		if (Int64Less(length, *sclpLen))
-		{
-			*sclpLen = length;
-		}
-	} /* XPROTECT */
-	
-	XEXCEPT
-	{
-	}
-	XEND;
-	
-	return(AAFRESULT_SUCCESS);
-}
+  // Override from AAFSourceReference
+  AAFRESULT STDMETHODCALLTYPE
+    ImplAAFSourceClip::GetSourceID (/*[retval][out]*/ aafUID_t *  /*pSourceID*/)
+  {
+    return AAFRESULT_NOT_IMPLEMENTED;
+  }
+
+  // Override from AAFSourceReference
+  AAFRESULT STDMETHODCALLTYPE
+    ImplAAFSourceClip::SetSourceID (/*[in]*/ aafUID_t   /*sourceID*/)
+  {
+    return AAFRESULT_NOT_IMPLEMENTED;
+  }
+
+  // Override from AAFSourceReference
+  AAFRESULT STDMETHODCALLTYPE
+    ImplAAFSourceClip::GetSourceMobSlotID (/*[retval][out]*/ aafTrackID_t *  /*pMobSlotID*/)
+  {
+    return AAFRESULT_NOT_IMPLEMENTED;
+  }
+
+  // Override from AAFSourceReference
+  AAFRESULT STDMETHODCALLTYPE
+    ImplAAFSourceClip::SetSourceMobSlotID (/*[in]*/ aafTrackID_t   /*mobSlotID*/)
+  {
+    return AAFRESULT_NOT_IMPLEMENTED;
+  }
 
 
+
+
+extern "C" const aafClassID_t CLSID_AAFSourceClip;
+
+OMDEFINE_STORABLE(AAFSourceClip, CLSID_AAFSourceClip);
 
