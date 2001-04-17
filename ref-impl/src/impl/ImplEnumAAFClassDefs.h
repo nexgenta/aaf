0562ba2f67c9cd778c7e818bfe03dcad89df6d59
tbingham <tbingham>
2001-04-17 04:44:13 +0000

    Add support to initialize properties from the DM class definition.

--- ref-impl/src/impl/ImplEnumAAFClassDefs.h
+++ ref-impl/src/impl/ImplEnumAAFClassDefs.h
@@ -2,38 +2,24 @@
 //@class    EnumAAFClassDefs | Implementation class for EnumAAFClassDefs
 #ifndef __ImplEnumAAFClassDefs_h__
 #define __ImplEnumAAFClassDefs_h__
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- *  prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+/***********************************************\
+*                                               *
+* Advanced Authoring Format                     *
+*                                               *
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+* Copyright (c) 1998-1999 Microsoft Corporation *
+*                                               *
+\***********************************************/
 
-class ImplAAFMetaDictionary;
+class ImplAAFClassDef;
 
-#include "ImplAAFClassDef.h"
-#include "OMReferenceContainerIter.h"
+#ifndef __ImplAAFObject_h__
+#include "ImplAAFObject.h"
+#endif
 
+
+typedef OMVariableSizeProperty<aafUID_t> classDefWeakRefArrayProp_t;
+typedef OMStrongReferenceVectorProperty<ImplAAFClassDef> classDefStrongRefArrayProp_t;
 
 
 class ImplEnumAAFClassDefs : public ImplAAFRoot
@@ -99,12 +85,15 @@ public:
 public:
   // SDK Internal 
   virtual AAFRESULT STDMETHODCALLTYPE
-	  SetIterator(ImplAAFMetaDictionary *pObj,
-				OMReferenceContainerIterator<ImplAAFClassDef>* iterator);
+    SetEnumProperty( ImplAAFObject *pObj, classDefWeakRefArrayProp_t *pProp);
+  virtual AAFRESULT STDMETHODCALLTYPE
+    SetEnumStrongProperty( ImplAAFObject *pObj, classDefStrongRefArrayProp_t *pProp);
 
 private:
-	ImplAAFMetaDictionary*	_enumObj;
-	OMReferenceContainerIterator<ImplAAFClassDef>*	_iterator;
+	aafUInt32                      _current;
+	ImplAAFObject                * _enumObj;
+	classDefWeakRefArrayProp_t   * _enumProp;
+	classDefStrongRefArrayProp_t * _enumStrongProp;
 };
 
 //
