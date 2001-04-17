217a0b883bc21f14724f2f5b39aa2bfbfb0bf034
tbingham <tbingham>
2001-04-17 09:08:15 +0000

    Add methods to ateempt registration of struct and enum offsets/sizes for builtin types; hack array of obs-to-init up to 2000 elements (this whole structure should be removed soon anyway).

--- ref-impl/src/impl/ImplAAFDictionary.h
+++ ref-impl/src/impl/ImplAAFDictionary.h
@@ -3,13 +3,32 @@
 #ifndef __ImplAAFDictionary_h__
 #define __ImplAAFDictionary_h__
 
-/************************************************\
-*												*
-* Advanced Authoring Format						*
-*												*
-* Copyright (c) 1998-1999 Avid Technology, Inc. *
-*												*
-\************************************************/
+/***********************************************************************
+ *
+ *              Copyright (c) 1998-1999 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and accompanying 
+ * documentation, and to distribute and sublicense application software
+ * incorporating this software for any purpose is hereby granted, 
+ * provided that (i) the above copyright notice and this permission
+ * notice appear in all copies of the software and related documentation,
+ * and (ii) the name Avid Technology, Inc. may not be used in any
+ * advertising or publicity relating to the software without the specific,
+ *  prior written permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+ * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+ * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+ * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+ * LIABILITY.
+ *
+ ************************************************************************/
 
 
 class ImplAAFBuiltinClasses;
@@ -23,6 +42,8 @@ class ImplAAFInterpolationDef;
 class ImplAAFOperationDef;
 class ImplAAFPluginDescriptor;
 class ImplAAFTypeDef;
+class ImplAAFTypeDefEnum;
+class ImplAAFTypeDefRecord;
 class ImplEnumAAFClassDefs;
 class ImplEnumAAFCodecDefs;
 class ImplEnumAAFContainerDefs;
@@ -393,6 +414,12 @@ public:
   // will put obj on queue to be initialized when everything's ready.
   void pvtInitObjectProperties (ImplAAFObject * pObj) const;
 
+  // Attempt to register the sizes of this type def if it is a
+  // built-in type.  Currently implemented for Enum and Record
+  // typedefs.
+  void pvtAttemptBuiltinSizeRegistration (ImplAAFTypeDefEnum * ptde) const;
+  void pvtAttemptBuiltinSizeRegistration (ImplAAFTypeDefRecord * ptdr) const;
+
 private:
 
   // Like the non-private LookupPropTypeByOMPid(), except will
@@ -439,7 +466,7 @@ private:
 
   private:
 	enum {
-	  kPvtMaxInitObjs = 200
+	  kPvtMaxInitObjs = 2000
 	};
 	ImplAAFObjectSP _objs[kPvtMaxInitObjs];
 	aafUInt32 _putIdx;
