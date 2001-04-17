02644632023f63f687024c1c3b70f03a378d4592
tbingham <tbingham>
2001-04-17 09:43:19 +0000

    char* -> wchar_t* in Object Manager API.

---
+++ ref-impl/src/OM/OMDataStreamProperty.cpp
@@ -0,0 +1,310 @@
+/***********************************************************************
+*
+*              Copyright (c) 1998-2000 Avid Technology, Inc.
+*
+* Permission to use, copy and modify this software and accompanying
+* documentation, and to distribute and sublicense application software
+* incorporating this software for any purpose is hereby granted,
+* provided that (i) the above copyright notice and this permission
+* notice appear in all copies of the software and related documentation,
+* and (ii) the name Avid Technology, Inc. may not be used in any
+* advertising or publicity relating to the software without the specific,
+* prior written permission of Avid Technology, Inc.
+*
+* THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+* WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+* IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+* SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+* OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+* OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+* ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+* RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+* ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+* LIABILITY.
+*
+************************************************************************/
+
+// @doc OMEXTERNAL
+#include "OMDataStreamProperty.h"
+
+#include "OMAssertions.h"
+
+#include "OMPropertySet.h"
+#include "OMStorable.h"
+#include "OMStoredObject.h"
+
+OMDataStreamProperty::OMDataStreamProperty(const OMPropertyId propertyId,
+                                           const wchar_t* name)
+  : OMProperty(propertyId, SF_DATA_STREAM, name),_stream(0)
+{
+}
+
+OMDataStreamProperty::~OMDataStreamProperty(void)
+{
+}
+
+  // @mfunc Save this <c OMDataStreamProperty>.
+  //   @this const
+void OMDataStreamProperty::save(void) const
+{
+  TRACE("OMDataStreamProperty::save");
+
+  OMStoredObject* store = _propertySet->container()->store();
+  ASSERT("Valid store", store != 0);
+
+  // Use the property name as the stream name
+  //
+  const char* streamName = name();
+  store->write(_propertyId,
+               _storedForm,
+               (void*)streamName,
+               strlen(streamName) + 1);
+
+  // The stream has never been written to but we want the stream to
+  // exist in the file, create it.
+  //
+  if (_stream == 0) {
+    OMDataStreamProperty* p = const_cast<OMDataStreamProperty*>(this);
+    p->create();
+  }
+}
+
+  // @mfunc Restore this <c OMDataStreamProperty>, the size of the
+  //        <c OMDataStreamProperty> is <p size>.
+  //   @parm The size of the <c OMDataStreamProperty>.
+void OMDataStreamProperty::restore(size_t size)
+{
+  TRACE("OMDataStreamProperty::restore");
+
+  OMStoredObject* store = _propertySet->container()->store();
+  ASSERT("Valid store", store != 0);
+
+  char* streamName = new char[size];
+  ASSERT("Valid heap pointer", streamName != 0);
+  store->read(_propertyId,
+              _storedForm,
+              streamName,
+              size);
+  ASSERT("Consistent property size", size == strlen(streamName) + 1);
+  ASSERT("Consistent property name", strcmp(streamName, name()) == 0);
+  delete [] streamName;
+
+  open();
+  setPresent();
+
+  POSTCONDITION("Properly opened stream", _stream != 0);
+}
+
+  // @mfunc The size, in bytes, of the data in this
+  //        <c OMDataStreamProperty>.
+  //   @rdesc  The size, in bytes, of the data in this
+  //           <c OMDataStreamProperty>.
+  //   @this const
+OMUInt64 OMDataStreamProperty::size(void) const
+{
+  TRACE("OMDataStreamProperty::size");
+
+  if (_stream == 0) {
+    OMDataStreamProperty* p = const_cast<OMDataStreamProperty*>(this);
+    p->create();
+  }
+  ASSERT("Valid stream", _stream != 0);
+
+  OMUInt64 result;
+  OMStoredObject* s = _propertySet->container()->store();
+  result = s->streamSize(_stream);
+ 
+  return result;
+}
+
+  // @mfunc Set the size, in bytes, of the data in this
+  //        <c OMDataStreamProperty>.
+  //   @rdesc  The new size, in bytes, of the data in this
+  //           <c OMDataStreamProperty>.
+void OMDataStreamProperty::setSize(const OMUInt64 newSize)
+{
+  TRACE("OMDataStreamProperty::setSize");
+
+  if (_stream == 0) {
+    create();
+  }
+  ASSERT("Valid stream", _stream != 0);
+
+  OMStoredObject* s = _propertySet->container()->store();
+ 
+  s->streamSetSize(_stream, newSize);
+
+  POSTCONDITION("Size properly set", size() == newSize);
+}
+
+  // @mfunc The current position for <f read()> and <f write()>, as an
+  //        offset in bytes from the begining of the data stream.
+  //   @rdesc The current position for <f read()> and <f write()>, as an
+  //          offset in bytes from the begining of the data stream.
+  //   @this const
+OMUInt64 OMDataStreamProperty::position(void) const
+{
+  TRACE("OMDataStreamProperty::position");
+
+  if (_stream == 0) {
+    OMDataStreamProperty* p = const_cast<OMDataStreamProperty*>(this);
+    p->create();
+  }
+  ASSERT("Valid stream", _stream != 0);
+
+  OMStoredObject* s = _propertySet->container()->store();
+ 
+  OMUInt64 result = s->streamPosition(_stream);
+
+  POSTCONDITION("Valid position", (result >= 0) && (result <= size()));
+  return result;
+}
+
+  // @mfunc Set the current position for <f read()> and <f write()>, as an
+  //        offset in bytes from the begining of the data stream.
+  //   @parm The position to use for subsequent calls to read() and
+  //         write() on this stream. The position is specified as an
+  //         offset in bytes from the begining of the data stream.
+  //   @this const
+void OMDataStreamProperty::setPosition(const OMUInt64 offset) const
+{
+  TRACE("OMDataStreamProperty::setPosition");
+
+  if (_stream == 0) {
+    OMDataStreamProperty* p = const_cast<OMDataStreamProperty*>(this);
+    p->create();
+  }
+  ASSERT("Valid stream", _stream != 0);
+
+  OMStoredObject* s = _propertySet->container()->store();
+ 
+  s->streamSetPosition(_stream, offset);
+
+  POSTCONDITION("Position properly set", position() == offset);
+}
+
+void OMDataStreamProperty::open(void)
+{
+  TRACE("OMDataStreamProperty::open");
+  PRECONDITION("Stream not already opened", _stream == 0);
+
+  OMStoredObject* s = _propertySet->container()->store();
+  _stream = s->openStream(name());
+
+  POSTCONDITION("Stream opened", _stream != 0);
+}
+
+void OMDataStreamProperty::create(void)
+{
+  TRACE("OMDataStreamProperty::create");
+  PRECONDITION("Stream not already created", _stream == 0);
+
+  OMStoredObject* s = _propertySet->container()->store();
+  _stream = s->createStream(name());
+
+  POSTCONDITION("Stream opened", _stream != 0);
+}
+
+void OMDataStreamProperty::close(void)
+{
+  TRACE("OMDataStreamProperty::close");
+  PRECONDITION("Stream not already closed", _stream != 0);
+
+  OMStoredObject* s = _propertySet->container()->store();
+  s->closeStream(_stream);
+
+  POSTCONDITION("Stream closed", _stream == 0);
+}
+
+  // @mfunc Attempt to read the number of bytes given by <p bytes>
+  //          from the data stream into the buffer at address
+  //          <p buffer>. The actual number of bytes read is returned
+  //          in <p bytesRead>.
+  //   @parm The address of the buffer into which the bytes should be read.
+  //   @parm The number of bytes to read.
+  //   @parm The actual number of bytes that were read.
+  //   @this const
+void OMDataStreamProperty::read(OMByte* buffer,
+                                const OMUInt32 bytes,
+                                OMUInt32& bytesRead) const
+{
+  TRACE("OMDataStreamProperty::read");
+
+  if (_stream == 0) {
+    OMDataStreamProperty* p = const_cast<OMDataStreamProperty*>(this);
+    p->create();
+  }
+
+  ASSERT("Valid stream", _stream != 0);
+  OMStoredObject* s = _propertySet->container()->store();
+  s->readFromStream(_stream, buffer, bytes, bytesRead);
+}
+
+  // @mfunc  Attempt to write the number of bytes given by <p bytes>
+  //         to the data stream from the buffer at address
+  //          <p buffer>. The actual number of bytes written is returned
+  //          in <p bytesWritten>.
+  //   @parm  The address of the buffer from which the bytes should be written.
+  //   @parm The number of bytes to write.
+  //   @parm The actual number of bytes that were written.
+void OMDataStreamProperty::write(const OMByte* buffer,
+                                 const OMUInt32 bytes,
+                                 OMUInt32& bytesWritten)
+{
+  TRACE("OMDataStreamProperty::write");
+
+  if (_stream == 0) {
+    create();
+  }
+  ASSERT("Valid stream", _stream != 0);
+
+  OMStoredObject* s = _propertySet->container()->store();
+  s->writeToStream(_stream, buffer, bytes, bytesWritten);
+}
+
+  // @mfunc The size of the raw bits of this
+  //        <c OMDataStreamProperty>. The size is given in bytes.
+  //   @rdesc The size of the raw bits of this
+  //          <c OMDataStreamProperty> in bytes.
+  //   @this const
+size_t OMDataStreamProperty::bitsSize(void) const
+{
+  TRACE("OMDataStreamProperty::bitsSize");
+
+  return sizeof(IStream*);
+}
+
+  // @mfunc Get the raw bits of this <c OMDataStreamProperty>.
+  //        The raw bits are copied to the buffer at address <p bits> which
+  //        is <p size> bytes in size.
+  //   @parm The address of the buffer into which the raw bits are copied.
+  //   @parm size_t | size | The size of the buffer.
+  //   @this const
+void OMDataStreamProperty::getBits(OMByte* bits, size_t ANAME(size)) const
+{
+  TRACE("OMDataStreamProperty::getBits");
+  PRECONDITION("Valid bits", bits != 0);
+  PRECONDITION("Valid size", size >= bitsSize());
+
+  memcpy(bits, &_stream, bitsSize());
+}
+
+  // @mfunc Set the raw bits of this <c OMDataStreamProperty>. The raw
+  //         bits are copied from the buffer at address <p bits> which
+  //         is <p size> bytes in size.
+  //   @parm const OMByte* | bits | The address of the buffer from which the
+  //         raw bits are copied.
+  //   @parm size_t | size | The size of the buffer.
+void OMDataStreamProperty::setBits(const OMByte* ANAME(bits),
+                                   size_t ANAME(size))
+{
+  TRACE("OMDataStreamProperty::setBits");
+  PRECONDITION("Valid bits", bits != 0);
+  PRECONDITION("Valid size", size >= bitsSize());
+
+  ASSERT("Unimplemented code not reached", false);
+}
+
+
+
---
+++ ref-impl/src/impl/ImplAAFTypeDefRename.cpp
@@ -0,0 +1,279 @@
+/***********************************************************************
+ *
+ *              Copyright (c) 1998-1999 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and accompanying 
+ * documentation, and to distribute and sublicense application software
+ * incorporating this software for any purpose is hereby granted, 
+ * provided that (i) the above copyright notice and this permission
+ * notice appear in all copies of the software and related documentation,
+ * and (ii) the name Avid Technology, Inc. may not be used in any
+ * advertising or publicity relating to the software without the specific,
+ * prior written permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+ * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+ * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+ * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+ * LIABILITY.
+ *
+ ************************************************************************/
+
+#include "AAFStoredObjectIDs.h"
+#include "AAFPropertyIDs.h"
+
+#ifndef __ImplAAFTypeDefRename_h__
+#include "ImplAAFTypeDefRename.h"
+#endif
+
+#ifndef __ImplAAFPropValData_h__
+#include "ImplAAFPropValData.h"
+#endif
+
+#ifndef __ImplAAFObjectCreation_h__
+#include "ImplAAFObjectCreation.h"
+#endif
+
+#include "ImplAAFDictionary.h"
+
+#include <assert.h>
+#include <string.h>
+
+extern "C" const aafClassID_t CLSID_AAFPropValData;
+
+
+ImplAAFTypeDefRename::ImplAAFTypeDefRename ()
+  : _RenamedType  ( PID_TypeDefinitionRename_RenamedType, 
+                    L"RenamedType", 
+                    L"/Dictionary/TypeDefinitions", 
+                    PID_MetaDefinition_Identification)
+{
+  _persistentProperties.put(_RenamedType.address());
+}
+
+
+ImplAAFTypeDefRename::~ImplAAFTypeDefRename ()
+{}
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFTypeDefRename::Initialize (
+      const aafUID_t & id,
+      ImplAAFTypeDef * pBaseType,
+      const aafCharacter * pTypeName)
+{
+  if (! pTypeName) return AAFRESULT_NULL_PARAM;
+  if (! pBaseType)  return AAFRESULT_NULL_PARAM;
+
+  HRESULT hr;
+
+  hr = ImplAAFMetaDefinition::Initialize(id, pTypeName, NULL);
+	if (AAFRESULT_FAILED (hr))
+    return hr;
+
+  _RenamedType = pBaseType;
+
+  return AAFRESULT_SUCCESS;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFTypeDefRename::GetBaseType (
+      ImplAAFTypeDef ** ppBaseType) const
+{
+  if (! ppBaseType)
+	return AAFRESULT_NULL_PARAM;
+
+   if(_RenamedType.isVoid())
+		return AAFRESULT_OBJECT_NOT_FOUND;
+  ImplAAFTypeDef *pTypeDef = _RenamedType;
+
+  *ppBaseType = pTypeDef;
+  assert (*ppBaseType);
+  (*ppBaseType)->AcquireReference ();
+  return AAFRESULT_SUCCESS;
+}
+
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFTypeDefRename::GetValue (
+      ImplAAFPropertyValue * pInPropVal,
+      ImplAAFPropertyValue ** ppOutPropVal)
+{
+  if (! pInPropVal) return AAFRESULT_NULL_PARAM;
+  if (! ppOutPropVal) return AAFRESULT_NULL_PARAM;
+
+  aafUInt32 inBitsSize;
+  ImplAAFPropValDataSP pOutPVData;
+  ImplAAFPropValDataSP pvd;
+  ImplAAFTypeDefSP ptd;
+
+  AAFRESULT hr;
+  hr = GetBaseType (&ptd);
+  if (AAFRESULT_FAILED (hr)) return hr;
+  assert (ptd);
+//  aafUInt32 elementSize = ptd->PropValSize();
+
+  assert (pInPropVal);
+  pvd = dynamic_cast<ImplAAFPropValData*> (pInPropVal);
+  assert (pvd);
+
+  hr = pvd->GetBitsSize (&inBitsSize);
+  if (! AAFRESULT_SUCCEEDED (hr)) return hr;
+
+  pOutPVData = (ImplAAFPropValData *)CreateImpl(CLSID_AAFPropValData);
+  if (! pOutPVData) return AAFRESULT_NOMEMORY;
+
+  // Bobt: Hack bugfix! SmartPointer operator= will automatically
+  // AddRef; CreateImpl *also* will addref, so we've got one too
+  // many.  Put us back to normal.
+  pOutPVData->ReleaseReference ();
+
+  assert (ptd);
+  hr = pOutPVData->Initialize (ptd);
+  if (AAFRESULT_FAILED(hr)) return hr;
+
+  hr = pOutPVData->AllocateFromPropVal (pvd,
+										0,
+										inBitsSize,
+										NULL);
+  if (AAFRESULT_FAILED(hr)) return hr;
+
+  assert (ppOutPropVal);
+  *ppOutPropVal = pOutPVData;
+  (*ppOutPropVal)->AcquireReference ();
+  assert (*ppOutPropVal);
+  return AAFRESULT_SUCCESS;
+}
+
+
+ImplAAFTypeDefSP ImplAAFTypeDefRename::BaseType () const
+{
+  ImplAAFTypeDefSP result;
+  AAFRESULT hr = GetBaseType (&result);
+  assert (AAFRESULT_SUCCEEDED (hr));
+  assert (result);
+  return result;
+}
+
+void ImplAAFTypeDefRename::reorder(OMByte* externalBytes,
+								   size_t externalBytesSize) const
+{
+  BaseType()->reorder (externalBytes, externalBytesSize);
+}
+
+
+size_t ImplAAFTypeDefRename::externalSize(OMByte* internalBytes,
+										  size_t internalBytesSize) const
+{
+  return BaseType()->externalSize (internalBytes, internalBytesSize);
+}
+
+
+void ImplAAFTypeDefRename::externalize(OMByte* internalBytes,
+									   size_t internalBytesSize,
+									   OMByte* externalBytes,
+									   size_t externalBytesSize,
+									   OMByteOrder byteOrder) const
+{
+  BaseType()->externalize (internalBytes,
+						   internalBytesSize,
+						   externalBytes,
+						   externalBytesSize,
+						   byteOrder);
+}
+
+
+size_t ImplAAFTypeDefRename::internalSize(OMByte* externalBytes,
+										  size_t externalBytesSize) const
+{
+  return BaseType()->internalSize (externalBytes, externalBytesSize);
+}
+
+
+void ImplAAFTypeDefRename::internalize(OMByte* externalBytes,
+									   size_t externalBytesSize,
+									   OMByte* internalBytes,
+									   size_t internalBytesSize,
+									   OMByteOrder byteOrder) const
+{
+  BaseType()->externalize (externalBytes,
+						   externalBytesSize,
+						   internalBytes,
+						   internalBytesSize,
+						   byteOrder);
+}
+
+
+
+aafBool ImplAAFTypeDefRename::IsFixedSize() const
+{
+  return BaseType()->IsFixedSize ();
+}
+
+
+size_t ImplAAFTypeDefRename::PropValSize() const
+{
+  return BaseType()->PropValSize ();
+}
+
+
+aafBool ImplAAFTypeDefRename::IsRegistered() const
+{
+  return BaseType()->IsRegistered ();
+}
+
+
+size_t ImplAAFTypeDefRename::NativeSize() const
+{
+  return BaseType()->NativeSize ();
+}
+
+
+OMProperty * ImplAAFTypeDefRename::pvtCreateOMProperty
+  (OMPropertyId pid,
+   const wchar_t * name) const
+{
+  return BaseType()->pvtCreateOMProperty (pid, name);
+}
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFTypeDefRename::GetTypeCategory (
+      eAAFTypeCategory_t * pTid)
+{
+  if (! pTid)
+	return AAFRESULT_NULL_PARAM;
+
+  *pTid = kAAFTypeCatRename;
+  return AAFRESULT_SUCCESS;
+}
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFTypeDefRename::RawAccessType (
+      ImplAAFTypeDef ** ppRawTypeDef)
+{
+  return BaseType()->RawAccessType (ppRawTypeDef);
+}
+
+
+bool ImplAAFTypeDefRename::IsAggregatable () const
+{ return BaseType()->IsAggregatable(); }
+
+bool ImplAAFTypeDefRename::IsStreamable () const
+{ return BaseType()->IsStreamable(); }
+
+bool ImplAAFTypeDefRename::IsFixedArrayable () const
+{ return BaseType()->IsFixedArrayable(); }
+
+bool ImplAAFTypeDefRename::IsVariableArrayable () const
+{ return BaseType()->IsVariableArrayable(); }
+
+bool ImplAAFTypeDefRename::IsStringable () const
+{ return BaseType()->IsStringable(); }
