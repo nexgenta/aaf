8b386ec01770b3a6ed940038835f7a1df00d9af6
tbingham <tbingham>
2001-04-17 11:46:54 +0000

    AAF/dodo/GenTargets.sh

--- dodo/GenTargets.sh
+++ dodo/GenTargets.sh
@@ -9,19 +9,12 @@ echo "#" Special case AAFTypes since no object is to be built only headers...
 echo "#" special case the utility classes since they will not be exposed by com 
 echo DODO_TARGETS = '\' 
 echo '	'AAFTypes.all \\c 
-for base in AAFRoot ${PRIVATE_AAFOBJECTS} ${AAFOBJECTS} ${AAFCOMINTERFACESONLY} ;  do \
+for base in AAFRoot ${AAFOBJECTS} ${AAFCOMINTERFACESONLY} ;  do \
 	echo '\' ; \
 	echo '	'$base.all \\c ; \
 done
 echo '\'
 echo '	'AAFModule.all \\c ;
-echo ''
-echo ''
-echo PRIVATE_FIDL_TARGETS = \\c 
-for base in ${PRIVATE_AAFOBJECTS} ;  do \
-	echo '\' ; \
-	echo '	'$base.fidl \\c ; \
-done
 echo ''
 echo ''
 echo FIDL_TARGETS = \\c 
@@ -31,13 +24,6 @@ for base in ${AAFOBJECTS} ${AAFCOMINTERFACESONLY} ;  do \
 done
 echo '\'
 echo '	'AAFModule.fidl \\c ;
-echo ''
-echo ''
-echo PRIVATE_FREFH_TARGETS = \\c 
-for base in AAFRoot ${PRIVATE_AAFOBJECTS} ;  do \
-	echo '\' ; \
-	echo '	'$base.frefh \\c ; \
-done
 echo ''
 echo ''
 echo FREFH_TARGETS = \\c 
