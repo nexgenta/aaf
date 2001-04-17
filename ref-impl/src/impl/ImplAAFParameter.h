944c826bb4199646e43928ef49f3aac05a103eb7
tbingham <tbingham>
2001-04-17 03:47:23 +0000

    Round 2 of effects checkins

--- ref-impl/src/impl/ImplAAFParameter.h
+++ ref-impl/src/impl/ImplAAFParameter.h
@@ -3,32 +3,14 @@
 #ifndef __ImplAAFParameter_h__
 #define __ImplAAFParameter_h__
 
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- *  prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+/***********************************************\
+*												*
+* Advanced Authoring Format						*
+*												*
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+* Copyright (c) 1998-1999 Microsoft Corporation *
+*												*
+\***********************************************/
 
 #ifndef __AAFTypes_h__
 #include "AAFTypes.h"
@@ -36,6 +18,7 @@
 
 class ImplAAFParameterDef;
 class ImplAAFTypeDef;
+class ImplAAFReferenceValue;
 
 #ifndef __ImplAAFObject_h__
 #include "ImplAAFObject.h"
@@ -56,20 +39,29 @@ protected:
 
 public:
   virtual AAFRESULT STDMETHODCALLTYPE
-    SetParameterDefinition (
+    ImplAAFParameter::SetParameterDefinition (
       ImplAAFParameterDef *pParmDef);
 
   virtual AAFRESULT STDMETHODCALLTYPE
-    GetParameterDefinition (
+    ImplAAFParameter::GetParameterDefinition (
       ImplAAFParameterDef **ppParmDef);	
 
   virtual AAFRESULT STDMETHODCALLTYPE
-    SetTypeDefinition (
+    ImplAAFParameter::SetTypeDefinition (
       ImplAAFTypeDef*  pTypeDef);
 
   virtual AAFRESULT STDMETHODCALLTYPE
-    GetTypeDefinition (
+    ImplAAFParameter::GetTypeDefinition (
       ImplAAFTypeDef **ppTypeDef);
+
+public:
+  // Declare this class to be storable.
+  //
+  OMDECLARE_STORABLE(ImplAAFParameter)
+
+  // Declare the module test method. The implementation of the will be be
+  // in /test/ImplAAFParameterTest.cpp.
+  static AAFRESULT test();
 
 private:
 
