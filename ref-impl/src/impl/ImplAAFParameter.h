11e38402680b7feea2e5f73d53aab6d74b73eaf3
tbingham <tbingham>
2001-04-17 03:48:30 +0000

    First round of effects checkins

--- ref-impl/src/impl/ImplAAFParameter.h
+++ ref-impl/src/impl/ImplAAFParameter.h
@@ -3,39 +3,43 @@
 #ifndef __ImplAAFParameter_h__
 #define __ImplAAFParameter_h__
 
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- *  prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+
+
+/***********************************************\
+*												*
+* Advanced Authoring Format						*
+*												*
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+* Copyright (c) 1998-1999 Microsoft Corporation *
+*												*
+\***********************************************/
+
+
+/***********************************************\
+*	Stub only.   Implementation not yet added	*
+\***********************************************/
+
 
 #ifndef __AAFTypes_h__
 #include "AAFTypes.h"
 #endif
 
-class ImplAAFParameterDef;
-class ImplAAFTypeDef;
+class ImplAAFDataDef;
+
+
+ 
+
+
 
 #ifndef __ImplAAFObject_h__
 #include "ImplAAFObject.h"
@@ -55,23 +59,53 @@ protected:
   virtual ~ImplAAFParameter ();
 
 public:
+
+
+  //****************
+  // SetLength()
+  //
   virtual AAFRESULT STDMETHODCALLTYPE
-    SetParameterDefinition (
-      ImplAAFParameterDef *pParmDef);
+    SetLength
+        // @parm [in] Length of this object
+        (aafLength_t  length);
 
+
+  //****************
+  // GetLength()
+  //
   virtual AAFRESULT STDMETHODCALLTYPE
-    GetParameterDefinition (
-      ImplAAFParameterDef **ppParmDef);	
+    GetLength
+        // @parm [out,retval] Length of this component
+        (aafLength_t *  pLength);
+	
 
+  //****************
+  // SetDataDefID()
+  //
   virtual AAFRESULT STDMETHODCALLTYPE
-    GetTypeDefinition (
-      ImplAAFTypeDef **ppTypeDef);
+    SetDataDefID
+        // @parm [in] Data Definition of this object
+        (aafUID_t  pDataDefID);
 
-    virtual const OMUniqueObjectIdentification& identification(void) const;
+  //****************
+  // GetDataDefID()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetDataDefID
+        // @parm [out,retval] Data Definition of this object
+        (aafUID_t *  pDataDefID);
 
-private:
 
-	OMFixedSizeProperty<aafUID_t>				_parmDef;
+
+
+public:
+  // Declare this class to be storable.
+  //
+  OMDECLARE_STORABLE(ImplAAFParameter)
+
+  // Declare the module test method. The implementation of the will be be
+  // in /test/ImplAAFParameterTest.cpp.
+  static AAFRESULT test();
 };
 
 #endif // ! __ImplAAFParameter_h__
