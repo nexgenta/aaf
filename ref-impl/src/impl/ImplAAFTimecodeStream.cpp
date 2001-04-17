26cbb16e50a4ea1dfb5c6cf3dd89e5bebb5c5761
tbingham <tbingham>
2001-04-17 06:07:04 +0000

    char* -> wchar_t* in Object Manager API.

--- ref-impl/src/impl/ImplAAFTimecodeStream.cpp
+++ ref-impl/src/impl/ImplAAFTimecodeStream.cpp
@@ -1,11 +1,29 @@
-/******************************************\
-*                                          *
-* Advanced Authoring Format                *
-*                                          *
-* Copyright (c) 1998 Avid Technology, Inc. *
-* Copyright (c) 1998 Microsoft Corporation *
-*                                          *
-\******************************************/
+/***********************************************************************
+ *
+ *              Copyright (c) 1998-1999 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and accompanying 
+ * documentation, and to distribute and sublicense application software
+ * incorporating this software for any purpose is hereby granted, 
+ * provided that (i) the above copyright notice and this permission
+ * notice appear in all copies of the software and related documentation,
+ * and (ii) the name Avid Technology, Inc. may not be used in any
+ * advertising or publicity relating to the software without the specific,
+ *  prior written permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+ * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+ * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+ * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+ * LIABILITY.
+ *
+ ************************************************************************/
 
 
 
@@ -25,9 +43,9 @@
 #include "aafErr.h"
 
 ImplAAFTimecodeStream::ImplAAFTimecodeStream ()
-: _sampleRate(		PID_TimecodeStream_SampleRate,	"SampleRate"),
-  _source(			PID_TimecodeStream_Source,		"Source"),
-  _sourceType(		PID_TimecodeStream_SourceType,	"SourceType")
+: _sampleRate(		PID_TimecodeStream_SampleRate,	L"SampleRate"),
+  _source(			PID_TimecodeStream_Source,		L"Source"),
+  _sourceType(		PID_TimecodeStream_SourceType,	L"SourceType")
 {
   _persistentProperties.put(_sampleRate.address());
   _persistentProperties.put(_source.address());
@@ -210,7 +228,7 @@ AAFRESULT STDMETHODCALLTYPE
 	aafPosition_t	testOffset, scanStart;
 	aafTimecode_t	baseTimecode, testTimecode;
 	aafPosition_t	increment = pTimecode->fps * 10;	// Skip at 10 second intervals
-	aafBool			found = AAFFalse;
+	aafBool			found = kAAFFalse;
 	aafInt32		error;
 	aafLength_t		len, numSamples;
 	aafUInt32		sampleSize;
@@ -238,7 +256,7 @@ AAFRESULT STDMETHODCALLTYPE
 			{
 				// TEST: Path #1 through code
 				*pOffset = testOffset;
-				found = AAFTrue;
+				found = kAAFTrue;
 			}
 			else
 			{
@@ -254,7 +272,7 @@ AAFRESULT STDMETHODCALLTYPE
 					{
 						// TEST: Path #2 through code
 						*pOffset = testOffset;
-						found = AAFTrue;
+						found = kAAFTrue;
 					}
 				}
 			}
@@ -306,7 +324,7 @@ AAFRESULT STDMETHODCALLTYPE
 				{
 					// TEST: Path #6 through code
 					*pOffset = testOffset;
-					found = AAFTrue;
+					found = kAAFTrue;
 					break;
 				}
 			}
@@ -325,13 +343,13 @@ AAFRESULT STDMETHODCALLTYPE
 				{
 					// TEST: Path #7 through code
 					*pOffset = testOffset;
-					found = AAFTrue;
+					found = kAAFTrue;
 					break;
 				}
 			}
 		}
 
-		if(found == AAFFalse)
+		if(found == kAAFFalse)
 		{
 			// TEST: Path #8 through code
 			return AAFRESULT_TIMECODE_NOT_FOUND;
@@ -639,6 +657,5 @@ AAFRESULT STDMETHODCALLTYPE
 
   return result;
 }
-OMDEFINE_STORABLE(ImplAAFTimecodeStream, AUID_AAFTimecodeStream);
 
 
