0f3d77eb18a7a5529b5772b1bbcad649c06ec2e0
tbingham <tbingham>
2001-04-17 07:18:46 +0000

    Adding stub implementation files for private subclasses of AAFPropertyValue to support object reference, both singletons and collections (sets and vectors).

--- ref-impl/src/impl/ImplAAFWeakRefSetValue.cpp
+++ ref-impl/src/impl/ImplAAFWeakRefSetValue.cpp
@@ -1,35 +1,34 @@
-//=---------------------------------------------------------------------=
-//
-// The contents of this file are subject to the AAF SDK Public
-// Source License Agreement (the "License"); You may not use this file
-// except in compliance with the License.  The License is available in
-// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
-// Association or its successor.
-// 
-// Software distributed under the License is distributed on an "AS IS"
-// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
-// the License for the specific language governing rights and limitations
-// under the License.
-// 
-// The Original Code of this file is Copyright 1998-2001, Licensor of the
-// AAF Association.
-// 
-// The Initial Developer of the Original Code of this file and the
-// Licensor of the AAF Association is Avid Technology.
-// All rights reserved.
-//
-//=---------------------------------------------------------------------=
+/***********************************************************************
+ *
+ *              Copyright (c) 2000 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and accompanying 
+ * documentation, and to distribute and sublicense application software
+ * incorporating this software for any purpose is hereby granted, 
+ * provided that (i) the above copyright notice and this permission
+ * notice appear in all copies of the software and related documentation,
+ * and (ii) the name Avid Technology, Inc. may not be used in any
+ * advertising or publicity relating to the software without the specific,
+ *  prior written permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+ * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+ * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+ * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+ * LIABILITY.
+ *
+ ************************************************************************/
 
 #include "ImplAAFWeakRefSetValue.h"
 
 #ifndef __ImplAAFRoot_h__
 #include "ImplAAFRoot.h"
-#endif
-
-// TBD: Note the following include for ImplAAFClassDef.h really belongs
-// in ImplAAFTypeDefWeakObjRef.h!
-#ifndef __ImplAAFClassDef_h__
-#include "ImplAAFClassDef.h"
 #endif
 
 #ifndef __ImplAAFTypeDefWeakObjRef_h__
@@ -86,77 +85,3 @@ AAFRESULT STDMETHODCALLTYPE ImplAAFWeakRefSetValue::WriteTo(
 {
   return (ImplAAFRefSetValue::WriteTo(pOmProp));
 }
-
-
-
-
-  
-//
-// ImplAAFRefContainer overrides
-//
-
-// Return the type of object references in the container.
-ImplAAFTypeDefObjectRef * ImplAAFWeakRefSetValue::GetElementType(void) const // the result is NOT reference counted.
-{
-  ImplAAFTypeDefObjectRef * pContainerElementType = NULL;
-  ImplAAFTypeDefSet *pContainerType = NULL;
-  AAFRESULT result = AAFRESULT_SUCCESS;
-  ImplAAFTypeDefSP pType, pElementType;
-  
-  result = GetType(&pType);
-  assert(AAFRESULT_SUCCEEDED(result));
-  if (AAFRESULT_SUCCEEDED(result))
-  {
-    pContainerType = dynamic_cast<ImplAAFTypeDefSet *>((ImplAAFTypeDef *)pType); // extract obj from smartptr
-    assert(NULL != pContainerType);
-    if (NULL != pContainerType)
-    {
-      result = pContainerType->GetElementType(&pElementType);
-      assert(AAFRESULT_SUCCEEDED(result));
-      if (AAFRESULT_SUCCEEDED(result))
-      {
-        pContainerElementType = dynamic_cast<ImplAAFTypeDefWeakObjRef *>((ImplAAFTypeDef *)pElementType); // extract obj from smartptr
-      }
-    }
-  }
-  
-  assert(pContainerElementType);
-  return pContainerElementType;
-}
-
-
-// Perform specialized validation for an object before it is added
-// to a container.
-AAFRESULT ImplAAFWeakRefSetValue::ValidateNewObject(ImplAAFStorable *pNewObject) const
-{
-  assert (isInitialized());
-  if (!isInitialized())
-    return AAFRESULT_NOT_INITIALIZED;
-
-  assert(NULL != pNewObject);
-  if (!pNewObject->attached())
-  {
-    return AAFRESULT_OBJECT_NOT_ATTACHED;
-  }
- 
-  // Hand off to the OMReferenceContainer
-  OMReferenceContainer* pReferenceContainer = referenceContainer();
-
-  // Object refernce containers should only contain a single reference
-  // to an object.
-  if (pReferenceContainer->containsObject(pNewObject))
-  {
-    return AAFRESULT_TABLE_DUP_KEY; // Object is already in the set!
-  }
-
-  return AAFRESULT_SUCCESS;
-}
-
-
-// Perform any specialized cleanup of any object after it has been removed
-// from the container.
-bool ImplAAFWeakRefSetValue::usesReferenceCounting(void) const
-{
-  return false;
-}
-  
