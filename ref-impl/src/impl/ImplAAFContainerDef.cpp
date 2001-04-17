9292ebd2a69e03f01087fb7d9762a9702183e97c
tbingham <tbingham>
2001-04-17 05:24:42 +0000

    Remove Initialize() from IAAFDefObject() and put it on primary interfaces of concrete derived classes.

--- ref-impl/src/impl/ImplAAFContainerDef.cpp
+++ ref-impl/src/impl/ImplAAFContainerDef.cpp
@@ -1,11 +1,29 @@
-/************************************************\
-*												*
-* Advanced Authoring Format						*
-*												*
-* Copyright (c) 1998-1999 Avid Technology, Inc. *
-* Copyright (c) 1998-1999 Microsoft Corporation *
-*												*
-\************************************************/
+/***********************************************************************
+ *
+ *              Copyright (c) 1998-1999 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and accompanying 
+ * documentation, and to distribute and sublicense application software
+ * incorporating this software for any purpose is hereby granted, 
+ * provided that (i) the above copyright notice and this permission
+ * notice appear in all copies of the software and related documentation,
+ * and (ii) the name Avid Technology, Inc. may not be used in any
+ * advertising or publicity relating to the software without the specific,
+ * prior written permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+ * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+ * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+ * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+ * LIABILITY.
+ *
+ ************************************************************************/
 
 #include "AAFStoredObjectIDs.h"
 
@@ -28,15 +46,38 @@ _isIdentified(		PID_ContainerDefinition_EssenceIsIdentified, "EssenceIsIdentifie
 ImplAAFContainerDef::~ImplAAFContainerDef ()
 {}
 
+  
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFContainerDef::Initialize (
+      const aafUID_t & id,
+	  const aafWChar * pName,
+	  const aafWChar * pDesc)
+{
+	if (pName == NULL || pDesc == NULL)
+	{
+	  return AAFRESULT_NULL_PARAM;
+	}
+	else
+	{
+	  return pvtInitialize(id, pName, pDesc);
+	}
+	return AAFRESULT_SUCCESS;
+}
+
 
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFContainerDef::EssenceIsIdentified (
       aafBool *pEssenceIsIdentified)
 {
+	
 	if (pEssenceIsIdentified == NULL)
 	{
 		return AAFRESULT_NULL_PARAM;
 	}
+	
+	else if (!_isIdentified.isPresent())
+		return AAFRESULT_PROP_NOT_PRESENT;
+	
 	else
 	{
 		*pEssenceIsIdentified = _isIdentified;
@@ -56,6 +97,5 @@ AAFRESULT STDMETHODCALLTYPE
 }
 
 
-OMDEFINE_STORABLE(ImplAAFContainerDef, AUID_AAFContainerDef);
 
 
