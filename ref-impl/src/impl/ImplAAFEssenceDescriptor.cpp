56647492162b49653dbebf4a5cf383482c165dda
tbingham <tbingham>
2001-04-17 08:48:38 +0000

    Added impl for AAFSourceMob EssenceDesc and FileDesc

--- ref-impl/src/impl/ImplAAFEssenceDescriptor.cpp
+++ ref-impl/src/impl/ImplAAFEssenceDescriptor.cpp
@@ -1,30 +1,20 @@
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- *  prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
 
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
 
 #ifndef __ImplAAFLocator_h__
 #include "ImplAAFLocator.h"
@@ -35,8 +25,7 @@
 #endif
 
 
-#include "AAFStoredObjectIDs.h"
-#include "AAFPropertyIDs.h"
+
 
 
 
@@ -46,36 +35,20 @@
 
 #include <assert.h>
 #include "AAFResult.h"
-#include "aafErr.h"
-#include "ImplAAFObjectCreation.h"
-
-extern "C" const aafClassID_t CLSID_EnumAAFLocators;
 
 ImplAAFEssenceDescriptor::ImplAAFEssenceDescriptor ()
-: _locators(         PID_EssenceDescriptor_Locator,          "Locator")
+: _locators(         PID_ESSENCE_DESC_LOCATORS,          "locators")
 {
   _persistentProperties.put(_locators.address());
 }
 
 
 ImplAAFEssenceDescriptor::~ImplAAFEssenceDescriptor ()
-{
-	// Release all of the locator pointers.
-	size_t size = _locators.getSize();
-	for (size_t i = 0; i < size; i++)
-	{
-		ImplAAFLocator *pLocator = _locators.setValueAt(0, i);
-		if (pLocator)
-		{
-		  pLocator->ReleaseReference();
-		  pLocator = 0;
-		}
-	}
-}
+{}
 
 
 AAFRESULT STDMETHODCALLTYPE
-    ImplAAFEssenceDescriptor::CountLocators (aafUInt32 *pCount)
+    ImplAAFEssenceDescriptor::GetNumLocators (aafInt32 *pCount)
 {
 	size_t	siz;
 	if (! pCount)
@@ -93,150 +66,58 @@ AAFRESULT STDMETHODCALLTYPE
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFEssenceDescriptor::AppendLocator (ImplAAFLocator *pLocator)
 {
-	if(pLocator == NULL)
-		return(AAFRESULT_NULL_PARAM);
-
 	_locators.appendValue(pLocator);
-	pLocator->AcquireReference();
-
 	return(AAFRESULT_SUCCESS);
 }
 
-  //@comm    Use this function to add a locator to be scanned last when searching for
+  //@comm    Use this function to add a locator to be scanned first when searching for
   // the essence (a new primary location).
 
 AAFRESULT STDMETHODCALLTYPE
-    ImplAAFEssenceDescriptor::PrependLocator (ImplAAFLocator *pLocator)
+    ImplAAFEssenceDescriptor::PrependLocator (ImplAAFLocator * /*pLocator*/)
 {
-	if(pLocator == NULL)
-		return(AAFRESULT_NULL_PARAM);
-
-	size_t			siz;
-	long			n;
-	ImplAAFLocator	*obj = NULL;
-
-	_locators.getSize(siz);
-	for(n = siz-1; n >= 0; n--)
-	{
-		_locators.getValueAt(obj, n);
-		_locators.setValueAt(NULL, n);
-		_locators.setValueAt(obj, n+1);
-	}
-	_locators.setValueAt(pLocator, 0);
-	pLocator->AcquireReference();
-
-	return AAFRESULT_SUCCESS;
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
 
   //@comm    Use this function to add a locator to be scanned first when searching for
   // the essence (a secondary location for the essence).
 
-
-AAFRESULT STDMETHODCALLTYPE
-    ImplAAFEssenceDescriptor::InsertLocatorAt (aafUInt32 index,
-											   ImplAAFLocator *pLocator)
-{
-  if (! pLocator) return AAFRESULT_NULL_PARAM;
-
-  aafUInt32 count;
-  AAFRESULT hr;
-  hr = CountLocators (&count);
-  if (AAFRESULT_FAILED (hr)) return hr;
-
-  if (index > count)
-	return AAFRESULT_BADINDEX;
-
-  return AAFRESULT_NOT_IMPLEMENTED;
-}
-
-
-AAFRESULT STDMETHODCALLTYPE
-    ImplAAFEssenceDescriptor::GetLocatorAt (aafUInt32 index,
-											ImplAAFLocator ** ppLocator)
-{
-  if (! ppLocator) return AAFRESULT_NULL_PARAM;
-
-  aafUInt32 count;
-  AAFRESULT hr;
-  hr = CountLocators (&count);
-  if (AAFRESULT_FAILED (hr)) return hr;
-
-  if (index >= count)
-	return AAFRESULT_BADINDEX;
-
-  return AAFRESULT_NOT_IMPLEMENTED;
-}
-
-
-AAFRESULT STDMETHODCALLTYPE
-    ImplAAFEssenceDescriptor::RemoveLocatorAt (aafUInt32 index)
-{
-  aafUInt32 count;
-  AAFRESULT hr;
-  hr = CountLocators (&count);
-  if (AAFRESULT_FAILED (hr)) return hr;
-
-  if (index >= count)
-	return AAFRESULT_BADINDEX;
-
-  return AAFRESULT_NOT_IMPLEMENTED;
-}
-
-
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFEssenceDescriptor::RemoveLocator (ImplAAFLocator * /*pLocator*/)
 {
-  return AAFRESULT_NOT_IN_CURRENT_VERSION;
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
 
 
 AAFRESULT STDMETHODCALLTYPE
-    ImplAAFEssenceDescriptor::GetLocators (ImplEnumAAFLocators **ppEnum)
+    ImplAAFEssenceDescriptor::EnumAAFAllLocators (ImplEnumAAFLocators **ppEnum)
 {
+#if FULL_TOOLKIT
 	ImplEnumAAFLocators		*theEnum = (ImplEnumAAFLocators *)CreateImpl (CLSID_EnumAAFLocators);
 		
 	XPROTECT()
 	{
-		CHECK(theEnum->SetEnumStrongProperty(this, &_locators));
+		CHECK(theEnum->SetEssenceDescriptor(this));
 		CHECK(theEnum->Reset());
 		*ppEnum = theEnum;
 	}
 	XEXCEPT
 	{
-		if (theEnum)
-		  theEnum->ReleaseReference();
-		theEnum = 0;
 		return(XCODE());
 	}
 	XEND;
 	
 	return(AAFRESULT_SUCCESS);
+#else
+	return AAFRESULT_NOT_IMPLEMENTED;
+#endif
 }
 
   //@comm The number of locators may be zero if the essence is in the current file.
 
-AAFRESULT STDMETHODCALLTYPE
-    ImplAAFEssenceDescriptor::GetOwningMobKind (aafMobKind_t *pMobKind)
-{
-	*pMobKind = kAAFAllMob;		// Abstract superclass, only match "all"
-	return(AAFRESULT_SUCCESS);
-}
 
-// Internal to the toolkit functions
-AAFRESULT
-    ImplAAFEssenceDescriptor::GetNthLocator (aafInt32 index, ImplAAFLocator **ppLocator)
-{
-	if(ppLocator == NULL)
-		return(AAFRESULT_NULL_PARAM);
 
-	ImplAAFLocator	*obj = NULL;
-	_locators.getValueAt(obj, index);
-	*ppLocator = obj;
-	if (obj)
-		obj->AcquireReference();
-	else
-		return AAFRESULT_NO_MORE_OBJECTS; // AAFRESULT_BADINDEX ???
+extern "C" const aafClassID_t CLSID_AAFEssenceDescriptor;
 
-	return AAFRESULT_SUCCESS;
-}
+OMDEFINE_STORABLE(ImplAAFEssenceDescriptor, CLSID_AAFEssenceDescriptor);
 
