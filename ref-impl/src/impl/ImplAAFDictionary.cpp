53ba4ac7fa19ce8af81b0367692b32462575b0a6
tbingham <tbingham>
2001-04-17 09:23:18 +0000

    Remove ObjRef and ObjRefArray from critical types list since they are now no longer builtin types; add code to attempt registration of struct and enum offsets/sizes for builtin types.

--- ref-impl/src/impl/ImplAAFDictionary.cpp
+++ ref-impl/src/impl/ImplAAFDictionary.cpp
@@ -1,10 +1,29 @@
-/************************************************\
-*												*
-* Advanced Authoring Format						*
-*												*
-* Copyright (c) 1998-1999 Avid Technology, Inc. *
-*												*
-\************************************************/
+/***********************************************************************
+ *
+ *              Copyright (c) 1998-1999 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and accompanying 
+ * documentation, and to distribute and sublicense application software
+ * incorporating this software for any purpose is hereby granted, 
+ * provided that (i) the above copyright notice and this permission
+ * notice appear in all copies of the software and related documentation,
+ * and (ii) the name Avid Technology, Inc. may not be used in any
+ * advertising or publicity relating to the software without the specific,
+ *  prior written permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+ * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+ * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+ * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+ * LIABILITY.
+ *
+ ************************************************************************/
 #ifndef __ImplAAFDictionary_h__
 #include "ImplAAFDictionary.h"
 #endif
@@ -2072,11 +2091,11 @@ void ImplAAFDictionary::pvtInitObjectProperties
 
 const static aafUID_t * sCriticalTypes[] =
 {
-  &kAAFTypeID_ObjRef,
-  &kAAFTypeID_WCharString,
+//  &kAAFTypeID_ObjRef,
+  &kAAFTypeID_String,
   &kAAFTypeID_AUID,
   &kAAFTypeID_AUIDArray,
-  &kAAFTypeID_ObjRefArray,
+//  &kAAFTypeID_ObjRefArray,
 };
 
 
@@ -2159,6 +2178,7 @@ ImplAAFObjectSP ImplAAFDictionary::pvtObjFifo::GetNext (void)
   return result;
 }
 
+
 void ImplAAFDictionary::pvtObjFifo::Append (ImplAAFObject * obj)
 {
   assert (obj);
@@ -2167,4 +2187,15 @@ void ImplAAFDictionary::pvtObjFifo::Append (ImplAAFObject * obj)
 }
 
 
+void ImplAAFDictionary::pvtAttemptBuiltinSizeRegistration
+  (ImplAAFTypeDefEnum * ptde) const
+{
+  ImplAAFBuiltinTypes::RegisterExistingType (ptde);
+}
 
+
+void ImplAAFDictionary::pvtAttemptBuiltinSizeRegistration
+  (ImplAAFTypeDefRecord * ptdr) const
+{
+  ImplAAFBuiltinTypes::RegisterExistingType (ptdr);
+}
