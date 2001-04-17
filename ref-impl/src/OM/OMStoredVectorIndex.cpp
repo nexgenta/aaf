e619b39dd07999268e22c8e64bccded14c8d1350
tbingham <tbingham>
2001-04-17 07:12:57 +0000

    Name changes and autoduck clean up for clarity and consistency with upcoming set index.

--- ref-impl/src/OM/OMStoredVectorIndex.cpp
+++ ref-impl/src/OM/OMStoredVectorIndex.cpp
@@ -1,31 +1,61 @@
+/***********************************************************************
+*
+*              Copyright (c) 1998-1999 Avid Technology, Inc.
+*
+* Permission to use, copy and modify this software and accompanying
+* documentation, and to distribute and sublicense application software
+* incorporating this software for any purpose is hereby granted,
+* provided that (i) the above copyright notice and this permission
+* notice appear in all copies of the software and related documentation,
+* and (ii) the name Avid Technology, Inc. may not be used in any
+* advertising or publicity relating to the software without the specific,
+* prior written permission of Avid Technology, Inc.
+*
+* THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+* WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+* IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+* SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+* OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+* OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+* ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+* RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+* ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+* LIABILITY.
+*
+************************************************************************/
+
 // @doc OMINTERNAL
 #include "OMStoredVectorIndex.h"
 #include "OMAssertions.h"
 
-OMStoredVectorIndex::OMStoredVectorIndex(size_t size)
-: _highWaterMark(0), _size(size), _entries(0), _keys(0)
+  // @mfunc Constructor.
+  //   @parm The capacity of this <c OMStoredVectorIndex>.
+OMStoredVectorIndex::OMStoredVectorIndex(size_t capacity)
+: _highWaterMark(0), _capacity(capacity), _entries(0), _names(0)
 {
   TRACE("OMStoredVectorIndex::OMStoredVectorIndex");
 
-  _keys = new OMUInt32[_size];
-  ASSERT("Valid heap pointer", _keys != 0);
+  _names = new OMUInt32[_capacity];
+  ASSERT("Valid heap pointer", _names != 0);
 
-  for (size_t i = 0; i < _size; i++) {
-    _keys[i] = 0;
+  for (size_t i = 0; i < _capacity; i++) {
+    _names[i] = 0;
   }
 }
 
+  // @mfunc Destructor.
 OMStoredVectorIndex::~OMStoredVectorIndex(void)
 {
   TRACE("OMStoredVectorIndex::~OMStoredVectorIndex");
 
-  delete [] _keys;
-  _keys = 0;
+  delete [] _names;
+  _names = 0;
 }
 
-  // @mfunc The high water mark in the set of keys assigned to
+  // @mfunc The high water mark in the set of names assigned to
   //        this <c OMStoredVectorIndex>.
-  //   @rdesc The highest previously allocated key.
+  //   @rdesc The highest previously allocated name.
   //   @this const
 OMUInt32 OMStoredVectorIndex::highWaterMark(void) const
 {
@@ -35,17 +65,23 @@ OMUInt32 OMStoredVectorIndex::highWaterMark(void) const
 }
 
   // @mfunc Insert a new element in this <c OMStoredVectorIndex>
-  //        at position <p position> with key <p key>.
+  //        at position <p position> with name <p name>.
+  //        The name of an element is an integer. Names are assigned
+  //        such that the names of existing elements do not have to
+  //        change when other elements are added to or removed from
+  //        the associated <c OMStrongReferenceVector>. The name is
+  //        independent of the element's logical or physical position
+  //        within the associated <c OMStrongReferenceVector>.
   //   @parm The position at which the new element should be inserted.
-  //   @parm The key assigned to the element.
-void OMStoredVectorIndex::insert(size_t position, OMUInt32 key)
+  //   @parm The name assigned to the element.
+void OMStoredVectorIndex::insert(size_t position, OMUInt32 name)
 {
   TRACE("OMStoredVectorIndex::insert");
-  PRECONDITION("Valid position", position < _size);
+  PRECONDITION("Valid position", position < _capacity);
 
   // Assumes sequential insertion.
 
-  _keys[position] = key;
+  _names[position] = name;
   _highWaterMark = _highWaterMark + 1;
   _entries = _entries + 1;
 }
@@ -63,14 +99,14 @@ size_t OMStoredVectorIndex::entries(void) const
   // @mfunc Iterate over the elements in this <c OMStoredVectorIndex>.
   //   @parm Iteration context. Set this to 0 to start with the
   //         "first" element.
-  //   @parm The key of the "current" element.
+  //   @parm The name of the "current" element.
   //   @this const
-void OMStoredVectorIndex::iterate(size_t& context, OMUInt32& key) const
+void OMStoredVectorIndex::iterate(size_t& context, OMUInt32& name) const
 {
   TRACE("OMStoredVectorIndex::iterate");
-  PRECONDITION("Valid context", context < _size);
+  PRECONDITION("Valid context", context < _capacity);
 
-  key = _keys[context];
+  name = _names[context];
   context = context + 1;
 }
 
--- ref-impl/src/OM/OMStoredVectorIndex.h
+++ ref-impl/src/OM/OMStoredVectorIndex.h
@@ -1,9 +1,38 @@
+/***********************************************************************
+*
+*              Copyright (c) 1998-1999 Avid Technology, Inc.
+*
+* Permission to use, copy and modify this software and accompanying
+* documentation, and to distribute and sublicense application software
+* incorporating this software for any purpose is hereby granted,
+* provided that (i) the above copyright notice and this permission
+* notice appear in all copies of the software and related documentation,
+* and (ii) the name Avid Technology, Inc. may not be used in any
+* advertising or publicity relating to the software without the specific,
+* prior written permission of Avid Technology, Inc.
+*
+* THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+* WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+* IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+* SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+* OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+* OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+* ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+* RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+* ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+* LIABILITY.
+*
+************************************************************************/
+
 // @doc OMINTERNAL
 #ifndef OMSTOREDVECTORINDEX_H
 #define OMSTOREDVECTORINDEX_H
 
 #include "OMPortability.h"
-#include "OMTypes.h"
+#include "OMDataTypes.h"
+
+#include <stddef.h>
 
   // @class The in-memory representation of the on-disk index for a
   //        stored object vector.
@@ -12,38 +41,40 @@ public:
   // @access Public members.
 
     // @cmember Constructor.
-  OMStoredVectorIndex(size_t size);
+  OMStoredVectorIndex(size_t capacity);
 
     // @cmember Destructor.
   ~OMStoredVectorIndex(void);
 
-    // @cmember The high water mark in the set of keys assigned to
+    // @cmember The high water mark in the set of names assigned to
     //          this <c OMStoredVectorIndex>.
-    // @this const
   OMUInt32 highWaterMark(void) const;
 
     // @cmember Insert a new element in this <c OMStoredVectorIndex>
-    //          at position <p position> with key <p key>.
-  void insert(size_t position, OMUInt32 key);
+    //          at position <p position> with name <p name>.
+    //          The name of an element is an integer. Names are assigned
+    //          such that the names of existing elements do not have to
+    //          change when other elements are added to or removed from
+    //          the associated <c OMStrongReferenceVector>. The name is
+    //          independent of the element's logical or physical position
+    //          within the associated <c OMStrongReferenceVector>.
+  void insert(size_t position, OMUInt32 name);
 
     // @cmember The number of elements in this <c OMStoredVectorIndex>.
-    // @this const
   size_t entries(void) const;
 
     // @cmember Iterate over the elements in this <c OMStoredVectorIndex>.
-    // @this const
-  void iterate(size_t& context, OMUInt32& key) const;
+  void iterate(size_t& context, OMUInt32& name) const;
 
     // @cmember Is this <c OMStoredVectorIndex> valid ?
-    // @this const
   bool isValid(void) const;
 
 private:
 
   OMUInt32 _highWaterMark;
-  size_t _size;
+  size_t _capacity;
   size_t _entries;
-  OMUInt32* _keys;
+  OMUInt32* _names;
 };
 
 #endif
