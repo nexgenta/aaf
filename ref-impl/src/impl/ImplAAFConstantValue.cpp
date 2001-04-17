47aca1bfd419a2960bd70fb0e27eca004746355a
tbingham <tbingham>
2001-04-17 04:19:09 +0000

    Start implementing effects classes

--- ref-impl/src/impl/ImplAAFConstantValue.cpp
+++ ref-impl/src/impl/ImplAAFConstantValue.cpp
@@ -1,29 +1,22 @@
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- *  prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+
+
+/***********************************************\
+*												*
+* Advanced Authoring Format						*
+*												*
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+* Copyright (c) 1998-1999 Microsoft Corporation *
+*												*
+\***********************************************/
 
 #ifndef __ImplAAFDataDef_h__
 #include "ImplAAFDataDef.h"
@@ -36,29 +29,29 @@
 #include "AAFStoredObjectIDs.h"
 #include "AAFPropertyIDs.h"
 
-#ifndef __ImplAAFConstantValue_h__
-#include "ImplAAFConstantValue.h"
+#ifndef __ImplAAFConstValue_h__
+#include "ImplAAFConstValue.h"
 #endif
 
 #include <assert.h>
 #include <string.h>
 
 
-ImplAAFConstantValue::ImplAAFConstantValue ()
+ImplAAFConstValue::ImplAAFConstValue ()
 : _value(			PID_ConstantValue_Value,			"Value")
 {
 	_persistentProperties.put(_value.address());
 }
 
 
-ImplAAFConstantValue::~ImplAAFConstantValue ()
+ImplAAFConstValue::~ImplAAFConstValue ()
 {}
 
 
 	
 /****/
 AAFRESULT STDMETHODCALLTYPE
-    ImplAAFConstantValue::GetValue (
+    ImplAAFConstValue::GetValue (
       aafUInt32  valueSize,
       aafDataBuffer_t  pValue,
       aafUInt32*  bytesRead)
@@ -78,7 +71,7 @@ AAFRESULT STDMETHODCALLTYPE
 
 
 AAFRESULT STDMETHODCALLTYPE
-    ImplAAFConstantValue::GetValueBufLen (
+    ImplAAFConstValue::GetValueBufLen (
       aafUInt32 *pLen)
 {
 	if(pLen == NULL)
@@ -92,7 +85,7 @@ AAFRESULT STDMETHODCALLTYPE
 
 /****/
 AAFRESULT STDMETHODCALLTYPE
-    ImplAAFConstantValue::SetValue (
+    ImplAAFConstValue::SetValue (
       aafUInt32  valueSize,
       aafDataBuffer_t  pValue)
 {
@@ -105,5 +98,6 @@ AAFRESULT STDMETHODCALLTYPE
 }
 
 
+OMDEFINE_STORABLE(ImplAAFConstValue, AUID_AAFConstValue);
 
 
