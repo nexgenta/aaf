1a4a36370885de07a4d5f106121b44703030ffe4
tbingham <tbingham>
2001-04-17 07:42:31 +0000

    Removed unreferenced variables.

--- examples/com-api/ComPropDirectDump/dumper.cpp
+++ examples/com-api/ComPropDirectDump/dumper.cpp
@@ -41,7 +41,9 @@
 #include <iostream.h>
 #include <iomanip.h>
 #include <fstream.h>
+#include <stdlib.h>
 #include <stdio.h> // for sprintf()
+#include <string.h>
 
 #ifndef __AAFSmartPointer_h__
 #include "AAFSmartPointer.h"
@@ -211,8 +213,11 @@ static void printTimeStamp (const aafTimeStamp_t & ts,
 	}
 	else
 	{
+		// I replaced assert(month_index >= 0) 2 lines below
+		// with this one because the value is unsigned and will
+		// never be negative. - Alex, 11-Sep-00
+		assert (ts.date.month > 0 );
 		aafUInt8 month_index = ts.date.month-1;
-		assert (month_index >= 0);
 		assert (month_index < (sizeof (monthNames) / sizeof (monthNames[0])));
 		strcpy (monthNameBuf, monthNames[month_index]);
 	}
