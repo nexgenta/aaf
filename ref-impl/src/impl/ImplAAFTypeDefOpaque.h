5d762f9a2f3ed168d1a620650c31a71cdb30793e
tbingham <tbingham>
2001-04-17 04:24:45 +0000

    Initial checkin for TypeDefOpaque.

--- ref-impl/src/impl/ImplAAFTypeDefOpaque.h
+++ ref-impl/src/impl/ImplAAFTypeDefOpaque.h
@@ -63,68 +63,11 @@ public:
          // @parm [out] the type id of the actual data
          aafUID_t *  pActualTypeID);
 
-  virtual AAFRESULT STDMETHODCALLTYPE
-   GetHandle (
-    // value to get data from.
-    ImplAAFPropertyValue * pPropVal,
-
-    // Size of preallocated buffer
-    aafUInt32  handleSize,
-
-    // Preallocated buffer to hold handle
-    aafDataBuffer_t  pHandle,
-
-    // Number of actual bytes read
-    aafUInt32*  bytesRead);
-
-  virtual AAFRESULT STDMETHODCALLTYPE
-   GetHandleBufLen (
-    // value to set.
-   ImplAAFPropertyValue * pPropVal,
-
-    // Pointer to an variable used to return the length
-   aafUInt32 *  pLen);
-
-  virtual AAFRESULT STDMETHODCALLTYPE
-	SetHandle (
-    // value to write data to.
-    ImplAAFPropertyValue * pPropVal,
-
-    // Size of preallocated buffer
-	aafUInt32  handleSize,
-
-    // buffer containing handle
-	aafDataBuffer_t  pHandle);
-
-  virtual AAFRESULT STDMETHODCALLTYPE
-    CreateValueFromHandle (
-    // pointer to buffer containing handle to use
-     aafMemPtr_t  pInitData,
-
-    // size of handle in pInitData
-    aafUInt32  initDataSize,
-
-    // newly created property value
-    ImplAAFPropertyValue ** ppOpaquePropertyValue);
-  
   // Override from AAFTypeDef
   virtual AAFRESULT STDMETHODCALLTYPE
     GetTypeCategory (/*[out]*/ eAAFTypeCategory_t *  pTid);
 
 public:
-
-  //****************
-  // Initialize() 
-  //   Called when we initialize as one of the "builtin" types.
-  //
-  AAFRESULT
-    Initialize
-        (// @parm [in, ref] auid to be used to identify this type
-         aafUID_constref  id,
-
-         // @parm [in, ref, string] friendly name of this type definition
-         aafCharacter_constptr  pTypeName);
-
 
   //****************
   // pvtInitialize() 
@@ -133,25 +76,11 @@ public:
   AAFRESULT
     pvtInitialize
         (// @parm [in, ref] auid to be used to identify this type
-       aafUID_constref  id,
+         aafUID_constref  id,
 
-       // @parm [in, ref, string] friendly name of this type definition
-       aafCharacter_constptr  pTypeName,
-       
-       // @parm [in] the type definition for kAAFTypeID_AUID.
-       ImplAAFTypeDef *pTypeDefAUID,
+         // @parm [in, ref, string] friendly name of this type definition
+         aafCharacter_constptr  pTypeName);
 
-       // @parm [in] the dictionary for this instance
-       ImplAAFDictionary *pDictionary);
-
-
-
-  // override from OMStorable.
-  virtual const OMClassId& classId(void) const;
-
-  // Override callbacks from OMStorable
-  virtual void onSave(void* clientContext) const;
-  virtual void onRestore(void* clientContext) const;
 
 protected:
   // Find the actual type definition from the dictionary.
@@ -160,19 +89,7 @@ protected:
   virtual AAFRESULT LookupActualType (aafUID_constref typeID, 
                                       ImplAAFTypeDef ** ppActualType) const;
 
-
-private:
-  //
-  // Utility to make sure that the given property value is for an opaque type.
-  //
-  AAFRESULT ValidateOpaquePropertyValue(ImplAAFPropertyValue * pOpaquePropertyValue);
-
-  //
-  // Utility to make sure that the given property value is for an opaque type.
-  //
-  AAFRESULT GetOpaqueHandleInfo(aafUInt32 handleSize, aafDataBuffer_t pHandle,
-                                aafUInt32& opaqueDataSize, aafDataBuffer_t& opaqueDataBits);
-
+	
 };
 
 #endif // ! __ImplAAFTypeDefOpaque_h__
