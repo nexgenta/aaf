f302f4ac0aed822ee93c3732a9e7bf85bdff24c9
tbingham <tbingham>
2001-04-17 09:21:22 +0000

    As initially generated by dodo.

--- ref-impl/src/impl/ImplAAFClassDef.h
+++ ref-impl/src/impl/ImplAAFClassDef.h
@@ -3,59 +3,35 @@
 #ifndef __ImplAAFClassDef_h__
 #define __ImplAAFClassDef_h__
 
-/***********************************************************************
- *
- *              Copyright (c) 1998-2000 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- * prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
 
-class ImplAAFObject;
-class ImplAAFDefObject;
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+
+class ImplEnumAAFPropertyDefs;
+
 class ImplAAFPropertyDef;
 
-template <class T> 
-class ImplAAFEnumerator;
-typedef ImplAAFEnumerator<ImplAAFPropertyDef> ImplEnumAAFPropertyDefs;
+class ImplAAFDefObject;
 
-#ifndef __ImplAAFMetaDefinition_h__
-#include "ImplAAFMetaDefinition.h"
+class ImplAAFTypeDef;
+
+
+
+
+
+#ifndef __ImplAAFDefObject_h__
+#include "ImplAAFDefObject.h"
 #endif
 
-#ifndef __ImplAAFPropertyDef_h__
-#include "ImplAAFPropertyDef.h"
-#endif
 
-#ifndef __ImplAAFTypeDef_h__
-#include "ImplAAFTypeDef.h"
-#endif
-
-#include "OMClassDefinition.h"
-#include "OMWeakRefProperty.h"
-#include "OMStrongRefSetProperty.h"
-
-class ImplAAFClassDef : public ImplAAFMetaDefinition,
-                        public OMClassDefinition
+class ImplAAFClassDef : public ImplAAFDefObject
 {
 public:
   //
@@ -68,24 +44,6 @@ protected:
   virtual ~ImplAAFClassDef ();
 
 public:
-
-  //****************
-  // Initialize()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    Initialize
-        // @parm [in] auid to be used to identify this type
-        (const aafUID_t & classID,
-
-		// Inheritance parent of this class
-		ImplAAFClassDef * pParentClass,
-
-		// Human-legible name
-		const aafCharacter * pClassName,
-
-		// Can objects of this class be instantiated
-		aafBool isConcrete);
-
 
   //****************
   // GetPropertyDefs()
@@ -106,42 +64,21 @@ public:
 
 
   //****************
-  // RegisterNewPropertyDef()
+  // AppendNewPropertyDef()
   //
   virtual AAFRESULT STDMETHODCALLTYPE
-    RegisterNewPropertyDef
+    AppendNewPropertyDef
         (// @parm [in] auid to be used to identify this property
-         const aafUID_t & id,
+         aafUID_t *  pID,
 
          // @parm [in, string] name of the new property
-         const aafCharacter *  pName,
+         wchar_t *  pName,
 
          // @parm [in] type of the new property
          ImplAAFTypeDef * pTypeDef,
 
          // @parm [in] true if new property is to be optional
          aafBool  isOptional,
-
-         // @parm [in] true if new property is a unique identifier
-         aafBool  isUniqueIdentifier,
-
-         // @parm [out] return pointer to newly created property def
-         ImplAAFPropertyDef ** ppPropDef);
-
-
-  //****************
-  // RegisterOpionalPropertyDef()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    RegisterOptionalPropertyDef
-        (// @parm [in] auid to be used to identify this property
-         const aafUID_t & id,
-
-         // @parm [in, string] name of the new property
-         const aafCharacter *  pName,
-
-         // @parm [in] type of the new property
-         ImplAAFTypeDef * pTypeDef,
 
          // @parm [out] return pointer to newly created property def
          ImplAAFPropertyDef ** ppPropDef);
@@ -153,10 +90,31 @@ public:
   virtual AAFRESULT STDMETHODCALLTYPE
     LookupPropertyDef
         (// @parm [in] auid reprepresenting property to look up
-         const aafUID_t & propID,
+         aafUID_t *  pPropID,
 
          // @parm [out] resulting property definition
-         ImplAAFPropertyDef ** ppPropDef) const;
+         ImplAAFPropertyDef ** ppPropDef);
+
+
+  //****************
+  // GetName()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetName
+        (// @parm [out, string, size_is(bufSize)] buffer into which name is to be written
+         wchar_t *  pName,
+
+         // @parm [in] size of *pName buffer in bytes
+         aafUInt32  bufSize);
+
+
+  //****************
+  // GetNameBufLen()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetNameBufLen
+        // @parm [out] size of required buffer, in bytes
+        (aafUInt32 *  pBufSize);
 
 
   //****************
@@ -165,235 +123,19 @@ public:
   virtual AAFRESULT STDMETHODCALLTYPE
     GetParent
         // @parm [out, retval] parent class definition
-        (ImplAAFClassDef ** ppClassDef);
-
-
-  //****************
-  // CreateInstance()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    CreateInstance
-        // @parm [out, retval] newly created object
-        (ImplAAFObject ** ppobject);
-
-  //****************
-  // IsRoot()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    IsRoot
-        (aafBool* isRootClass);
-
-  //****************
-  // IsConcrete()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    IsConcrete
-        (aafBool* pResult);
-
-  //****************
-  // IsUniquelyIdentified
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    IsUniquelyIdentified
-        (aafBoolean_t * isUniquelyIdentified);
-
-  //****************
-  // GetUniqueIdentifier
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    GetUniqueIdentifier
-        (ImplAAFPropertyDef ** ppUniqueIdentifier);
+        (ImplAAFClassDef ** pClassDef);
 
 
 public:
-
-	bool
-    PvtIsPropertyDefRegistered (
-      aafUID_constref propId);
-
-  //****************
-  // SetParent()
+  // Declare this class to be storable.
   //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    SetParent
-        (ImplAAFClassDef *pClassDef);
+  OMDECLARE_STORABLE(ImplAAFClassDef)
 
-  //****************
-  // SetBootstrapParent()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    SetBootstrapParent
-        (ImplAAFClassDef *pClassDef);
-
-  //****************
-  // pvtInitialize()
-  //
-  // Same as Initialize(), but takes guid for parent class instead of
-  // object pointer.
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    pvtInitialize
-        // @parm [in] auid to be used to identify this type
-        (const aafUID_t & classID,
-
-		// Inheritance parent of this class
-		ImplAAFClassDef * pParentClass,
-
-		// Human-legible name
-		const aafCharacter * pClassName,
-
-		// Can objects of this class be instantiated
-		aafBool isConcrete);
-
-
-  // Returns true if this class can be instantiated.
-  aafBool pvtIsConcrete () const;
-
-  // Private method to set the "IsConcrete" property (used for bootstrap).
-  void pvtSetIsConcrete (aafBoolean_t isConcrete);
-
-  // Private method to unconditionally register a property def (ignoring
-  // whether or not property is optional or not, or if this class has
-  // already been registered).
-  AAFRESULT STDMETHODCALLTYPE
-    pvtRegisterPropertyDef
-        (const aafUID_t & id,
-         const aafCharacter *  pName,
-         const aafUID_t & typeId,
-         aafBool  isOptional,
-         aafBool  isUniqueIdentifier,
-         ImplAAFPropertyDef ** ppPropDef);
-
-
-  // Appends an existing property def object.
-  AAFRESULT STDMETHODCALLTYPE
-    pvtRegisterExistingPropertyDef
-        (// PropertyDef to append
-		 ImplAAFPropertyDef * pPropDef);
-
-
-  //****************
-  // LookupPropertyDefbyOMPid()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    LookupPropertyDefbyOMPid
-        (// @parm [in] pid reprepresenting property to look up
-         OMPropertyId omPid,
-
-         // @parm [out] resulting property definition
-         ImplAAFPropertyDef ** ppPropDef) const;
-
-  // Make sure that the type definition of each property definition
-  // has been loaded into memory.
-  void AssurePropertyTypesLoaded ();
-
-  // Find the unique identifier property defintion for this class or any parent class
-  // (RECURSIVE)
-  ImplAAFPropertyDef * pvtGetUniqueIdentifier(void); // result is NOT reference counted.
-
-
-  // override from OMStorable.
-  virtual const OMClassId& classId(void) const;
-
-  // Override callbacks from OMStorable
-  virtual void onSave(void* clientContext) const;
-  virtual void onRestore(void* clientContext) const;
-
-
-  // Method is called after associated class has been added to MetaDictionary.
-  // If this method fails the class is removed from the MetaDictionary and the
-  // registration method will fail.
-  virtual HRESULT CompleteClassRegistration(void);
-
-private:
-
-  //
-  // Private implementation classes to share lookup code between
-  // methods which lookup by auid, and ones which look up by OMPid.
-  //
-  // Abstract base class which allows comparison of a property
-  // identifier to a property definition.
-  class pvtPropertyIdentifier
-  {
-  public:
-	// Returns kAAFTrue if this property identifier matches the given
-	// property definition.
-	virtual aafBool DoesMatch
-    (const ImplAAFPropertyDef * pTestPropDef) const = 0;
-  };
-
-  //
-  // Concrete class implementing property identifier as OM PID
-  //
-  class pvtPropertyIdentifierOMPid : public pvtPropertyIdentifier
-  {
-  public:
-	pvtPropertyIdentifierOMPid (const OMPropertyId & id)
-	  : _id (id) {}
-
-	aafBool DoesMatch (const ImplAAFPropertyDef * pTestPropDef) const;
-	
-  private:
-	OMPropertyId _id;
-  };
-
-  //
-  // Concrete class implementing property identifier as a Property ID
-  // AUID.
-  //
-  class pvtPropertyIdentifierAUID : public pvtPropertyIdentifier
-  {
-  public:
-	pvtPropertyIdentifierAUID (const aafUID_t & id)
-	  : _id (id) {}
-
-	aafBool DoesMatch (const ImplAAFPropertyDef * pTestPropDef) const;
-
-  private:
-	aafUID_t _id;
-  };
-
-
-  // Provide two level property search routines that are overloaded.
-
-  // Low-level recursive methods the OM find method instead of a linear search
-  // that will necessarily load all of the property definitions for the class
-  // definition instance.
-  AAFRESULT
-    generalLookupPropertyDef (
-      aafUID_constref propId,
-      ImplAAFPropertyDef ** ppPropDef);
-
-  //
-  // The generalized lookup method which uses a pvtPropertyIdentifier
-  // as the property ID.
-  //
-  AAFRESULT 
-    generalLookupPropertyDef (
-      const pvtPropertyIdentifier & propId,
-      ImplAAFPropertyDef ** ppPropDef);
-
-  // OMWeakReferenceProperty<ImplAAFClassDef> _ParentClass;
-  OMWeakReferenceProperty<ImplAAFClassDef>         _ParentClass;
-
-  OMStrongReferenceSetProperty<OMUniqueObjectIdentification, ImplAAFPropertyDef> _Properties;
-
-  OMFixedSizeProperty<aafBool> _IsConcrete;
-
-  ImplAAFClassDef	*_BootstrapParent;
-
-  bool _propTypesLoaded;
+  // Declare the module test method. The implementation of the will be be
+  // in /test/ImplAAFClassDefTest.cpp.
+  static AAFRESULT test();
 };
 
-//
-// smart pointer
-//
-
-#ifndef __ImplAAFSmartPointer_h__
-// caution! includes assert.h
-#include "ImplAAFSmartPointer.h"
-#endif
-
-typedef ImplAAFSmartPointer<ImplAAFClassDef> ImplAAFClassDefSP;
-
 #endif // ! __ImplAAFClassDef_h__
+
+
