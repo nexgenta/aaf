9a68f582161dabaa98a4c54320c7c0aa2b770e6a
tbingham <tbingham>
2001-04-17 05:44:58 +0000

    adding liability comments

--- test/com/ComModTestAAF/ModuleTests/CAAFScopeReferenceTest.cpp
+++ test/com/ComModTestAAF/ModuleTests/CAAFScopeReferenceTest.cpp
@@ -1,24 +1,41 @@
 // @doc INTERNAL
-// @com This file implements the module test for CAAFDefinitionObject
-/******************************************\
-*                                          *
-* Advanced Authoring Format                *
-*                                          *
-* Copyright (c) 1998 Avid Technology, Inc. *
-* Copyright (c) 1998 Microsoft Corporation *
-*                                          *
-\******************************************/
+// @com This file implements the module test for CAAFScopeReference
+/***********************************************\
+*                                               *
+* Advanced Authoring Format                     *
+*                                               *
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+*                                               *
+\***********************************************/
 
 
 
-/******************************************\
-*                                          *
-* Advanced Authoring Format                *
-*                                          *
-* Copyright (c) 1998 Avid Technology, Inc. *
-* Copyright (c) 1998 Microsoft Corporation *
-*                                          *
-\******************************************/
+/***********************************************************************
+ *
+ *              Copyright (c) 1998-1999 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and accompanying 
+ * documentation, and to distribute and sublicense application software
+ * incorporating this software for any purpose is hereby granted, 
+ * provided that (i) the above copyright notice and this permission
+ * notice appear in all copies of the software and related documentation,
+ * and (ii) the name Avid Technology, Inc. may not be used in any
+ * advertising or publicity relating to the software without the specific,
+ *  prior written permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+ * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+ * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+ * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+ * LIABILITY.
+ *
+ ************************************************************************/
 
 
 /***********************************************\
@@ -37,62 +54,14 @@
 
 
 
-#include "CAAFScopeReference.h"
-#include "CAAFScopeReference.h"
-#ifndef __CAAFScopeReference_h__
-#error - improperly defined include guard
-#endif
+#include "AAFTypes.h" //Use #include "AAF.h" for functional module test.
+#include "AAFResult.h"
 
 
-#include <iostream.h>
-
-// Temporarily necessary global declarations.
-extern "C" const CLSID CLSID_AAFScopeReference; // generated
-
-
-HRESULT CAAFScopeReference::test()
+extern "C" HRESULT CAAFScopeReference_test()
 {
-  HRESULT hr = AAFRESULT_NOT_IMPLEMENTED;
-  IAAFScopeReference *pObject = NULL;
-
-  try
-    {
-      // Attempt to create an AAFScopeReference.
-      hr =  CoCreateInstance(
-                             CLSID_AAFScopeReference,
-                             NULL, 
-                             CLSCTX_INPROC_SERVER, 
-                             IID_IAAFScopeReference, (void **)&pObject);
-      if (FAILED(hr))
-        {
-          cerr << "CAAFScopeReference::test...FAILED!";
-          cerr << hr;
-          cerr << "\tCoCreateInstance(&CLSID_AAFScopeReference, NULL,"
-            " CLSCTX_INPROC_SERVER, &IID_IAAFScopeReference, ...);" <<
-              endl;
-          return hr;
-        }
-
-      // module-specific tests go here
-
-      if (pObject)
-        pObject->Release();
-      return AAFRESULT_NOT_IMPLEMENTED;
-
-    }
-  catch (...)
-    {
-      cerr << "CAAFScopeReference::test...Caught general C++"
-        " exception!" << endl; 
-    }
-
-  // Cleanup our object if it exists.
-  if (pObject)
-    pObject->Release();
-
-  return hr;
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
-
 
 
 
