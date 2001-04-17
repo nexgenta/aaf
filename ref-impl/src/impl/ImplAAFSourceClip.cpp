61611d5d4858dab96b67c09c3e8076c2f63172c6
tbingham <tbingham>
2001-04-17 06:47:07 +0000

    Zero object pointers after release to avoid BoundsChecker confusion.

--- ref-impl/src/impl/ImplAAFSourceClip.cpp
+++ ref-impl/src/impl/ImplAAFSourceClip.cpp
@@ -3,7 +3,6 @@
 * Advanced Authoring Format                *
 *                                          *
 * Copyright (c) 1998 Avid Technology, Inc. *
-* Copyright (c) 1998 Microsoft Corporation *
 *                                          *
 \******************************************/
 
@@ -188,7 +187,8 @@ AAFRESULT STDMETHODCALLTYPE
 	XEXCEPT
 	  {
 		if(head)
-			head->ReleaseReference();
+		  head->ReleaseReference();
+		head = 0;
 		return(XCODE());
 	  }
 	XEND;
