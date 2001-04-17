4f8c9a5af26f147c16431c800f08feb190fb517d
tbingham <tbingham>
2001-04-17 08:19:51 +0000

    Initial versions.

--- ref-impl/src/impl/ImplAAFTypeDefWeakObjRef.h
+++ ref-impl/src/impl/ImplAAFTypeDefWeakObjRef.h
@@ -3,42 +3,23 @@
 #ifndef __ImplAAFTypeDefWeakObjRef_h__
 #define __ImplAAFTypeDefWeakObjRef_h__
 
-/***********************************************************************
- *
- *              Copyright (c) 1998-2000 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- *  prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
 
 
-class ImplAAFPropertyValue;
-
-class ImplAAFClassDef;
+#ifndef __ImplAAFClassDef_h__
+#include "ImplAAFClassDef.h"
+#endif
 
 #ifndef __ImplAAFTypeDefObjectRef_h__
 #include "ImplAAFTypeDefObjectRef.h"
 #endif
-#include "OMWeakRefProperty.h"
 
 
 class ImplAAFTypeDefWeakObjRef : public ImplAAFTypeDefObjectRef
@@ -55,97 +36,54 @@ protected:
 
 public:
 
-  //****************
-  // Initialize()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    Initialize
-        (// @parm [in] auid to be used to identify this type
-         const aafUID_t & id,
-
-         // @parm [in] class def of objects permitted to be referenced
-         ImplAAFClassDef * pObjType,
-
-         // @parm [in, string] friendly name of this type definition
-         const aafCharacter * pTypeName,
-
-         // @parm [in] Number of property def IDs in pTargetSet
-         aafUInt32  ids,
-
-         // @parm [in, size_is(ids)] List of property definition IDs indicating the property where
-         // the target is to be found.
-         aafUID_constptr  pTargetSet);
-
   // Override from AAFTypeDefObjectRef
   virtual AAFRESULT STDMETHODCALLTYPE
-    CreateValue (/*[in]*/ ImplAAFRoot * pObj,
-      /*[out]*/ ImplAAFPropertyValue ** ppPropVal);
+    Initialize
+        (aafUID_t *  pID,
+         ImplAAFClassDef * pObjType,
+         wchar_t *  pTypeName);
+
 
   // Override from AAFTypeDefObjectRef
   virtual AAFRESULT STDMETHODCALLTYPE
     SetObject (/*[in]*/ ImplAAFPropertyValue * pPropVal,
-      /*[in]*/ ImplAAFRoot * ppObject);
+      /*[in]*/ ImplAAFObject * ppObject);
 
   // Override from AAFTypeDefObjectRef
   virtual AAFRESULT STDMETHODCALLTYPE
     GetObject (/*[in]*/ ImplAAFPropertyValue * pPropVal,
-      /*[out]*/ ImplAAFRoot ** ppObject);
+      /*[out]*/ ImplAAFObject ** ppObject);
 
   // Override from AAFTypeDefObjectRef
   virtual AAFRESULT STDMETHODCALLTYPE
     GetObjectType (/*[out]*/ ImplAAFClassDef ** ppObjType);
+
+  // Override from AAFTypeDefObjectRef
+  virtual AAFRESULT STDMETHODCALLTYPE
+    CreateValue (/*[in]*/ ImplAAFObject * pObj,
+      /*[out]*/ ImplAAFPropertyValue ** ppPropVal);
 
   // Override from AAFTypeDef
   virtual AAFRESULT STDMETHODCALLTYPE
     GetTypeCategory (/*[out]*/ eAAFTypeCategory_t *  pTid);
 
 
-private:
-  // Synchronize the array of OM pids with the current TargetSet property.
-  AAFRESULT SyncTargetPidsFromTargetSet(void);
-  
-  // Synchronize the TargetSet property from the current targetPids OM pid array.
-  AAFRESULT SyncTargetSetFromTargetPids(void);
-
 public:
-
-
-  // Override from AAFTypeDefObjectRef
-  virtual AAFRESULT STDMETHODCALLTYPE
-    pvtInitialize
-        (const aafUID_t & id,
-         const ImplAAFClassDef *pType,
-         const aafCharacter * pTypeName);
-
-  // overrides from ImplAAFTypeDef
+  // Declare this class to be storable.
   //
-  aafBool IsFixedSize (void) const;
-  size_t PropValSize (void) const;
-  aafBool IsRegistered (void) const;
-  size_t NativeSize (void) const;
+  OMDECLARE_STORABLE(ImplAAFTypeDefWeakObjRef)
 
-  virtual OMProperty * 
-    pvtCreateOMProperty (OMPropertyId pid,
-							const wchar_t * name) const;
+  // Declare the module test method. The implementation of the will be be
+  // in /test/ImplAAFTypeDefWeakObjRefTest.cpp.
+  static AAFRESULT test();
 
 
+  // non-published overrides from AAFTypeDef
+  aafBool IsFixedSize (void);
+  size_t PropValSize (void);
 
-  // override from OMStorable.
-  virtual const OMClassId& classId(void) const;
-
-  // Override callbacks from OMStorable
-  virtual void onSave(void* clientContext) const;
-  virtual void onRestore(void* clientContext) const;
-
-private:
-  // Persistent member properties
-  OMWeakReferenceProperty<ImplAAFClassDef> _referencedType;
-  OMVariableSizeProperty<aafUID_t> _targetSet; // array of property definition ids
-  
-  // Transient members
-  OMPropertyId * _targetPids;
-  aafUInt32 _targetPidCount;
-  OMPropertyId _uniqueIdentifierPid;
+  // OMStrongReferenceProperty<ImplAAFClassDef> _ReferencedType;
+  ImplAAFClassDef * _ReferencedType;
 };
 
 
