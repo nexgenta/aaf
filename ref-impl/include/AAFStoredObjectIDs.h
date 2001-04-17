040e3d1477eb0747fd576d9a9ab24cbf2e00431f
tbingham <tbingham>
2001-04-17 05:00:59 +0000

    Uses the AAFMetaDictionary.h created from the SMPTE spreadsheet to declare or define the AUID's for all stored objects.

--- ref-impl/include/AAFStoredObjectIDs.h
+++ ref-impl/include/AAFStoredObjectIDs.h
@@ -1,38 +1,17 @@
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- *  prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
- 
- /***********************************************\
-*                                               *
-* File: AAFStoredObjectIDs.h                    *
-*                                               *
-* Purpose: Definitions for controlling          *
-*          AUID initialization                  *
-*                                               *
-\***********************************************/
+/******************************************\
+*                                          *
+* File: AAFStoredObjectIDs.h               *
+*                                          *
+* Purpose: Definitions for controlling     * 
+*          AUID initialization             *
+*                                          *
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
 #ifndef __AAFStoredObjectIDs_h__
 
 #ifndef __AAFTypes_h__
@@ -41,49 +20,43 @@
 
 // Define the AAF_CLASS macro to produce the const stored object AUID's
 #ifndef INIT_AUID 
-#define AAF_CLASS(name, dataid, parent, concrete) extern "C" const aafUID_t AUID_AAF##name;
+#define AAF_CLASS(name, dataid, parent) extern "C" aafUID_t AUID_AAF##name;
 #else
-#define AAF_CLASS(name, dataid, parent, concrete) extern "C" const aafUID_t AUID_AAF##name = dataid;
+#define AAF_CLASS(name, dataid, parent) extern "C" aafUID_t AUID_AAF##name = dataid;
 #endif
 // The AAF reference implementation is still not quite in sync with SMPTE
 // so we have to alias some of the SMPTE names to their corresponding
 // name in AAF.
 //
 #define AUID_AAFClassDefinition AUID_AAFClassDef
-#define AUID_AAFCodecDefinition AUID_AAFCodecDef
+#define AUID_AAFConstantValue AUID_AAFConstValue
 #define AUID_AAFDataDefinition AUID_AAFDataDef
 #define AUID_AAFDefinitionObject AUID_AAFDefObject
 #define AUID_AAFEdgeCode AUID_AAFEdgecode
-#define AUID_AAFOperationDefinition AUID_AAFOperationDef
+#define AUID_AAFEffectDefinition AUID_AAFEffectDef
+#define AUID_AAFEssenceGroup AUID_AAFMediaGroup
+#define AUID_AAFFilmDescriptor AUID_AAFMediaFilmDescriptor
+#define AUID_AAFGroup AUID_AAFEffectInvocation
 #define AUID_AAFInterchangeObject AUID_AAFObject
 #define AUID_AAFJPEGImageData AUID_AAFJPEGData
 #define AUID_AAFMIDIFileData AUID_AAFMIDIData
+#define AUID_AAFMIDIFileDescriptor AUID_AAFMIDIDescriptor
 #define AUID_AAFParameterDefinition AUID_AAFParameterDef
-#define AUID_AAFInterpolationDefinition AUID_AAFInterpolationDef
 #define AUID_AAFPropertyDefinition AUID_AAFPropertyDef
 #define AUID_AAFTypeDefinition AUID_AAFTypeDef
-#define AUID_AAFTypeDefinitionCharacter AUID_AAFTypeDefCharacter
-#define AUID_AAFTypeDefinitionEnumeration AUID_AAFTypeDefEnum
-#define AUID_AAFTypeDefinitionExtendibleEnumeration AUID_AAFTypeDefExtEnum
-#define AUID_AAFTypeDefinitionFixedArray AUID_AAFTypeDefFixedArray
-#define AUID_AAFTypeDefinitionInteger AUID_AAFTypeDefInt
-#define AUID_AAFTypeDefinitionRecord AUID_AAFTypeDefRecord
-#define AUID_AAFTypeDefinitionRename AUID_AAFTypeDefRename
-#define AUID_AAFTypeDefinitionSet AUID_AAFTypeDefSet
-#define AUID_AAFTypeDefinitionStream AUID_AAFTypeDefStream
-#define AUID_AAFTypeDefinitionString AUID_AAFTypeDefString
-#define AUID_AAFTypeDefinitionIndirect AUID_AAFTypeDefIndirect
-#define AUID_AAFTypeDefinitionOpaque AUID_AAFTypeDefOpaque
-#define AUID_AAFTypeDefinitionStrongObjectReference AUID_AAFTypeDefStrongObjRef
-#define AUID_AAFTypeDefinitionVariableArray AUID_AAFTypeDefVariableArray
-#define AUID_AAFTypeDefinitionWeakObjectReference AUID_AAFTypeDefWeakObjRef
 #define AUID_AAFUNIXLocator AUID_AAFUnixLocator
-#define AUID_AAFCodecDefinition AUID_AAFCodecDef
-#define AUID_AAFContainerDefinition AUID_AAFContainerDef
-#define AUID_AAFPluginDefinition AUID_AAFPluginDef
-#define AUID_AAFPluggableDefinition AUID_AAFPluggableDef
+
 
 // Include the inside-out macro files that invokes the AAF_CLASS macros.
 #include "AAFMetaDictionary.h"
+
+// cleanup any defined macros.
+#undef AAF_TABLE_BEGIN
+#undef AAF_TABLE_END
+#undef AAF_CLASS
+#undef AAF_CLASS_END
+#undef AAF_CLASS_SEPARATOR
+#undef AAF_PROPERTY
+
 
 #endif // __AAFStoredObjectIDs_h__
