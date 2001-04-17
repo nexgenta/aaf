04e0759a49b74d174f0bedd9c18e20dc1090d178
tbingham <tbingham>
2001-04-17 05:20:50 +0000

    AAF/ref-impl/src/impl/ImplAAFTimelineMobSlot.h

--- ref-impl/src/impl/ImplAAFTimelineMobSlot.h
+++ ref-impl/src/impl/ImplAAFTimelineMobSlot.h
@@ -1,34 +1,17 @@
+
 //@doc
 //@class    AAFTimelineMobSlot | Implementation class for AAFTimelineMobSlot
 #ifndef __ImplAAFTimelineMobSlot_h__
 #define __ImplAAFTimelineMobSlot_h__
 
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- *  prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -37,7 +20,12 @@
 // aafBool,
 // aafRational_t,
 // aafPosition_t,
-// aafSlotID_t,
+// aafTrackID_t,
+
+const int CLSID_AAFTIMELINEMOBSLOT = 130;
+
+const int PID_TIMELINEMOBSLOT_EDITRATE	= 0;
+
 
 
 
@@ -45,6 +33,7 @@
 #include "ImplAAFMobSlot.h"
 #endif
 
+#include "OMProperty.h"
 
 
 class ImplAAFTimelineMobSlot : public ImplAAFMobSlot
@@ -57,9 +46,6 @@ public:
   ImplAAFTimelineMobSlot ();
   ~ImplAAFTimelineMobSlot ();
 
-  virtual AAFRESULT STDMETHODCALLTYPE
-	Initialize ();
-
 
   //****************
   // GetEditRate()
@@ -70,43 +56,38 @@ public:
 
 
   //****************
-  // SetEditRate()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    SetEditRate
-        (const aafRational_t &  value);  //@parm [in] Edit rate property value
-
-
-  //****************
   // GetOrigin()
   //
   virtual AAFRESULT STDMETHODCALLTYPE
     GetOrigin
         (aafPosition_t *  result);  //@parm [out,retval] Origin property value
 
+
   //****************
-  // SetOrigin()
+  // GetTrackID()
   //
   virtual AAFRESULT STDMETHODCALLTYPE
-    SetOrigin
-        (aafPosition_t  value);  //@parm [in] Origin property value
+    GetTrackID
+        (aafTrackID_t *  result);  //@parm [out,retval] Track id of the Mob Slot
+
+
+  // Override from AAFMobSlot
+  virtual AAFRESULT STDMETHODCALLTYPE
+    IsATrack (/*[out,retval]*/ aafBool *  retval);
+
 
 public:
-// Internal to the SDK, but available to other SDK internal code.
-virtual AAFRESULT FindSegment(aafPosition_t offset,
-										  ImplAAFSegment **segment,
-										  aafRational_t *srcRate,
-										  aafPosition_t *diffPos);
-  virtual AAFRESULT ConvertToEditRate(aafPosition_t tmpPos,
-										aafRational_t destRate,
-										aafPosition_t *convertPos);
-  virtual AAFRESULT ConvertToMyRate(aafPosition_t tmpPos,
-										  ImplAAFMobSlot *srcSlot,
-										aafPosition_t *convertPos);
+  // Declare the module test method. The implementation of the will be be
+  // in /test/ImplAAFTimelineMobSlotTest.cpp.
+	static AAFRESULT test();
+
+	AAFRESULT GetTrackDesc(aafInt32 nameSize,		// IN - Size of name buffer
+							aafString_t *name,			// IN/OUT preallocated buffer to return name
+							aafPosition_t *origin,	// OUT - Origin property value
+							aafTrackID_t *trackID);	// OUT - ttrack id property value
 
 protected:
 	OMFixedSizeProperty<aafRational_t>	_editRate;
-	OMFixedSizeProperty<aafPosition_t>	_origin; 
 
 };
 
