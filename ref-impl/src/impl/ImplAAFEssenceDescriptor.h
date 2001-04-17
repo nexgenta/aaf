c12d9cd31ca86998374323cc6ff5ab1bde632a05
tbingham <tbingham>
2001-04-17 04:40:32 +0000

    Added impl for AAFSourceMob EssenceDesc and FileDesc

--- ref-impl/src/impl/ImplAAFEssenceDescriptor.h
+++ ref-impl/src/impl/ImplAAFEssenceDescriptor.h
@@ -3,40 +3,35 @@
 #ifndef __ImplAAFEssenceDescriptor_h__
 #define __ImplAAFEssenceDescriptor_h__
 
+#include "OMStorable.h"
 
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
 
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- *  prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
 
 class ImplAAFLocator;
 
-template <class T> 
-class ImplAAFEnumerator;
-typedef ImplAAFEnumerator<ImplAAFLocator> ImplEnumAAFLocators;
+class ImplEnumAAFLocators;
+
+
+
+
+
+
 
 #ifndef __ImplAAFObject_h__
 #include "ImplAAFObject.h"
@@ -45,7 +40,9 @@ typedef ImplAAFEnumerator<ImplAAFLocator> ImplEnumAAFLocators;
 #include "ImplAAFLocator.h"
 #endif
 
-#include "OMStrongRefVectorProperty.h"
+#include "OMProperty.h"
+
+const int PID_ESSENCE_DESC_LOCATORS      = 0;
 
 
 class ImplAAFEssenceDescriptor : public ImplAAFObject
@@ -61,12 +58,12 @@ public:
 
 
   //****************
-  // CountLocators()
+  // GetNumLocators()
   //
   virtual AAFRESULT STDMETHODCALLTYPE
-    CountLocators
+    GetNumLocators
 		// @parm [out] Returns the number of locators
-        (aafUInt32 *  pCount);
+        (aafInt32 *  pCount);
   //@comm The number of locators may be zero if the essence is in the current file.
 
   //****************
@@ -89,38 +86,6 @@ public:
   //@comm    Use this function to add a locator to be scanned first when searching for
   // the essence (a secondary location for the essence).
 
-
-  //****************
-  // InsertLocatorAt()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    InsertLocatorAt
-		// @parm [in] place to insert locator
-        (aafUInt32 index,
-		// @parm [in] Locator to insert
-		 ImplAAFLocator * pLocator);
-
-
-  //****************
-  // GetLocatorAt()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    GetLocatorAt
-		// @parm [in] index of locator to get
-        (aafUInt32 index,
-		// @parm [in] returned locator
-		 ImplAAFLocator ** ppLocator);
-
-
-  //****************
-  // GetLocatorAt()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    RemoveLocatorAt
-		// @parm [in] index of locator to remove
-        (aafUInt32 index);
-
-
   //****************
   // RemoveLocator()
   //
@@ -130,22 +95,25 @@ public:
         (ImplAAFLocator * pLocator);
 
   //****************
-  // GetLocators()
+  // EnumAAFAllLocators()
   //
   virtual AAFRESULT STDMETHODCALLTYPE
-    GetLocators
+    EnumAAFAllLocators
 		// @parm [out] An enumerator to the locators on this essence descriptor
         (ImplEnumAAFLocators ** ppEnum);
   //@comm The number of locators may be zero if the essence is in the current file.
 
 
-public:
-	// Functions internal to the toolkit
-	virtual AAFRESULT STDMETHODCALLTYPE
-		GetOwningMobKind (aafMobKind_t *pMobKind);
 
-	virtual AAFRESULT
-		GetNthLocator (aafInt32 index, ImplAAFLocator **ppLocator);
+
+public:
+  // Declare this class to be storable.
+  //
+  OMDECLARE_STORABLE(ImplAAFEssenceDescriptor)
+
+  // Declare the module test method. The implementation of the will be be
+  // in /test/ImplAAFEssenceDescriptorTest.cpp.
+  static AAFRESULT test();
 
 private:
     OMStrongReferenceVectorProperty<ImplAAFLocator> _locators;
