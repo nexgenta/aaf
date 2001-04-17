792566e497b468b729282da1ecde7efe5bc458f4
tbingham <tbingham>
2001-04-17 04:01:27 +0000

    Fixed pluginDefinition

--- ref-impl/include/AAFClassDefUIDs.h
+++ ref-impl/include/AAFClassDefUIDs.h
@@ -1,12 +1,31 @@
 #ifndef __AAFClassDefUIDs_h__
 #define __AAFClassDefUIDs_h__
-/***********************************************\
-*                                               *
-* Advanced Authoring Format                     *
-*                                               *
-* Copyright (c) 1998-1999 Avid Technology, Inc. *
-*                                               *
-\***********************************************/
+/***********************************************************************
+ *
+ *              Copyright (c) 1998-1999 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and accompanying 
+ * documentation, and to distribute and sublicense application software
+ * incorporating this software for any purpose is hereby granted, 
+ * provided that (i) the above copyright notice and this permission
+ * notice appear in all copies of the software and related documentation,
+ * and (ii) the name Avid Technology, Inc. may not be used in any
+ * advertising or publicity relating to the software without the specific,
+ *  prior written permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+ * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+ * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+ * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+ * LIABILITY.
+ *
+ ************************************************************************/
 
 //
 // GUIDs for built-in class definitions
@@ -34,6 +53,7 @@ id;
 #define kAAFClassID_ParameterDefinition kAAFClassID_ParameterDef
 #define kAAFClassID_PropertyDefinition kAAFClassID_PropertyDef
 #define kAAFClassID_TypeDefinition kAAFClassID_TypeDef
+#define kAAFClassID_TypeDefinitionCharacter kAAFClassID_TypeDefCharacter
 #define kAAFClassID_TypeDefinitionEnumeration kAAFClassID_TypeDefEnum
 #define kAAFClassID_TypeDefinitionExtendibleEnumeration kAAFClassID_TypeDefExtEnum
 #define kAAFClassID_TypeDefinitionFixedArray kAAFClassID_TypeDefFixedArray
@@ -43,12 +63,15 @@ id;
 #define kAAFClassID_TypeDefinitionSet kAAFClassID_TypeDefSet
 #define kAAFClassID_TypeDefinitionStream kAAFClassID_TypeDefStream
 #define kAAFClassID_TypeDefinitionString kAAFClassID_TypeDefString
+#define kAAFClassID_TypeDefinitionIndirect kAAFClassID_TypeDefIndirect
+#define kAAFClassID_TypeDefinitionOpaque kAAFClassID_TypeDefOpaque
 #define kAAFClassID_TypeDefinitionStrongObjectReference kAAFClassID_TypeDefStrongObjRef
 #define kAAFClassID_TypeDefinitionVariableArray kAAFClassID_TypeDefVariableArray
 #define kAAFClassID_TypeDefinitionWeakObjectReference kAAFClassID_TypeDefWeakObjRef
 #define kAAFClassID_UNIXLocator kAAFClassID_UnixLocator
 #define kAAFClassID_CodecDefinition kAAFClassID_CodecDef
 #define kAAFClassID_ContainerDefinition kAAFClassID_ContainerDef
+#define kAAFClassID_PluginDefinition kAAFClassID_PluginDef
 #define kAAFClassID_PluggableDefinition kAAFClassID_PluggableDef
 
 #include "AAFMetaDictionary.h"
