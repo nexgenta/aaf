31ec98c506a02284526c2b2d63d737ea3e220741
tbingham <tbingham>
2001-04-17 03:34:14 +0000

    Added VaryingValue, Pulldown, and Interpolation plugins

--- ref-impl/src/impl/ImplAAFInterpolationDef.h
+++ ref-impl/src/impl/ImplAAFInterpolationDef.h
@@ -4,31 +4,32 @@
 #define __ImplAAFInterpolationDef_h__
 
 
-//=---------------------------------------------------------------------=
-//
-// The contents of this file are subject to the AAF SDK Public
-// Source License Agreement (the "License"); You may not use this file
-// except in compliance with the License.  The License is available in
-// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
-// Association or its successor.
-// 
-// Software distributed under the License is distributed on an "AS IS"
-// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
-// the License for the specific language governing rights and limitations
-// under the License.
-// 
-// The Original Code of this file is Copyright 1998-2001, Licensor of the
-// AAF Association.
-// 
-// The Initial Developer of the Original Code of this file and the
-// Licensor of the AAF Association is Avid Technology.
-// All rights reserved.
-//
-//=---------------------------------------------------------------------=
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
 
 
 
 class ImplAAFClassDef;
+
+
+/***********************************************\
+*												*
+* Advanced Authoring Format						*
+*												*
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+* Copyright (c) 1998-1999 Microsoft Corporation *
+*												*
+\***********************************************/
+
+
+
+
 
 #ifndef __ImplAAFDefObject_h__
 #include "ImplAAFDefObject.h"
@@ -44,19 +45,22 @@ public:
   //********
   ImplAAFInterpolationDef ();
 
-
-  //****************
-  // Initialize()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    Initialize
-        // @parm [in] Pointer to an AUID reference
-        (const aafUID_t & id,
-		 const aafCharacter *name,
-		 const aafCharacter *description);
-
 protected:
   virtual ~ImplAAFInterpolationDef();
+
+public:
+
+  
+
+
+public:
+  // Declare this class to be storable.
+  //
+  OMDECLARE_STORABLE(ImplAAFInterpolationDef)
+
+  // Declare the module test method. The implementation of the will be be
+  // in /test/ImplAAFInterpolationDefTest.cpp.
+  static AAFRESULT test();
 };
 
 #endif // ! __ImplAAFInterpolationDef_h__
