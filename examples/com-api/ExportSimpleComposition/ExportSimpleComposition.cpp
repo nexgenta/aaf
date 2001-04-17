0b4b810975d15ec31b9b2cb0426356161079e049
tbingham <tbingham>
2001-04-17 06:11:20 +0000

    Change a constant name

--- examples/com-api/ExportSimpleComposition/ExportSimpleComposition.cpp
+++ examples/com-api/ExportSimpleComposition/ExportSimpleComposition.cpp
@@ -175,13 +175,15 @@ static HRESULT CreateAAFFile(aafWChar * pFileName, testDataFile_t *dataFile, tes
 		remove(chFileName);
 	}
 
+	aafProductVersion_t v;
+	v.major = 1;
+	v.minor = 0;
+	v.tertiary = 0;
+	v.patchLevel = 0;
+	v.type = kAAFVersionUnknown;
 	ProductInfo.companyName = L"AAF-East: Avid Technology";
 	ProductInfo.productName = L"Export Simple Composition";
-	ProductInfo.productVersion.major = 1;
-	ProductInfo.productVersion.minor = 0;
-	ProductInfo.productVersion.tertiary = 0;
-	ProductInfo.productVersion.patchLevel = 0;
-	ProductInfo.productVersion.type = kAAFVersionUnknown;
+	ProductInfo.productVersion = &v;
 	ProductInfo.productVersionString = NULL;
 	ProductInfo.productID = NIL_UID;
 	ProductInfo.platform = NULL;
@@ -255,7 +257,7 @@ static HRESULT CreateAAFFile(aafWChar * pFileName, testDataFile_t *dataFile, tes
 			/* Create the Essence Data specifying the codec, container, edit rate and sample rate */
 			check(pMasterMob->CreateEssence(1,				// Slot ID
 				pSoundDef,			// MediaKind
-				CodecWave,			// codecID
+				kAAFCodecWAVE,		// codecID
 				editRate,			// edit rate
 				sampleRate,			// sample rate
 				kAAFCompressionDisable,
