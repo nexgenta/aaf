1da683131216f51fc6e699cd8b0f43184f144c92
tbingham <tbingham>
2001-04-17 04:59:30 +0000

    Implementation of AAFIdentification

--- ref-impl/src/impl/ImplAAFIdentification.h
+++ ref-impl/src/impl/ImplAAFIdentification.h
@@ -3,39 +3,40 @@
 #ifndef __ImplAAFIdentification_h__
 #define __ImplAAFIdentification_h__
 
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- * prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+
+
+
+
 
 
 #ifndef __ImplAAFObject_h__
 #include "ImplAAFObject.h"
 #endif
 
-#include "OMWideStringProperty.h"
+#if OM_PRESENT
+#include "property.h"
+
+const int CLSID_AAFIDENTIFICATION = 44;
+
+const int PID_IDENTIFICATION_COMPANYNAME          = 0;
+const int PID_IDENTIFICATION_PRODUCTNAME          = 1;
+//const int PID_IDENTIFICATION_PRODUCTVERSION       = 2;
+const int PID_IDENTIFICATION_PRODUCTVERSIONSTRING = 2;
+//const int PID_IDENTIFICATION_PRODUCTID            = 4;
+const int PID_IDENTIFICATION_DATE                 = 3;
+//const int PID_IDENTIFICATION_TOOLKITVERSION       = 6;
+const int PID_IDENTIFICATION_PLATFORM             = 4;
+//const int PID_IDENTIFICATION_GENERATION           = 8;
+#endif
 
 class ImplAAFIdentification : public ImplAAFObject
 {
@@ -44,45 +45,31 @@ public:
   // Constructor/destructor
   //
   //********
+  ImplAAFIdentification ();
   ~ImplAAFIdentification ();
-  
-  ImplAAFIdentification();
 
-  virtual AAFRESULT STDMETHODCALLTYPE
-	Initialize (aafCharacter_constptr companyName,
-				aafCharacter_constptr productName,
-				aafCharacter_constptr productVersionString,
-				aafUID_constref productID);
+#if OM_PRESENT
+  AAFIdentification(
+    const char* companyName,
+    const char* productName,
+    const aafProductVersion_t* productVersion,
+    const char* productVersionString,
+    // const AUID* productId,
+    const aafTimeStamp_t date,
+    const aafProductVersion_t* toolKitVersion,
+    const char* platform
+    // const AUID* generation
+    );
 
+  virtual int classId(void) const;
+#endif
 
   //****************
   // GetCompanyName()
   //
   virtual AAFRESULT STDMETHODCALLTYPE
     GetCompanyName
-	    (// @parm [out, size_is(bufSize), string] buffer into which name is written
-         wchar_t *  pName,
-
-		 // @parm [in] The size of the pName buffer
-		 aafUInt32  bufSize);
-
-
-  //****************
-  // GetCompanyNameBufLen()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    GetCompanyNameBufLen
-		// @parm [out] required buffer length
-        (aafUInt32 *  pLen);
-
-
-  //****************
-  // SetCompanyName()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    SetCompanyName
-		// @parm [in, string] The Company Name
-        (const aafCharacter * pName);
+        (aafString_t *  pCompanyName);  //@parm [out,retval] The Company Name
 
 
   //****************
@@ -90,29 +77,7 @@ public:
   //
   virtual AAFRESULT STDMETHODCALLTYPE
     GetProductName
-	    (// @parm [out, size_is(bufSize), string] buffer into which name is written
-         wchar_t *  pName,
-
-		 // @parm [in] The size of the pName buffer
-		 aafUInt32  bufSize);
-
-
-  //****************
-  // GetProductNameBufLen()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    GetProductNameBufLen
-		// @parm [out] Returned length
-        (aafUInt32 *  pLen);
-
-
-  //****************
-  // SetProductName()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    SetProductName
-		// @parm [in, string] The Product Name
-        (const aafCharacter * pName);
+        (aafString_t *  pProductName);  //@parm [out,retval] The Product Name
 
 
   //****************
@@ -120,47 +85,7 @@ public:
   //
   virtual AAFRESULT STDMETHODCALLTYPE
     GetProductVersionString
-	    (// @parm [out, size_is(bufSize), string] buffer into which the string is written
-         wchar_t *  pVersionString,
-
-		 // @parm [in] The size of the pVersionString buffer
-		 aafUInt32  bufSize);
-
-
-  //****************
-  // GetProductVersionStringBufLen()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    GetProductVersionStringBufLen
-		// @parm [out] required buffer length
-        (aafUInt32 *  pLen);
-
-
-  //****************
-  // SetProductVersionString()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    SetProductVersionString
-		// @parm [in, string] The Product Version String
-        (const aafCharacter * pVersionString);
-
-
-  //****************
-  // GetProductVersion()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    GetProductVersion
-		// @parm [out] The Product Version
-        (aafProductVersion_t *  pVersion);
-
-
-  //****************
-  // SetProductVersion()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    SetProductVersion
-		// @parm [in] The Product Version
-        (aafProductVersion_constref version);
+        (aafString_t *  pProductVersionString);  //@parm [out,retval] The Product Version String
 
 
   //****************
@@ -168,17 +93,7 @@ public:
   //
   virtual AAFRESULT STDMETHODCALLTYPE
     GetProductID
-		// @parm [out] The Product ID
-        (aafUID_t *  pProductID);
-
-
-  //****************
-  // SetProductID()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    SetProductID
-		// @parm [in] The Product ID
-        (const aafUID_t & productID);
+        (aafUID_t *  pProductID);  //@parm [out,retval] The Product ID
 
 
   //****************
@@ -186,17 +101,15 @@ public:
   //
   virtual AAFRESULT STDMETHODCALLTYPE
     GetDate
-		// @parm [out] The date-time stamp
-        (aafTimeStamp_t *  pTimestamp);
+        (aafTimeStamp_t *  pTimestamp);  //@parm [out,retval] The date-time stamp
 
 
   //****************
-  // GetRefImplVersion()
+  // GetToolkitVersion()
   //
   virtual AAFRESULT STDMETHODCALLTYPE
-    GetRefImplVersion
-		// @parm [out] The Reference Implementation Version
-        (aafProductVersion_t *  pVersion);
+    GetToolkitVersion
+        (aafProductVersion_t *  pToolkitVersion);  //@parm [out,retval] The Toolkit Version
 
 
   //****************
@@ -204,40 +117,73 @@ public:
   //
   virtual AAFRESULT STDMETHODCALLTYPE
     GetPlatform
-	    (// @parm [out, size_is(bufSize), string] The Platform
-         wchar_t *  pPlatform,
-
-		 // @parm [in] The size of the pPlatform buffer
-		 aafUInt32  bufSize);
-
-
-  //****************
-  // GetPlatformBufLen()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    GetPlatformBufLen
-		// @parm [out] Returned length
-        (aafUInt32 *  pLen);
+        (aafString_t *  pPlatform);  //@parm [out,retval] The Platform
 
 
   //****************
   // GetGeneration()
   //
   virtual AAFRESULT STDMETHODCALLTYPE
-    GetGenerationID
-		// @parm [out] The unique generation
-        (aafUID_t *  pGeneration);
+    GetGeneration
+        (aafUID_t *  pGeneration);  //@parm [out,retval] The unique generation
 
+
+  //****************
+  // SetCompanyName()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    SetCompanyName
+        (aafString_t *  pCompanyName);  //@parm [in,ref] The Company Name
+
+
+  //****************
+  // SetProductName()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    SetProductName
+        (aafString_t *  pProductName);  //@parm [in,ref] The Product Name
+
+
+  //****************
+  // SetProductVersion()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    SetProductVersion
+        (aafProductVersion_t *  pProductVersion);  //@parm [in,ref] The Product Version
+
+
+  //****************
+  // SetProductVersionString()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    SetProductVersionString
+        (aafString_t *  pProductVersionString);  //@parm [in,ref] The Product Version String
+
+
+  //****************
+  // SetProductID()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    SetProductID
+        (aafUID_t *  pProductID);  //@parm [in,ref] The Product ID
+
+	
+public:
+  // Declare the module test method. The implementation of the will be be
+  // in /test/ImplAAFIdentificationTest.cpp.
+  static AAFRESULT test();
+#if OM_PRESENT
 private:
-  OMWideStringProperty                      _companyName;
-  OMWideStringProperty						_productName;
-  OMFixedSizeProperty<aafProductVersion_t>	_productVersion;
-  OMWideStringProperty                      _productVersionString;
-  OMFixedSizeProperty<aafUID_t>             _productId;
-  OMFixedSizeProperty<aafTimeStamp_t>       _date;
-  OMFixedSizeProperty<aafProductVersion_t>	_toolkitVersion;
-  OMWideStringProperty                      _platform;
-  OMFixedSizeProperty<aafUID_t>             _generation;
+  StringProperty                             _companyName;
+  StringProperty                             _productName;
+  // StructuredProperty<aafProductVersion_t> _productVersion;
+  StringProperty                             _productVersionString;
+  // FixedSizeProperty<AUID>                 _productId;
+  FixedSizeProperty<aafTimeStamp_t>          _date;
+  // StructuredProperty<aafProductVersion_t> _toolkitVersion;
+  StringProperty                             _platform;
+  // FixedSizeProperty<AUID>                 _generation;
+#endif
 };
 
 #endif // ! __ImplAAFIdentification_h__
