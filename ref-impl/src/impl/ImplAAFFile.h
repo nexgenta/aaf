5c4f7c1e43b2139b19444c70dbb57baa4b61d517
tbingham <tbingham>
2001-04-17 10:09:22 +0000

    Change code so that ImplAAFDictionary implements the OMClassFactory interface instead of ImplAAFClassFactory

--- ref-impl/src/impl/ImplAAFFile.h
+++ ref-impl/src/impl/ImplAAFFile.h
@@ -1,5 +1,5 @@
 //@doc
-//@class    AAFFile | Implementation class for AAFFile
+//@class  AAFFile | Implementation class for AAFFile
 #ifndef __ImplAAFFile_h__
 #define __ImplAAFFile_h__
 
@@ -12,120 +12,114 @@
 *                                          *
 \******************************************/
 
-#include "AAFTypes.h"
 #include "ImplAAFRoot.h"
 
-#include "Container.h"
 
-typedef enum
-{
-	kOmCreate, kOmModify, kOmOpenRead, kOmUndefined = -1
-}				openType_t;
+
+
+
 
 //
 // Forward declaration
 //
-struct IAAFFile;
-class AAFFile;
-struct IAAFMedia;
-class AAFMedia;
+class OMFile;
+class ImplAAFDictionary;
+class ImplAAFFile;
 class ImplAAFHeader;
-struct IAAFSession;
 class ImplAAFSession;
 class ImplAAFDataDef;
 
 class ImplAAFFile : public ImplAAFRoot
 {
 public:
+
+  //
+  // methods to support API
+  //
+
+  virtual AAFRESULT STDMETHODCALLTYPE
+	Initialize ();
+
+  virtual AAFRESULT STDMETHODCALLTYPE
+	OpenExistingRead (wchar_t * pFileName,
+					  aafUInt32 modeFlags);
+
+  virtual AAFRESULT STDMETHODCALLTYPE
+	OpenExistingModify (wchar_t * pFileName,
+						aafUInt32 modeFlags,
+						aafProductIdentification_t * pIdent);
+
+  virtual AAFRESULT STDMETHODCALLTYPE
+	OpenNewModify (wchar_t * pFileName,
+				   aafUInt32 modeFlags,
+				   aafProductIdentification_t * pIdent);
+
+  virtual AAFRESULT STDMETHODCALLTYPE
+	OpenTransient (aafProductIdentification_t * pIdent);
+
+  virtual AAFRESULT STDMETHODCALLTYPE
+	Close ();
+
+  virtual AAFRESULT STDMETHODCALLTYPE
+	Save ();
+
+  virtual AAFRESULT STDMETHODCALLTYPE
+	SaveAs (wchar_t * pFileName,
+			aafUInt32 modeFlags);
+
+  virtual AAFRESULT STDMETHODCALLTYPE
+	Revert ();
+
+  virtual AAFRESULT STDMETHODCALLTYPE
+	GetHeader (ImplAAFHeader ** header);
+
+  virtual AAFRESULT STDMETHODCALLTYPE
+	GetRevision (aafFileRev_t *  rev);
+  
+
   //
   // Constructor/destructor
   //
   //********
   ImplAAFFile ();
   virtual ~ImplAAFFile ();
-#if 0
-  OMDECLARE_STORABLE(ImplAAFFile)
-#endif
-  //***********************************************************
-  // METHOD NAME: Close()
-  //
-  // DESCRIPTION:
-  // @mfunc AAFRESULT | AAFFile | Close |
-  // Closes an AAF file, saving the result.
-  // @end
-  virtual AAFRESULT STDMETHODCALLTYPE
-    Close ();
 
-  //***********************************************************
-  // METHOD NAME: GetRevision()
-  //
-  // DESCRIPTION:
-  // @mfunc AAFRESULT | AAFFile | GetRevision |
-  // Get the revision of the ciurrent AAF file.
-  // @end
-  // 
-  virtual AAFRESULT STDMETHODCALLTYPE
-  GetRevision (
-    // @parm aafFileRev_t * | rev | [out] Revision of the current file
-    aafFileRev_t *  rev
-  );
-
-  virtual AAFRESULT STDMETHODCALLTYPE
-  GetHeader (
-    ImplAAFHeader ** header
-  );
 
 public:
   // Declare the module test method. The implementation of the will be be
   // in /test/ImplAAFFileTest.cpp.
   static AAFRESULT test();
 
-AAFRESULT Create(
-			aafWChar*		stream, 
-			ImplAAFSession *	session, 
-			aafFileRev_t		rev);
-AAFRESULT OpenRead(
-			aafWChar*		stream, 
-			ImplAAFSession *	session);
-AAFRESULT OpenModify(
-			aafWChar*		stream, 
-			ImplAAFSession *	session);
+
 private:
 
-	AAFRESULT InternOpenFile(aafWChar* stream, 
-								   ImplAAFSession * session,
-								   OMLContainerUseMode useMode, 
-								   openType_t type);
-  
+  // Private state for this file.
+  typedef enum _openType_t
+  {
+    kOmCreate = 0,
+	kOmModify = 1,
+	kOmOpenRead = 2,
+	kOmUndefined = -1
+  } openType_t;
+	
+
   void InternalReleaseObjects();
 
-		aafInt32       	_cookie;
-		aafFileFormat_t _fmt;
-		OMContainer     *_container;
-		aafInt16           _byteOrder;
-		openType_t		_openType;
-		ImplAAFFile			*_prevFile;
-		ImplAAFHeader *     _head;		// Needed by Head object
-#if FULL_TOOLKIT
-		aafCloseMediaPtr _closeMediaProc;
-		aafBool			_customStreamFuncsExist;	//!!!	
-		struct aafCodecStreamFuncs _streamFuncs;	//!!!	
-		aafRawStream_t	*_rawFile;	//!!!	
-		aafLocatorFailureCB _locatorFailureCallback;	//!!!	
-		aafCodecID_t	_rawCodecID;	//!!!	
-		void           	*_rawFileDesc;		//!!!/* If non-omfi file */
-		AAFMedia *   _topMedia;	//!!!
-#endif
-		aafBool         _semanticCheckEnable;	//!!!  /* Used to stop recursion in checks */
-		ImplAAFSession		 *_session;		//!!!  used by file checker
-		ImplAAFDataDef *	_nilKind;// !!!
-		ImplAAFDataDef *	_pictureKind;// !!!
-		ImplAAFDataDef *	_soundKind;// !!!
-#ifdef AAF_ERROR_TRACE
-		char			*_stackTrace;	//!!!	
-		aafInt32		_stackTraceSize;	//!!!	
-#endif
-		aafFileRev_t     _setrev;
+  aafInt32			_cookie;
+  OMFile			*_file;
+  ImplAAFDictionary *_dictionary;
+  aafInt16			_byteOrder;
+  openType_t			_openType;
+  ImplAAFHeader *   _head;		// Needed by Head object
+  aafBool   _semanticCheckEnable;	//!!!  /* Used to stop recursion in checks */
+  ImplAAFDataDef *	_nilKind;// !!!
+  ImplAAFDataDef *	_pictureKind;// !!!
+  ImplAAFDataDef *	_soundKind;// !!!
+  aafFileRev_t   _setrev;
+  aafBool _initialized;
+  aafBool _open;
+  aafUInt32 _modeFlags;
+  aafProductIdentification_t _ident;
 };
 
 #endif // ! __ImplAAFFile_h__
