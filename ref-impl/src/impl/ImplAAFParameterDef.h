962356626646df6358349d581826e34513300f87
tbingham <tbingham>
2001-04-17 04:16:11 +0000

    First round of effects checkins

--- ref-impl/src/impl/ImplAAFParameterDef.h
+++ ref-impl/src/impl/ImplAAFParameterDef.h
@@ -3,41 +3,42 @@
 #ifndef __ImplAAFParameterDef_h__
 #define __ImplAAFParameterDef_h__
 
-//=---------------------------------------------------------------------=
-//
-// The contents of this file are subject to the AAF SDK Public
-// Source License Agreement (the "License"); You may not use this file
-// except in compliance with the License.  The License is available in
-// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
-// Association or its successor.
-// 
-// Software distributed under the License is distributed on an "AS IS"
-// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
-// the License for the specific language governing rights and limitations
-// under the License.
-// 
-// The Original Code of this file is Copyright 1998-2001, Licensor of the
-// AAF Association.
-// 
-// The Initial Developer of the Original Code of this file and the
-// Licensor of the AAF Association is Avid Technology.
-// All rights reserved.
-//
-//=---------------------------------------------------------------------=
+
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
 
 
-#ifndef __ImplAAFDefObject_h__
-#include "ImplAAFDefObject.h"
+
+class ImplAAFReferenceValue;
+
+class ImplEnumAAFReferenceValues;
+
+
+/***********************************************\
+*												*
+* Advanced Authoring Format						*
+*												*
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+* Copyright (c) 1998-1999 Microsoft Corporation *
+*												*
+\***********************************************/
+
+ 
+
+
+
+#ifndef __ImplAAFPluggableDef_h__
+#include "ImplAAFPluggableDef.h"
 #endif
 
-#ifndef __ImplAAFTypeDeft_h__
-#include "ImplAAFTypeDef.h"
-#endif
 
-#include "OMWeakRefProperty.h"
-#include "OMWideStringProperty.h"
-
-class ImplAAFParameterDef : public ImplAAFDefObject
+class ImplAAFParameterDef : public ImplAAFPluggableDef
 {
 public:
   //
@@ -45,19 +46,6 @@ public:
   //
   //********
   ImplAAFParameterDef ();
-
-
-  //****************
-  // Initialize()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    Initialize
-        // @parm [in] Pointer to an AUID reference
-        (const aafUID_t & id,
-		 const aafCharacter *name,
-		 const aafCharacter *description,
-     ImplAAFTypeDef * pType);
-
 
 protected:
   virtual ~ImplAAFParameterDef ();
@@ -69,17 +57,17 @@ public:
   // GetParameterDataDefID()
   //
   virtual AAFRESULT STDMETHODCALLTYPE
-    GetTypeDefinition
+    GetParameterDataDefID
         // @parm [retval][out] Pointer to an AUID reference
-        (ImplAAFTypeDef **  pParameterDataDefID);
+        (aafUID_t *  pParameterDataDefID);
 
   //****************
   // SetParameterDataDefID()
   //
   virtual AAFRESULT STDMETHODCALLTYPE
-    SetTypeDef
+    SetParameterDataDefID
         // @parm [in] an AUID reference
-        (ImplAAFTypeDef * ParameterDataDefID);
+        (aafUID_t  ParameterDataDefID);
 
   //****************
   // GetDisplayUnits()
@@ -87,10 +75,10 @@ public:
   virtual AAFRESULT STDMETHODCALLTYPE
     GetDisplayUnits
         (// @parm [in,string] DisplayUnits
-         aafCharacter *  pDisplayUnits,
+         wchar_t *  pDisplayUnits,
 
          // @parm [in] length of the buffer to hold DisplayUnits
-         aafUInt32  bufSize);
+         aafInt32  bufSize);
 
   //****************
   // GetDisplayUnitsBufLen()
@@ -98,7 +86,7 @@ public:
   virtual AAFRESULT STDMETHODCALLTYPE
     GetDisplayUnitsBufLen
         // @parm [out] DisplayUnits
-        (aafUInt32 *  pLen);
+        (aafInt32 *  pLen);
 
 
 
@@ -108,12 +96,36 @@ public:
   virtual AAFRESULT STDMETHODCALLTYPE
     SetDisplayUnits
         // @parm [in, string] DisplayUnits
-        (const aafCharacter *  pDisplayUnits);
+        (wchar_t *  pDisplayUnits);
 
-private:
-	OMWeakReferenceProperty<ImplAAFTypeDef>					_typeDef;
-	OMWideStringProperty									_displayUnits;
+  //****************
+  // AddReferenceValue()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    AddReferenceValue
+        // @parm [in] Ref value to be added
+        (ImplAAFReferenceValue * pReferenceValue);
+
+  //****************
+  // GetRefValues()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetRefValues
+        // @parm [out,retval] Reference value Enumeration
+        (ImplEnumAAFReferenceValues ** ppEnum);
+
+
+
+public:
+  // Declare this class to be storable.
+  //
+  OMDECLARE_STORABLE(ImplAAFParameterDef)
+
+  // Declare the module test method. The implementation of the will be be
+  // in /test/ImplAAFParameterDefTest.cpp.
+  static AAFRESULT test();
 };
 
 #endif // ! __ImplAAFParameterDef_h__
+
 
