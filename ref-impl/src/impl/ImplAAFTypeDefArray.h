bd55e217bc65bc5ba78eab4b382868b2e366fcbb
tbingham <tbingham>
2001-04-17 09:18:18 +0000

    added additional validation for Fixed and Variable arrays. Centralized creation process into base implementation. Also, replaced assert's with HRESULT return codes for Insert, Prepend and Remove methods

--- ref-impl/src/impl/ImplAAFTypeDefArray.h
+++ ref-impl/src/impl/ImplAAFTypeDefArray.h
@@ -1,19 +1,38 @@
 #ifndef __ImplAAFTypeDefArray_h__
 #define __ImplAAFTypeDefArray_h__
 
-/******************************************\
-*                                          *
-* Advanced Authoring Format                *
-*                                          *
-* Copyright (c) 1998 Avid Technology, Inc. *
-* Copyright (c) 1998 Microsoft Corporation *
-*                                          *
-\******************************************/
+/***********************************************************************
+ *
+ *              Copyright (c) 1998-1999 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and accompanying 
+ * documentation, and to distribute and sublicense application software
+ * incorporating this software for any purpose is hereby granted, 
+ * provided that (i) the above copyright notice and this permission
+ * notice appear in all copies of the software and related documentation,
+ * and (ii) the name Avid Technology, Inc. may not be used in any
+ * advertising or publicity relating to the software without the specific,
+ *  prior written permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+ * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+ * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+ * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+ * LIABILITY.
+ *
+ ************************************************************************/
 
 #ifndef __ImplAAFTypeDef_h__
 #include "ImplAAFTypeDef.h"
 #endif
 
+class ImplAAFPropertyValue;
 
 class ImplAAFTypeDefArray : public ImplAAFTypeDef
 {
@@ -35,7 +54,7 @@ public:
   virtual AAFRESULT STDMETHODCALLTYPE
     GetType
         // @parm [out] type of elements in this array
-        (ImplAAFTypeDef ** ppTypeDef) = 0;
+        (ImplAAFTypeDef ** ppTypeDef) const = 0;
 
 
   //****************
@@ -51,7 +70,7 @@ public:
          aafUInt32  numElements,
 
          // @parm [out] newly-created property value
-         ImplAAFPropertyValue ** ppPropVal);
+         ImplAAFPropertyValue ** ppPropVal) ; //= 0;   //--cf make this a pure virtual method
 
 
   //****************
@@ -129,10 +148,46 @@ public:
          aafUInt32  dataSize);
 
 
+  // Override callbacks from OMStorable
+  virtual void onSave(void* clientContext) const;
+  virtual void onRestore(void* clientContext) const;
+
 protected:
+	
+	//returns number of elements in this array
   virtual aafUInt32 pvtCount (ImplAAFPropertyValue * pInPropVal) const = 0;
-  //
-  // returns number of elements in this array
+
+  //Validates the input source 
+  virtual AAFRESULT STDMETHODCALLTYPE
+	  ValidateInputParams (ImplAAFPropertyValue ** ppElementValues,
+						aafUInt32  numElements);
+
+  //Creates a new PV
+  virtual AAFRESULT STDMETHODCALLTYPE
+	  CreateValue(ImplAAFPropertyValue ** ppPropVal, 
+								 aafUInt32 dataSize = 0); //overloaded function
+
+  //Copies individual element-values into the new PV
+  virtual AAFRESULT STDMETHODCALLTYPE
+	  CopyValuesIntoValue (ImplAAFPropertyValue ** ppElementValues,
+										  aafUInt32  numElements,
+										  aafUInt32  sizeElem,
+										  ImplAAFPropertyValue ** ppPropVal) ;
+
+  //Function to test appropriate array-ability;  pure-virtual
+  virtual bool IsArrayable(ImplAAFTypeDef * pSourceTypeDef) const = 0;
 };
+
+//
+// smart pointer
+//
+
+#ifndef __ImplAAFSmartPointer_h__
+// caution! includes assert.h
+#include "ImplAAFSmartPointer.h"
+#endif
+
+typedef ImplAAFSmartPointer<ImplAAFTypeDefArray> ImplAAFTypeDefArraySP;
+
 
 #endif // ! __ImplAAFTypeDefArray_h__
