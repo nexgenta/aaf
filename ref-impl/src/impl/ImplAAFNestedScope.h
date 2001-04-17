ba07cdb49016877f9cdcccd9d71a055bdf13c97d
tbingham <tbingham>
2001-04-17 04:41:30 +0000

    Fix FULL_TOOLKIT and !!! markers

--- ref-impl/src/impl/ImplAAFNestedScope.h
+++ ref-impl/src/impl/ImplAAFNestedScope.h
@@ -4,32 +4,14 @@
 #define __ImplAAFNestedScope_h__
 
 
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- *  prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
 
 
 #ifndef __AAFTypes_h__
@@ -46,13 +28,21 @@
  *
  *************************************************************************/
 
+#ifndef __ImplAAFSegment_h__
 #include "ImplAAFSegment.h"
+#endif
 
-template <class T> 
-class ImplAAFEnumerator;
-typedef ImplAAFEnumerator<ImplAAFSegment> ImplEnumAAFSegments;
+class ImplEnumAAFSegments;
 
-#include "OMStrongRefVectorProperty.h"
+
+
+
+
+
+#ifndef __ImplAAFSegment_h__
+#include "ImplAAFSegment.h"
+#endif
+
 
 class ImplAAFNestedScope : public ImplAAFSegment
 {
@@ -78,67 +68,37 @@ public:
         (ImplAAFSegment * pSegment);
 
   //****************
-  // PrependSegment()
+  // RemoveSegment()
   //
   virtual AAFRESULT STDMETHODCALLTYPE
-    PrependSegment
+    RemoveSegment
         // @parm [in] Pointer to segment to be added
         (ImplAAFSegment * pSegment);
 
   //****************
-  // InsertSegmentAt()
+  // GetSlots()
   //
   virtual AAFRESULT STDMETHODCALLTYPE
-    InsertSegmentAt
-        // @parm [in] index where segment is to be inserted
-        (aafUInt32 index,
-        // @parm [in] Pointer to segment to be added
-		 ImplAAFSegment * pSegment);
-
-  //****************
-  // CountSegments()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    CountSegments
-        // @parm [out\, retval] number of segments
-        (aafUInt32 * pResult);
-
-  //****************
-  // RemoveSegment()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    RemoveSegmentAt
-        // @parm [in] index of segment to be removed
-        (aafUInt32 index);
-
-  //****************
-  // GetSegmentAt()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    GetSegmentAt
-        // @parm [in] index of segment to retrieve
-        (aafUInt32 index,
-        // @parm [out, retval] retrieved segment
-		 ImplAAFSegment ** ppSegment);
-
-  //****************
-  // GetSegments()
-  //
-  virtual AAFRESULT STDMETHODCALLTYPE
-    GetSegments
+    GetSlots
         // @parm [retval][out] Slots - segment list  enumeration
         (ImplEnumAAFSegments ** ppEnum);
 
 public:
 	// SDK-internal
 
-	virtual AAFRESULT ChangeContainedReferences(aafMobID_constref from,
-												aafMobID_constref to);
+	virtual AAFRESULT ChangeContainedReferences(aafUID_t *from, aafUID_t *to);
 
 
-private:
+public:
+  // Declare this class to be storable.
+  //
+  OMDECLARE_STORABLE(ImplAAFNestedScope)
 
-  // Persistent Properties	
+  // Declare the module test method. The implementation of the will be be
+  // in /test/ImplAAFNestedScopeTest.cpp.
+  static AAFRESULT test();
+
+	// Persistent Properties	
   OMStrongReferenceVectorProperty<ImplAAFSegment>		_slots;
 };
 
