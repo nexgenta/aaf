be55853676f6237b4b363531113743219219e22c
tbingham <tbingham>
2001-04-17 08:27:09 +0000

    Put character property sub-classes in their own files.

--- ref-impl/src/OM/OMCharacterStringPropertyT.h
+++ ref-impl/src/OM/OMCharacterStringPropertyT.h
@@ -1,28 +1,31 @@
-//=---------------------------------------------------------------------=
-//
-// The contents of this file are subject to the AAF SDK Public
-// Source License Agreement (the "License"); You may not use this file
-// except in compliance with the License.  The License is available in
-// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
-// Association or its successor.
-// 
-// Software distributed under the License is distributed on an "AS IS"
-// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
-// the License for the specific language governing rights and limitations
-// under the License.
-// 
-// The Original Code of this file is Copyright 1998-2001, Licensor of the
-// AAF Association.
-// 
-// The Initial Developer of the Original Code of this file and the
-// Licensor of the AAF Association is Avid Technology.
-// All rights reserved.
-//
-//=---------------------------------------------------------------------=
+/***********************************************************************
+*
+*              Copyright (c) 1998-2000 Avid Technology, Inc.
+*
+* Permission to use, copy and modify this software and accompanying
+* documentation, and to distribute and sublicense application software
+* incorporating this software for any purpose is hereby granted,
+* provided that (i) the above copyright notice and this permission
+* notice appear in all copies of the software and related documentation,
+* and (ii) the name Avid Technology, Inc. may not be used in any
+* advertising or publicity relating to the software without the specific,
+* prior written permission of Avid Technology, Inc.
+*
+* THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+* WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+* IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+* SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+* OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+* OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+* ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+* RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+* ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+* LIABILITY.
+*
+************************************************************************/
 
 // @doc OMEXTERNAL
-// @author Tim Bingham | tjb | Avid Technology, Inc. |
-//         OMCharacterStringProperty
 #ifndef OMCHARACTERSTRINGPROPERTYT_H
 #define OMCHARACTERSTRINGPROPERTYT_H
 
@@ -30,16 +33,16 @@
 
 // class OMCharacterStringProperty<CharacterType>
 
-template <typename CharacterType>
+template<typename CharacterType>
 OMCharacterStringProperty<CharacterType>::OMCharacterStringProperty(
                                                  const OMPropertyId propertyId,
-                                                 const wchar_t* name)
+                                                 const char* name)
 : OMVariableSizeProperty<CharacterType>(propertyId, name)
 {
   TRACE("OMCharacterStringProperty<CharacterType>::OMCharacterStringProperty");
 }
 
-template <typename CharacterType>
+template<typename CharacterType>
 OMCharacterStringProperty<CharacterType>::~OMCharacterStringProperty(void)
 {
   TRACE(
@@ -53,8 +56,8 @@ OMCharacterStringProperty<CharacterType>::~OMCharacterStringProperty(void)
   //                                  comprise the string.
   //   @rdesc The result of the conversion as a value of type
   //          pointer to <p CharacterType>.
-template <typename CharacterType>
-OMCharacterStringProperty<CharacterType>::operator const CharacterType*() const
+template<typename CharacterType>
+OMCharacterStringProperty<CharacterType>::operator const CharacterType* ()
 {
   return reinterpret_cast<const CharacterType*>(_bits);
 }
@@ -64,7 +67,7 @@ OMCharacterStringProperty<CharacterType>::operator const CharacterType*() const
   //   @tcarg class | CharacterType | The type of the characters that
   //                                  comprise the string.
   //   @parm The string of <p CharacterType> characters to assign.
-template <typename CharacterType>
+template<typename CharacterType>
 void OMCharacterStringProperty<CharacterType>::assign(
                                           const CharacterType* characterString)
 {
@@ -85,7 +88,7 @@ void OMCharacterStringProperty<CharacterType>::assign(
   //                                  comprise the string.
   //   @rdesc The length of this <c OMCharacterStringProperty>.
   //   @this const
-template <typename CharacterType>
+template<typename CharacterType>
 size_t OMCharacterStringProperty<CharacterType>::length(void) const
 {
   return (size() - sizeof(CharacterType)) / sizeof(CharacterType);
@@ -99,7 +102,7 @@ size_t OMCharacterStringProperty<CharacterType>::length(void) const
   //   @parm  A  string of <p CharacterType> characters.
   //   @rdesc The length of the the string of <p CharacterType>
   //          characters <p characterString>.
-template <typename CharacterType>
+template<typename CharacterType>
 size_t OMCharacterStringProperty<CharacterType>::stringLength(
                                           const CharacterType* characterString)
 {
