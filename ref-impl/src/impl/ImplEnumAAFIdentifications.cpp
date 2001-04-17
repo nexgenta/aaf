2f71d7ef98988940156ca29ceb4be962c97d3cae
tbingham <tbingham>
2001-04-17 04:41:32 +0000

    Fix more FULL_TOOLKIT and !!! places

--- ref-impl/src/impl/ImplEnumAAFIdentifications.cpp
+++ ref-impl/src/impl/ImplEnumAAFIdentifications.cpp
@@ -1,29 +1,262 @@
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- * prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
 
-// This module has been made obsolete by the implementation of a common enumerator
-// template class.
+
+#ifndef __ImplAAFIdentification_h__
+#include "ImplAAFIdentification.h"
+#endif
+
+
+
+
+#ifndef __ImplEnumAAFIdentifications_h__
+#include "ImplEnumAAFIdentifications.h"
+#endif
+
+#include "ImplAAFHeader.h"
+#include "ImplAAFDictionary.h"
+#include "ImplAAFObjectCreation.h"
+
+#include <assert.h>
+#include <string.h>
+#include "aafErr.h"
+
+extern "C" const aafClassID_t CLSID_EnumAAFIdentifications;
+
+ImplEnumAAFIdentifications::ImplEnumAAFIdentifications ()
+{
+	_current = 0;
+	_enumObj = NULL;
+	_enumProp = NULL;
+	_enumStrongProp = NULL;
+}
+
+
+ImplEnumAAFIdentifications::~ImplEnumAAFIdentifications ()
+{
+	if (_enumObj)
+	{
+		_enumObj->ReleaseReference();
+		_enumObj = NULL;
+	}
+}
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplEnumAAFIdentifications::NextOne (
+      ImplAAFIdentification **ppIdentification)
+{
+	aafUInt32			numElem;
+//	aafUID_t			value;
+	ImplAAFHeader		*head = NULL;
+	ImplAAFDictionary	*dict = NULL;
+
+	if(_enumProp != NULL)
+		numElem = _enumProp->size() / sizeof(aafUID_t);
+	else if(_enumStrongProp != NULL)
+	{
+		size_t	siz;
+		
+		_enumStrongProp->getSize(siz);
+		numElem = siz;
+	}
+	else
+		return(AAFRESULT_INCONSISTANCY);
+
+	if(ppIdentification == NULL)
+		return(AAFRESULT_NULL_PARAM);
+	if(_current >= numElem)
+		return AAFRESULT_NO_MORE_OBJECTS;
+	XPROTECT()
+	{
+//!!!		if(_enumProp != NULL)
+//		{
+//			_enumProp->getValueAt(&value, _current);
+//			CHECK(_enumObj->MyHeadObject(&head));
+//			CHECK(head->GetDictionary (&dict));
+//			CHECK(dict->LookupParameterDefinition(&value, ppIdentification));
+//			head->ReleaseReference();
+//			head = NULL;
+//			dict->ReleaseReference();
+//			dict = NULL;
+//		}
+//		else
+			if(_enumStrongProp != NULL)
+		{
+			_enumStrongProp->getValueAt(*ppIdentification, _current);
+			(*ppIdentification)->AcquireReference();
+		}
+		else
+			RAISE(AAFRESULT_INCONSISTANCY);
+		_current++;
+		if (head) {
+			head->ReleaseReference();
+			head = NULL;
+		}
+		if (dict) {
+			dict->ReleaseReference();
+			dict = NULL;
+		}
+	}
+	XEXCEPT
+	{
+		if(head)
+			head->ReleaseReference();
+		if(dict)
+			dict->ReleaseReference();
+	}
+	XEND;
+
+	return(AAFRESULT_SUCCESS); 
+}
+
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplEnumAAFIdentifications::Next (
+      aafInt32  count,
+      ImplAAFIdentification ** ppIdentifications,
+      aafInt32 * pNumFetched)
+{
+	ImplAAFIdentification**	ppDef;
+	aafInt32				numDefs;
+	HRESULT					hr;
+
+	if ((pNumFetched == NULL && count != 1) || (pNumFetched != NULL && count == 1))
+		return E_INVALIDARG;
+
+	// Point at the first component in the array.
+	ppDef = ppIdentifications;
+	for (numDefs = 0; numDefs < count; numDefs++)
+	{
+		hr = NextOne(ppDef);
+		if (FAILED(hr))
+			break;
+
+		// Point at the next component in the array.  This
+		// will increment off the end of the array when
+		// numComps == count-1, but the for loop should
+		// prevent access to this location.
+		ppDef++;
+	}
+	
+	if (pNumFetched)
+		*pNumFetched = numDefs;
+
+	return hr;
+}
+
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplEnumAAFIdentifications::Skip (
+      aafUInt32 count)
+{
+	AAFRESULT	hr;
+	aafUInt32	newCurrent;
+	aafUInt32	numElem;
+
+//!!!	if(_enumProp != NULL)
+//		numElem = _enumProp->size() / sizeof(aafUID_t);
+//	else
+		if(_enumStrongProp != NULL)
+	{
+		size_t	siz;
+		
+		_enumStrongProp->getSize(siz);
+		numElem = siz;
+	}
+	else
+		return(AAFRESULT_INCONSISTANCY);
+
+	newCurrent = _current + count;
+
+	if(newCurrent < numElem)
+	{
+		_current = newCurrent;
+		hr = AAFRESULT_SUCCESS;
+	}
+	else
+	{
+		hr = E_FAIL;
+	}
+
+	return hr;
+}
+
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplEnumAAFIdentifications::Reset ()
+{
+	_current = 0;
+	return AAFRESULT_SUCCESS;
+}
+
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplEnumAAFIdentifications::Clone (
+      ImplEnumAAFIdentifications **ppEnum)
+{
+	ImplEnumAAFIdentifications	*result;
+	AAFRESULT					hr;
+
+	result = (ImplEnumAAFIdentifications *)CreateImpl(CLSID_EnumAAFIdentifications);
+	if (result == NULL)
+		return E_FAIL;
+
+//!!!	if(_enumProp != NULL)
+//		hr = result->SetEnumProperty(_enumObj, _enumProp);
+//	else
+		if(_enumStrongProp != NULL)
+		hr = result->SetEnumStrongProperty(_enumObj, _enumStrongProp);
+	if (SUCCEEDED(hr))
+	{
+		result->_current = _current;
+		*ppEnum = result;
+	}
+	else
+	{
+		result->ReleaseReference();
+		*ppEnum = NULL;
+	}
+	
+	return hr;
+}
+
+
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplEnumAAFIdentifications::SetEnumProperty( ImplAAFObject *pObj, identWeakRefArrayProp_t *pProp)
+{
+	if (_enumObj)
+		_enumObj->ReleaseReference();
+	_enumObj = pObj;
+	if (pObj)
+		pObj->AcquireReference();
+	_enumProp = pProp;				// Don't refcount, same lifetime as the object.
+	_enumStrongProp = NULL;
+
+	return AAFRESULT_SUCCESS;
+}
+
+AAFRESULT STDMETHODCALLTYPE
+    ImplEnumAAFIdentifications::SetEnumStrongProperty( ImplAAFObject *pObj, identStrongRefArrayProp_t *pProp)
+{
+	if (_enumObj)
+		_enumObj->ReleaseReference();
+	_enumObj = pObj;
+	if (pObj)
+		pObj->AcquireReference();
+	/**/
+	_enumStrongProp = pProp;		// Don't refcount, same lifetime as the object.
+	_enumProp = NULL;
+
+	return AAFRESULT_SUCCESS;
+}
--- ref-impl/src/impl/ImplEnumAAFIdentifications.h
+++ ref-impl/src/impl/ImplEnumAAFIdentifications.h
@@ -4,38 +4,107 @@
 #define __ImplEnumAAFIdentifications_h__
 
 
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- *  prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
 
-#include "ImplAAFEnumerator.h"
 
-#include "ImplAAFIdentification.h"
+class ImplAAFIdentification;
 
-typedef ImplAAFEnumerator<ImplAAFIdentification> ImplEnumAAFIdentifications;
+
+#ifndef __ImplAAFObject_h__
+#include "ImplAAFObject.h"
+#endif
+
+typedef OMVariableSizeProperty<aafUID_t> identWeakRefArrayProp_t;
+typedef OMStrongReferenceVectorProperty<ImplAAFIdentification> identStrongRefArrayProp_t;
+
+class ImplEnumAAFIdentifications : public ImplAAFRoot
+{
+public:
+  //
+  // Constructor/destructor
+  //
+  //********
+  ImplEnumAAFIdentifications ();
+
+protected:
+  virtual ~ImplEnumAAFIdentifications ();
+
+public:
+
+  //****************
+  // NextOne()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    NextOne
+        // @parm [out, retval] The Next Identification
+        (ImplAAFIdentification ** ppIdentification);
+
+
+  //****************
+  // Next()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    Next
+        (// @parm [in] number of identifications requested
+         aafInt32  count,
+
+         // @parm [out, size_is(count), length_is(*pNumFetched)] array to receive identification objects
+         ImplAAFIdentification ** ppIdentifications,
+
+         // @parm [out] number of actual Identifications fetched into ppIdentifications array
+         aafInt32 *  pNumFetched);
+
+
+  //****************
+  // Skip()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    Skip
+        // @parm [in] Number of elements to skip
+        (aafUInt32  count);
+
+
+  //****************
+  // Reset()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    Reset ();
+
+
+
+  //****************
+  // Clone()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    Clone
+        // @parm [out, retval] new enumeration
+        (ImplEnumAAFIdentifications ** ppEnum);
+
+public:
+  // SDK Internal 
+  virtual AAFRESULT STDMETHODCALLTYPE
+    SetEnumProperty( ImplAAFObject *pObj, identWeakRefArrayProp_t *pProp);
+  virtual AAFRESULT STDMETHODCALLTYPE
+    SetEnumStrongProperty( ImplAAFObject *pObj, identStrongRefArrayProp_t *pProp);
+
+
+public:
+  // Declare the module test method. The implementation of the will be be
+  // in /test/ImplEnumAAFIdentificationsTest.cpp.
+  static AAFRESULT test();
+private:
+	aafUInt32						_current;
+	ImplAAFObject					*_enumObj;
+	identWeakRefArrayProp_t			*_enumProp;
+	identStrongRefArrayProp_t		*_enumStrongProp;
+};
 
 #endif // ! __ImplEnumAAFIdentifications_h__
 
