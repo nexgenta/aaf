3072e5aad5e3a921a0461bcca06dcacdf1742114
tbingham <tbingham>
2001-04-17 03:47:35 +0000

    Added TypeDefIndirect: enabled for ConstantValue, ControlPoint and TaggedValue. The AAFTypeDefIndirect module test is incomplete...

--- ref-impl/src/impl/ImplAAFParameter.h
+++ ref-impl/src/impl/ImplAAFParameter.h
@@ -3,14 +3,32 @@
 #ifndef __ImplAAFParameter_h__
 #define __ImplAAFParameter_h__
 
-/***********************************************\
-*												*
-* Advanced Authoring Format						*
-*												*
-* Copyright (c) 1998-1999 Avid Technology, Inc. *
-* Copyright (c) 1998-1999 Microsoft Corporation *
-*												*
-\***********************************************/
+/***********************************************************************
+ *
+ *              Copyright (c) 1998-1999 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and accompanying 
+ * documentation, and to distribute and sublicense application software
+ * incorporating this software for any purpose is hereby granted, 
+ * provided that (i) the above copyright notice and this permission
+ * notice appear in all copies of the software and related documentation,
+ * and (ii) the name Avid Technology, Inc. may not be used in any
+ * advertising or publicity relating to the software without the specific,
+ *  prior written permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+ * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+ * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+ * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+ * LIABILITY.
+ *
+ ************************************************************************/
 
 #ifndef __AAFTypes_h__
 #include "AAFTypes.h"
@@ -18,7 +36,6 @@
 
 class ImplAAFParameterDef;
 class ImplAAFTypeDef;
-class ImplAAFReferenceValue;
 
 #ifndef __ImplAAFObject_h__
 #include "ImplAAFObject.h"
@@ -39,34 +56,22 @@ protected:
 
 public:
   virtual AAFRESULT STDMETHODCALLTYPE
-    ImplAAFParameter::SetParameterDefinition (
+    SetParameterDefinition (
       ImplAAFParameterDef *pParmDef);
 
   virtual AAFRESULT STDMETHODCALLTYPE
-    ImplAAFParameter::GetParameterDefinition (
+    GetParameterDefinition (
       ImplAAFParameterDef **ppParmDef);	
 
   virtual AAFRESULT STDMETHODCALLTYPE
-    ImplAAFParameter::SetTypeDefinition (
-      ImplAAFTypeDef*  pTypeDef);
-
-  virtual AAFRESULT STDMETHODCALLTYPE
-    ImplAAFParameter::GetTypeDefinition (
+    GetTypeDefinition (
       ImplAAFTypeDef **ppTypeDef);
 
-public:
-  // Declare this class to be storable.
-  //
-  OMDECLARE_STORABLE(ImplAAFParameter)
-
-  // Declare the module test method. The implementation of the will be be
-  // in /test/ImplAAFParameterTest.cpp.
-  static AAFRESULT test();
+    virtual const OMUniqueObjectIdentification& identification(void) const;
 
 private:
 
 	OMFixedSizeProperty<aafUID_t>				_parmDef;
-	OMFixedSizeProperty<aafUID_t>				_typeDef;
 };
 
 #endif // ! __ImplAAFParameter_h__
