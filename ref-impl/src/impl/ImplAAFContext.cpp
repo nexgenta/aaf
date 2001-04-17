7c6931143482ef4cec7bbd8c2dc717532e0dc3fe
tbingham <tbingham>
2001-04-17 07:51:42 +0000

    Use and persist GUIDs instead of small integers for class ids

--- ref-impl/src/impl/ImplAAFContext.cpp
+++ ref-impl/src/impl/ImplAAFContext.cpp
@@ -1,142 +1,181 @@
 
-//=---------------------------------------------------------------------=
-//
-// The contents of this file are subject to the AAF SDK Public
-// Source License Agreement (the "License"); You may not use this file
-// except in compliance with the License.  The License is available in
-// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
-// Association or its successor.
-// 
-// Software distributed under the License is distributed on an "AS IS"
-// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
-// the License for the specific language governing rights and limitations
-// under the License.
-// 
-// The Original Code of this file is Copyright 1998-2001, Licensor of the
-// AAF Association.
-// 
-// The Initial Developer of the Original Code of this file and the
-// Licensor of the AAF Association is Avid Technology.
-// All rights reserved.
-//
-//=---------------------------------------------------------------------=
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
 
 
 
 
 
 
-#ifndef __ImplAAFContext_h__
-#include "ImplAAFContext.h"
+#ifndef __ImplAAFSession_h__
+#include "ImplAAFSession.h"
 #endif
 
-#include "ImplAAFObjectCreation.h"
+#include "OMUtilities.h"
 
-#include "AAFResult.h"
+#include "ImplAAFObjectCreation.h"
+#include "ImplAAFFile.h"
 
 #include <assert.h>
-#include "ImplAAFPluginManager.h"
 
-#include "OMObjectManager.h"
+extern "C" const aafClassID_t CLSID_AAFFile;
 
-extern "C" const aafClassID_t CLSID_AAFPluginManager;
+ImplAAFSession::ImplAAFSession ()
+{}
 
 
+ImplAAFSession::~ImplAAFSession ()
+{}
 
-// Make private helper class a friend so that it
-// may call the destructor. This helper class is used
-// to ensure that the singleton context instance is 
-// cleaned up properly.
 
-class ImplAAFContextHelper
+AAFRESULT STDMETHODCALLTYPE
+    ImplAAFSession::EndSession ()
 {
-public:
-  ImplAAFContextHelper();
-  ~ImplAAFContextHelper();
-};
-
-
-
-
-
-// single instance of this class; initialized by first call to GetInstance().
-/*static*/ ImplAAFContext * ImplAAFContext::_singleton = 0;
-
-// Create an instance of the context helper. The helper's destructor will delete the context.
-static ImplAAFContextHelper g_ContextHelper;
-
-
-
-
-// Create and save the context.
-ImplAAFContextHelper::ImplAAFContextHelper()
-{
+  return AAFRESULT_SUCCESS;
 }
 
-// Cleanup the context singleton if one exists.
-ImplAAFContextHelper::~ImplAAFContextHelper()
-{
-  if (ImplAAFContext::_singleton)
+  //****************
+  // CreateFile()
+  //
+  AAFRESULT STDMETHODCALLTYPE
+    ImplAAFSession::CreateFile
+        (aafDataBuffer_t  filePath,   //@parm [in] File path [replace with object later]
+		 aafFileRev_t  rev,   //@parm [in] File revision to create
+         ImplAAFFile ** file)  //@parm [out] Current AAF file
   {
-    delete ImplAAFContext::_singleton;
+	ImplAAFRoot	*pRoot;
+
+	pRoot = CreateImpl(CLSID_AAFFile);
+	if (!pRoot)
+		return(0x80004005L);	// TODO: change this to AAFRESULT_FAILED
+
+	*file = static_cast<ImplAAFFile*>(pRoot);
+	(*file)->Create(filePath, this, rev);
+
+	return(AAFRESULT_SUCCESS);
   }
-}
 
-
-/*static*/ ImplAAFContext * ImplAAFContext::GetInstance ()
-{
-  if (! _singleton)
+  //****************
+  // OpenReadFile()
+  //
+  AAFRESULT STDMETHODCALLTYPE
+    ImplAAFSession::OpenReadFile
+        (aafDataBuffer_t  filePath,   //@parm [in] File path [replace with object later]
+		 ImplAAFFile ** file)  //@parm [out] Current AAF file
   {
-    _singleton = new ImplAAFContext;
-    assert(_singleton);
-    if (_singleton)
-      _singleton->InitPluginManager(); 
+	ImplAAFRoot	*pRoot;
+
+	pRoot = CreateImpl(CLSID_AAFFile);
+	if (!pRoot)
+		return(0x80004005L);	// TODO: change this to AAFRESULT_FAILED
+
+	*file = static_cast<ImplAAFFile*>(pRoot);
+	(*file)->OpenRead(filePath, this);
+
+	return(AAFRESULT_SUCCESS);
   }
-  return _singleton;
-}
 
-ImplAAFContext::ImplAAFContext ()
-{
-  // There Can Be Only One!
-  assert(NULL == _singleton);
-  _singleton = this;
-
-  _plugins = NULL;
-  initializeObjectManager();
-}
-
-ImplAAFContext::~ImplAAFContext ()
-{
-  if (_plugins)
+  //****************
+  // OpenModifyFile()
+  //
+  AAFRESULT STDMETHODCALLTYPE
+	  ImplAAFSession::OpenModifyFile
+        (aafDataBuffer_t  filePath,   //@parm [in] File path [replace with object later]
+		 ImplAAFFile ** file)  //@parm [out] Current AAF file
   {
-    _plugins->ReleaseReference();
-    _plugins = 0;
+	AAFRESULT hr;
+	ImplAAFRoot	*pRoot;
+
+	pRoot = CreateImpl(CLSID_AAFFile);
+	if (!pRoot)
+		return(0x80004005L);	// TODO: change this to AAFRESULT_FAILED
+
+	*file = static_cast<ImplAAFFile*>(pRoot);
+	hr = (*file)->OpenModify(filePath, this);
+
+	return(hr);
   }
+
+ImplAAFFile *ImplAAFSession::GetTopFile()
+{
+	return(_topFile);
+}
+
+void ImplAAFSession::SetTopFile(ImplAAFFile *file)
+{
+	_topFile = file;
+}
+
+OMLSession ImplAAFSession::GetContainerSession(void)
+{
+	return((OMLSession)NULL);
+}
+
+aafProductIdentification_t *ImplAAFSession::GetDefaultIdent(void)
+{
+	return(_defaultIdent);
+}
+
+  //***********************************************************
+  // METHOD NAME: SetCurrentIdentification()
+  //
+  // DESCRIPTION:
+  // @mfunc AAFRESULT | AAFSession | SetCurrentIdentification |
+  // Sets the object which identifies the creator of the file.
+  // @end
+  // 
+AAFRESULT STDMETHODCALLTYPE
+ImplAAFSession::SetDefaultIdentification (
+    // @parm aafProductIdentification | ident | [in] a struct from which it is initialized
+    aafProductIdentification_t  *ident
+  )
+  {
+	  _defaultIdent = ident;
+
+    if ((_defaultIdent != 0) && (_defaultIdent->productName != 0)) {
+      setProgramName((const char *)_defaultIdent->productName);
+    } else {
+      setProgramName("Unknown");
+    }
+
+	  return(AAFRESULT_SUCCESS);
+  }
+
+
+AAFRESULT STDMETHODCALLTYPE
+ImplAAFSession::GetIdentification (
+    aafProductIdentification_t  *ident)
+{
+  // BobT: this should check for null, and return the appropriate
+  // error code.
+  assert (ident);
   
-  // Thare Can Be Only One!
-  _singleton = 0;
-  finalizeObjectManager();
+  //
+  // BobT: Horrible hack! We want to return the identification to the
+  // user, but our aafProductIdentification_t struct only contains
+  // pointers!  Fix it later; for now we'll return pointers to
+  // internal data (DANGEROUS!) to the caller...
+  //
+  assert (_defaultIdent);
+  *ident = *_defaultIdent;
+
+  return (AAFRESULT_SUCCESS);
 }
 
-void ImplAAFContext::InitPluginManager (void)
+AAFRESULT STDMETHODCALLTYPE
+ImplAAFSession::BeginSession (
+    aafProductIdentification_t  *ident
+  )
 {
-  if(_plugins == NULL)
-  {
-    _plugins = (ImplAAFPluginManager *)CreateImpl(CLSID_AAFPluginManager);
-    assert(NULL != _plugins);
-    if (_plugins)
-      _plugins->Init();
-  }
+  return SetDefaultIdentification(ident);
 }
 
-class ImplAAFPluginManager *ImplAAFContext::GetPluginManager (void)
-{
-  if (_plugins)
-    _plugins->AcquireReference();
-  return(_plugins);
-}
+extern "C" const aafClassID_t CLSID_AAFSession;
 
-// extern "C" const aafClassID_t CLSID_AAFContext;
-// CLSID for AAFContext
-// {1B5D0F20-FCC5-11d2-BFBC-00104BC9156D}
-// const aafClassID_t CLSID_AAFContext = { 0x1b5d0f20, 0xfcc5, 0x11d2, { 0xbf, 0xbc, 0x0, 0x10, 0x4b, 0xc9, 0x15, 0x6d } };
+OMDEFINE_STORABLE(AAFSession, CLSID_AAFSession);
