2d1dd8422a5e20121317e280093c3ec8080884fa
tbingham <tbingham>
2001-04-17 08:37:57 +0000

    Comments only. Added comments with autoduck tags to OMPropertySet, OMStoredObject and OMProperty.

--- ref-impl/include/OM/OMProperty.h
+++ ref-impl/include/OM/OMProperty.h
@@ -1,3 +1,5 @@
+// @doc
+
 #ifndef OMPROPERTY_H
 #define OMPROPERTY_H
 
@@ -13,17 +15,46 @@ const int TID_WEAK_OBJECT_REFERENCE          = 3;
 class OMStoredObject;
 class OMStorable;
 
+  // @class Abstract base class for persistent properties supported by
+  //        the Object Manager.
 class OMProperty {
 public:
+  // @access Public members.
+
+    // @cmember Constructor.
   OMProperty(int pid, const char* name);
+
+    // @cmember Constructor.
   OMProperty(int pid, const int type, const char* name);
+
+    // @cmember Destructor.
   virtual ~OMProperty(void);
+
+    // @cmember Save this <c OMProperty> to the <c OMStoredObject>
+    //          <p s>.
+    // @this const
   virtual void saveTo(OMStoredObject& s) const = 0;
+
+    // @cmember Restore this <c OMProperty> from the
+    //          <c OMStoredObject> <p s>, the size of the <c OMProperty>
+    //          is <p size>.
   virtual void restoreFrom(OMStoredObject& s, size_t size) = 0;
+
+    // @cmember The name of this <c OMProperty>.
+    // @this const 
   const char* name(void) const;
+
+    // @cmember The property id of this <c OMProperty>.
+    // @this const
   int pid(void) const;
+
+    // @cmember Inform this <c OMProperty> that it is a property of
+    //          the <c OMStorable> object <p containingObject>.
   void setContainingObject(const OMStorable* containingObject);
+
+    // @cmember The address of this <c OMProperty> object.
   OMProperty* address(void);
+
 protected:
   int _pid;
   int _type;
@@ -37,6 +68,7 @@ template <typename ReferencedObject>
 class OMReferenceProperty : public OMProperty {
 public:
   OMReferenceProperty(int pid, const int type, const char* name);
+  virtual ~OMReferenceProperty(void);
   virtual void setValue(const ReferencedObject*& object);
   virtual void getValue(ReferencedObject*& object) const = 0;
 protected:
@@ -51,6 +83,7 @@ class OMStrongReferenceProperty :
                                  public OMReferenceProperty<ReferencedObject> {
 public:
   OMStrongReferenceProperty(int pid, const char* name);
+  virtual ~OMStrongReferenceProperty(void);
   virtual void getValue(ReferencedObject*& object) const;
   virtual void setValue(const ReferencedObject*& object);
   OMStrongReferenceProperty<ReferencedObject>& operator =
@@ -64,6 +97,7 @@ template <typename ReferencedObject>
 class OMWeakReferenceProperty : public OMReferenceProperty<ReferencedObject> {
 public:
   OMWeakReferenceProperty(int pid, const char* name);
+  virtual ~OMWeakReferenceProperty(void);
   virtual void getValue(ReferencedObject*& object) const;
   OMWeakReferenceProperty<ReferencedObject>& operator =
                                               (const ReferencedObject*& value);
@@ -76,6 +110,7 @@ class OMSimpleProperty : public OMProperty {
 public:
   OMSimpleProperty(int pid, const char* name, size_t valueSize);
   OMSimpleProperty(int pid, const char* name);
+  virtual ~OMSimpleProperty(void);
   virtual void saveTo(OMStoredObject& s) const;
   size_t size(void) const;
 protected:
@@ -90,6 +125,7 @@ template <typename PropertyType>
 class OMFixedSizeProperty : public OMSimpleProperty {
 public:
   OMFixedSizeProperty(int pid, const char* name);
+  virtual ~OMFixedSizeProperty(void);
   void getValue(PropertyType& value) const;
   void setValue(const PropertyType& value);
   OMFixedSizeProperty<PropertyType>& operator = (const PropertyType& value);
@@ -102,6 +138,7 @@ template <typename PropertyType>
 class OMVariableSizeProperty : public OMSimpleProperty {
 public:
   OMVariableSizeProperty(int pid, const char* name);
+  virtual ~OMVariableSizeProperty(void);
   void getValue(PropertyType* value, size_t valueSize) const;
   void setValue(const PropertyType* value, size_t valueSize);
   bool copyToBuffer(PropertyType* buffer, size_t bufferSize) const;
@@ -111,12 +148,14 @@ public:
 class OMCollectionProperty : public OMProperty {
 public:
   OMCollectionProperty(int pid, const int type, const char* name);
+  virtual ~OMCollectionProperty(void);
 };
 
 template <typename ReferencedObject>
 class OMStrongReferenceVectorProperty : public OMCollectionProperty {
 public:
   OMStrongReferenceVectorProperty(int pid, const char* name);
+  virtual ~OMStrongReferenceVectorProperty(void);
   virtual void saveTo(OMStoredObject& s) const;
   virtual void restoreFrom(OMStoredObject& s, size_t size);
   void getSize(size_t& size) const;
@@ -139,6 +178,7 @@ class OMCharacterStringProperty :
                                  public OMVariableSizeProperty<CharacterType> {
 public:
   OMCharacterStringProperty(int pid, const char * name);
+  virtual ~OMCharacterStringProperty(void);
   operator const CharacterType* (void);
   operator const CharacterType* (void) const;
   void assign(const CharacterType* characterString);
@@ -152,12 +192,14 @@ private:
 class OMStringProperty : public OMCharacterStringProperty<char> {
 public:
   OMStringProperty(int pid, const char* name);
+  virtual ~OMStringProperty(void);
   OMStringProperty& operator = (const char* value);
 };
 
 class OMWideStringProperty : public OMCharacterStringProperty<wchar_t> {
 public:
   OMWideStringProperty(int pid, const char* name);
+  virtual ~OMWideStringProperty(void);
   OMWideStringProperty& operator = (const wchar_t* value);
 };
 
