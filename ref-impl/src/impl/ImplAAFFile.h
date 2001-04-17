8b26d123765883c799a92a24f4ccee2156f25010
tbingham <tbingham>
2001-04-17 10:06:42 +0000

    Add comments for promoter drop.

--- ref-impl/src/impl/ImplAAFFile.h
+++ ref-impl/src/impl/ImplAAFFile.h
@@ -19,7 +19,7 @@
 
 typedef enum
 {
-	kOmCreate, kOmModify, kOmOpenRead
+	kOmCreate, kOmModify, kOmOpenRead, kOmUndefined = -1
 }				openType_t;
 
 //
@@ -32,48 +32,69 @@ class AAFMedia;
 class ImplAAFHeader;
 struct IAAFSession;
 class ImplAAFSession;
-class ImplAAFDataDefinition;
+class ImplAAFDataDef;
 
 class ImplAAFFile : public ImplAAFRoot
 {
 public:
+
+  //
+  // methods to support API
+  //
+
+  virtual AAFRESULT STDMETHODCALLTYPE
+	Initialize ();
+
+  virtual AAFRESULT STDMETHODCALLTYPE
+	OpenExistingRead (wchar_t * pFileName,
+					  aafUInt32 modeFlags);
+
+  virtual AAFRESULT STDMETHODCALLTYPE
+	OpenExistingModify (wchar_t * pFileName,
+						aafUInt32 modeFlags,
+						aafProductIdentification_t * pIdent);
+
+  virtual AAFRESULT STDMETHODCALLTYPE
+	OpenNewModify (wchar_t * pFileName,
+				   aafUInt32 modeFlags,
+				   aafProductIdentification_t * pIdent);
+
+  virtual AAFRESULT STDMETHODCALLTYPE
+	OpenTransient (aafProductIdentification_t * pIdent);
+
+  virtual AAFRESULT STDMETHODCALLTYPE
+	Close ();
+
+  virtual AAFRESULT STDMETHODCALLTYPE
+	ForceClose ();
+
+  virtual AAFRESULT STDMETHODCALLTYPE
+	Save ();
+
+  virtual AAFRESULT STDMETHODCALLTYPE
+	SaveAs (wchar_t * pFileName,
+			aafUInt32 modeFlags);
+
+  virtual AAFRESULT STDMETHODCALLTYPE
+	Revert ();
+
+  virtual AAFRESULT STDMETHODCALLTYPE
+	GetHeader (ImplAAFHeader ** header);
+
+  virtual AAFRESULT STDMETHODCALLTYPE
+	GetRevision (aafFileRev_t *  rev);
+  
+
   //
   // Constructor/destructor
   //
   //********
   ImplAAFFile ();
   virtual ~ImplAAFFile ();
-
+#if 0
   OMDECLARE_STORABLE(ImplAAFFile)
+#endif
 
-  //***********************************************************
-  // METHOD NAME: Close()
-  //
-  // DESCRIPTION:
-  // @mfunc AAFRESULT | AAFFile | Close |
-  // Closes an AAF file, saving the result.
-  // @end
-  virtual AAFRESULT STDMETHODCALLTYPE
-    Close ();
-
-  //***********************************************************
-  // METHOD NAME: GetRevision()
-  //
-  // DESCRIPTION:
-  // @mfunc AAFRESULT | AAFFile | GetRevision |
-  // Get the revision of the ciurrent AAF file.
-  // @end
-  // 
-  virtual AAFRESULT STDMETHODCALLTYPE
-  GetRevision (
-    // @parm aafFileRev_t * | rev | [out] Revision of the current file
-    aafFileRev_t *  rev
-  );
-
-  virtual AAFRESULT STDMETHODCALLTYPE
-  GetHeader (
-    ImplAAFHeader ** header
-  );
 
 public:
   // Declare the module test method. The implementation of the will be be
@@ -96,6 +117,8 @@ private:
 								   ImplAAFSession * session,
 								   OMLContainerUseMode useMode, 
 								   openType_t type);
+  
+  void InternalReleaseObjects();
 
 		aafInt32       	_cookie;
 		aafFileFormat_t _fmt;
@@ -116,14 +139,18 @@ private:
 #endif
 		aafBool         _semanticCheckEnable;	//!!!  /* Used to stop recursion in checks */
 		ImplAAFSession		 *_session;		//!!!  used by file checker
-		ImplAAFDataDefinition *	_nilKind;// !!!
-		ImplAAFDataDefinition *	_pictureKind;// !!!
-		ImplAAFDataDefinition *	_soundKind;// !!!
+		ImplAAFDataDef *	_nilKind;// !!!
+		ImplAAFDataDef *	_pictureKind;// !!!
+		ImplAAFDataDef *	_soundKind;// !!!
 #ifdef AAF_ERROR_TRACE
 		char			*_stackTrace;	//!!!	
 		aafInt32		_stackTraceSize;	//!!!	
 #endif
 		aafFileRev_t     _setrev;
+
+  aafBool _initialized;
+  aafBool _open;
+  aafProductIdentification_t _ident;
 };
 
 #endif // ! __ImplAAFFile_h__
