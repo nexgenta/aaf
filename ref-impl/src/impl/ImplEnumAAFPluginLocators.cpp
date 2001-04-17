30188b5c69584fc94e7ae5285530177dba2fec8c
tbingham <tbingham>
2001-04-17 04:52:17 +0000

    Added AAFPluginDescriptor and related enum

--- ref-impl/src/impl/ImplEnumAAFPluginLocators.cpp
+++ ref-impl/src/impl/ImplEnumAAFPluginLocators.cpp
@@ -1,29 +1,11 @@
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- *  prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+/************************************************\
+*												*
+* Advanced Authoring Format						*
+*												*
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+* Copyright (c) 1998-1999 Microsoft Corporation *
+*												*
+\************************************************/
 
 
 #ifndef __ImplAAFLocator_h__
@@ -68,7 +50,7 @@ AAFRESULT STDMETHODCALLTYPE
 
     XPROTECT()
 	{
-		CHECK(_cPluginDesc->CountLocators (&siz));
+		CHECK(_cPluginDesc->GetNumLocators (&siz));
 		if(cur < siz)
 		{
 			CHECK(_cPluginDesc->GetNthLocator (cur, ppAAFLocator));
@@ -86,60 +68,19 @@ AAFRESULT STDMETHODCALLTYPE
 
 AAFRESULT STDMETHODCALLTYPE
     ImplEnumAAFPluginLocators::Next (
-      aafUInt32  count,
-      ImplAAFLocator ** ppAAFLocators,
-      aafUInt32 *  pFetched)
+      aafUInt32  /*count*/,
+      ImplAAFLocator ** /*ppAAFLocators*/,
+      aafUInt32 *  /*pFetched*/)
 {
-	ImplAAFLocator**	ppLoc;
-	aafUInt32			numLoc;
-	HRESULT				hr;
-
-	if ((pFetched == NULL && count != 1) || (pFetched != NULL && count == 1))
-		return E_INVALIDARG;
-
-	// Point at the first component in the array.
-	ppLoc = ppAAFLocators;
-	for (numLoc = 0; numLoc < count; numLoc++)
-	{
-		hr = NextOne(ppLoc);
-		if (FAILED(hr))
-			break;
-
-		// Point at the next component in the array.  This
-		// will increment off the end of the array when
-		// numComps == count-1, but the for loop should
-		// prevent access to this location.
-		ppLoc++;
-	}
-	
-	if (pFetched)
-		*pFetched = numLoc;
-
-	return AAFRESULT_SUCCESS;
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
 
 
 AAFRESULT STDMETHODCALLTYPE
     ImplEnumAAFPluginLocators::Skip (
-      aafUInt32  count)
+      aafUInt32  /*count*/)
 {
-	AAFRESULT	hr;
-	aafUInt32	newCurrent, siz;
-
-	newCurrent = _current + count;
-
-    _cPluginDesc->CountLocators(&siz);
-	if(newCurrent < siz)
-	{
-		_current = newCurrent;
-		hr = AAFRESULT_SUCCESS;
-	}
-	else
-	{
-		hr = E_FAIL;
-	}
-
-	return hr;
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
 
 
@@ -170,9 +111,8 @@ AAFRESULT STDMETHODCALLTYPE
 	}
 	else
 	{
-	  result->ReleaseReference();
-	  result = 0;
-	  *ppEnum = NULL;
+		result->ReleaseReference();
+		*ppEnum = NULL;
 	}
 	
 	return hr;
@@ -181,11 +121,10 @@ AAFRESULT STDMETHODCALLTYPE
 
 
 AAFRESULT
-    ImplEnumAAFPluginLocators::SetPluginDescriptor(ImplAAFPluginDef *pPDesc)
+    ImplEnumAAFPluginLocators::SetPluginDescriptor(ImplAAFPluginDescriptor *pPDesc)
 {
 	if (_cPluginDesc)
-	  _cPluginDesc->ReleaseReference();
-	_cPluginDesc = 0;
+		_cPluginDesc->ReleaseReference();
 
 	_cPluginDesc = pPDesc;
 
