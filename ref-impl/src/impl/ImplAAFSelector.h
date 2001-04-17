fbaa7f6dc646c83b9966e03841c835e80026fb03
tbingham <tbingham>
2001-04-17 04:58:10 +0000

    Initial Implementation: persistence of properties and Get/Set Methods

--- ref-impl/src/impl/ImplAAFSelector.h
+++ ref-impl/src/impl/ImplAAFSelector.h
@@ -4,46 +4,26 @@
 #define __ImplAAFSelector_h__
 
 
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- *  prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
 
 
 class ImplAAFSegment;
 
-template <class T> 
-class ImplAAFEnumerator;
-typedef ImplAAFEnumerator<ImplAAFSegment> ImplEnumAAFSegments;
+class ImplEnumAAFSegments;
+
+
 
 #ifndef __ImplAAFSegment_h__
 #include "ImplAAFSegment.h"
 #endif
 
-#include "OMStrongRefProperty.h"
-#include "OMStrongRefVectorProperty.h"
 
 class ImplAAFSelector : public ImplAAFSegment
 {
@@ -84,12 +64,6 @@ public:
         // @parm [in] Segment to append to the Alternate list of segments
         (ImplAAFSegment * pSegment);
 
-  //****************
-  // RemoveAlternateSegment()
-  //
-	virtual AAFRESULT STDMETHODCALLTYPE
-    RemoveAlternateSegment
-		(ImplAAFSegment* pSegment);
 
   //****************
   // GetNumAlternateSegments()
@@ -108,22 +82,16 @@ public:
         (ImplEnumAAFSegments ** ppEnum);
 
   AAFRESULT GetNthSegment(aafUInt32 index, ImplAAFSegment** ppSegment);
-public:
-	//SDK-private calls
-	virtual AAFRESULT GetMinimumBounds(aafPosition_t rootPos, aafLength_t rootLen,
-										ImplAAFMob *mob, ImplAAFMobSlot *track,
-										aafMediaCriteria_t *mediaCrit,
-										aafPosition_t currentObjPos,
-										aafOperationChoice_t *effectChoice,
-										ImplAAFComponent	*prevObject,
-										ImplAAFComponent *nextObject,
-										ImplAAFScopeStack *scopeStack,
-										aafPosition_t *diffPos, aafLength_t *minLength,
-										ImplAAFOperationGroup **groupObject, aafInt32	*nestDepth,
-										ImplAAFComponent **found, aafBool *foundTransition);
-	virtual AAFRESULT ChangeContainedReferences(aafMobID_constref from,
-												aafMobID_constref to);
 
+
+public:
+  // Declare this class to be storable.
+  //
+  OMDECLARE_STORABLE(ImplAAFSelector)
+
+  // Declare the module test method. The implementation of the will be be
+  // in /test/ImplAAFSelectorTest.cpp.
+  static AAFRESULT test();
 private:
 	OMStrongReferenceProperty<ImplAAFSegment>		_selected;
 	OMStrongReferenceVectorProperty<ImplAAFSegment>	_alternates;
