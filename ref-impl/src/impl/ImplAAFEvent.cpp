604d632ba010b60f5131d14f634d6622f90a88bb
tbingham <tbingham>
2001-04-17 04:20:21 +0000

    Initial checkin of dodo generated impl and module test sources.

--- ref-impl/src/impl/ImplAAFEvent.cpp
+++ ref-impl/src/impl/ImplAAFEvent.cpp
@@ -7,27 +7,24 @@
 *                                          *
 \******************************************/
 
-#ifndef __ImplAAFEvent_h__
-#include "ImplAAFEvent.h"
-#endif
+
+
+
+
 
 
 #include "AAFStoredObjectIDs.h"
-#include "AAFPropertyIDs.h"
-#include <AAFResult.h>
 
+#ifndef __ImplAAFEvent_h__
+#include "ImplAAFEvent.h"
+#endif
 
 #include <assert.h>
 #include <string.h>
 
 
-ImplAAFEvent::ImplAAFEvent ():
-  _position(PID_Event_Position, "Position"),
-  _comment(PID_Event_Comment, "Comment")
-{
-  _persistentProperties.put(_position.address());
-  _persistentProperties.put(_comment.address());
-}
+ImplAAFEvent::ImplAAFEvent ()
+{}
 
 
 ImplAAFEvent::~ImplAAFEvent ()
@@ -36,74 +33,45 @@ ImplAAFEvent::~ImplAAFEvent ()
 
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFEvent::GetPosition (
-      aafPosition_t *  pPosition)
+      aafPosition_t *  /*pPosition*/)
 {
-  if (NULL == pPosition)
-    return AAFRESULT_NULL_PARAM;
-
-  *pPosition = _position;
-  return AAFRESULT_SUCCESS;
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
 
 
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFEvent::SetPosition (
-      aafPosition_t  Position)
+      aafPosition_t  /*Position*/)
 {
-  // Why is the argument pass-by-value?
-  _position = Position;
-
-  return AAFRESULT_SUCCESS;
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
 
 
 
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFEvent::SetComment (
-      wchar_t *  pComment)
+      wchar_t *  /*pComment*/)
 {
-	if (NULL == pComment)
-		return (AAFRESULT_NULL_PARAM);
-	
-	_comment = pComment;
-	
-	return (AAFRESULT_SUCCESS);
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
 
 
 
 AAFRESULT STDMETHODCALLTYPE
-ImplAAFEvent::GetComment (wchar_t *  pComment,
-						  aafUInt32  bufSize)
+    ImplAAFEvent::GetComment (
+      wchar_t *  /*pComment*/,
+      aafUInt32  /*bufSize*/)
 {
-	if (NULL == pComment)
-		return(AAFRESULT_NULL_PARAM);
-	
-	if (!_comment.isPresent())
-		return AAFRESULT_PROP_NOT_PRESENT;
-	
-	bool stat = _comment.copyToBuffer(pComment, bufSize);
-	if (!stat)
-		return AAFRESULT_SMALLBUF;
-	
-	return (AAFRESULT_SUCCESS); 
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
 
 
 
 AAFRESULT STDMETHODCALLTYPE
     ImplAAFEvent::GetCommentBufLen (
-      aafUInt32 *  pBufSize)
+      aafUInt32 *  /*pBufSize*/)
 {
-	if (NULL == pBufSize)
-		return (AAFRESULT_NULL_PARAM);
-	
-	if (!_comment.isPresent())
-		return AAFRESULT_PROP_NOT_PRESENT;
-	
-	*pBufSize = _comment.size();
-	
-	return (AAFRESULT_SUCCESS); 
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
 
 
--- ref-impl/src/impl/ImplAAFHTMLClip.cpp
+++ ref-impl/src/impl/ImplAAFHTMLClip.cpp
@@ -1,49 +1,30 @@
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- * prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+
+
+
+
+
+
+#include "AAFStoredObjectIDs.h"
 
 #ifndef __ImplAAFHTMLClip_h__
 #include "ImplAAFHTMLClip.h"
 #endif
 
-
-#include "AAFStoredObjectIDs.h"
-#include "AAFPropertyIDs.h"
-
 #include <assert.h>
 #include <string.h>
 
 
-ImplAAFHTMLClip::ImplAAFHTMLClip () :
-  _beginAnchor(PID_HTMLClip_BeginAnchor, "BeginAnchor"),
-  _endAnchor(PID_HTMLClip_EndAnchor, "EndAnchor")
-{
-  _persistentProperties.put(_beginAnchor.address());
-  _persistentProperties.put(_endAnchor.address());
-}
+ImplAAFHTMLClip::ImplAAFHTMLClip ()
+{}
 
 
 ImplAAFHTMLClip::~ImplAAFHTMLClip ()
@@ -51,98 +32,56 @@ ImplAAFHTMLClip::~ImplAAFHTMLClip ()
 
 
 AAFRESULT STDMETHODCALLTYPE
-ImplAAFHTMLClip::GetBeginAnchor (aafCharacter *  pName,
-								 aafInt32  bufSize)
+    ImplAAFHTMLClip::GetBeginAnchor (
+      wchar_t *  /*pName*/,
+      aafInt32  /*bufSize*/)
 {
-	if (NULL == pName)
-		return(AAFRESULT_NULL_PARAM);
-	
-	if (!_beginAnchor.isPresent())
-		return AAFRESULT_PROP_NOT_PRESENT;
-	
-	bool stat = _beginAnchor.copyToBuffer(pName, bufSize);
-	if (! stat)
-		return AAFRESULT_SMALLBUF;	// Shouldn't the API have a length parm?
-	
-	return (AAFRESULT_SUCCESS);
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
 
 
 AAFRESULT STDMETHODCALLTYPE
-ImplAAFHTMLClip::GetBeginAnchorBufLen (aafUInt32 *  pLen)
+    ImplAAFHTMLClip::GetBeginAnchorBufLen (
+      aafUInt32 *  /*pLen*/)
 {
-	if (NULL == pLen)
-		return (AAFRESULT_NULL_PARAM);
-	
-	if(!_beginAnchor.isPresent())
-		return AAFRESULT_PROP_NOT_PRESENT;
-
-	*pLen = _beginAnchor.size();
-	
-	return (AAFRESULT_SUCCESS); 
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
 
 
 AAFRESULT STDMETHODCALLTYPE
-ImplAAFHTMLClip::SetBeginAnchor (const aafCharacter *  pName)
+    ImplAAFHTMLClip::SetBeginAnchor (
+      wchar_t *  /*pName*/)
 {
-	if (NULL == pName)
-		return(AAFRESULT_NULL_PARAM);
-	
-	_beginAnchor = pName;
-	
-	return (AAFRESULT_SUCCESS); 
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
 
 
 AAFRESULT STDMETHODCALLTYPE
-ImplAAFHTMLClip::GetEndAnchor (aafCharacter *  pName,
-							   aafInt32  bufSize)
+    ImplAAFHTMLClip::GetEndAnchor (
+      wchar_t *  /*pName*/,
+      aafInt32  /*bufSize*/)
 {
-	if (NULL == pName)
-		return(AAFRESULT_NULL_PARAM);
-	
-	if (!_endAnchor.isPresent())
-		return AAFRESULT_PROP_NOT_PRESENT;
-
-	bool stat = _endAnchor.copyToBuffer(pName, bufSize);
-	if (! stat)
-		return AAFRESULT_SMALLBUF;	// Shouldn't the API have a length parm?
-	
-	return (AAFRESULT_SUCCESS);
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
-
 
 
 AAFRESULT STDMETHODCALLTYPE
-ImplAAFHTMLClip::GetEndAnchorBufLen (aafUInt32 *  pLen)
+    ImplAAFHTMLClip::GetEndAnchorBufLen (
+      aafUInt32 *  /*pLen*/)
 {
-	if (NULL == pLen)
-		return (AAFRESULT_NULL_PARAM);
-	
-	if (!_endAnchor.isPresent())
-		return AAFRESULT_PROP_NOT_PRESENT;
-	
-	*pLen = _endAnchor.size();
-	
-	return (AAFRESULT_SUCCESS); 
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
-
 
 
 AAFRESULT STDMETHODCALLTYPE
-ImplAAFHTMLClip::SetEndAnchor (const aafCharacter *  pName)
+    ImplAAFHTMLClip::SetEndAnchor (
+      wchar_t *  /*pName*/)
 {
-	if (NULL == pName)
-		return(AAFRESULT_NULL_PARAM);
-	
-	_endAnchor = pName;
-	
-	return (AAFRESULT_SUCCESS); 
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
 
 
 
-
+OMDEFINE_STORABLE(ImplAAFHTMLClip, AUID_AAFHTMLClip);
 
 
