8613704526a6248e98ba8708c178c42029027d84
tbingham <tbingham>
2001-04-17 08:48:01 +0000

    Change aafNumSlots_t from signed to unsigned (now is aafUInt32).

--- test/com/ComModTestAAF/ModuleTests/CAAFHeaderTest.cpp
+++ test/com/ComModTestAAF/ModuleTests/CAAFHeaderTest.cpp
@@ -1,13 +1,31 @@
 // @doc INTERNAL
 // @com This file implements the module test for CAAFHeader.
-/******************************************\
-*                                          *
-* Advanced Authoring Format                *
-*                                          *
-* Copyright (c) 1998 Avid Technology, Inc. *
-* Copyright (c) 1998 Microsoft Corporation *
-*                                          *
-\******************************************/
+/***********************************************************************
+ *
+ *              Copyright (c) 1998-1999 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and accompanying 
+ * documentation, and to distribute and sublicense application software
+ * incorporating this software for any purpose is hereby granted, 
+ * provided that (i) the above copyright notice and this permission
+ * notice appear in all copies of the software and related documentation,
+ * and (ii) the name Avid Technology, Inc. may not be used in any
+ * advertising or publicity relating to the software without the specific,
+ *  prior written permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+ * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+ * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+ * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+ * LIABILITY.
+ *
+ ************************************************************************/
 
 #include "AAF.h"
 
@@ -353,13 +371,13 @@ void HeaderTest::openMobs()
   assert(NULL == _pSourceMob);
 
 
-  aafInt32 mobCount = 0;
+  aafNumSlots_t mobCount = 0;
   check(_pHeader->GetNumMobs(kAllMob, &mobCount));
   if (gMaxMobCount != mobCount)
     check(AAFRESULT_TEST_FAILED);
  
   check(_pHeader->EnumAAFAllMobs(NULL, &_pEnumMobs));
-  for (aafInt32 item = 0; item < mobCount; ++item)
+  for (aafUInt32 item = 0; item < mobCount; ++item)
   {
     check(_pEnumMobs->NextOne(&_pMob));
 
