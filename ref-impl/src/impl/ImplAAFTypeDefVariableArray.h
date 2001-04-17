1a839b51596c94bec3ae571d2eb37ca223a08c86
tbingham <tbingham>
2001-04-17 07:32:33 +0000

    Add RawAccessType() method.

--- ref-impl/src/impl/ImplAAFTypeDefVariableArray.h
+++ ref-impl/src/impl/ImplAAFTypeDefVariableArray.h
@@ -1,13 +1,32 @@
 #ifndef __ImplAAFTypeDefVariableArray_h__
 #define __ImplAAFTypeDefVariableArray_h__
 
-/******************************************\
-*                                          *
-* Advanced Authoring Format                *
-*                                          *
-* Copyright (c) 1998 Avid Technology, Inc. *
-*                                          *
-\******************************************/
+/***********************************************************************
+ *
+ *              Copyright (c) 1998-1999 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and accompanying 
+ * documentation, and to distribute and sublicense application software
+ * incorporating this software for any purpose is hereby granted, 
+ * provided that (i) the above copyright notice and this permission
+ * notice appear in all copies of the software and related documentation,
+ * and (ii) the name Avid Technology, Inc. may not be used in any
+ * advertising or publicity relating to the software without the specific,
+ *  prior written permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+ * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+ * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+ * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+ * LIABILITY.
+ *
+ ************************************************************************/
 
 class ImplAAFPropertyValue;
 
@@ -76,6 +95,15 @@ public:
          ImplAAFPropertyValue * pMemberPropVal);
 
 
+  //****************
+  // CreateEmptyValue()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    CreateEmptyValue
+        (// @parm [out] newly-created property empty value,
+         ImplAAFPropertyValue ** ppPropVal);
+			 
+
   // Override from AAFTypeDef
   virtual AAFRESULT STDMETHODCALLTYPE
     GetTypeCategory (/*[out]*/ eAAFTypeCategory_t *  pTid);
@@ -110,6 +138,21 @@ public:
                            OMByteOrder byteOrder) const;
 
 
+  //****************
+  // pvtInitialize()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    pvtInitialize
+        (// @parm [in] auid to be used to identify this type
+         const aafUID_t *  pID,
+
+         // @parm [in] type of each element to be contained in this array
+         const aafUID_t * pTypeID,
+
+         // @parm [in] friendly name of this type definition
+         wchar_t *  pTypeName);
+
+
 protected:
   // override from ImplAAFTypeDefArray
   virtual aafUInt32 pvtCount (ImplAAFPropertyValue * pInPropVal) const;
@@ -128,6 +171,17 @@ public:
     pvtCreateOMPropertyMBS (OMPropertyId pid,
 							const char * name) const;
 
+  virtual AAFRESULT STDMETHODCALLTYPE
+    RawAccessType
+        (ImplAAFTypeDef ** ppRawTypeDef);
+
+public:
+  // Overrides from ImplAAFTypeDef
+  virtual bool IsAggregatable () const;
+  virtual bool IsStreamable () const;
+  virtual bool IsFixedArrayable () const;
+  virtual bool IsVariableArrayable () const;
+  virtual bool IsStringable () const;
 
 private:
   // OMWeakReferenceProperty<ImplAAFTypeDef> _ElementType;
