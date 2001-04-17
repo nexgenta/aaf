d67ee89d8a7e2e8ed796825db7a2cd5f354ba6a7
tbingham <tbingham>
2001-04-17 06:01:49 +0000

    Newly checked-in test.

--- test/com/ComModTestAAF/ModuleTests/CEnumAAFPropertyDefsTest.cpp
+++ test/com/ComModTestAAF/ModuleTests/CEnumAAFPropertyDefsTest.cpp
@@ -1,99 +1,33 @@
 // @doc INTERNAL
 // @com This file implements the module test for CEnumAAFPropertyDefs
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- *  prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+/***********************************************\
+*                                               *
+* Advanced Authoring Format                     *
+*                                               *
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+* Copyright (c) 1998-1999 Microsoft Corporation *
+*                                               *
+\***********************************************/
 
-#include <iostream.h>
-#include <stdio.h>
-#include <wchar.h>
-
-#include "AAF.h"
+#include "AAFTypes.h" //Use #include "AAF.h" for functional module test.
 #include "AAFResult.h"
 
-#include "CEnumeratorTest.h"
+// Required function prototype.
+extern "C" HRESULT CEnumAAFPropertyDefs_test(void);
 
-class CEnumAAFPropertyDefsTest: public 
-	CEnumeratorTest<IEnumAAFPropertyDefs,IAAFPropertyDef>
+HRESULT CEnumAAFPropertyDefs_test()
 {
-public:
-	CEnumAAFPropertyDefsTest()
-	{
-		_pKnownClassDef=0;
-	}
-	// Before performing any testing, we loop up a known class definition, so we
-	// can perform our test by enumerating over its properties.
-	void LookupKnownClassDef(IAAFDictionary *pDictionary)
-	{
-		if(!_pKnownClassDef)
-			checkResult(pDictionary->LookupClassDef(AUID_AAFTapeDescriptor,
-			&_pKnownClassDef));
-	}
-	HRESULT CountItems(IAAFDictionary *pDictionary,aafUInt32 *piCount)
-	{
-		LookupKnownClassDef(pDictionary);
-		return(_pKnownClassDef->CountPropertyDefs(piCount));
-	}
-	HRESULT GetItems(IAAFDictionary *pDictionary,
-						IEnumAAFPropertyDefs **ppEnumerator)
-	{
-		LookupKnownClassDef(pDictionary);
-		return(_pKnownClassDef->GetPropertyDefs(ppEnumerator));
-	}
-	aafBool ItemIsPresent(IAAFDictionary *pDictionary,aafUID_t& Id)
-	{
-		LookupKnownClassDef(pDictionary);
-		IAAFSmartPointer<IAAFPropertyDef> pPropertyDef;
-		return(_pKnownClassDef->LookupPropertyDef(Id,&pPropertyDef)
-			==AAFRESULT_SUCCESS?kAAFTrue:kAAFFalse);
-	}
-	~CEnumAAFPropertyDefsTest()
-	{
-		if(_pKnownClassDef)
-			_pKnownClassDef->Release();
-	}
-
-private:
-	IAAFClassDef *_pKnownClassDef;
-};
-
-extern "C" HRESULT CEnumAAFPropertyDefs_test()
-{
-	try
-	{
-		CEnumAAFPropertyDefsTest Test;
-		Test.Run();
-	}
-	catch(HRESULT& rResult)
-	{
-		return(rResult);
-	}
-
-	return AAFRESULT_SUCCESS;
+  return AAFRESULT_NOT_IMPLEMENTED;
 }
+
+
+
+
+
+
+
+
+
 
 
 
