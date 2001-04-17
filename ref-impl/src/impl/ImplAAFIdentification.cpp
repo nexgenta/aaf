8af5f01a376c925b16f6c8779c579efb42ab5153
tbingham <tbingham>
2001-04-17 07:01:50 +0000

    Add (required) generation property.

--- ref-impl/src/impl/ImplAAFIdentification.cpp
+++ ref-impl/src/impl/ImplAAFIdentification.cpp
@@ -1,11 +1,30 @@
 
-/******************************************\
-*                                          *
-* Advanced Authoring Format                *
-*                                          *
-* Copyright (c) 1998 Avid Technology, Inc. *
-*                                          *
-\******************************************/
+/***********************************************************************
+ *
+ *              Copyright (c) 1998-1999 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and accompanying 
+ * documentation, and to distribute and sublicense application software
+ * incorporating this software for any purpose is hereby granted, 
+ * provided that (i) the above copyright notice and this permission
+ * notice appear in all copies of the software and related documentation,
+ * and (ii) the name Avid Technology, Inc. may not be used in any
+ * advertising or publicity relating to the software without the specific,
+ *  prior written permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+ * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+ * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+ * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+ * LIABILITY.
+ *
+ ************************************************************************/
 
 
 
@@ -29,15 +48,15 @@ ImplAAFIdentification::~ImplAAFIdentification ()
 {}
 
 ImplAAFIdentification::ImplAAFIdentification():
-_companyName(         PID_Identification_CompanyName,          "CompanyName"),
-_productName(         PID_Identification_ProductName,          "ProductName"),
-_productVersion(      PID_Identification_ProductVersion,       "ProductVersion"),
-_productVersionString(PID_Identification_ProductVersionString, "ProductVersionString"),
-_productId(			  PID_Identification_ProductID,            "ProductID"),
-_date(                PID_Identification_Date,                 "Date"),
-_toolkitVersion(      PID_Identification_ToolkitVersion,       "ToolkitVersion"),
-_platform(            PID_Identification_Platform,             "Platform")
-// _generation(       PID_Identification_GenerationAUID,       "GenerationAUID")
+  _companyName(         PID_Identification_CompanyName,          "CompanyName"),
+  _productName(         PID_Identification_ProductName,          "ProductName"),
+  _productVersion(      PID_Identification_ProductVersion,       "ProductVersion"),
+  _productVersionString(PID_Identification_ProductVersionString, "ProductVersionString"),
+  _productId(			  PID_Identification_ProductID,            "ProductID"),
+  _date(                PID_Identification_Date,                 "Date"),
+  _toolkitVersion(      PID_Identification_ToolkitVersion,       "ToolkitVersion"),
+  _platform(            PID_Identification_Platform,             "Platform"),
+  _generation(       PID_Identification_GenerationAUID,       "GenerationAUID")
 {
   // Insert the properties into the persistent property set.
   //
@@ -49,7 +68,7 @@ _platform(            PID_Identification_Platform,             "Platform")
   _persistentProperties.put(   _date.address());
   _persistentProperties.put(_toolkitVersion.address());
   _persistentProperties.put(   _platform.address());
-  // _persistentProperties.put(_generation.address());
+  _persistentProperties.put(_generation.address());
 
   
 #if defined(_WIN32)
@@ -73,15 +92,15 @@ ImplAAFIdentification::ImplAAFIdentification(
                                      const wchar_t* platform
                                      // const AUID* generation
                                      ):
-_companyName(         PID_Identification_CompanyName,          "Company Name"),
-_productName(         PID_Identification_ProductName,          "Product Name"),
-_productVersion(      PID_Identification_ProductVersion,       "Product Version"),
-_productVersionString(PID_Identification_ProductVersionString, "Product Version String"),
-_productId(			  PID_Identification_ProductID,            "Product ID"),
-_date(                PID_Identification_Date,                 "Date"),
-_toolkitVersion(      PID_Identification_ToolkitVersion,       "Toolkit Version"),
-_platform(            PID_Identification_Platform,             "Platform")
-// _generation(       PID_Identification_GenerationAUID,       "Generation AUID")
+  _companyName(         PID_Identification_CompanyName,          "CompanyName"),
+  _productName(         PID_Identification_ProductName,          "ProductName"),
+  _productVersion(      PID_Identification_ProductVersion,       "ProductVersion"),
+  _productVersionString(PID_Identification_ProductVersionString, "ProductVersionString"),
+  _productId(			  PID_Identification_ProductID,            "ProductID"),
+  _date(                PID_Identification_Date,                 "Date"),
+  _toolkitVersion(      PID_Identification_ToolkitVersion,       "ToolkitVersion"),
+  _platform(            PID_Identification_Platform,             "Platform"),
+  _generation(       PID_Identification_GenerationAUID,       "GenerationAUID")
 {
   // Insert the properties into the persistent property set.
   //
@@ -93,7 +112,7 @@ _platform(            PID_Identification_Platform,             "Platform")
   _persistentProperties.put(   _date.address());
   _persistentProperties.put(_toolkitVersion.address());
   _persistentProperties.put(   _platform.address());
-  // _persistentProperties.put(_generation.address());
+  _persistentProperties.put(_generation.address());
 
   // Initialize members.
   //
