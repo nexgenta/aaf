e64494dcf96bdf695df35e7b7525fb307727fd63
tbingham <tbingham>
2001-04-17 08:04:59 +0000

    Stubbed versions of OM{XML|KLV}StoredStream.

--- ref-impl/src/OM/OMXMLStoredStream.cpp
+++ ref-impl/src/OM/OMXMLStoredStream.cpp
@@ -1,24 +1,29 @@
-//=---------------------------------------------------------------------=
-//
-// The contents of this file are subject to the AAF SDK Public
-// Source License Agreement (the "License"); You may not use this file
-// except in compliance with the License.  The License is available in
-// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
-// Association or its successor.
-// 
-// Software distributed under the License is distributed on an "AS IS"
-// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
-// the License for the specific language governing rights and limitations
-// under the License.
-// 
-// The Original Code of this file is Copyright 1998-2001, Licensor of the
-// AAF Association.
-// 
-// The Initial Developer of the Original Code of this file and the
-// Licensor of the AAF Association is Avid Technology.
-// All rights reserved.
-//
-//=---------------------------------------------------------------------=
+/***********************************************************************
+*
+*              Copyright (c) 1998-2000 Avid Technology, Inc.
+*
+* Permission to use, copy and modify this software and accompanying
+* documentation, and to distribute and sublicense application software
+* incorporating this software for any purpose is hereby granted,
+* provided that (i) the above copyright notice and this permission
+* notice appear in all copies of the software and related documentation,
+* and (ii) the name Avid Technology, Inc. may not be used in any
+* advertising or publicity relating to the software without the specific,
+* prior written permission of Avid Technology, Inc.
+*
+* THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+* WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+* IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+* SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+* OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+* OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+* ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+* RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+* ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+* LIABILITY.
+*
+************************************************************************/
 
 // @doc OMINTERNAL
 // @author Tim Bingham | tjb | Avid Technology, Inc. | OMXMLStoredStream 
@@ -41,7 +46,7 @@ OMXMLStoredStream::~OMXMLStoredStream(void)
   PRECONDITION("Stream not open", _store == 0);
 }
 
-void OMXMLStoredStream::read(void* ANAME(data), size_t ANAME(size)) const
+void OMXMLStoredStream::read(void* data, size_t size) const
 {
   TRACE("OMXMLStoredStream::read");
   PRECONDITION("Valid store", _store != 0);
@@ -51,9 +56,9 @@ void OMXMLStoredStream::read(void* ANAME(data), size_t ANAME(size)) const
   ASSERT("Unimplemented code not reached", false); // tjb TBS
 }
 
-void OMXMLStoredStream::read(OMByte* ANAME(data),
-                             const OMUInt32 ANAME(bytes),
-                             OMUInt32& /* bytesRead */) const
+void OMXMLStoredStream::read(OMByte* data,
+                             const OMUInt32 bytes,
+                             OMUInt32& bytesRead) const
 {
   TRACE("OMXMLStoredStream::read");
   PRECONDITION("Valid store", _store != 0);
@@ -63,7 +68,7 @@ void OMXMLStoredStream::read(OMByte* ANAME(data),
   ASSERT("Unimplemented code not reached", false); // tjb TBS
 }
 
-void OMXMLStoredStream::write(void* ANAME(data), size_t ANAME(size))
+void OMXMLStoredStream::write(void* data, size_t size)
 {
   TRACE("OMXMLStoredStream::write");
   PRECONDITION("Valid store", _store != 0);
@@ -73,9 +78,9 @@ void OMXMLStoredStream::write(void* ANAME(data), size_t ANAME(size))
   ASSERT("Unimplemented code not reached", false); // tjb TBS
 }
 
-void OMXMLStoredStream::write(const OMByte* ANAME(data),
-                              const OMUInt32 ANAME(bytes),
-                              OMUInt32& /* bytesWritten */)
+void OMXMLStoredStream::write(const OMByte* data,
+                              const OMUInt32 bytes,
+                              OMUInt32& bytesWritten)
 {
   TRACE("OMXMLStoredStream::write");
   PRECONDITION("Valid store", _store != 0);
@@ -95,7 +100,7 @@ OMUInt64 OMXMLStoredStream::size(void) const
   return result;
 }
 
-void OMXMLStoredStream::setSize(const OMUInt64 /* newSize */)
+void OMXMLStoredStream::setSize(const OMUInt64 newSize)
 {
   TRACE("OMXMLStoredStream::setSize");
   PRECONDITION("Valid store", _store != 0);
@@ -112,7 +117,7 @@ OMUInt64 OMXMLStoredStream::position(void) const
   return 0;
 }
 
-void OMXMLStoredStream::setPosition(const OMUInt64 /* offset */)
+void OMXMLStoredStream::setPosition(const OMUInt64 offset)
 {
   TRACE("OMXMLStoredStream::setPosition");
   PRECONDITION("Valid store", _store != 0);
