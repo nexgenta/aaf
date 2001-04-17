2e415ee305be5d00abaf29b088f4e44348f7964f
tbingham <tbingham>
2001-04-17 05:36:01 +0000

    Add standard codecs project

--- test/com/ComModTestAAF/ModuleTests/CAAFEssencePluginTest.cpp
+++ test/com/ComModTestAAF/ModuleTests/CAAFEssencePluginTest.cpp
@@ -1,14 +1,5 @@
 // @doc INTERNAL
-// @com This file implements the module test for CAAFEssencePlugin
-/***********************************************\
-*                                               *
-* Advanced Authoring Format                     *
-*                                               *
-* Copyright (c) 1998-1999 Avid Technology, Inc. *
-* Copyright (c) 1998-1999 Microsoft Corporation *
-*                                               *
-\***********************************************/
-
+// @com This file implements the module test for CAAFDefinitionObject
 /******************************************\
 *                                          *
 * Advanced Authoring Format                *
@@ -18,25 +9,56 @@
 *                                          *
 \******************************************/
 
-
-
-
-
-
 #include "CAAFEssencePlugin.h"
 #include "CAAFEssencePlugin.h"
 #ifndef __CAAFEssencePlugin_h__
 #error - improperly defined include guard
 #endif
 
-#include "AAFResult.h"
-
+#include <iostream.h>
 
 HRESULT CAAFEssencePlugin::test()
 {
-  return AAFRESULT_NOT_IMPLEMENTED;
-}
+  HRESULT hr = AAFRESULT_NOT_IMPLEMENTED;
+  IAAFEssencePlugin *pObject = NULL;
 
+  try
+	{
+	  // Attempt to create an AAFEssencePlugin.
+	  hr =  CoCreateInstance(
+							 CLSID_AAFEssencePlugin,
+							 NULL, 
+							 CLSCTX_INPROC_SERVER, 
+							 IID_IAAFEssencePlugin, (void **)&pObject);
+	  if (FAILED(hr))
+		{
+		  cerr << "CAAFEssencePlugin::test...FAILED!";
+		  cerr << hr;
+		  cerr << "\tCoCreateInstance(&CLSID_AAFEssencePlugin, NULL,"
+			" CLSCTX_INPROC_SERVER, &IID_IAAFEssencePlugin, ...);" <<
+			  endl;
+		  return hr;
+		}
+
+	  // module-specific tests go here
+
+      if (pObject)
+        pObject->Release();
+	  return AAFRESULT_NOT_IMPLEMENTED;
+
+	}
+  catch (...)
+	{
+	  cerr << "CAAFEssencePlugin::test...Caught general C++"
+		" exception!" << endl; 
+	}
+
+  // Cleanup our object if it exists.
+  if (pObject)
+	pObject->Release();
+
+  return hr;
+}
 
 
 
