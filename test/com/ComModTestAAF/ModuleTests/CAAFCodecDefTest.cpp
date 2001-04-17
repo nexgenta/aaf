c759cd922d388234516d891bfaf212a2f2cb1fd4
tbingham <tbingham>
2001-04-17 06:43:07 +0000

    Added ContainerDef CodecDef DefObject

--- test/com/ComModTestAAF/ModuleTests/CAAFCodecDefTest.cpp
+++ test/com/ComModTestAAF/ModuleTests/CAAFCodecDefTest.cpp
@@ -1,33 +1,44 @@
 // @doc INTERNAL
-// @com This file implements the module test for CAAFCodecDef
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- * prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+// @com This file implements the module test for CAAFDefinitionObject
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
 
-#include "AAF.h"
+
+
+
+
+
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+ 
+/***********************************************\
+*	Stub only.   Implementation not yet added	*
+\***********************************************/
+
+
+
+
+#include "CAAFCodecDef.h"
+#include "CAAFCodecDef.h"
+#ifndef __CAAFCodecDef_h__
+#error - improperly defined include guard
+#endif
+
+// Temporarily necessary global declarations.
+extern "C" const CLSID CLSID_AAFCodecDef; // generated
 
 
 #include <iostream.h>
@@ -36,14 +47,9 @@
 #include <assert.h>
 #include <string.h>
 
-#include "AAFClassDefUIDs.h"
 #include "AAFStoredObjectIDs.h"
 #include "AAFResult.h"
-#include "AAFDataDefs.h"
 #include "AAFDefUIDs.h"
-#include "AAFCodecDefs.h"
-
-#include "CAAFBuiltinDefs.h"
 
 // Cross-platform utility to delete a file.
 static void RemoveTestFile(const wchar_t* pFileName)
@@ -78,33 +84,26 @@ static HRESULT OpenAAFFile(aafWChar*			pFileName,
 	aafProductIdentification_t	ProductInfo;
 	HRESULT						hr = AAFRESULT_SUCCESS;
 
-	aafProductVersion_t v;
-	v.major = 1;
-	v.minor = 0;
-	v.tertiary = 0;
-	v.patchLevel = 0;
-	v.type = kAAFVersionUnknown;
 	ProductInfo.companyName = L"AAF Developers Desk";
-	ProductInfo.productName = L"AAFCodecDef Test";
-	ProductInfo.productVersion = &v;
+	ProductInfo.productName = L"AAFMasterMob Test";
+	ProductInfo.productVersion.major = 1;
+	ProductInfo.productVersion.minor = 0;
+	ProductInfo.productVersion.tertiary = 0;
+	ProductInfo.productVersion.patchLevel = 0;
+	ProductInfo.productVersion.type = kVersionUnknown;
 	ProductInfo.productVersionString = NULL;
-	ProductInfo.productID = UnitTestProductID;
+	ProductInfo.productID = -1;
 	ProductInfo.platform = NULL;
 
-	*ppFile = NULL;
-
-	if(mode == kAAFMediaOpenAppend)
+	if(mode == kMediaOpenAppend)
 		hr = AAFFileOpenNewModify(pFileName, 0, &ProductInfo, ppFile);
 	else
 		hr = AAFFileOpenExistingRead(pFileName, 0, ppFile);
 
 	if (FAILED(hr))
 	{
-		if (*ppFile)
-		{
-			(*ppFile)->Release();
-			*ppFile = NULL;
-		}
+		(*ppFile)->Release();
+		*ppFile = NULL;
 		return hr;
 	}
   
@@ -122,57 +121,41 @@ static HRESULT OpenAAFFile(aafWChar*			pFileName,
 static HRESULT CreateAAFFile(aafWChar * pFileName)
 {
 	IAAFFile*		pFile = NULL;
-	IAAFHeader *        pHeader = NULL;
-	IAAFDictionary*  pDictionary = NULL;
-	IAAFCodecDef*	pPlugDef = NULL;
-	IAAFDataDef		*pDataDef = NULL;
-	IAAFClassDef	*classDef = NULL;
-	bool bFileOpen = false;
+  IAAFHeader *        pHeader = NULL;
+  IAAFDictionary*  pDictionary = NULL;
+  IAAFPluggableDef*	pPlugDef;
+  bool bFileOpen = false;
 	HRESULT			hr = S_OK;
-	aafUID_t		uid;
 /*	long			test;
 */
 
-	try
-	{
-		// Remove the previous test file if any.
-		RemoveTestFile(pFileName);
+  try
+  {
+    // Remove the previous test file if any.
+    RemoveTestFile(pFileName);
 
 
-		// Create the AAF file
-		checkResult(OpenAAFFile(pFileName, kAAFMediaOpenAppend, /*&pSession,*/ &pFile, &pHeader));
-		bFileOpen = true;
+	  // Create the AAF file
+	  checkResult(OpenAAFFile(pFileName, kMediaOpenAppend, /*&pSession,*/ &pFile, &pHeader));
+    bFileOpen = true;
 
-		// Get the AAF Dictionary so that we can create valid AAF objects.
-		checkResult(pHeader->GetDictionary(&pDictionary));
-		CAAFBuiltinDefs defs (pDictionary);
+    // Get the AAF Dictionary so that we can create valid AAF objects.
+    checkResult(pHeader->GetDictionary(&pDictionary));
     
-		checkResult(defs.cdCodecDef()->
-					CreateInstance(IID_IAAFCodecDef,
-								   (IUnknown **)&pPlugDef));
+	checkResult(pDictionary->CreateInstance(&AUID_AAFCodecDef,
+							  IID_IAAFPluggableDef, 
+							  (IUnknown **)&pPlugDef));
     
-		uid = kAAFCodecWAVE;
-		checkResult(pPlugDef->Initialize (uid, L"TestCodec", L"TestCodecDescription"));
 
-		checkResult(pPlugDef->AddEssenceKind (defs.ddMatte()));
-		checkResult(pDictionary->RegisterCodecDef(pPlugDef));
-		uid = kAAFClassID_WAVEDescriptor;
-		checkResult(pDictionary->LookupClassDef(uid, &classDef));
-		checkResult(pPlugDef->SetFileDescriptorClass (classDef));
-	}
-	catch (HRESULT& rResult)
-	{
-		hr = rResult;
-	}
+	  checkResult(pDictionary->RegisterPluggableDefinition(pPlugDef));
+  }
+  catch (HRESULT& rResult)
+  {
+    hr = rResult;
+  }
 
 
   // Cleanup and return
-  if (classDef)
-    classDef->Release();
-
-  if (pDataDef)
-    pDataDef->Release();
-
   if (pPlugDef)
     pPlugDef->Release();
 
@@ -185,11 +168,11 @@ static HRESULT CreateAAFFile(aafWChar * pFileName)
   if (pFile)
   {  // Close file
     if (bFileOpen)
-	  {
-		  pFile->Save();
-		  pFile->Close();
-	  }
-    pFile->Release();
+	{
+		pFile->Save();
+		pFile->Close();
+	}
+     pFile->Release();
   }
 
   return hr;
@@ -197,39 +180,22 @@ static HRESULT CreateAAFFile(aafWChar * pFileName)
 
 static HRESULT ReadAAFFile(aafWChar* pFileName)
 {
-	IAAFFile*				pFile = NULL;
-	IAAFHeader*				pHeader = NULL;
-	IAAFDictionary*			 pDictionary = NULL;
-	IAAFCodecDef			*pCodec = NULL;
-	IAAFDataDef				*pDataDef = NULL;
-	IEnumAAFCodecFlavours	*pEnum = NULL;
-	bool					bFileOpen = false;
-	aafBool					testResult;
-	aafUID_t				codecID = kAAFCodecWAVE;
-	aafUID_t				readFlavour, checkFlavour = kAAFNilCodecFlavour;
-	HRESULT					hr = S_OK;
+	IAAFFile*		pFile = NULL;
+	IAAFHeader*		pHeader = NULL;
+	IAAFDictionary*  pDictionary = NULL;
+	IEnumAAFPluggableDefs *pPlug = NULL;
+  bool bFileOpen = false;
+	HRESULT			hr = S_OK;
 
 	try
 	{
 	  // Open the AAF file
-	  checkResult(OpenAAFFile(pFileName, kAAFMediaOpenReadOnly, &pFile, &pHeader));
+	  checkResult(OpenAAFFile(pFileName, kMediaOpenReadOnly, &pFile, &pHeader));
 		bFileOpen = true;
 
 		checkResult(pHeader->GetDictionary(&pDictionary));
-		CAAFBuiltinDefs defs (pDictionary);
-		checkResult(pDictionary->LookupCodecDef(codecID, &pCodec));
-
-		checkResult(pCodec->IsEssenceKindSupported (defs.ddMatte(), &testResult));
-		checkExpression (testResult == kAAFTrue, AAFRESULT_TEST_FAILED);
-		checkResult(pCodec->IsEssenceKindSupported (defs.ddPicture(), &testResult));
-		checkExpression (testResult == kAAFFalse, AAFRESULT_TEST_FAILED);
-		checkResult(pCodec->EnumCodecFlavours (&pEnum));
-		checkResult(pEnum->NextOne (&readFlavour));
-		checkExpression (memcmp(&readFlavour, &checkFlavour, sizeof(checkFlavour)) == 0,
-						 AAFRESULT_TEST_FAILED);
-		checkResult(pCodec->AreThereFlavours (&testResult));
-		checkExpression (kAAFFalse == testResult,
-						 AAFRESULT_TEST_FAILED);
+	
+	  checkResult(pDictionary->GetPluggableDefinitions(&pPlug));
 	}
 	catch (HRESULT& rResult)
 	{
@@ -237,18 +203,9 @@ static HRESULT ReadAAFFile(aafWChar* pFileName)
 	}
 
 	// Cleanup and return
-	if (pEnum)
-		pEnum->Release();
-	if (pDataDef)
-		pDataDef->Release();
-	if (pCodec)
-		pCodec->Release();
-	if (pDictionary)
-		pDictionary->Release();
-
 	if (pHeader)
 		pHeader->Release();
-
+      
 	if (pFile)
 	{  // Close file
 		if (bFileOpen)
@@ -260,10 +217,10 @@ static HRESULT ReadAAFFile(aafWChar* pFileName)
 }
  
 
-extern "C" HRESULT CAAFCodecDef_test()
+HRESULT CAAFCodecDef::test()
 {
 	HRESULT hr = AAFRESULT_NOT_IMPLEMENTED;
-	aafWChar * pFileName = L"AAFCodecDefTest.aaf";
+	aafWChar * pFileName = L"CodecDefTest.aaf";
 
 	try
 	{
@@ -273,16 +230,20 @@ extern "C" HRESULT CAAFCodecDef_test()
 	}
 	catch (...)
 	{
-		cerr << "CAAFCodecDef_test...Caught general C++ exception!" << endl; 
+		cerr << "CAAFCodecDef::test...Caught general C++ exception!" << endl; 
 	}
 
 	// When all of the functionality of this class is tested, we can return success.
 	// When a method and its unit test have been implemented, remove it from the list.
 	if (SUCCEEDED(hr))
 	{
-		cout << "The following IAAFCodecDef methods have not been tested:" << endl; 
-		cout << "     SetFileDescriptorClass" << endl; 
+		cout << "The following IAAFCodecDef methods have not been implemented:" << endl; 
+		cout << "     IsEssenceKindSupported" << endl; 
+		cout << "     AppendEssenceKind" << endl; 
+		cout << "     AreThereFlavours" << endl; 
 		cout << "     GetFileDescriptorClass" << endl; 
+		cout << "     SetFileDescriptorClass" << endl; 
+		cout << "     EnumCodecFlavours" << endl; 
 		hr = AAFRESULT_TEST_PARTIAL_SUCCESS;
 	}
 
