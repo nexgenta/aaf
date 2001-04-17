649bfb541d043fd15a6e51c96e72eb5f1329d89f
tbingham <tbingham>
2001-04-17 04:00:07 +0000

    IDL Updates to match May Summit

--- ref-impl/src/impl/ImplAAFBaseClassFactory.cpp
+++ ref-impl/src/impl/ImplAAFBaseClassFactory.cpp
@@ -1,30 +1,3 @@
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- *  prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -49,7 +22,7 @@
 
 //
 // Declare the corresponding CLSID's external.
-#define AAF_CLASS(name, dataid, parent, concrete)\
+#define AAF_CLASS(name, dataid, parent)\
   extern "C" aafClassID_t CLSID_AAF##name;
 
 // The AAF reference implementation is still not quite in sync with SMPTE
@@ -57,19 +30,20 @@
 // name in AAF.
 //
 #define CLSID_AAFClassDefinition CLSID_AAFClassDef
+#define CLSID_AAFConstantValue CLSID_AAFConstValue
 #define CLSID_AAFDataDefinition CLSID_AAFDataDef
 #define CLSID_AAFDefinitionObject CLSID_AAFDefObject
 #define CLSID_AAFEdgeCode CLSID_AAFEdgecode
 #define CLSID_AAFOperationDefinition CLSID_AAFOperationDef
+#define CLSID_AAFEssenceGroup CLSID_AAFMediaGroup
+#define CLSID_AAFFilmDescriptor CLSID_AAFMediaFilmDescriptor
 #define CLSID_AAFInterchangeObject CLSID_AAFObject
 #define CLSID_AAFJPEGImageData CLSID_AAFJPEGData
 #define CLSID_AAFMIDIFileData CLSID_AAFMIDIData
 #define CLSID_AAFParameterDefinition CLSID_AAFParameterDef
 #define CLSID_AAFPropertyDefinition CLSID_AAFPropertyDef
 #define CLSID_AAFTypeDefinition CLSID_AAFTypeDef
-#define CLSID_AAFTypeDefinitionCharacter CLSID_AAFTypeDefCharacter
 #define CLSID_AAFTypeDefinitionEnumeration CLSID_AAFTypeDefEnum
-#define CLSID_AAFTypeDefinitionExtendibleEnumeration CLSID_AAFTypeDefExtEnum
 #define CLSID_AAFTypeDefinitionFixedArray CLSID_AAFTypeDefFixedArray
 #define CLSID_AAFTypeDefinitionInteger CLSID_AAFTypeDefInt
 #define CLSID_AAFTypeDefinitionRecord CLSID_AAFTypeDefRecord
@@ -77,19 +51,19 @@
 #define CLSID_AAFTypeDefinitionSet CLSID_AAFTypeDefSet
 #define CLSID_AAFTypeDefinitionStream CLSID_AAFTypeDefStream
 #define CLSID_AAFTypeDefinitionString CLSID_AAFTypeDefString
-#define CLSID_AAFTypeDefinitionIndirect CLSID_AAFTypeDefIndirect
-#define CLSID_AAFTypeDefinitionOpaque CLSID_AAFTypeDefOpaque
 #define CLSID_AAFTypeDefinitionStrongObjectReference CLSID_AAFTypeDefStrongObjRef
 #define CLSID_AAFTypeDefinitionVariableArray CLSID_AAFTypeDefVariableArray
 #define CLSID_AAFTypeDefinitionWeakObjectReference CLSID_AAFTypeDefWeakObjRef
 #define CLSID_AAFUNIXLocator CLSID_AAFUnixLocator
 #define CLSID_AAFCodecDefinition CLSID_AAFCodecDef
 #define CLSID_AAFContainerDefinition CLSID_AAFContainerDef
-#define CLSID_AAFInterpolationDefinition CLSID_AAFInterpolationDef
+#define CLSID_AAFPluggableDefinition CLSID_AAFPluggableDef
 
 //
 // Include the AAF macro invocations.
 #include "AAFMetaDictionary.h"
+// cleanup symbols.
+#undef AAF_CLASS
 
 
 
@@ -102,9 +76,14 @@ typedef struct tagAAFObjectEntry_t
 	const aafClassID_t* pClassID;
 } AAFObjectEntry_t;
 
+// cleanup symbols.
+#undef AAF_TABLE_BEGIN
+#undef AAF_CLASS
+#undef AAF_CLASS_SEPARATOR
+#undef AAF_TABLE_END
 #define AAF_TABLE_BEGIN() static AAFObjectEntry_t gAAFObjectTable[] = {
 
-#define AAF_CLASS(name, id, parent, concrete)\
+#define AAF_CLASS(name, id, parent)\
 { #name, &AUID_AAF##name, &CLSID_AAF##name }
 
 #define AAF_CLASS_SEPARATOR() ,
@@ -114,6 +93,11 @@ typedef struct tagAAFObjectEntry_t
 // Include the AAF macro invocations.
 // This will define all of the entries in the gAAFObjectTable.
 #include "AAFMetaDictionary.h"
+// cleanup symbols.
+#undef AAF_TABLE_BEGIN
+#undef AAF_CLASS
+#undef AAF_CLASS_SEPARATOR
+#undef AAF_TABLE_END
 
 // Define a table of pointers to entries on the gAAFObjectTable. We use 
 // this table to sort and search for AUID's in the gAAFObjectTable.
@@ -181,14 +165,14 @@ ImplAAFBaseClassFactory::~ImplAAFBaseClassFactory(void)
 
 // Global function that looksup the built-in code class id for the corresponding
 // auid.
-const aafClassID_t* ImplAAFBaseClassFactory::LookupClassID(const aafUID_t & auid)
+const aafClassID_t* ImplAAFBaseClassFactory::LookupClassID(const aafUID_t* pAUID)
 {
   // Return NULL if the given AUID cannot be found.
   const aafClassID_t *pClassID = NULL;
 
   // Lookup the class id in the predefined "base class" table.
   AAFObjectEntry_t **ppResult = NULL;
-  AAFObjectEntry_t key = {"KEY", &auid, NULL};
+  AAFObjectEntry_t key = {"KEY", pAUID, NULL};
   AAFObjectEntry_t *pKey = &key;
   
   // Use standard library's binary search routine.
@@ -202,3 +186,4 @@ const aafClassID_t* ImplAAFBaseClassFactory::LookupClassID(const aafUID_t & auid
 
   return (pClassID);
 }
+
