bc0c0aebe72813ccfd56e6a4a8e8cd6585548f34
tbingham <tbingham>
2001-04-17 07:05:18 +0000

    COM version of AAF info program

--- examples/com-api/comClientTest/AAFInfo.cpp
+++ examples/com-api/comClientTest/AAFInfo.cpp
@@ -1,47 +1,55 @@
 /***********************************************************************
  *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
+ *              Copyright (c) 1996 Avid Technology, Inc.
  *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- *  prior written permission of Avid Technology, Inc.
+ * Permission to use, copy and modify this software and to distribute
+ * and sublicense application software incorporating this software for
+ * any purpose is hereby granted, provided that (i) the above
+ * copyright notice and this permission notice appear in all copies of
+ * the software and related documentation, and (ii) the name Avid
+ * Technology, Inc. may not be used in any advertising or publicity
+ * relating to the software without the specific, prior written
+ * permission of Avid Technology, Inc.
  *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
  * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
  * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
+ * SPECIAL, INCIDENTAL, INDIRECT, CONSEQUENTIAL OR OTHER DAMAGES OF
+ * ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE, INCLUDING, 
+ * WITHOUT  LIMITATION, DAMAGES RESULTING FROM LOSS OF USE,
+ * DATA OR PROFITS, AND WHETHER OR NOT ADVISED OF THE POSSIBILITY OF
+ * DAMAGE, REGARDLESS OF THE THEORY OF LIABILITY.
  *
  ************************************************************************/
-
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-// Include the AAF interface declarations.
-#include "AAF.h"
+#include "AAFTypes.h"
 
-#if defined(macintosh) || defined(_MAC)
-#include "DataInput.h"
+#if defined(_MAC) || defined(macintosh)
+#include <wprintf.h>
+#include <initguid.h> // define all of the AAF guids.
+#include "AAF.h"
+#else
+#include "AAF.h"
+// TODO: This should not be here, I added them for now to get a good link.
+const CLSID CLSID_AAFSession = { 0xF0C10891, 0x3073, 0x11d2, { 0x80, 0x4A, 0x00, 0x60, 0x08, 0x14, 0x3E, 0x6F } };
 #endif
 
 static void     FatalErrorCode(HRESULT errcode, int line, char *file)
 {
   printf("Error '%0x' returned at line %d in %s\n", errcode, line, file);
-  // we don't need to exit on failure
-  //exit(1); 
+  exit(1);
+}
+
+static void     FatalError(char *message)
+{
+  printf(message);
+  exit(1);
 }
 
 static HRESULT moduleErrorTmp = S_OK;/* note usage in macro */
@@ -54,172 +62,93 @@ static HRESULT moduleErrorTmp = S_OK;/* note usage in macro */
 #define assert(b, msg) \
   if (!(b)) {fprintf(stderr, "ASSERT: %s\n\n", msg); exit(1);}
 
-
-static void convert(wchar_t* wcName, size_t length, const char* name)
-{
-  assert((name /* && *name */), "Valid input name");
-  assert(wcName != 0, "Valid output buffer");
-  assert(length > 0, "Valid output buffer size");
-  
-  size_t status = mbstowcs(wcName, name, length);
-  if (status == (size_t)-1) {
-    fprintf(stderr, "Error : Failed to convert'%s' to a wide character string.\n\n", name);
-    exit(1);  
-  }
-}
-
-static void convert(char* cName, size_t length, const wchar_t* name)
-{
-  assert((name /* && *name */), "Valid input name");
-  assert(cName != 0, "Valid output buffer");
-  assert(length > 0, "Valid output buffer size");
-
-  size_t status = wcstombs(cName, name, length);
-  if (status == (size_t)-1) {
-    fprintf(stderr, ": Error : Conversion failed.\n\n");
-    exit(1);  
-  }
-}
-
-static void convert(char* cName, size_t length, const char* name)
-{
-  assert((name /* && *name */), "Valid input name");
-  assert(cName != 0, "Valid output buffer");
-  assert(length > 0, "Valid output buffer size");
-
-  size_t sourceLength = strlen(name);
-  if (sourceLength < length - 1) {
-    strncpy(cName, name, length);
-  } else {
-    fprintf(stderr, "Error : Failed to copy '%s'.\n\n", name);
-    exit(1);  
-  }
-}
-
-static void convert(wchar_t* wName, size_t length, const wchar_t* name)
-{
-  assert((name /* && *name */), "Valid input name");
-  assert(wName != 0, "Valid output buffer");
-  assert(length > 0, "Valid output buffer size");
-
-  size_t sourceLength = 0;
-  while (*name)
-    ++sourceLength;
-  if (sourceLength < length - 1) {
-    // Copy the string if there is enough room in the destinition buffer.
-    while (*wName++ = *name++)
-      ;
-  } else {
-    fprintf(stderr, "Error : Failed to copy '%s'.\n\n", name);
-    exit(1);  
-  }
-}
-
-
 static void printIdentification(IAAFIdentification* pIdent)
 {
-  aafWChar wchName[500];
-  char chName[1000];
-    
-    
-  check(pIdent->GetCompanyName(wchName, sizeof (wchName)));
-  convert(chName, sizeof(chName), wchName);
-  printf("CompanyName          = \"%s\"\n", chName);
+	aafString_t companyName;
+	check(pIdent->GetCompanyName(&companyName));
+	wprintf(L"CompanyName          = \"%s\"\n", companyName.value);
 
-  check(pIdent->GetProductName(wchName, sizeof (wchName)));
-  convert(chName, sizeof(chName), wchName);
-  printf("ProductName          = \"%s\"\n", chName);
+	aafString_t productName;
+	check(pIdent->GetProductName(&productName));
+	wprintf(L"ProductName          = \"%s\"\n", productName.value);
 
-  check(pIdent->GetProductVersionString(wchName, sizeof (wchName)));
-  convert(chName, sizeof(chName), wchName);
-  printf("ProductVersionString = \"%s\"\n", chName);
+	aafString_t productVersionString;
+	check(pIdent->GetProductVersionString(&productVersionString));
+	wprintf(L"ProductVersionString = \"%s\"\n", productVersionString.value);
 
-  check(pIdent->GetPlatform(wchName, sizeof (wchName)));
-  convert(chName, sizeof(chName), wchName);
-  printf("Platform             = \"%s\"\n", chName);
+	aafString_t platform;
+	check(pIdent->GetPlatform(&platform));
+	wprintf(L"Platform             = \"%s\"\n", platform.value);
 }
 
-static void ReadAAFFile(aafWChar * pFileName)
+static void ReadAAFFile(unsigned char * pFileName)
 {
-  HRESULT hr = S_OK;
-  IAAFFile * pFile = NULL;
+	IAAFSession *				pSession = NULL;
+	IAAFFile *					pFile = NULL;
+	IAAFHeader *				pHeader = NULL;
+	IAAFIdentification *		pIdent;
+	aafProductIdentification_t	ProductInfo;
 
+	ProductInfo.companyName = (unsigned char *)"AAF Developers Desk. NOT!";
+	ProductInfo.productName = (unsigned char *)"Make AVR Example. NOT!";
+	ProductInfo.productVersion.major = 1;
+	ProductInfo.productVersion.minor = 0;
+	ProductInfo.productVersion.tertiary = 0;
+	ProductInfo.productVersion.patchLevel = 0;
+	ProductInfo.productVersion.type = kVersionUnknown;
+	ProductInfo.productVersionString = NULL;
+	ProductInfo.productID = -1;
+	ProductInfo.platform = NULL;
+	  
+	check(CoCreateInstance(CLSID_AAFSession,
+						   NULL, 
+						   CLSCTX_INPROC_SERVER, 
+						   IID_IAAFSession, 
+						   (void **)&pSession));
+	  
+	check(pSession->SetDefaultIdentification(&ProductInfo));
 
-  hr = AAFFileOpenExistingRead (pFileName, 0, &pFile);
-  check(hr); // display error message
-  if (SUCCEEDED(hr))
-  {
-    IAAFHeader * pHeader = NULL;
+	check(pSession->OpenReadFile(pFileName, &pFile));
+  
+	check(pFile->GetHeader(&pHeader));
 
-    hr = pFile->GetHeader(&pHeader);
-    check(hr); // display error message
-    if (SUCCEEDED(hr))
-    {
-      IAAFIdentification *    pIdent = NULL;
+	check(pHeader->GetLastIdentification(&pIdent));
 
-      hr = pHeader->GetLastIdentification(&pIdent);
-      check(hr); // display error message
-      if (SUCCEEDED(hr))
-      {
-        fprintf(stdout, "LastIdentification\n");
-        printIdentification(pIdent);
+	fprintf(stdout, "LastIdentification\n");
+	printIdentification(pIdent);
 
-        pIdent->Release();
-        pIdent = NULL;
+	check(pFile->Close());
 
-        aafNumSlots_t n;
-        hr = pHeader->CountMobs(kAAFAllMob, &n);
-        check(hr);
-        printf("Number of Mobs       = %d\n", n);
-      }
-      pHeader->Release();
-      pHeader = NULL;
-    }
+	check(pSession->EndSession());
 
-    hr = pFile->Close();
-    check(hr);
-
-    pFile->Release();
-    pFile = NULL;
-  }
+	if (pHeader) pHeader->Release();
+	if (pFile) pFile->Release();
+	if (pSession) pSession->Release();
 }
 
-// simple helper class to initialize and cleanup AAF library.
-struct CAAFInitialize
+// simple helper class to initialize and cleanup COM library.
+struct CComInitialize
 {
-  CAAFInitialize(const char *dllname = NULL)
-  {
-  	printf("Attempting to load the AAF dll...\n");
-    check(AAFLoad(dllname));
-    printf("DONE\n");
-  }
+	CComInitialize()
+	{
+		CoInitialize(NULL);
+	}
 
-  ~CAAFInitialize()
-  {
-    AAFUnload();
-  }
+	~CComInitialize()
+	{
+		CoUninitialize();
+	}
 };
 
-
-
-int main(int argumentCount, char* argumentVector[])
+main()
 {
-  if (argumentCount != 2) {
-    fprintf(stderr, "Error : wrong number of arguments\n");
-    return(1);
-  }
+	CComInitialize comInit;
 
-  char* inputFileName = argumentVector[1];
+	ReadAAFFile((unsigned char *)"Foo.aaf");
 
-  wchar_t wInputFileName[256];
-  convert(wInputFileName, 256, inputFileName);
+	fprintf(stdout, "Done\n");
 
-  CAAFInitialize aafInit;
 
-  ReadAAFFile(wInputFileName);
-
-  fprintf(stdout, "Done\n");
-
-  return(0);
+	return(0);
 }
 
