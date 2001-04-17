faefd1c1ca61d59faff2645cef052bdab90abf2f
tbingham <tbingham>
2001-04-17 03:27:12 +0000

    Added filtered Mob Enumerator and MobEnum test

--- ref-impl/src/impl/ImplAAFCompositionMob.h
+++ ref-impl/src/impl/ImplAAFCompositionMob.h
@@ -3,39 +3,32 @@
 #ifndef __ImplAAFCompositionMob_h__
 #define __ImplAAFCompositionMob_h__
 
+#include "OMStorable.h"
 
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- *  prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
+
+
 
 
 #ifndef __ImplAAFMob_h__
 #include "ImplAAFMob.h"
 #endif
-
 
 
 class ImplAAFCompositionMob : public ImplAAFMob
@@ -52,12 +45,15 @@ public:
 
   //@access Public Members
   //****************
-  // Initialize()
+  // SetInitialValues()
   //
   virtual AAFRESULT STDMETHODCALLTYPE
-    Initialize
+    SetInitialValues
 	    (// @parm [in] Mob name [optional]
-         aafWChar *  name);
+         aafString_t *  name,
+
+		 // @parm [in] Whether or not the mob is a primary mob
+		 aafBool  isPrimary);
   //@rdesc Error code [see below].
 
   //****************
@@ -120,10 +116,15 @@ public:
   );
 
 
-private:
-	OMFixedSizeProperty<aafLength_t>	_defaultFadeLen;
-	OMFixedSizeProperty<aafFadeType_t>	_defaultFadeType;
-	OMFixedSizeProperty<aafRational_t>	_defaultFadeEditUnit;
+public:
+  // Declare this class to be storable.
+  //
+  OMDECLARE_STORABLE(ImplAAFCompositionMob)
+
+  // Declare the module test method. The implementation of the will be be
+  // in /test/ImplAAFCompositionMobTest.cpp.
+  static AAFRESULT test();
 };
 
 #endif // ! __ImplAAFCompositionMob_h__
+
