2c867fb02a69429436f515bd6f2eff083a2b82d6
tbingham <tbingham>
2001-04-17 08:24:40 +0000

    Add support for extendible enumerations.

--- ref-impl/src/impl/ImplAAFTypeDefsGen.h
+++ ref-impl/src/impl/ImplAAFTypeDefsGen.h
@@ -12,7 +12,7 @@
  * notice appear in all copies of the software and related documentation,
  * and (ii) the name Avid Technology, Inc. may not be used in any
  * advertising or publicity relating to the software without the specific,
- *  prior written permission of Avid Technology, Inc.
+ * prior written permission of Avid Technology, Inc.
  *
  * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
@@ -52,7 +52,7 @@ struct TypeInteger              \
 static TypeInteger s_AAFAllTypeIntegers [] = {
 
 #define AAF_TYPE_DEFINITION_INTEGER(name, id, size, signed) \
-  {L##"aaf" L#name, id, size, AAF##signed, 1},
+  {L##"aaf" L#name, id, size, kAAF##signed, 1},
 
 #define AAF_TYPE_TABLE_END()  \
 0 };
@@ -70,21 +70,21 @@ static TypeInteger s_AAFAllTypeIntegers [] = {
 // Define structs to describe each member of an enumeration typedef,
 // and to describe the entire enumeration typedef.
 //
-#define AAF_TYPE_TABLE_BEGIN()  \
-                                \
-struct TypeEnumerationMember    \
-{                               \
-  wchar_t *        memberName;  \
-  aafInt64         memberValue; \
-};                              \
-                                \
-struct TypeEnumeration          \
-{                               \
-  aafUID_t   typeID;            \
-  wchar_t *  typeName;          \
-  aafUID_t * elementType;       \
-  size_t     size;              \
-  TypeEnumerationMember ** members;  \
+#define AAF_TYPE_TABLE_BEGIN()             \
+                                           \
+struct TypeEnumerationMember               \
+{                                          \
+  wchar_t *        memberName;             \
+  aafInt64         memberValue;            \
+};                                         \
+                                           \
+struct TypeEnumeration                     \
+{                                          \
+  aafUID_t                 typeID;         \
+  const wchar_t *          typeName;       \
+  const aafUID_t *         pElementTypeId; \
+  size_t                   size;           \
+  TypeEnumerationMember ** members;        \
 };
 
 #define AAF_TYPE_DEFINITION_ENUMERATION_MEMBER(name, value, parent) \
@@ -127,7 +127,7 @@ static const TypeEnumerationMember *s_TypeEnumerationMembers_##name[] = {
 static const TypeEnumeration s_TypeEnumeration_##name = \
 { id, \
   L#name, \
-  (aafUID_t *)& TYPE_GUID_NAME(type), \
+	/*(aafUID_t *)*/& TYPE_GUID_NAME(type), \
   sizeof (aaf##name##_t), \
   (TypeEnumerationMember **) s_TypeEnumerationMembers_##name, \
  };
@@ -170,22 +170,22 @@ static TypeEnumeration * s_AAFAllTypeEnumerations [] = {
 // Define structs to describe each member of a record typedef, and to
 // describe the entire record typedef.
 //
-#define AAF_TYPE_TABLE_BEGIN()  \
-                                \
-struct TypeRecordMember         \
-{                               \
-  aafUID_t *       memberType;  \
-  wchar_t *        memberName;  \
-  size_t           memberOffset; \
-  eAAFTypeCategory_t typeCat;   \
-};                              \
-                                \
-struct TypeRecord               \
-{                               \
-  aafUID_t   typeID;            \
-  wchar_t *  typeName;          \
-  size_t     size;              \
-  TypeRecordMember ** members;  \
+#define AAF_TYPE_TABLE_BEGIN()      \
+                                    \
+struct TypeRecordMember             \
+{                                   \
+  const aafUID_t *   pMemberTypeId; \
+  wchar_t *          memberName;    \
+  size_t             memberOffset;  \
+  eAAFTypeCategory_t typeCat;       \
+};                                  \
+                                    \
+struct TypeRecord                   \
+{                                   \
+  aafUID_t   typeID;                \
+  wchar_t *  typeName;              \
+  size_t     size;                  \
+  TypeRecordMember ** members;      \
 };
 
 #define AAF_TYPE_RECORD_MEMBER(recordName, memberType, memberName) \
@@ -268,10 +268,10 @@ static TypeRecord * s_AAFAllTypeRecords [] = {
 #define AAF_TYPE_TABLE_BEGIN()  \
 struct TypeVaryingArray         \
 {                               \
-  wchar_t *  typeName;          \
-  aafUID_t   typeId;            \
-  aafUID_t * baseType;          \
-  int       isValid;            \
+  const wchar_t *  typeName;    \
+  aafUID_t         typeId;      \
+  const aafUID_t * pBaseTypeId; \
+  int              isValid;     \
 };                              \
                                 \
 static TypeVaryingArray s_AAFAllTypeVaryingArrays [] = {
@@ -279,7 +279,7 @@ static TypeVaryingArray s_AAFAllTypeVaryingArrays [] = {
 #define AAF_TYPE(x) kAAFTypeID_##x
 
 #define AAF_TYPE_DEFINITION_VARYING_ARRAY(name, id, type) \
-  {L##"aaf" L#name, id, (aafUID_t *)& type, 1},
+  {L##"aaf" L#name, id, &type, 1},
 
 #define AAF_TYPE_TABLE_END()  \
 0 };
@@ -294,11 +294,11 @@ static TypeVaryingArray s_AAFAllTypeVaryingArrays [] = {
 #define AAF_TYPE_TABLE_BEGIN()  \
 struct TypeFixedArray           \
 {                               \
-  wchar_t *  typeName;          \
-  aafUID_t   typeId;            \
-  aafUID_t * baseType;          \
-  aafUInt32  count;             \
-  int       isValid;            \
+  const wchar_t *  typeName;    \
+  aafUID_t         typeId;      \
+  const aafUID_t * pBaseTypeId; \
+  aafUInt32        count;       \
+  int              isValid;     \
 };                              \
                                 \
 static TypeFixedArray s_AAFAllTypeFixedArrays [] = {
@@ -306,7 +306,7 @@ static TypeFixedArray s_AAFAllTypeFixedArrays [] = {
 #define AAF_TYPE(x) kAAFTypeID_##x
 
 #define AAF_TYPE_DEFINITION_FIXED_ARRAY(name, id, type, count) \
-  {L##"aaf" L#name, id, (aafUID_t *)& type, count, 1},
+  {L##"aaf" L#name, id, &type, count, 1},
 
 #define AAF_TYPE_TABLE_END()  \
 0 };
@@ -321,10 +321,10 @@ static TypeFixedArray s_AAFAllTypeFixedArrays [] = {
 #define AAF_TYPE_TABLE_BEGIN()  \
 struct TypeRename               \
 {                               \
-  wchar_t *  typeName;          \
-  aafUID_t   typeId;            \
-  aafUID_t * baseType;          \
-  int        isValid;           \
+  const wchar_t *  typeName;    \
+  aafUID_t         typeId;      \
+  const aafUID_t * pBaseTypeId; \
+  int              isValid;     \
 };                              \
                                 \
 static TypeRename s_AAFAllTypeRenames [] = {
@@ -347,10 +347,10 @@ static TypeRename s_AAFAllTypeRenames [] = {
 #define AAF_TYPE_TABLE_BEGIN()  \
 struct TypeString               \
 {                               \
-  wchar_t *  typeName;          \
-  aafUID_t   typeId;            \
-  aafUID_t * baseType;          \
-  int       isValid;            \
+  const wchar_t *  typeName;    \
+  aafUID_t         typeId;      \
+  const aafUID_t * pBaseTypeId; \
+  int              isValid;     \
 };                              \
                                 \
 static TypeString s_AAFAllTypeStrings [] = {
@@ -373,16 +373,62 @@ static TypeString s_AAFAllTypeStrings [] = {
 #define AAF_TYPE_TABLE_BEGIN()  \
 struct TypeCharacter            \
 {                               \
-  wchar_t * typeName;           \
-  aafUID_t  typeID;             \
-  aafUInt8  size;               \
-  int       isValid;            \
+  const wchar_t * typeName;     \
+  aafUID_t        typeID;       \
+  aafUInt8        size;         \
+  int             isValid;      \
 };                              \
                                 \
 static TypeCharacter s_AAFAllTypeCharacters [] = {
 
-#define AAF_TYPE_DEFINITION_CHARACTER(name, id, size) \
-  {L##"aaf" L#name, id, size, 1},
+#define AAF_TYPE_DEFINITION_CHARACTER(name, id) \
+  {L##"aaf" L#name, id, 2, 1},
+
+#define AAF_TYPE_TABLE_END()  \
+0 };
+
+#include "AAFMetaDictionary.h"
+
+
+
+//
+// Pass 13b:  Do stuff for indirect types. (there should be only one!)
+//
+#define AAF_TYPE_TABLE_BEGIN()  \
+struct TypeIndirect             \
+{                               \
+  const wchar_t * typeName;     \
+  aafUID_t        typeID;       \
+  int             isValid;      \
+};                              \
+                                \
+static TypeIndirect s_AAFAllTypeIndirects [] = {
+
+#define AAF_TYPE_DEFINITION_INDIRECT(name, id) \
+  {L##"aaf" L#name, id, 1},
+
+#define AAF_TYPE_TABLE_END()  \
+0 };
+
+#include "AAFMetaDictionary.h"
+
+
+
+//
+// Pass 13c:  Do stuff for opaque types. (there should be only one!)
+//
+#define AAF_TYPE_TABLE_BEGIN()  \
+struct TypeOpaque             \
+{                               \
+  const wchar_t * typeName;     \
+  aafUID_t        typeID;       \
+  int             isValid;      \
+};                              \
+                                \
+static TypeOpaque s_AAFAllTypeOpaques [] = {
+
+#define AAF_TYPE_DEFINITION_OPAQUE(name, id) \
+  {L##"aaf" L#name, id, 1},
 
 #define AAF_TYPE_TABLE_END()  \
 0 };
@@ -398,10 +444,10 @@ static TypeCharacter s_AAFAllTypeCharacters [] = {
 #define AAF_TYPE_TABLE_BEGIN()  \
 struct TypeStrongRef            \
 {                               \
-  wchar_t *  typeName;          \
-  aafUID_t   typeId;            \
-  aafUID_t * refdType;          \
-  int       isValid;            \
+  const wchar_t *  typeName;    \
+  aafUID_t         typeId;      \
+  const aafUID_t * pRefdTypeId; \
+  int              isValid;     \
 };                              \
                                 \
 static TypeStrongRef s_AAFAllTypeStrongRefs [] = {
@@ -427,10 +473,10 @@ static TypeStrongRef s_AAFAllTypeStrongRefs [] = {
 #define AAF_TYPE_TABLE_BEGIN()  \
 struct TypeStrongRefSet         \
 {                               \
-  wchar_t *  typeName;          \
-  aafUID_t   typeId;            \
-  aafUID_t * refdType;          \
-  int       isValid;            \
+  const wchar_t *  typeName;    \
+  aafUID_t         typeId;      \
+  const aafUID_t * pRefdTypeId; \
+  int              isValid;     \
 };                              \
                                 \
 static TypeStrongRefSet s_AAFAllTypeStrongRefSets [] = {
@@ -454,10 +500,10 @@ static TypeStrongRefSet s_AAFAllTypeStrongRefSets [] = {
 #define AAF_TYPE_TABLE_BEGIN()  \
 struct TypeStrongRefVector      \
 {                               \
-  wchar_t *  typeName;          \
-  aafUID_t   typeId;            \
-  aafUID_t * refdType;          \
-  int       isValid;            \
+  const wchar_t *  typeName;    \
+  aafUID_t         typeId;      \
+  const aafUID_t * pRefdTypeId; \
+  int              isValid;     \
 };                              \
                                 \
 static TypeStrongRefVector s_AAFAllTypeStrongRefVectors [] = {
@@ -481,10 +527,10 @@ static TypeStrongRefVector s_AAFAllTypeStrongRefVectors [] = {
 #define AAF_TYPE_TABLE_BEGIN()  \
 struct TypeWeakRef              \
 {                               \
-  wchar_t *  typeName;          \
-  aafUID_t   typeId;            \
-  aafUID_t * refdType;          \
-  int       isValid;            \
+  const wchar_t *  typeName;    \
+  aafUID_t         typeId;      \
+  const aafUID_t * pRefdTypeId; \
+  int              isValid;     \
 };                              \
                                 \
 static TypeWeakRef s_AAFAllTypeWeakRefs [] = {
@@ -509,10 +555,10 @@ static TypeWeakRef s_AAFAllTypeWeakRefs [] = {
 #define AAF_TYPE_TABLE_BEGIN()  \
 struct TypeWeakRefSet           \
 {                               \
-  wchar_t *  typeName;          \
-  aafUID_t   typeId;            \
-  aafUID_t * refdType;          \
-  int       isValid;            \
+  const wchar_t *  typeName;    \
+  aafUID_t         typeId;      \
+  const aafUID_t * pRefdTypeId; \
+  int              isValid;     \
 };                              \
                                 \
 static TypeWeakRefSet s_AAFAllTypeWeakRefSets [] = {
@@ -536,10 +582,10 @@ static TypeWeakRefSet s_AAFAllTypeWeakRefSets [] = {
 #define AAF_TYPE_TABLE_BEGIN()  \
 struct TypeWeakRefVector        \
 {                               \
-  wchar_t *  typeName;          \
-  aafUID_t   typeId;            \
-  aafUID_t * refdType;          \
-  int       isValid;            \
+  const wchar_t *  typeName;    \
+  aafUID_t         typeId;      \
+  const aafUID_t * pRefdTypeId; \
+  int              isValid;     \
 };                              \
                                 \
 static TypeWeakRefVector s_AAFAllTypeWeakRefVectors [] = {
@@ -554,6 +600,104 @@ static TypeWeakRefVector s_AAFAllTypeWeakRefVectors [] = {
 0 };
 
 #include "AAFMetaDictionary.h"
+
+
+//
+// pass 20: For extendible enumerations, define individual enumeration
+// fields
+//
+
+//
+// Define structs to describe each member of an extendible enumeration
+// typedef, and to describe the entire extendible enumeration typedef.
+//
+#define AAF_TYPE_TABLE_BEGIN()             \
+                                           \
+struct TypeExtEnumerationMember            \
+{                                          \
+  wchar_t *        memberName;             \
+  aafUID_t         memberValue;            \
+};                                         \
+                                           \
+struct TypeExtEnumeration                  \
+{                                          \
+  aafUID_t                 typeID;         \
+  const wchar_t *          typeName;       \
+  TypeExtEnumerationMember ** members;     \
+};
+
+#define AAF_TYPE_DEFINITION_EXTENDIBLE_ENUMERATION_MEMBER(name, auid, container) \
+static const TypeExtEnumerationMember s_TypeExtEnumerationMember_##container##_##name = \
+{ L#name, \
+  auid \
+};
+
+#define AAF_TYPE(x) aaf##x##_t
+#define AAF_REFERENCE_TYPE(type, target)      AAF_TYPE(target##type)
+#define AAF_REFERENCE_TYPE_NAME(type, target) AAF_TYPE(target##type)
+#include "AAFMetaDictionary.h"
+
+
+//
+// pass 21: For extendible enumerations, create the (null-terminated)
+// field list for each extendible enumeration
+//
+
+#define AAF_TYPE_DEFINITION_EXTENDIBLE_ENUMERATION(name, id) \
+static const TypeExtEnumerationMember *s_TypeExtEnumerationMembers_##name[] = {
+
+#define AAF_TYPE_DEFINITION_EXTENDIBLE_ENUMERATION_MEMBER(name, value, container) \
+	&s_TypeExtEnumerationMember_##container##_##name,
+
+#define AAF_TYPE_DEFINITION_EXTENDIBLE_ENUMERATION_END(name) \
+  0 } ;
+
+#define AAF_TYPE(x) _aaf##x##_t
+#define AAF_REFERENCE_TYPE(type, target)      AAF_TYPE(target##type)
+#define AAF_REFERENCE_TYPE_NAME(type, target) AAF_TYPE(target##type)
+#include "AAFMetaDictionary.h"
+
+
+//
+// pass 22: For extendible enumerations, create each extendible
+// enumeration definition
+//
+
+#define AAF_TYPE_DEFINITION_EXTENDIBLE_ENUMERATION(name, id) \
+static const TypeExtEnumeration s_TypeExtEnumeration_##name = \
+{ id, \
+  L#name, \
+  (TypeExtEnumerationMember **) s_TypeExtEnumerationMembers_##name, \
+ };
+
+#define AAF_TYPE(x) x
+// #define AAF_REFERENCE_TYPE(type, target)      AAF_TYPE(target##type)
+// #define AAF_REFERENCE_TYPE_NAME(type, target) AAF_TYPE(target##type)
+#include "AAFMetaDictionary.h"
+
+
+//
+// pass 32: For extendible enumerations, create a master
+// null-terminated list of all type def extendible enumerations
+//
+
+#define AAF_TYPE_TABLE_BEGIN()   \
+static TypeExtEnumeration * s_AAFAllTypeExtEnumerations [] = {
+
+#define AAF_TYPE_DEFINITION_EXTENDIBLE_ENUMERATION(name, id) \
+  (TypeExtEnumeration*)& s_TypeExtEnumeration_##name,
+
+#define AAF_TYPE_TABLE_END()   \
+0 };
+
+#define AAF_TYPE(x) _aaf##x##_t
+#define AAF_REFERENCE_TYPE(type, target)      AAF_TYPE(target##type)
+#define AAF_REFERENCE_TYPE_NAME(type, target) AAF_TYPE(target##type)
+#include "AAFMetaDictionary.h"
+
+//
+// Done with extendible enumerations.
+//
 
 
 #endif // ! __ImplAAFTypeDefsGen_h__
