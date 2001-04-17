ae0596f7df758cdbcce0c7d1448c4c66adaa1616
tbingham <tbingham>
2001-04-17 06:53:33 +0000

    Change a constant name

--- test/com/ComModTestAAF/ModuleTests/CEnumAAFCodecFlavoursTest.cpp
+++ test/com/ComModTestAAF/ModuleTests/CEnumAAFCodecFlavoursTest.cpp
@@ -78,13 +78,15 @@ static HRESULT OpenAAFFile(aafWChar*			pFileName,
 	aafProductIdentification_t	ProductInfo;
 	HRESULT						hr = AAFRESULT_SUCCESS;
 
+	aafProductVersion_t v;
+	v.major = 1;
+	v.minor = 0;
+	v.tertiary = 0;
+	v.patchLevel = 0;
+	v.type = kAAFVersionUnknown;
 	ProductInfo.companyName = L"AAF Developers Desk";
 	ProductInfo.productName = L"EnumAAFCodecFlavours Test";
-	ProductInfo.productVersion.major = 1;
-	ProductInfo.productVersion.minor = 0;
-	ProductInfo.productVersion.tertiary = 0;
-	ProductInfo.productVersion.patchLevel = 0;
-	ProductInfo.productVersion.type = kAAFVersionUnknown;
+	ProductInfo.productVersion = &v;
 	ProductInfo.productVersionString = NULL;
 	ProductInfo.productID = UnitTestProductID;
 	ProductInfo.platform = NULL;
@@ -146,7 +148,7 @@ static HRESULT CreateAAFFile(aafWChar * pFileName)
 		CAAFBuiltinDefs defs (pDictionary);
     
 		checkResult(AAFGetPluginManager(&mgr));
-		checkResult(mgr->CreatePluginDefinition (CodecWave, pDictionary, &pDef));
+		checkResult(mgr->CreatePluginDefinition (kAAFCodecWAVE, pDictionary, &pDef));
 
 		checkResult(pDef->QueryInterface(IID_IAAFCodecDef, (void **)&pCodecDef));
 		checkResult(pCodecDef->AddEssenceKind (defs.ddSound()));
@@ -200,9 +202,9 @@ static HRESULT ReadAAFFile(aafWChar* pFileName)
 	IEnumAAFCodecFlavours	*pEnum = NULL;
 	bool bFileOpen = false;
 	aafBool			testResult;
-	aafUID_t		codecID = CodecWave;
+	aafUID_t		codecID = kAAFCodecWAVE;
 	// aafUID_t		testMatte = DDEF_Matte;
-	aafUID_t		checkFlavour = NilCodecFlavour;
+	aafUID_t		checkFlavour = kAAFNilCodecFlavour;
 	aafUID_t		testFlavour;
 	HRESULT			hr = S_OK;
 
