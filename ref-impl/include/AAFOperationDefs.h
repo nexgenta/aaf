6028ea3637e91a979f8c71a38616095b6f1ede08
tbingham <tbingham>
2001-04-17 03:03:25 +0000

    Change DataDef internals & split of dataDef defines

--- ref-impl/include/AAFOperationDefs.h
+++ ref-impl/include/AAFOperationDefs.h
@@ -1,24 +1,11 @@
-//=---------------------------------------------------------------------=
-//
-// The contents of this file are subject to the AAF SDK Public
-// Source License Agreement (the "License"); You may not use this file
-// except in compliance with the License.  The License is available in
-// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
-// Association or its successor.
-// 
-// Software distributed under the License is distributed on an "AS IS"
-// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
-// the License for the specific language governing rights and limitations
-// under the License.
-// 
-// The Original Code of this file is Copyright 1998-2001, Licensor of the
-// AAF Association.
-// 
-// The Initial Developer of the Original Code of this file and the
-// Licensor of the AAF Association is Avid Technology.
-// All rights reserved.
-//
-//=---------------------------------------------------------------------=
+/***********************************************\
+*												*
+* Advanced Authoring Format						*
+*												*
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+* Copyright (c) 1998-1999 Microsoft Corporation *
+*												*
+\***********************************************/
 
 // Following are the UIDs of some well-known operation definition objects in AAF.  Most of the definitions
 // are for effects, as that is currently the most common operation performed.
@@ -60,8 +47,6 @@ const aafUID_t kAAFEffectMonoAudioPan = { 0x9d2ea893, 0x968, 0x11d3, { 0x8a, 0x3
 const aafUID_t kAAFEffectMonoAudioGain = { 0x9d2ea894, 0x968, 0x11d3, { 0x8a, 0x38, 0x0, 0x50, 0x4, 0xe, 0xf7, 0xd2 } };
 // {9D2EA895-0968-11d3-8A38-0050040EF7D2}
 const aafUID_t kAAFEffectStereoAudioGain = { 0x9d2ea895, 0x968, 0x11d3, { 0x8a, 0x38, 0x0, 0x50, 0x4, 0xe, 0xf7, 0xd2 } };
-// {8D896AD0-2261-11d3-8A4C-0050040EF7D2}
-const aafUID_t kAAFEffectMonoAudioMixdown ={ 0x8d896ad0, 0x2261, 0x11d3, {0x8a, 0x4c, 0x0, 0x50, 0x4, 0xe, 0xf7, 0xd2 } };
 
 // {1575E350-FCA3-11d2-8A2A-0050040EF7D2}
 const aafUID_t kAAFOperationUnknown = { 0x1575e350, 0xfca3, 0x11d2, { 0x8a, 0x2a, 0x0, 0x50, 0x4, 0xe, 0xf7, 0xd2 } };
