ae4526a7a008c9d247aded2cea37dc11181dead9
tbingham <tbingham>
2001-04-17 03:09:57 +0000

    Interface cleanup from Drop27 review pass 1

--- ref-impl/include/AAFInterpolatorDefs.h
+++ ref-impl/include/AAFInterpolatorDefs.h
@@ -1,24 +1,11 @@
-//=---------------------------------------------------------------------=
-//
-// The contents of this file are subject to the AAF SDK Public
-// Source License Agreement (the "License"); You may not use this file
-// except in compliance with the License.  The License is available in
-// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
-// Association or its successor.
-// 
-// Software distributed under the License is distributed on an "AS IS"
-// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
-// the License for the specific language governing rights and limitations
-// under the License.
-// 
-// The Original Code of this file is Copyright 1998-2001, Licensor of the
-// AAF Association.
-// 
-// The Initial Developer of the Original Code of this file and the
-// Licensor of the AAF Association is Avid Technology.
-// All rights reserved.
-//
-//=---------------------------------------------------------------------=
+/***********************************************\
+*												*
+* Advanced Authoring Format						*
+*												*
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+* Copyright (c) 1998-1999 Microsoft Corporation *
+*												*
+\***********************************************/
 
 // Following are the UIDs of the well-known Interpolator definition objects in AAF.
 //
@@ -28,3 +15,7 @@ const aafUID_t NoInterpolator		= { 0x5B6C85A3, 0x0EDE, 0x11d3, { 0x80, 0xA9, 0x0
 const aafUID_t LinearInterpolator   = { 0x5B6C85A4, 0x0EDE, 0x11d3, { 0x80, 0xA9, 0x00, 0x60, 0x08, 0x14, 0x3e, 0x6f } };
 const aafUID_t ConstantInterpolator		= { 0x5B6C85A5, 0x0EDE, 0x11d3, { 0x80, 0xA9, 0x00, 0x60, 0x08, 0x14, 0x3E, 0x6F } };
 const aafUID_t BSplineInterpolator  = { 0x5B6C85A6, 0x0EDE, 0x11d3, { 0x80, 0xA9, 0x00, 0x60, 0x08, 0x14, 0x3e, 0x6f } };
+
+// DataTypes to pass to interpolators
+const aafUID_t kAAFExpLong = { 0x6AEFC512, 0x9041, 0x11d2, { 0x80, 0x88, 0x00, 0x60, 0x08, 0x14, 0x3E, 0x6F } };		// TomR:Added to support all options in the union
+const aafUID_t kAAFExpRational = { 0x6AEFC513, 0x9041, 0x11d2, { 0x80, 0x88, 0x00, 0x60, 0x08, 0x14, 0x3E, 0x6F } }; 	// TomR:Added to support all options in the union
