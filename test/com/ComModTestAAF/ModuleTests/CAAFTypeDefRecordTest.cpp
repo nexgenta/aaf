47b58ef7861e557b7415a600febc3b4d6a612458
tbingham <tbingham>
2001-04-17 05:53:09 +0000

    Type def for record (structured) types

--- test/com/ComModTestAAF/ModuleTests/CAAFTypeDefRecordTest.cpp
+++ test/com/ComModTestAAF/ModuleTests/CAAFTypeDefRecordTest.cpp
@@ -1,13 +1,13 @@
 // @doc INTERNAL
-// @com This file implements the module test for CAAFTypeDefRecord
-/***********************************************\
-*                                               *
-* Advanced Authoring Format                     *
-*                                               *
-* Copyright (c) 1998-1999 Avid Technology, Inc. *
-* Copyright (c) 1998-1999 Microsoft Corporation *
-*                                               *
-\***********************************************/
+// @com This file implements the module test for CAAFDefinitionObject
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
 
 
 
@@ -21,13 +21,56 @@
 #error - improperly defined include guard
 #endif
 
-#include "AAFResult.h"
+
+#include <iostream.h>
+
+// Temporarily necessary global declarations.
+extern "C" const CLSID CLSID_AAFTypeDefRecord; // generated
 
 
 HRESULT CAAFTypeDefRecord::test()
 {
-  return AAFRESULT_NOT_IMPLEMENTED;
+  HRESULT hr = AAFRESULT_NOT_IMPLEMENTED;
+  IAAFTypeDefRecord *pObject = NULL;
+
+  try
+    {
+      // Attempt to create an AAFTypeDefRecord.
+      hr =  CoCreateInstance(
+                             CLSID_AAFTypeDefRecord,
+                             NULL, 
+                             CLSCTX_INPROC_SERVER, 
+                             IID_IAAFTypeDefRecord, (void **)&pObject);
+      if (FAILED(hr))
+        {
+          cerr << "CAAFTypeDefRecord::test...FAILED!";
+          cerr << hr;
+          cerr << "\tCoCreateInstance(&CLSID_AAFTypeDefRecord, NULL,"
+            " CLSCTX_INPROC_SERVER, &IID_IAAFTypeDefRecord, ...);" <<
+              endl;
+          return hr;
+        }
+
+      // module-specific tests go here
+
+      if (pObject)
+        pObject->Release();
+      return AAFRESULT_NOT_IMPLEMENTED;
+
+    }
+  catch (...)
+    {
+      cerr << "CAAFTypeDefRecord::test...Caught general C++"
+        " exception!" << endl; 
+    }
+
+  // Cleanup our object if it exists.
+  if (pObject)
+    pObject->Release();
+
+  return hr;
 }
+
 
 
 
