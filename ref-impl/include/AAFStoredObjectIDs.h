3f90a3870c4a37cbcd1296db06a01b682c81df77
tbingham <tbingham>
2001-04-17 04:57:30 +0000

    .

--- ref-impl/include/AAFStoredObjectIDs.h
+++ ref-impl/include/AAFStoredObjectIDs.h
@@ -1,17 +1,16 @@
-/******************************************\
-*                                          *
-* File: AAFStoredObjectIDs.h               *
-*                                          *
-* Purpose: Definitions for controlling     * 
-*          AUID initialization             *
-*                                          *
-*                                          *
-* Advanced Authoring Format                *
-*                                          *
-* Copyright (c) 1998 Avid Technology, Inc. *
-* Copyright (c) 1998 Microsoft Corporation *
-*                                          *
-\******************************************/
+/***********************************************\
+*                                               *
+* File: AAFStoredObjectIDs.h                    *
+*                                               *
+* Purpose: Definitions for controlling          *
+*          AUID initialization                  *
+*                                               *
+*                                               *
+* Advanced Authoring Format                     *
+*                                               *
+* Copyright (c) 1998-1999 Avid Technology, Inc. *
+*                                               *
+\***********************************************/
 #ifndef __AAFStoredObjectIDs_h__
 
 #ifndef __AAFTypes_h__
@@ -20,22 +19,19 @@
 
 // Define the AAF_CLASS macro to produce the const stored object AUID's
 #ifndef INIT_AUID 
-#define AAF_CLASS(name, dataid, parent) extern "C" aafUID_t AUID_AAF##name;
+#define AAF_CLASS(name, dataid, parent, concrete) extern "C" aafUID_t AUID_AAF##name;
 #else
-#define AAF_CLASS(name, dataid, parent) extern "C" aafUID_t AUID_AAF##name = dataid;
+#define AAF_CLASS(name, dataid, parent, concrete) extern "C" aafUID_t AUID_AAF##name = dataid;
 #endif
 // The AAF reference implementation is still not quite in sync with SMPTE
 // so we have to alias some of the SMPTE names to their corresponding
 // name in AAF.
 //
 #define AUID_AAFClassDefinition AUID_AAFClassDef
-#define AUID_AAFConstantValue AUID_AAFConstValue
 #define AUID_AAFDataDefinition AUID_AAFDataDef
 #define AUID_AAFDefinitionObject AUID_AAFDefObject
 #define AUID_AAFEdgeCode AUID_AAFEdgecode
 #define AUID_AAFOperationDefinition AUID_AAFOperationDef
-#define AUID_AAFEssenceGroup AUID_AAFMediaGroup
-#define AUID_AAFFilmDescriptor AUID_AAFMediaFilmDescriptor
 #define AUID_AAFInterchangeObject AUID_AAFObject
 #define AUID_AAFJPEGImageData AUID_AAFJPEGData
 #define AUID_AAFMIDIFileData AUID_AAFMIDIData
@@ -43,6 +39,7 @@
 #define AUID_AAFPropertyDefinition AUID_AAFPropertyDef
 #define AUID_AAFTypeDefinition AUID_AAFTypeDef
 #define AUID_AAFTypeDefinitionEnumeration AUID_AAFTypeDefEnum
+#define AUID_AAFTypeDefinitionExtendibleEnumeration AUID_AAFTypeDefExtEnum
 #define AUID_AAFTypeDefinitionFixedArray AUID_AAFTypeDefFixedArray
 #define AUID_AAFTypeDefinitionInteger AUID_AAFTypeDefInt
 #define AUID_AAFTypeDefinitionRecord AUID_AAFTypeDefRecord
@@ -60,14 +57,5 @@
 
 // Include the inside-out macro files that invokes the AAF_CLASS macros.
 #include "AAFMetaDictionary.h"
-
-// cleanup any defined macros.
-#undef AAF_TABLE_BEGIN
-#undef AAF_TABLE_END
-#undef AAF_CLASS
-#undef AAF_CLASS_END
-#undef AAF_CLASS_SEPARATOR
-#undef AAF_PROPERTY
-
 
 #endif // __AAFStoredObjectIDs_h__
