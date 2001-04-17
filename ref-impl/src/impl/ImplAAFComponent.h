c3f854c183b53fbe22c7ddacc6d518aa63ee7c38
tbingham <tbingham>
2001-04-17 04:08:27 +0000

    Add KLV Data

--- ref-impl/src/impl/ImplAAFComponent.h
+++ ref-impl/src/impl/ImplAAFComponent.h
@@ -3,22 +3,42 @@
 #ifndef __ImplAAFComponent_h__
 #define __ImplAAFComponent_h__
 
+#ifndef __ImplAAFDataDef_h__
+#include "ImplAAFDataDef.h"
+#endif
 
-class ImplAAFDataDef;
 class ImplAAFMob;
 class ImplAAFMobSlot;
 class ImplAAFOperationDef;
 class ImplAAFOperationGroup;
 class ImplAAFScopeStack;
 
-/******************************************\
-*                                          *
-* Advanced Authoring Format                *
-*                                          *
-* Copyright (c) 1998 Avid Technology, Inc. *
-* Copyright (c) 1998 Microsoft Corporation *
-*                                          *
-\******************************************/
+/***********************************************************************
+ *
+ *              Copyright (c) 1998-1999 Avid Technology, Inc.
+ *
+ * Permission to use, copy and modify this software and accompanying 
+ * documentation, and to distribute and sublicense application software
+ * incorporating this software for any purpose is hereby granted, 
+ * provided that (i) the above copyright notice and this permission
+ * notice appear in all copies of the software and related documentation,
+ * and (ii) the name Avid Technology, Inc. may not be used in any
+ * advertising or publicity relating to the software without the specific,
+ * prior written permission of Avid Technology, Inc.
+ *
+ * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+ * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+ * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+ * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+ * LIABILITY.
+ *
+ ************************************************************************/
 
 
 
@@ -27,6 +47,13 @@ class ImplAAFScopeStack;
 #include "ImplAAFObject.h"
 #endif
 
+#ifndef __ImplAAFKLVData_h__
+#include "ImplAAFKLVData.h"
+#endif
+
+#ifndef __ImplEnumAAFKLVData_h__
+#include "ImplEnumAAFKLVData.h"
+#endif
 
 typedef 
 enum _implCompType_t
@@ -46,14 +73,12 @@ public:
   ImplAAFComponent ();
   ~ImplAAFComponent ();
 
-  OMDECLARE_STORABLE(ImplAAFComponent)
-
   //****************
   // SetLength()
   //
   virtual AAFRESULT STDMETHODCALLTYPE
     SetLength
-        (aafLength_t *  length);  //@parm [in] Length of this object
+        (const aafLength_t & length);  //@parm [in] Length of this object
 
 
   //****************
@@ -69,7 +94,7 @@ public:
   //
   virtual AAFRESULT STDMETHODCALLTYPE
     SetDataDef
-        (aafUID_t *  datadef);  //@parm [in] DataDef of this object
+        (ImplAAFDataDef * pDataDef);  //@parm [in] DataDef of this object
 
 
   //****************
@@ -77,13 +102,41 @@ public:
   //
   virtual AAFRESULT STDMETHODCALLTYPE
     GetDataDef
-        (aafUID_t *  datadef);  //@parm [retval][out] DataDef of this object
+        (ImplAAFDataDef ** ppDataDef);  //@parm [retval][out] DataDef of this object
 
+  //****************
+  // AppendKLVData()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    AppendKLVData
+        (ImplAAFKLVData * pData);  //@parm [in,ref] Data
+
+  //****************
+  // RemoveKLVData()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    RemoveKLVData
+        (ImplAAFKLVData * pData);
+
+  //****************
+  // CountKLVData()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    CountKLVData
+        (aafUInt32 *  pNumComments);  //@parm [out,retval] Number  of KLVData
+
+
+  //****************
+  // GetKLVData()
+  //
+  virtual AAFRESULT STDMETHODCALLTYPE
+    GetKLVData
+        (ImplEnumAAFKLVData ** ppEnum);  //@parm [out,retval] KLVData
 
 public:
 	AAFRESULT SetNewProps(
 				aafLength_t length,		// IN - Length  property value
-				aafUID_t *dataDef);		// IN - DataDef property value
+				ImplAAFDataDef * pDataDef);		// IN - DataDef property value
 	virtual AAFRESULT AccumulateLength(aafLength_t *length);
 	virtual AAFRESULT GetMinimumBounds(aafPosition_t rootPos, aafLength_t rootLen,
 										ImplAAFMob *mob, ImplAAFMobSlot *track,
@@ -98,12 +151,14 @@ public:
 										ImplAAFComponent **found, aafBool *foundTransition);
 
 	virtual AAFRESULT GetComponentType(implCompType_t* pType) {*pType = kComponent; return AAFRESULT_SUCCESS;}
-	virtual AAFRESULT ChangeContainedReferences(aafUID_t *from, aafUID_t *to);
+	virtual AAFRESULT ChangeContainedReferences(aafMobID_constref from,
+												aafMobID_constref to);
 
 
 private:
-	OMFixedSizeProperty<aafUID_t>		_dataDef;
+	OMWeakReferenceProperty<ImplAAFDataDef>		_dataDef;
 	OMFixedSizeProperty<aafLength_t>	_length;
+    OMStrongReferenceVectorProperty<ImplAAFKLVData> _KLVData;
 };
 
 #endif // ! __ImplAAFComponent_h__
