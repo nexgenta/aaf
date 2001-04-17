282a6ab9aae8759733b36a35cd65c4846bb890c1
tbingham <tbingham>
2001-04-17 04:32:06 +0000

    Basic property direct support.

--- ref-impl/src/impl/ImplAAFTypeDefStream.h
+++ ref-impl/src/impl/ImplAAFTypeDefStream.h
@@ -4,32 +4,14 @@
 #define __ImplAAFTypeDefStream_h__
 
 
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- * prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+/******************************************\
+*                                          *
+* Advanced Authoring Format                *
+*                                          *
+* Copyright (c) 1998 Avid Technology, Inc. *
+* Copyright (c) 1998 Microsoft Corporation *
+*                                          *
+\******************************************/
 
 
 
@@ -40,7 +22,6 @@
 #include "ImplAAFTypeDef.h"
 #endif
 
-#include "OMWeakRefProperty.h"
 
 class ImplAAFTypeDefStream : public ImplAAFTypeDef
 {
@@ -62,13 +43,13 @@ public:
   virtual AAFRESULT STDMETHODCALLTYPE
     Initialize
         (// @parm [in] auid to be used to identify this type
-         const aafUID_t & id,
+         aafUID_t *  pID,
 
          // @parm [in] type of each element to be contained in this array
          ImplAAFTypeDef * pTypeDef,
 
          // @parm [in] friendly name of this type definition
-         const aafCharacter * pTypeName);
+         wchar_t *  pTypeName);
 
 
   //****************
@@ -153,43 +134,16 @@ public:
          aafUInt32  numElements);
 
 
-  //*************************************************************
-  //
-  // Overrides from OMType, via inheritace through ImplAAFTypeDef
-  //
-  //*************************************************************
-
-  /*
-  virtual void reorder(OMByte* bytes,
-                       size_t bytesSize) const;
-
-  virtual size_t externalSize(void) const;
-
-  virtual void externalize(OMByte* internalBytes,
-                           size_t internalBytesSize,
-                           OMByte* externalBytes,
-                           size_t externalBytesSize,
-                           OMByteOrder byteOrder) const;
-
-  virtual size_t internalSize(void) const;
-
-  virtual void internalize(OMByte* externalBytes,
-                           size_t externalBytesSize,
-                           OMByte* internalBytes,
-                           size_t internalBytesSize,
-                           OMByteOrder byteOrder) const;
-						   */
-
 public:
-  // Overrides from ImplAAFTypeDef
-  virtual bool IsAggregatable () const;
-  virtual bool IsStreamable () const;
-  virtual bool IsFixedArrayable () const;
-  virtual bool IsVariableArrayable () const;
-  virtual bool IsStringable () const;
+  // Declare this class to be storable.
+  //
+  OMDECLARE_STORABLE(ImplAAFTypeDefStream)
 
-private:
-	OMWeakReferenceProperty<ImplAAFTypeDef>		_elementType;
+  // Declare the module test method. The implementation of the will be be
+  // in /test/ImplAAFTypeDefStreamTest.cpp.
+  static AAFRESULT test();
 };
 
 #endif // ! __ImplAAFTypeDefStream_h__
+
+
