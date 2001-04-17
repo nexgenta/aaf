773f7b73509fdc0105c5ea444db74dc73175c8de
tbingham <tbingham>
2001-04-17 07:20:57 +0000

    Add saveWideString(). Add autoduck comments.

--- ref-impl/src/OM/OMUtilities.cpp
+++ ref-impl/src/OM/OMUtilities.cpp
@@ -1,3 +1,31 @@
+/***********************************************************************
+*
+*              Copyright (c) 1998-1999 Avid Technology, Inc.
+*
+* Permission to use, copy and modify this software and accompanying
+* documentation, and to distribute and sublicense application software
+* incorporating this software for any purpose is hereby granted,
+* provided that (i) the above copyright notice and this permission
+* notice appear in all copies of the software and related documentation,
+* and (ii) the name Avid Technology, Inc. may not be used in any
+* advertising or publicity relating to the software without the specific,
+* prior written permission of Avid Technology, Inc.
+*
+* THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+* WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+* IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+* SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+* OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+* OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+* ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+* RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+* ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+* LIABILITY.
+*
+************************************************************************/
+
+// @doc OMEXTERNAL
 #include "OMUtilities.h"
 
 #include "OMAssertions.h"
@@ -24,6 +52,8 @@ void setProgramName(const char* name)
 
 const char* getProgramName(void)
 {
+  TRACE("getProgramName");
+
   return programName;
 }
 
@@ -45,10 +75,11 @@ OMByteOrder hostByteOrder(void)
   return result;
 }
 
-// Same as strlen(), but for wide characters.
-//
 size_t lengthOfWideString(const wchar_t* string)
 {
+  TRACE("lengthOfWideString");
+
+  PRECONDITION("Valid string", validWideString(string));
   const wchar_t* p = string;
   size_t length = 0;
   while (*p != 0) {
@@ -58,12 +89,16 @@ size_t lengthOfWideString(const wchar_t* string)
   return length;
 }
 
-// Same as strncpy(), but for wide characters.
-//
 wchar_t* copyWideString(wchar_t* destination,
                         const wchar_t* source,
                         const size_t length)
 {
+  TRACE("lengthOfWideString");
+
+  PRECONDITION("Valid destination", destination != 0);
+  PRECONDITION("Valid source string", validWideString(source));
+  PRECONDITION("Valid length", length > 0);
+
   wchar_t* d = destination;
   const wchar_t* s = source;
   size_t i = 0;
@@ -75,6 +110,18 @@ wchar_t* copyWideString(wchar_t* destination,
     *d++ = 0;
   }
   return destination;
+}
+
+wchar_t* saveWideString(const wchar_t* string)
+{
+  TRACE("saveWideString");
+
+  PRECONDITION("Valid string", validWideString(string));
+  size_t length = lengthOfWideString(string) + 1;
+  wchar_t* result = new wchar_t[length];
+  ASSERT("Valid heap pointer", result != 0);
+  copyWideString(result, string, length);
+  return result;
 }
 
 size_t lengthOfOMWideString(const OMWideCharacter* string)
--- ref-impl/src/OM/OMUtilities.h
+++ ref-impl/src/OM/OMUtilities.h
@@ -1,3 +1,31 @@
+/***********************************************************************
+*
+*              Copyright (c) 1998-1999 Avid Technology, Inc.
+*
+* Permission to use, copy and modify this software and accompanying
+* documentation, and to distribute and sublicense application software
+* incorporating this software for any purpose is hereby granted,
+* provided that (i) the above copyright notice and this permission
+* notice appear in all copies of the software and related documentation,
+* and (ii) the name Avid Technology, Inc. may not be used in any
+* advertising or publicity relating to the software without the specific,
+* prior written permission of Avid Technology, Inc.
+*
+* THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+* WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+* IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
+* SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
+* OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
+* OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
+* ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
+* RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
+* ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
+* LIABILITY.
+*
+************************************************************************/
+
+// @doc OMEXTERNAL
 #ifndef OMUTILITIES_H
 #define OMUTILITIES_H
 
@@ -5,17 +33,49 @@
 
 #include <stddef.h>
 
+  // @func Set the name of the program to be use in error messages.
+  //   @parm The program name.
 void setProgramName(const char* name);
 
+  // @func Get the name of the program to be used in error messages.
+  //   @rdesc The program name.
 const char* getProgramName(void);
 
+  // @func Get the byte order used on the host computer.
+  //   @rdesc The host byte order.
 OMByteOrder hostByteOrder(void);
 
+  // @func The length of the wide character string <p string> in characters
+  //       excluding the terminating null character. Same as strlen(),
+  //       but for wide characters.
+  //   @parm The wide character string.
+  //   @rdesc The wide character string length in characters.
 size_t lengthOfWideString(const wchar_t* string);
 
+  // @func Copy a wide character string. Same as strncpy(), but for wide
+  //       characters. Exactly <p length> characters are always written to
+  //       the <p destination> buffer. The <p destination> buffer must be
+  //       at least <p length> characters in size. If the buffer is too
+  //       small this error is not detected. If <p length> is greater than
+  //       the length of <p source> then then <p destination> is padded
+  //       with nulls and <p destination> will be properly null terminated.
+  //       If <p length> is less than the length of <p source> only length
+  //       characters will be copied and <p destination> will not be properly
+  //       null terminated. If the <p source> and <p destination> wide
+  //       character strings overlap this error is not detected,.
+  //   @parm The destination buffer.
+  //   @parm The wide character string to copy.
+  //   @parm The number of characters to copy.
+  //   @rdesc The resulting wide character string.
 wchar_t* copyWideString(wchar_t* destination,
                         const wchar_t* source,
                         const size_t length);
+
+  // @func Save a wide character string. Same as strsave() but for wide
+  //       characters.
+  //   @parm The wide character string to save.
+  //   @rdesc The saved wide character string.
+wchar_t* saveWideString(const wchar_t* string);
 
 size_t lengthOfOMWideString(const OMWideCharacter* string);
 
