f7a06f53e62793f1daf2d2b11627deaa4cc1e620
tbingham <tbingham>
2001-05-23 19:39:56 +0000

    Fix compilation warnings caused by unused arguments.

--- ref-impl/plugins/CAAFCDCICodec.cpp
+++ ref-impl/plugins/CAAFCDCICodec.cpp
@@ -1,29 +1,24 @@
-/***********************************************************************
- *
- *              Copyright (c) 2000 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- * prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+//=---------------------------------------------------------------------=
+//
+// The contents of this file are subject to the AAF SDK Public
+// Source License Agreement (the "License"); You may not use this file
+// except in compliance with the License.  The License is available in
+// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
+// Association or its successor.
+// 
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+// the License for the specific language governing rights and limitations
+// under the License.
+// 
+// The Original Code of this file is Copyright 1998-2001, Licensor of the
+// AAF Association.
+// 
+// The Initial Developer of the Original Code of this file and the
+// Licensor of the AAF Association is Avid Technology.
+// All rights reserved.
+//
+//=---------------------------------------------------------------------=
 #include "CAAFCDCICodec.h"
 
 #include <assert.h>
@@ -213,7 +208,7 @@ HRESULT STDMETHODCALLTYPE
 }
 
 HRESULT STDMETHODCALLTYPE
-    CAAFCDCICodec::GetIndexedDefinitionID (aafUInt32 index, aafUID_t *uid)
+    CAAFCDCICodec::GetIndexedDefinitionID (aafUInt32 /* index */, aafUID_t *uid)
 {
 	if(NULL == uid)
 		return AAFRESULT_NULL_PARAM;
@@ -516,7 +511,7 @@ HRESULT STDMETHODCALLTYPE
 }
 
 HRESULT STDMETHODCALLTYPE
-    CAAFCDCICodec::GetIndexedFlavourID (aafUInt32  index,
+    CAAFCDCICodec::GetIndexedFlavourID (aafUInt32  /* index */,
         aafUID_t *  pFlavour)
 {
 	if(pFlavour == NULL)
@@ -539,7 +534,7 @@ HRESULT STDMETHODCALLTYPE
 }
 
 HRESULT STDMETHODCALLTYPE
-    CAAFCDCICodec::GetIndexedDataDefinition (aafUInt32  index,
+    CAAFCDCICodec::GetIndexedDataDefinition (aafUInt32  /* index */,
         aafUID_t * pFlavour)
 {
 	if (NULL == pFlavour)
@@ -561,7 +556,7 @@ HRESULT STDMETHODCALLTYPE
 }	
 
 HRESULT STDMETHODCALLTYPE
-    CAAFCDCICodec::GetCodecDisplayName (aafUID_constref flavour,
+    CAAFCDCICodec::GetCodecDisplayName (aafUID_constref /* flavour */,
         aafCharacter *  pName,
         aafUInt32  bufSize)
 {
@@ -738,9 +733,9 @@ HRESULT STDMETHODCALLTYPE
 HRESULT STDMETHODCALLTYPE
     CAAFCDCICodec::ValidateEssence (IAAFSourceMob *fileMob,
         IAAFEssenceStream *stream,
-		aafCheckVerbose_t  verbose,
-        aafCheckWarnings_t warning,
-         aafUInt32  bufSize,
+		aafCheckVerbose_t  /* verbose */,
+        aafCheckWarnings_t /* warning */,
+         aafUInt32  /* bufSize */,
 		wchar_t *  pName,
         aafUInt32  *bytesWritten)
 {
@@ -769,7 +764,7 @@ CAAFCDCICodec::Create (IAAFSourceMob *p_srcmob,
   aafUID_constref essenceKind,
   aafRational_constref sampleRate,
   IAAFEssenceStream * stream,
-  aafCompressEnable_t compEnable)
+  aafCompressEnable_t /* compEnable */)
 {
     HRESULT	    hr = S_OK;
 
@@ -838,7 +833,7 @@ HRESULT STDMETHODCALLTYPE
     CAAFCDCICodec::Open (IAAFSourceMob *p_srcmob,
 		aafMediaOpenMode_t  openMode,
     IAAFEssenceStream * stream,
-    aafCompressEnable_t compEnable)
+    aafCompressEnable_t /* compEnable */)
 {
     HRESULT hr = S_OK;
 
--- ref-impl/plugins/CAAFEssenceFileContainer.cpp
+++ ref-impl/plugins/CAAFEssenceFileContainer.cpp
@@ -1,34 +1,30 @@
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- * prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+//=---------------------------------------------------------------------=
+//
+// The contents of this file are subject to the AAF SDK Public
+// Source License Agreement (the "License"); You may not use this file
+// except in compliance with the License.  The License is available in
+// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
+// Association or its successor.
+// 
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+// the License for the specific language governing rights and limitations
+// under the License.
+// 
+// The Original Code of this file is Copyright 1998-2001, Licensor of the
+// AAF Association.
+// 
+// The Initial Developer of the Original Code of this file and the
+// Licensor of the AAF Association is Avid Technology.
+// All rights reserved.
+//
+//=---------------------------------------------------------------------=
 
 #include "CAAFEssenceFileContainer.h"
 #include "CAAFEssenceFileStream.h"
 
 #include <assert.h>
+#include <string.h>
 #include "AAFResult.h"
 #include "AAFDefUIDs.h"
 #include "aafErr.h"
@@ -36,10 +32,7 @@
 #include "AAFContainerDefs.h"
 
 #include <errno.h>
-
-#if defined(_MAC) || defined(macintosh)
-#include <wstring.h>
-#endif
+#include <wchar.h>
 
 const aafUID_t  EXAMPLE_FILE_PLUGIN =	{ 0x914B3AD1, 0xEDE7, 0x11d2, { 0x80, 0x9F, 0x00, 0x60, 0x08, 0x14, 0x3e, 0x6f } };
 
@@ -151,7 +144,7 @@ HRESULT STDMETHODCALLTYPE
 }
 
 HRESULT STDMETHODCALLTYPE
-    CAAFEssenceFileContainer::GetIndexedDefinitionID (aafUInt32 index, aafUID_t *uid)
+    CAAFEssenceFileContainer::GetIndexedDefinitionID (aafUInt32 /* index */, aafUID_t *uid)
 {
 	if(uid == NULL)
 		return AAFRESULT_NULL_PARAM;
@@ -169,7 +162,7 @@ HRESULT STDMETHODCALLTYPE
 
 
 HRESULT STDMETHODCALLTYPE
-    CAAFEssenceFileContainer::GetIndexedDefinitionObject (aafUInt32 index, IAAFDictionary *dict, IAAFDefObject **def)
+    CAAFEssenceFileContainer::GetIndexedDefinitionObject (aafUInt32 /* index */, IAAFDictionary *dict, IAAFDefObject **def)
 {
 	aafUID_t			uid;
 	IAAFContainerDef	*container = NULL;
@@ -341,9 +334,9 @@ HRESULT STDMETHODCALLTYPE
 
 
 HRESULT STDMETHODCALLTYPE
-    CAAFEssenceFileContainer::CreateEssenceStreamWriteOnly (const aafCharacter * pName,
-        aafMobID_constptr pMobID,
-        IAAFEssenceStream ** ppEssenceStream)
+    CAAFEssenceFileContainer::CreateEssenceStreamWriteOnly (const aafCharacter * /* pName */,
+        aafMobID_constptr /* pMobID */,
+        IAAFEssenceStream ** /* ppEssenceStream */)
 {
   return HRESULT_NOT_IMPLEMENTED;
 }
@@ -436,6 +429,10 @@ HRESULT STDMETHODCALLTYPE
 //
 // 
 // 
+inline int EQUAL_UID(const GUID & a, const GUID & b)
+{
+  return (0 == memcmp((&a), (&b), sizeof (aafUID_t)));
+}
 HRESULT CAAFEssenceFileContainer::InternalQueryInterface
 (
     REFIID riid,
@@ -447,14 +444,14 @@ HRESULT CAAFEssenceFileContainer::InternalQueryInterface
         return E_INVALIDARG;
 
     // We support the IAAFEssenceContainer interface 
-    if (riid == IID_IAAFEssenceContainer) 
+    if (EQUAL_UID(riid,IID_IAAFEssenceContainer)) 
     { 
         *ppvObj = (IAAFEssenceContainer *)this; 
         ((IUnknown *)*ppvObj)->AddRef();
         return S_OK;
     }
     // and the IAAFPlugin interface
-    else if (riid == IID_IAAFPlugin) 
+    else if (EQUAL_UID(riid,IID_IAAFPlugin)) 
     { 
         *ppvObj = (IAAFPlugin *)this; 
         ((IUnknown *)*ppvObj)->AddRef();
