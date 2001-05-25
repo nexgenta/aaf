44f40287cf5c8ba43dab12c5f52e8fc9a8cb5563
tbingham <tbingham>
2001-05-25 17:25:23 +0000

    Tone down the output of make by applying a couple of @s.

--- build/rules.mk
+++ build/rules.mk
@@ -1,5 +1,31 @@
+###############################################################################
+#
+# The contents of this file are subject to the AAF SDK Public
+# Source License Agreement (the "License"); You may not use this file
+# except in compliance with the License.  The License is available in
+# AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
+# Association or its successor.
+# 
+# Software distributed under the License is distributed on an "AS IS"
+# basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+# the License for the specific language governing rights and limitations
+# under the License.
+# 
+# The Original Code of this file is Copyright 1998-2001, Licensor of the
+# AAF Association.
+# 
+# The Initial Developer of the Original Code of this file and the
+# Licensor of the AAF Association is Avid Technology.
+# All rights reserved.
+#
+###############################################################################
+
 #------------------------------------------------------------------------------
+#
+# rules.mk
+#
 # This file contains makefile rules necessary to build the AAF toolkit.
+#
 #------------------------------------------------------------------------------
 
 #------------------------------------------------------------------------------
@@ -18,11 +44,19 @@ $(OBJDIR)/%$(OBJ): %.$(CPP_EXTENSION)
 # files.
 #------------------------------------------------------------------------------
 
-$(OBJDIR)/%.d : %.$(CPP_EXTENSION)
-	$(MKDIRCMD) $(OBJDIR);$(DFILECMD)
+$(OBJDIR)/%.d : %.c 
+	@echo Generating dependencies for $<; \
+	$(SHELL) -ec 'if [ ! -d $(OBJDIR) ]; then $(MKDIR) $(OBJDIR); fi; \
+	$(CC) -M $(CFLAGS) $(INCLUDES) $< \
+	| sed '\''s,\($(*F)\)\.o[ :]*,$(@D)/\1.o $@ : ,g'\'' > $@; \
+	[ -s $@ ] || rm -f $@'
 
-$(OBJDIR)/%.d : %.c
-	$(MKDIRCMD) $(OBJDIR);$(DFILECMD)
+$(OBJDIR)/%.d : %.$(CPP_EXTENSION)
+	@echo Generating dependencies for $<; \
+	$(SHELL) -ec 'if [ ! -d $(OBJDIR) ]; then $(MKDIR) $(OBJDIR); fi; \
+	$(CC) -M $(CFLAGS) $(INCLUDES) $< \
+	| sed '\''s,\($(*F)\)\.o[ :]*,$(@D)/\1.o $@ : ,g'\'' > $@; \
+	[ -s $@ ] || rm -f $@'
 
 
 #------------------------------------------------------------------------------
@@ -42,6 +76,10 @@ $(BINDIR):
 #------------------------------------------------------------------------------
 # Now includes the header file dependencies makefiles.
 #------------------------------------------------------------------------------
+
+ifneq ($(GENDEPS), 0)
+-include $(DEPS)
+endif
 
 # Disable header dependencies for now, because they make the build process 
 # approximately 10^64 times slower.
