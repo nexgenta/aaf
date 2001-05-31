cf57be277f9067296a1cc6a8d4b6cbc3871cf178
tbingham <tbingham>
2001-05-31 01:36:49 +0000

    Don't call getProgramName().

--- ref-impl/src/impl/ImplAAFTypeDefIndirect.cpp
+++ ref-impl/src/impl/ImplAAFTypeDefIndirect.cpp
@@ -1,29 +1,24 @@
-/***********************************************************************
-*
-*              Copyright (c) 1998-2000 Avid Technology, Inc.
-*
-* Permission to use, copy and modify this software and accompanying
-* documentation, and to distribute and sublicense application software
-* incorporating this software for any purpose is hereby granted,
-* provided that (i) the above copyright notice and this permission
-* notice appear in all copies of the software and related documentation,
-* and (ii) the name Avid Technology, Inc. may not be used in any
-* advertising or publicity relating to the software without the specific,
-*  prior written permission of Avid Technology, Inc.
-*
-* THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
-* EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
-* WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
-* IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
-* SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
-* OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
-* OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
-* ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
-* RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
-* ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
-* LIABILITY.
-*
-************************************************************************/
+//=---------------------------------------------------------------------=
+//
+// The contents of this file are subject to the AAF SDK Public
+// Source License Agreement (the "License"); You may not use this file
+// except in compliance with the License.  The License is available in
+// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
+// Association or its successor.
+// 
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+// the License for the specific language governing rights and limitations
+// under the License.
+// 
+// The Original Code of this file is Copyright 1998-2001, Licensor of the
+// AAF Association.
+// 
+// The Initial Developer of the Original Code of this file and the
+// Licensor of the AAF Association is Avid Technology.
+// All rights reserved.
+//
+//=---------------------------------------------------------------------=
 
 #ifndef __AAFTypes_h__
 #include "AAFTypes.h"
@@ -126,7 +121,6 @@ static void formatError(DWORD errorCode)
 static void checkResult(AAFRESULT resultCode)
 {
   TRACE("checkFile");
-  ASSERT("Valid program name", validString(getProgramName()));
 
   if (AAFRESULT_FAILED(resultCode)) {
     formatError(resultCode);
@@ -785,6 +779,9 @@ AAFRESULT STDMETHODCALLTYPE
   if (!supportedActualType (pActualType))
     return AAFRESULT_INVALID_PARAM;
 
+  // Any padding and offsets must be registered to use this method.
+  if (!pActualType->IsRegistered())
+    return AAFRESULT_NOT_REGISTERED;
 
   // If this type is registered then this type's id will be written into the
   // new indirect type value (*pPropVal).
@@ -1129,6 +1126,11 @@ AAFRESULT STDMETHODCALLTYPE
                                  &pActualType, &actualValueDataSize);
   if (AAFRESULT_FAILED(result))
     return result;
+
+
+  // Any padding and offsets must be registered to use this method.
+  if (!pActualType->IsRegistered())
+    return AAFRESULT_NOT_REGISTERED;
 
   //
   // Now we are ready to copy the bits from remaining bits of the
