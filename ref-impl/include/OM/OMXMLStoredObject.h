ab739200690c733d9f18fe7340389113edfafbef
tbingham <tbingham>
2001-07-12 18:35:36 +0000

    Prototype XML support

--- ref-impl/include/OM/OMXMLStoredObject.h
+++ ref-impl/include/OM/OMXMLStoredObject.h
@@ -1,29 +1,24 @@
-/***********************************************************************
-*
-*              Copyright (c) 1998-2000 Avid Technology, Inc.
-*
-* Permission to use, copy and modify this software and accompanying
-* documentation, and to distribute and sublicense application software
-* incorporating this software for any purpose is hereby granted,
-* provided that (i) the above copyright notice and this permission
-* notice appear in all copies of the software and related documentation,
-* and (ii) the name Avid Technology, Inc. may not be used in any
-* advertising or publicity relating to the software without the specific,
-* prior written permission of Avid Technology, Inc.
-*
-* THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
-* EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
-* WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
-* IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
-* SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
-* OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
-* OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
-* ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
-* RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
-* ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
-* LIABILITY.
-*
-************************************************************************/
+//=---------------------------------------------------------------------=
+//
+// The contents of this file are subject to the AAF SDK Public
+// Source License Agreement (the "License"); You may not use this file
+// except in compliance with the License.  The License is available in
+// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
+// Association or its successor.
+// 
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+// the License for the specific language governing rights and limitations
+// under the License.
+// 
+// The Original Code of this file is Copyright 1998-2001, Licensor of the
+// AAF Association.
+// 
+// The Initial Developer of the Original Code of this file and the
+// Licensor of the AAF Association is Avid Technology.
+// All rights reserved.
+//
+//=---------------------------------------------------------------------=
 
 // @doc OMINTERNAL
 #ifndef OMXMLSTOREDOBJECT_H
@@ -32,6 +27,7 @@
 #include "OMStoredObject.h"
 #include "OMDataTypes.h"
 #include "OMFile.h"
+#include "OMIOStream.h"
 
 class OMSimpleProperty;
 class OMDataStream;
@@ -57,6 +53,12 @@ public:
     // @cmember Open the root <c OMXMLStoredObject> in the raw storage
     //          <p rawStorage> for modification.
   static OMXMLStoredObject* openModify(OMRawStorage* rawStorage);
+
+    // @cmember Create a new root <c OMXMLStoredObject> in the raw storage
+    //          <p rawStorage>. The byte order of the newly created root
+    //          is given by <p byteOrder>.
+  static OMXMLStoredObject* createWrite(OMRawStorage* rawStorage,
+                                        const OMByteOrder byteOrder);
 
     // @cmember Create a new root <c OMXMLStoredObject> in the raw storage
     //          <p rawStorage>. The byte order of the newly created root
@@ -212,9 +214,34 @@ private:
   // @access Private members.
 
     // @cmember Constructor.
-  OMXMLStoredObject(OMRawStorage* s);
+  OMXMLStoredObject(OMRawStorage* s, const OMByteOrder byteOrder);
 
-  OMRawStorage* _store;
+  OMRawStorage& _store;
+  OMIOStream _stream;
+  OMByteOrder _byteOrder;
+
+  static OMUInt16 _seed; // For use during save()
+
+  // Binary data formatting
+
+  void output(void);
+
+    // Dump a single byte.
+    //
+  void print(unsigned char ch);
+
+    // Flush any pending dumped bytes.
+    //
+  void flush(void);
+
+  static char _table[128];
+
+  char map(int c);
+
+  enum {BYTESPERLINE  = 16}; // Number of dumped bytes to put on a line.
+  unsigned char _buffer[BYTESPERLINE];
+  int _count;
+  int _line;
 
 };
 
