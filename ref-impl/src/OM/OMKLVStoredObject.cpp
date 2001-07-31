e837ba318d90ecd2f5c93895e4aa00a8a4b240a8
tbingham <tbingham>
2001-07-31 14:41:06 +0000

    Add stubs for OM{MSS|KLV|XML}StoredObject::isRecognized().

--- ref-impl/src/OM/OMKLVStoredObject.cpp
+++ ref-impl/src/OM/OMKLVStoredObject.cpp
@@ -1,29 +1,24 @@
-/***********************************************************************
-*
-*              Copyright (c) 1998-2000 Avid Technology, Inc.
-*
-* Permission to use, copy and modify this software and accompanying
-* documentation, and to distribute and sublicense application software
-* incorporating this software for any purpose is hereby granted,
-* provided that (i) the above copyright notice and this permission
-* notice appear in all copies of the software and related documentation,
-* and (ii) the name Avid Technology, Inc. may not be used in any
-* advertising or publicity relating to the software without the specific,
-* prior written permission of Avid Technology, Inc.
-*
-* THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
-* EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
-* WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
-* IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
-* SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
-* OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
-* OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
-* ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
-* RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
-* ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
-* LIABILITY.
-*
-************************************************************************/
+//=---------------------------------------------------------------------=
+//
+// The contents of this file are subject to the AAF SDK Public
+// Source License Agreement (the "License"); You may not use this file
+// except in compliance with the License.  The License is available in
+// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
+// Association or its successor.
+// 
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+// the License for the specific language governing rights and limitations
+// under the License.
+// 
+// The Original Code of this file is Copyright 1998-2001, Licensor of the
+// AAF Association.
+// 
+// The Initial Developer of the Original Code of this file and the
+// Licensor of the AAF Association is Avid Technology.
+// All rights reserved.
+//
+//=---------------------------------------------------------------------=
 
 // @doc OMINTERNAL
 #include "OMKLVStoredObject.h"
@@ -88,6 +83,49 @@ OMKLVStoredObject* OMKLVStoredObject::createModify(OMRawStorage* rawStorage,
   PRECONDITION("Compatible raw storage", rawStorage->isPositionable());
   OMKLVStoredObject* result= new OMKLVStoredObject(rawStorage, byteOrder);
   ASSERT("Valid heap pointer", result != 0);
+  return result;
+}
+
+  // @mfunc Is the file named <p fileName> a recognized file ?
+  //        If so, the result is true, and the signature is returned
+  //        in <p signature>.
+  //   @parm The name of the file to check.
+  //   @parm If recognized, the file signature.
+  //   @rdesc True if the file is recognized, false otherwise.
+bool OMKLVStoredObject::isRecognized(const wchar_t* /* fileName */,
+                                     OMFileSignature& /* signature */)
+{
+  bool result = false;
+  TRACE("OMKLVStoredObject::isRecognized");
+  ASSERT("Unimplemented code not reached", false);
+  return result;
+}
+
+  // @mfunc Does <p rawStorage> contain a recognized file ?
+  //        If so, the result is true, and the signature is returned
+  //        in <p signature>.
+  //   @parm The <c OMRawStorage> to check.
+  //   @parm If recognized, the file signature.
+  //   @rdesc True if the <c OMRawStorage> contains a recognized
+  //          file, false otherwise.
+bool OMKLVStoredObject::isRecognized(OMRawStorage* /* rawStorage */,
+                                     OMFileSignature& /* signature */)
+{
+  bool result = false;
+  TRACE("OMKLVStoredObject::isRecognized");
+  ASSERT("Unimplemented code not reached", false);
+  return result;
+}
+
+  // @mfunc Is <p signature> recognized ?
+  //        If so, the result is true, and the encoding in <p encoding>.
+  //   @parm The signature to check.
+  //   @rdesc True if the signature is recognized, false otherwise.
+bool OMKLVStoredObject::isRecognized(const OMFileSignature& /* signature */)
+{
+  bool result = false;
+  TRACE("OMKLVStoredObject::isRecognized");
+  ASSERT("Unimplemented code not reached", false);
   return result;
 }
 
