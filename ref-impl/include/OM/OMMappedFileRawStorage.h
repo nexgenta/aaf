0eb18ea183262752d7003d1bfd7e1a41484ed453
tbingham <tbingham>
2001-08-01 16:57:40 +0000

    Make position() and setPosition() public.

--- ref-impl/include/OM/OMMappedFileRawStorage.h
+++ ref-impl/include/OM/OMMappedFileRawStorage.h
@@ -1,29 +1,24 @@
-/***********************************************************************
-*
-*              Copyright (c) 1998-2000 Avid Technology, Inc.
-*
-* Permission to use, copy and modify this software and accompanying
-* documentation, and to distribute and sublicense application software
-* incorporating this software for any purpose is hereby granted,
-* provided that (i) the above copyright notice and this permission
-* notice appear in all copies of the software and related documentation,
-* and (ii) the name Avid Technology, Inc. may not be used in any
-* advertising or publicity relating to the software without the specific,
-* prior written permission of Avid Technology, Inc.
-*
-* THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
-* EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
-* WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
-* IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
-* SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
-* OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
-* OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
-* ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
-* RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
-* ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
-* LIABILITY.
-*
-************************************************************************/
+//=---------------------------------------------------------------------=
+//
+// The contents of this file are subject to the AAF SDK Public
+// Source License Agreement (the "License"); You may not use this file
+// except in compliance with the License.  The License is available in
+// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
+// Association or its successor.
+// 
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+// the License for the specific language governing rights and limitations
+// under the License.
+// 
+// The Original Code of this file is Copyright 1998-2001, Licensor of the
+// AAF Association.
+// 
+// The Initial Developer of the Original Code of this file and the
+// Licensor of the AAF Association is Avid Technology.
+// All rights reserved.
+//
+//=---------------------------------------------------------------------=
 
 // @doc OMINTERNAL
 #ifndef OMMAPPEDFILERAWSTORAGE_H
@@ -41,7 +36,8 @@
   //        This is an Object Manager built-in implementation of the
   //        <c OMRawStorage> interface.
   //
-  //   @base public | OMRawStorage
+  //   @base public | <c OMRawStorage>
+  //   @cauthor Tim Bingham | tjb | Avid Technology, Inc.
 class OMMappedFileRawStorage : public OMRawStorage {
 public:
   // @access Static members.
@@ -65,17 +61,11 @@ public:
 
   // @access Public members.
 
-    // @cmember Constructor.
-  OMMappedFileRawStorage(/* TBS */ OMFile::OMAccessMode accessMode);
-
     // @cmember Destructor.
   virtual ~OMMappedFileRawStorage(void);
 
     // @cmember Is it possible to read from this <c OMMappedFileRawStorage> ?
   virtual bool isReadable(void) const;
-
-    // @cmember Is it possible to write to this <c OMMappedFileRawStorage> ?
-  virtual bool isWritable(void) const;
 
     // @cmember Attempt to read the number of bytes given by <p byteCount>
     //          from the current position in this <c OMMappedFileRawStorage>
@@ -89,6 +79,24 @@ public:
   virtual void read(OMByte* bytes,
                     OMUInt32 byteCount,
                     OMUInt32& bytesRead) const;
+
+    // @cmember Attempt to read the number of bytes given by <p byteCount>
+    //          from offset <p position> in this <c OMMappedFileRawStorage>
+    //          into the buffer at address <p bytes>.
+    //          The actual number of bytes read is returned in <p bytesRead>.
+    //          Reading from positions greater than
+    //          <mf OMMappedFileRawStorage::size> causes <p bytesRead>
+    //          to be less than <p byteCount>.
+    //          Reading bytes that have never been written returns undefined
+    //          data in <p bytes>.
+    //          @precondition <f isReadable()> && <f isPositionable()>
+  virtual void readAt(OMUInt64 position,
+                      OMByte* bytes,
+                      OMUInt32 byteCount,
+                      OMUInt32& bytesRead) const;
+
+    // @cmember Is it possible to write to this <c OMMappedFileRawStorage> ?
+  virtual bool isWritable(void) const;
 
     // @cmember Attempt to write the number of bytes given by <p byteCount>
     //          to the current position in this <c OMMappedFileRawStorage>
@@ -104,12 +112,29 @@ public:
                      OMUInt32 byteCount,
                      OMUInt32& bytesWritten);
 
-    // @cmember May this <c OMMappedFileRawStorage> be changed in size ?
-  virtual bool isSizeable(void) const;
+    // @cmember Attempt to write the number of bytes given by <p byteCount>
+    //          to offset <p position> in this <c OMMappedFileRawStorage>
+    //          from the buffer at address <p bytes>.
+    //          The actual number of bytes written is returned in
+    //          <p bytesWritten>.
+    //          Writing to positions greater than
+    //          <mf OMMappedFileRawStorage::size> causes this
+    //          <c OMMappedFileRawStorage> to be extended, however such
+    //          extension can fail, causing <p bytesWritten> to be less than
+    //          <p byteCount>.
+    //          @precondition <f isWritable()> && <f isPositionable()>
+    //   @devnote How is failure to extend indicated ?
+  virtual void writeAt(OMUInt64 position,
+                       const OMByte* bytes,
+                       OMUInt32 byteCount,
+                       OMUInt32& bytesWritten);
 
-    // @cmember The current size of this <c OMMappedFileRawStorage> in bytes.
-    //          precondition - isSizeable()
-  virtual OMUInt64 size(void) const;
+    // @cmember May this <c OMMappedFileRawStorage> be changed in size ?
+  virtual bool isExtendible(void) const;
+
+    // @cmember The current extent of this <c OMMappedFileRawStorage> in bytes.
+    //          precondition - isPositionable()
+  virtual OMUInt64 extent(void) const;
 
     // @cmember Set the size of this <c OMMappedFileRawStorage> to <p newSize>
     //          bytes. If <p newSize> is greater than
@@ -120,8 +145,12 @@ public:
     //          Truncation may also result in the current position for
     //          <f read()> and <f write()> being set to
     //          <mf OMMappedFileRawStorage::size>.
-    //          precondition - isSizeable()
-  virtual void setSize(OMUInt64 newSize);
+    //          precondition - isExtendible()
+  virtual void extend(OMUInt64 newSize);
+
+    // @cmember The current size of this <c OMMappedFileRawStorage> in bytes.
+    //          precondition - isPositionable()
+  virtual OMUInt64 size(void) const;
 
     // @cmember May the current position, for <f read()> and <f write()>,
     //          of this <c OMMappedFileRawStorage> be changed ?
@@ -137,11 +166,17 @@ public:
     //          offset in bytes from the beginning of this
     //          <c OMMappedFileRawStorage>.
     //          precondition - isPositionable()
-  virtual void setPosition(OMUInt64 newPosition);
+  virtual void setPosition(OMUInt64 newPosition) const;
 
     // @cmember Synchronize this <c OMMappedFileRawStorage> with its external
     //          representation.
   virtual void synchronize(void);
+
+private:
+  // @access Private members.
+
+    // @cmember none | OMMappedFileRawStorage | accessMode | Constructor.
+  OMMappedFileRawStorage(/* TBS */ OMFile::OMAccessMode accessMode);
 
 };
 
