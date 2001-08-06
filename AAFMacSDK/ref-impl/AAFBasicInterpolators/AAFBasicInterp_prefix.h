e8cb3d5708fd8ea214c2083a54365a58aae9ecb2
whoopy <whoopy>
2001-08-06 11:34:44 +0000

    Migrate to CW6: Prefix files for Shared Libraries now need  _MSL_IMP_EXP __declspec(dllimport) and _MSL_NO_LOCALE

--- AAFMacSDK/ref-impl/AAFBasicInterpolators/AAFBasicInterp_prefix.h
+++ AAFMacSDK/ref-impl/AAFBasicInterpolators/AAFBasicInterp_prefix.h
@@ -1,4 +1,25 @@
-// >>> © 1998 Avid Technology, Inc.  All rights reserved. <<<
+//=---------------------------------------------------------------------=
+//
+// The contents of this file are subject to the AAF SDK Public
+// Source License Agreement (the "License"); You may not use this file
+// except in compliance with the License.  The License is available in
+// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
+// Association or its successor.
+//
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+// the License for the specific language governing rights and limitations
+// under the License.
+//
+// The Original Code of this file is Copyright 1998-2001, Licensor of the
+// AAF Association.
+//
+// The Initial Developer of the Original Code of this file and the
+// Licensor of the AAF Association is Avid Technology.
+// All rights reserved.
+//
+//=---------------------------------------------------------------------=
+
 #define __dest_os	__mac_os
 
 //#define CHECKMEMORYLEAKS // Uncomment this line to turn leak checking on
@@ -6,6 +27,12 @@
 // Symbols needed for OLE/ActiveX headers.
 #define _MAC
 #define NONAMELESSUNION
+
+#if (__MSL_CPP__ >= 0x6007)
+// required for CW6 ShLib (DLL) targets
+#define _MSL_IMP_EXP __declspec(dllimport)
+#define _MSL_NO_LOCALE
+#endif
 
 // We need to include some of the mac headers before the ole/ActiveX headers
 // so that certain include guards will be properly defined. This will keep
