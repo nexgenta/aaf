39993038f057de1130e19fbb099ddb6ec2acf601
whoopy <whoopy>
2001-08-06 12:55:14 +0000

    Migrate to CW6: Shared libs now require _MSL_IMP_EXP __declspec(dllimport) and _MSL_NO_LOCALE

--- AAFMacSDK/ref-impl/om/AAFOM_prefix.h
+++ AAFMacSDK/ref-impl/om/AAFOM_prefix.h
@@ -1,5 +1,25 @@
+//=---------------------------------------------------------------------=
+//
+// The contents of this file are subject to the AAF SDK Public
+// Source License Agreement (the "License"); You may not use this file
+// except in compliance with the License.  The License is available in
+// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
+// Association or its successor.
+//
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+// the License for the specific language governing rights and limitations
+// under the License.
+//
+// The Original Code of this file is Copyright 1998-2001, Licensor of the
+// AAF Association.
+//
+// The Initial Developer of the Original Code of this file and the
+// Licensor of the AAF Association is Avid Technology.
+// All rights reserved.
+//
+//=---------------------------------------------------------------------=
 
-// >>> © 1998 Avid Technology, Inc.  All rights reserved. <<<
 #define __dest_os	__mac_os
 
 #define _MSL_USING_NAMESPACE
@@ -9,17 +29,22 @@
 #define _MAC
 #define NONAMELESSUNION
 
+#if (__MSL_CPP__ >= 0x6007)
+// required for CW6 ShLib (DLL) targets
+#define _MSL_IMP_EXP __declspec(dllimport)
+#define _MSL_NO_LOCALE
+#endif
+
 #include "MacHeaders.h"
 
 
-// Define to enable the module testing of the shared library.
-#define AAF_MODULE_TEST
-//#ifndef _DEBUG
-//#define _DEBUG
-//#endif
+// Conditional debugging and ansi assertions disabled
+#ifndef NDEBUG
+#define NDEBUG
+#endif
 
 // Enable PRECONDITION, POSTCONDITION, ASSERT, INVARIANT and IMPLIES macros.
-#define OM_ENABLE_DEBUG
+//#define OM_ENABLE_DEBUG
 
 // Uncomment the following line if you want to see an exhausted list
 // of the calling sequence within the Object Manager.
