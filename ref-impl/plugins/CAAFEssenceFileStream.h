0bf8a4ceaf897de5c3bc7cabd9bce9b0b3307736
whoopy <whoopy>
2001-12-26 21:28:38 +0000

    Fix GLibC 2.2 fpos_t problem by using off_t and platform specific 64bit seeks

--- ref-impl/plugins/CAAFEssenceFileStream.h
+++ ref-impl/plugins/CAAFEssenceFileStream.h
@@ -3,32 +3,27 @@
 #ifndef __CAAFEssenceFileStream_h__
 #define __CAAFEssenceFileStream_h__
 
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- * prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+//=---------------------------------------------------------------------=
+//
+// The contents of this file are subject to the AAF SDK Public
+// Source License Agreement (the "License"); You may not use this file
+// except in compliance with the License.  The License is available in
+// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
+// Association or its successor.
+// 
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+// the License for the specific language governing rights and limitations
+// under the License.
+// 
+// The Original Code of this file is Copyright 1998-2001, Licensor of the
+// AAF Association.
+// 
+// The Initial Developer of the Original Code of this file and the
+// Licensor of the AAF Association is Avid Technology.
+// All rights reserved.
+//
+//=---------------------------------------------------------------------=
 
 
 
@@ -78,9 +73,10 @@ public:
   //
 
   // Write some number of bytes to the stream exactly and with no formatting or compression.
-  STDMETHOD (Write)
-    (/*[in,size_is(buflen)]*/ aafDataBuffer_t  buffer, // to a buffer
-     /*[in]*/ aafInt32  buflen); // of this size 
+  STDMETHOD(Write)
+    (/*[in]*/ aafUInt32  bytes, // write this many bytes
+     /*[out, size_is(bytes), length_is(*bytesWritten)]*/ aafDataBuffer_t  buffer, // here is the buffer
+     /*[out,ref]*/ aafUInt32 *  bytesWritten); // return bytes actually written.
 
   // Read some number of bytes from the stream exactly and with no formatting or compression.
   STDMETHOD (Read)
@@ -90,24 +86,24 @@ public:
 
   // Seek to the absolute byte offset into the stream.
   STDMETHOD (Seek)
-    (/*[in]*/ aafInt64  byteOffset); // The absolute byte offset into the stream. 
+    (/*[in]*/ aafPosition_t  byteOffset); // The absolute byte offset into the stream. 
 
   // Seek forward or backward the given byte count.
   STDMETHOD (SeekRelative)
     (/*[in]*/ aafInt32  byteOffset); // The relative byte offset into the stream. 
 
-  // Returns AAFTrue if the byte offset is within the stream.
+  // Returns kAAFTrue if the byte offset is within the stream.
   STDMETHOD (IsPosValid)
-    (/*[in]*/ aafInt64  byteOffset, // The absolute byte offset into the stream.
-     /*[out]*/ aafBool *  isValid); // The result. 
+    (/*[in]*/ aafPosition_t  byteOffset, // The absolute byte offset into the stream.
+     /*[out]*/ aafBoolean_t *  isValid); // The result. 
 
   // Returns the position within the stream.
   STDMETHOD (GetPosition)
-    (/*[out]*/ aafInt64 *  position); // The position within the stream. 
+    (/*[out]*/ aafPosition_t *  position); // The position within the stream. 
 
   // Returns the length of the stream.
   STDMETHOD (GetLength)
-    (/*[out]*/ aafInt64 *  position); // The length of the stream. 
+    (/*[out]*/ aafLength_t *  position); // The length of the stream. 
 
   // Ensure that all bits are written.
   STDMETHOD (FlushCache)
@@ -117,7 +113,7 @@ public:
   // Sets the size of the cache buffer used for further operations.
 			// Destroys the current contents of the cache.
   STDMETHOD (SetCacheSize)
-    (/*[in]*/ aafInt32  itsSize); // The size of the cache buffer. 
+    (/*[in]*/ aafUInt32  itsSize); // The size of the cache buffer. 
 
 
 
@@ -269,7 +265,7 @@ public :
   FileStreamOp _lastStreamOp;
   //
   // Cached eof position for openRead and openAppend modes.
-  fpos_t _startingEOF;
+  aafPosition_t _startingEOF;
 };
 
 #endif // ! __CAAFEssenceFileStream_h__
