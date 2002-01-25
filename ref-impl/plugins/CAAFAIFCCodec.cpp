0e3990ef0a665e276ff482e00bd4fa9ddc138bb0
jptrainor <jptrainor>
2002-01-25 15:07:21 +0000

    Return AAFRESULT_EOF in WriteSamples if essence size will exceed 2GB.

--- ref-impl/plugins/CAAFAIFCCodec.cpp
+++ ref-impl/plugins/CAAFAIFCCodec.cpp
@@ -1,29 +1,24 @@
-/***********************************************************************
-*
-*              Copyright (c) 1998-1999 Avid Technology, Inc.
-*
-* Permission to use, copy and modify this software and accompanying 
-* documentation, and to distribute and sublicense application software
-* incorporating this software for any purpose is hereby granted, 
-* provided that (i) the above copyright notice and this permission
-* notice appear in all copies of the software and related documentation,
-* and (ii) the name Avid Technology, Inc. may not be used in any
-* advertising or publicity relating to the software without the specific,
-* prior written permission of Avid Technology, Inc.
-*
-* THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
-* EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
-* WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
-* IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
-* SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
-* OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
-* OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
-* ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
-* RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
-* ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
-* LIABILITY.
-*
-************************************************************************/
+//=---------------------------------------------------------------------=
+//
+// The contents of this file are subject to the AAF SDK Public
+// Source License Agreement (the "License"); You may not use this file
+// except in compliance with the License.  The License is available in
+// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
+// Association or its successor.
+// 
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+// the License for the specific language governing rights and limitations
+// under the License.
+// 
+// The Original Code of this file is Copyright 1998-2001, Licensor of the
+// AAF Association.
+// 
+// The Initial Developer of the Original Code of this file and the
+// Licensor of the AAF Association is Avid Technology.
+// All rights reserved.
+//
+//=---------------------------------------------------------------------=
 #include "CAAFAIFCCodec.h"
 
 #include <assert.h>
@@ -60,6 +55,13 @@ static double          ConvertFromIeeeExtended(char *bytes);
 static void            ConvertToIeeeExtended(double num, char *bytes);
 
 static void SplitBuffers(void *original, aafUInt32 srcSamples, aafUInt16 sampleSize, aafUInt16 numDest, interleaveBufAIFF_t *destPtr);
+
+inline void checkResult(AAFRESULT r)
+{
+  if (AAFRESULT_SUCCESS != r)
+    throw HRESULT(r);
+}
+
 
 HRESULT STDMETHODCALLTYPE
 CAAFAIFCCodec::CountDefinitions (aafUInt32 *pDefCount)
@@ -522,6 +524,12 @@ CAAFAIFCCodec::WriteSamples (aafUInt32  nSamples,
 		return AAFRESULT_NULL_PARAM;
 	if (0 == nSamples)
 		return AAFRESULT_INVALID_PARAM;
+
+	aafPosition_t offset;
+	checkResult(_stream->GetPosition(&offset));
+	if ( offset + buflen >  (aafPosition_t)2*1024*1024*1024-1 ) {
+	  return AAFRESULT_EOF;
+	}
 	
 	// If there multiple channels in the AIFC file and the data is interleaved
 	// then write all of the interleaved samples together.
