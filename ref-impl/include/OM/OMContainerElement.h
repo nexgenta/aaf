86db1634b66fb8c03e3911d9d9587f8661c18654
tbingham <tbingham>
2002-04-22 19:11:08 +0000

    Initial implementation of reference counted definitioins.

--- ref-impl/include/OM/OMContainerElement.h
+++ ref-impl/include/OM/OMContainerElement.h
@@ -1,29 +1,24 @@
-/***********************************************************************
-*
-*              Copyright (c) 1998-2000 Avid Technology, Inc.
-*
-* Permission to use, copy and modify this software and accompanying
-* documentation, and to distribute and sublicense application software
-* incorporating this software for any purpose is hereby granted,
-* provided that (i) the above copyright notice and this permission
-* notice appear in all copies of the software and related documentation,
-* and (ii) the name Avid Technology, Inc. may not be used in any
-* advertising or publicity relating to the software without the specific,
-* prior written permission of Avid Technology, Inc.
-*
-* THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
-* EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
-* WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
-* IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
-* SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
-* OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
-* OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
-* ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
-* RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
-* ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
-* LIABILITY.
-*
-************************************************************************/
+//=---------------------------------------------------------------------=
+//
+// The contents of this file are subject to the AAF SDK Public
+// Source License Agreement (the "License"); You may not use this file
+// except in compliance with the License.  The License is available in
+// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
+// Association or its successor.
+// 
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+// the License for the specific language governing rights and limitations
+// under the License.
+// 
+// The Original Code of this file is Copyright 1998-2001, Licensor of the
+// AAF Association.
+// 
+// The Initial Developer of the Original Code of this file and the
+// Licensor of the AAF Association is Avid Technology.
+// All rights reserved.
+//
+//=---------------------------------------------------------------------=
 
 // @doc OMINTERNAL
 #ifndef OMCONTAINERELEMENT_H
@@ -275,9 +270,36 @@ public:
     // @cmember The unique key of this <c OMStrongReferenceSetElement>.
   void* identification(void) const;
 
+  // Special value for the reference count indicating that this
+  // <c OMStrongReferenceSetElement> is not reference counted.
+  //
+  enum {sticky = 0xffffffff};
+
     // @cmember The count of weak references to this
     //          <c OMStrongReferenceSetElement>.
   OMUInt32 referenceCount(void) const;
+
+    // @cmember Increase the count of weak references to this
+    //          <c OMStrongReferenceSetElement> by one. Return
+    //          the new value of the reference count.
+  OMUInt32 increaseReferenceCount(void);
+
+    // @cmember Decrease the count of weak references to this
+    //          <c OMStrongReferenceSetElement> by one. Return
+    //          the new value of the reference count.
+  OMUInt32 decreaseReferenceCount(void);
+
+    // @cmember Is this a sticky <c OMStrongReferenceSetElement>?
+    //          If so, this <c OMStrongReferenceSetElement> is always
+    //          persistent. If not, this <c OMStrongReferenceSetElement> is
+    //          only persistent if it has a non-zero <f referenceCount()>.
+  bool isSticky(void) const;
+
+    // @cmember Unstick this <c OMStrongReferenceSetElement>.
+  void clearSticky(void);
+
+    // @cmember Make this <c OMStrongReferenceSetElement> sticky.
+  void setSticky(void);
 
 private:
   // @access Private members.
