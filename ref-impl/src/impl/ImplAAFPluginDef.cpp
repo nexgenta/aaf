4ea65a36efc43564f0641dc32a11284e8b1839bc
tbingham <tbingham>
2002-05-10 17:47:50 +0000

    Remove obsolete commented out code.

--- ref-impl/src/impl/ImplAAFPluginDef.cpp
+++ ref-impl/src/impl/ImplAAFPluginDef.cpp
@@ -1,29 +1,24 @@
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- * prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+//=---------------------------------------------------------------------=
+//
+// The contents of this file are subject to the AAF SDK Public
+// Source License Agreement (the "License"); You may not use this file
+// except in compliance with the License.  The License is available in
+// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
+// Association or its successor.
+// 
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+// the License for the specific language governing rights and limitations
+// under the License.
+// 
+// The Original Code of this file is Copyright 1998-2001, Licensor of the
+// AAF Association.
+// 
+// The Initial Developer of the Original Code of this file and the
+// Licensor of the AAF Association is Avid Technology.
+// All rights reserved.
+//
+//=---------------------------------------------------------------------=
 
 #ifndef __ImplAAFNetworkLocator_h__
 #include "ImplAAFNetworkLocator.h"
@@ -109,8 +104,8 @@ ImplAAFPluginDef::~ImplAAFPluginDef ()
 	}
 
 	// Release all of the other locator pointers.
-	size_t size = _locators.getSize();
-	for (size_t i = 0; i < size; i++)
+	size_t count = _locators.count();
+	for (size_t i = 0; i < count; i++)
 	{
 		ImplAAFLocator *pLocator = _locators.clearValueAt(i);
 		if (pLocator)
@@ -331,9 +326,17 @@ AAFRESULT STDMETHODCALLTYPE
 	  {
 		ImplAAFNetworkLocator *pOldLoc = _manufacturerURL;
 		if (pOldLoc)
+		{
+		  if( pOldLoc == pManufacturerInfo )
+			return AAFRESULT_SUCCESS;
+
 		  pOldLoc->ReleaseReference();
-		pOldLoc = 0;
+		  pOldLoc = 0;
+		}
 	  }
+
+	if( pManufacturerInfo->attached() )
+		return AAFRESULT_OBJECT_ALREADY_ATTACHED;
 
 	_manufacturerURL = pManufacturerInfo;
 	
@@ -698,13 +701,12 @@ AAFRESULT STDMETHODCALLTYPE
     ImplAAFPluginDef::CountLocators (
       aafUInt32 *pCount)
 {
-	size_t	siz;
 	if (! pCount)
 	{
 		return AAFRESULT_NULL_PARAM;
 	}
 
-	_locators.getSize(siz);
+	size_t	siz = _locators.count();
 	*pCount = siz;
 	return(AAFRESULT_SUCCESS);
 }
@@ -717,6 +719,9 @@ AAFRESULT STDMETHODCALLTYPE
 {
 	if(pLocator == NULL)
 		return(AAFRESULT_NULL_PARAM);
+
+	if(pLocator->attached())
+		return(AAFRESULT_OBJECT_ALREADY_ATTACHED);
 
 	_locators.appendValue(pLocator);
 	pLocator->AcquireReference();
@@ -732,6 +737,9 @@ AAFRESULT STDMETHODCALLTYPE
 {
 	if(pLocator == NULL)
 		return(AAFRESULT_NULL_PARAM);
+
+	if(pLocator->attached())
+		return(AAFRESULT_OBJECT_ALREADY_ATTACHED);
 
 	_locators.prependValue(pLocator);
 	pLocator->AcquireReference();
@@ -756,6 +764,9 @@ AAFRESULT STDMETHODCALLTYPE
 
 	if (index > count)
 	  return AAFRESULT_BADINDEX;
+
+	if(pLocator->attached())
+		return(AAFRESULT_OBJECT_ALREADY_ATTACHED);
 
 	_locators.insertAt(pLocator,index);
 	pLocator->AcquireReference();
@@ -856,7 +867,6 @@ AAFRESULT STDMETHODCALLTYPE
 		if (theEnum)
 		  theEnum->ReleaseReference();
 		theEnum = 0;
-		return(XCODE());
 	}
 	XEND;
 	
@@ -887,7 +897,6 @@ AAFRESULT STDMETHODCALLTYPE
   _defObj = def;
 
   return AAFRESULT_SUCCESS;
-//	assert(_dataDef.isVoid());
 }
 
 
@@ -910,13 +919,12 @@ AAFRESULT STDMETHODCALLTYPE
 AAFRESULT
     ImplAAFPluginDef::GetNumLocators (aafInt32 *pCount)
 {
-	size_t	siz;
 	if (! pCount)
 	{
 		return AAFRESULT_NULL_PARAM;
 	}
 
-	_locators.getSize(siz);
+	size_t	siz = _locators.count();
 	*pCount = siz;
 	return(AAFRESULT_SUCCESS);
 }
