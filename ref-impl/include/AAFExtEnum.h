50dea9f35df2a8684441e339dd5036bdebaef097
tbingham <tbingham>
2002-05-29 19:19:06 +0000

    Use generated header.

--- ref-impl/include/AAFExtEnum.h
+++ ref-impl/include/AAFExtEnum.h
@@ -1,42 +1,59 @@
-#ifndef __AAFClassDefUIDs_h__
-#define __AAFClassDefUIDs_h__
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- *  prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+#ifndef __AAFExtEnum_h__
+#define __AAFExtEnum_h__
 
+//=---------------------------------------------------------------------=
 //
-// GUIDs for built-in class definitions
+// This file was GENERATED for the AAF SDK
+//
+// The contents of this file are subject to the AAF SDK Public
+// Source License Agreement (the "License"); You may not use this file
+// except in compliance with the License.  The License is available in
+// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
+// Association or its successor.
+//
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+// the License for the specific language governing rights and limitations
+// under the License.
+//
+// The Original Code of this file is Copyright 1998-2002, Licensor of the
+// AAF Association.
+//
+// The Initial Developer of the Original Code of this file and the
+// Licensor of the AAF Association is Avid Technology.
+// All rights reserved.
+//
+//=---------------------------------------------------------------------=
+
+
+#include "AAFTypes.h"
+
+// AAF extensible enumeration member UIDs.
 //
 
-// Define the AAF_CLASS macro to create class GUID definitions.
-// Include the meta dictionary.
+// Members of OperationCategory
 //
-#define AAF_TYPE_DEFINITION_EXTENDIBLE_ENUMERATION_MEMBER(name, guid, container) \
-const aafUID_t name = guid;
+//{0d010102-0101-0100-060e-2b3404010101}
+const aafUID_t kAAFOperationEffect =
+{0x0d010102, 0x0101, 0x0100, {0x06, 0x0e, 0x2b, 0x34, 0x04, 0x01, 0x01, 0x01}};
 
-#include "AAFMetaDictionary.h"
+// Members of GammaType
+//
+// None currently defined
 
-#endif // ! __AAFClassDefUIDs_h__
+// Members of CategoryType
+//
+//{0d010102-0101-0200-060e-2b3404010101}
+const aafUID_t kAAFPluginCatagoryEffect =
+{0x0d010102, 0x0101, 0x0200, {0x06, 0x0e, 0x2b, 0x34, 0x04, 0x01, 0x01, 0x01}};
+
+//{0d010102-0101-0300-060e-2b3404010101}
+const aafUID_t kAAFPluginCatagoryCodec =
+{0x0d010102, 0x0101, 0x0300, {0x06, 0x0e, 0x2b, 0x34, 0x04, 0x01, 0x01, 0x01}};
+
+//{0d010102-0101-0400-060e-2b3404010101}
+const aafUID_t kAAFPluginCatagoryInterpolation =
+{0x0d010102, 0x0101, 0x0400, {0x06, 0x0e, 0x2b, 0x34, 0x04, 0x01, 0x01, 0x01}};
+
+
+#endif // ! __AAFExtEnum_h__
