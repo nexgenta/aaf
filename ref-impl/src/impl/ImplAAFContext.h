7c729e6325f5b8e45195e7b83e281f1ec73e20f5
tbingham <tbingham>
2002-07-25 22:26:39 +0000

    Don't destroy the context object at static destrutor time - instead do it explicitly.

--- ref-impl/src/impl/ImplAAFContext.h
+++ ref-impl/src/impl/ImplAAFContext.h
@@ -3,13 +3,27 @@
 #ifndef __ImplAAFContext_h__
 #define __ImplAAFContext_h__
 
-/******************************************\
-*                                          *
-* Advanced Authoring Format                *
-*                                          *
-* Copyright (c) 1998 Avid Technology, Inc. *
-*                                          *
-\******************************************/
+//=---------------------------------------------------------------------=
+//
+// The contents of this file are subject to the AAF SDK Public
+// Source License Agreement (the "License"); You may not use this file
+// except in compliance with the License.  The License is available in
+// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
+// Association or its successor.
+// 
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+// the License for the specific language governing rights and limitations
+// under the License.
+// 
+// The Original Code of this file is Copyright 1998-2001, Licensor of the
+// AAF Association.
+// 
+// The Initial Developer of the Original Code of this file and the
+// Licensor of the AAF Association is Avid Technology.
+// All rights reserved.
+//
+//=---------------------------------------------------------------------=
 
 #include "AAFTypes.h"
 #include "ImplAAFRoot.h"
@@ -33,6 +47,7 @@ public:
   //
   static ImplAAFContext * GetInstance ();
 
+  static void DeleteInstance(void);
 
 public:
 
@@ -53,11 +68,6 @@ private:
 
   ImplAAFPluginManager	*_plugins;
 
-  // Make private helper class a friend so that it
-  // may call the destructor. This helper class is used
-  // to ensure that the singleton context instance is 
-  // cleaned up properly.
-  friend class ImplAAFContextHelper;
 };
 
 #endif // ! __ImplAAFContext_h__
