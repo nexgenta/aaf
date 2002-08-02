c69ead14c85c5d3dba3caec6c121cc2fd3e4906b
tbingham <tbingham>
2002-08-02 22:43:33 +0000

    Remove variables declared but not used.

--- ref-impl/src/impl/ImplAAFObject.cpp
+++ ref-impl/src/impl/ImplAAFObject.cpp
@@ -1,29 +1,24 @@
-/***********************************************************************
- *
- *              Copyright (c) 1998-2001 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- * prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+//=---------------------------------------------------------------------=
+//
+// The contents of this file are subject to the AAF SDK Public
+// Source License Agreement (the "License"); You may not use this file
+// except in compliance with the License.  The License is available in
+// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
+// Association or its successor.
+// 
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+// the License for the specific language governing rights and limitations
+// under the License.
+// 
+// The Original Code of this file is Copyright 1998-2001, Licensor of the
+// AAF Association.
+// 
+// The Initial Developer of the Original Code of this file and the
+// Licensor of the AAF Association is Avid Technology.
+// All rights reserved.
+//
+//=---------------------------------------------------------------------=
 
 
 #ifndef __ImplAAFObject_h__
@@ -305,7 +300,6 @@ AAFRESULT ImplPropertyCollection::Initialize
 )
 {
   ImplAAFPropertyDefSP pPropDef;
-  AAFRESULT rReturned = AAFRESULT_SUCCESS;
 
   // make sure we haven't called this before
   assert (0 == _properties.count());
@@ -324,7 +318,6 @@ AAFRESULT ImplPropertyCollection::Initialize
   if (AAFRESULT_FAILED(ar)) return ar;
   assert (pClassDef);
 
-  OMProperty * pOmProp = NULL;
   while (1)
 	{
 	  ImplEnumAAFPropertyDefsSP pPropEnum;
@@ -424,7 +417,6 @@ ImplAAFProperty * ImplPropertyCollection::FindProperty(OMPropertyId pid) const /
 ImplAAFObject::ImplAAFObject ()
   : _generation(PID_InterchangeObject_Generation, L"Generation"),
 	_pProperties (0),
-	_cachedDefinition (0),
 	_apSavedProps (0),
 	_savedPropsSize (0),
 	_savedPropsCount (0)
@@ -517,8 +509,6 @@ ImplAAFObject::SavedProp::~SavedProp ()
 
 ImplAAFObject::~ImplAAFObject ()
 {
-  _cachedDefinition = 0; // we don't need to reference count this defintion
-
   if (_pProperties)
 	delete _pProperties;
 
@@ -634,48 +624,6 @@ AAFRESULT STDMETHODCALLTYPE
 
   assert (_generation.isPresent());
   *pResult = _generation;
-  return AAFRESULT_SUCCESS;
-}
-
-
-
-AAFRESULT STDMETHODCALLTYPE
-    ImplAAFObject::GetDefinition (ImplAAFClassDef ** ppClassDef)
-{
-  if (! ppClassDef)
-	return AAFRESULT_NULL_PARAM;
-
-  if (! _cachedDefinition)
-	{
-	  AAFRESULT ar;
-	  ImplAAFDictionarySP pDict;
-	  ar = GetDictionary(&pDict);
-	  if (AAFRESULT_FAILED (ar))
-		return ar;
-	  assert (pDict);
-
-	  aafUID_t classID;
-	  ar = GetObjectClass (&classID);
-	  assert (AAFRESULT_SUCCEEDED (ar));
-
-	  ImplAAFClassDef * tmp;
-	  ar = pDict->LookupClassDef(classID, &tmp);
-	  if (AAFRESULT_FAILED (ar))
-		return ar;
-	  if (! _cachedDefinition)
-		_cachedDefinition = tmp;
-	  assert (_cachedDefinition);
-		
-	  // We don't need to reference count the definitions since
-	  // they are owned by the dictionary.
-	  aafInt32 count = _cachedDefinition->ReleaseReference();
-	  assert(0 < count);
-	}
-  assert (ppClassDef);
-  *ppClassDef = _cachedDefinition;
-  assert (*ppClassDef);
-  (*ppClassDef)->AcquireReference ();
-
   return AAFRESULT_SUCCESS;
 }
 
@@ -1171,6 +1119,7 @@ void ImplAAFObject::InitOMProperties (ImplAAFClassDef * pClassDef)
   assert (pClassDef);
   AAFRESULT ar;
 
+
   //
   // Init base class properties first
   //
@@ -1258,7 +1207,7 @@ const OMClassId& ImplAAFObject::classId(void) const
 {
   const aafUID_t null_uid = { 0 };
   assert (! EqualAUID (&_soid, &null_uid));
-  return *reinterpret_cast<const OMClassId* const>(&_soid);
+  return *reinterpret_cast<const OMClassId*>(&_soid);
 }
 
 
