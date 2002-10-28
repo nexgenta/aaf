a38464a4cf4d7a758f2f9228ab3e52d077794e65
akharkev <akharkev>
2002-10-28 22:33:21 +0000

    Include <iomanip.h> (fixes gcc3 compilation error).

--- test/com/ComModTestAAF/ModuleTests/CAAFTransitionTest.cpp
+++ test/com/ComModTestAAF/ModuleTests/CAAFTransitionTest.cpp
@@ -1,48 +1,45 @@
 // @doc INTERNAL
 // @com This file implements the module test for CAAFTransition
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- * prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+//=---------------------------------------------------------------------=
+//
+// The contents of this file are subject to the AAF SDK Public
+// Source License Agreement (the "License"); You may not use this file
+// except in compliance with the License.  The License is available in
+// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
+// Association or its successor.
+// 
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+// the License for the specific language governing rights and limitations
+// under the License.
+// 
+// The Original Code of this file is Copyright 1998-2001, Licensor of the
+// AAF Association.
+// 
+// The Initial Developer of the Original Code of this file and the
+// Licensor of the AAF Association is Avid Technology.
+// All rights reserved.
+//
+//=---------------------------------------------------------------------=
 
 #include "AAF.h"
 
 
 #include <iostream.h>
+#include <iomanip.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "AAFStoredObjectIDs.h"
-#include "aafCvt.h"
+//#include "aafCvt.h"
 #include "AAFResult.h"
+#include "ModuleTest.h"
 #include "AAFDataDefs.h"
 #include "AAFDefUIDs.h"
 
 #include "CAAFBuiltinDefs.h"
 
 // This values are used for testing purposes
-static aafUID_t    fillerUID = DDEF_Picture;
 static aafLength_t  fillerLength = 3200;
 static aafMobID_t	zeroMobID = { 0 };
 
@@ -148,7 +145,8 @@ static HRESULT CreateAAFFile(aafWChar * pFileName)
 	aafUID_t					effectID = kTestEffectID;
 	aafUID_t					parmID = kTestParmID;
 
-	CvtInt32toLength(100, transitionLength);
+	//CvtInt32toLength(100, transitionLength);
+	transitionLength = 100;
 	aafProductVersion_t v;
 	v.major = 1;
 	v.minor = 0;
@@ -283,9 +281,8 @@ static HRESULT CreateAAFFile(aafWChar * pFileName)
 
     checkResult(pConstantValue->QueryInterface (IID_IAAFParameter, (void **)&pParm));
     checkResult(pOperationGroup->AddParameter (pParm));
-//    pParm->Release();
- //	IAAFSegment*				pSegment = NULL;
-   pParm = NULL;
+    pParm->Release();
+    pParm = NULL;
     pConstantValue->Release();
     pConstantValue = NULL;
 
@@ -440,22 +437,9 @@ static HRESULT ReadAAFFile(aafWChar * pFileName)
 	aafLength_t					transitionLength;
 	aafPosition_t				cutPoint;
 
-	aafProductIdentification_t	ProductInfo;
 	aafNumSlots_t				numMobs;
 	aafUInt32					numComponents = 0;
 	HRESULT						hr = S_OK;
-
-	aafProductVersion_t v;
-	v.major = 1;
-	v.minor = 0;
-	v.tertiary = 0;
-	v.patchLevel = 0;
-	v.type = kAAFVersionUnknown;
-	ProductInfo.companyName = L"AAF Developers Desk. NOT!";
-	ProductInfo.productName = L"AAFTransition Test. NOT!";
-	ProductInfo.productVersion = &v;
-	ProductInfo.productVersionString = NULL;
-	ProductInfo.platform = NULL;
 
 	try
 	{
@@ -579,21 +563,25 @@ static HRESULT ReadAAFFile(aafWChar * pFileName)
 	return hr;
 }
 
-extern "C" HRESULT CAAFTransition_test()
+extern "C" HRESULT CAAFTransition_test(testMode_t mode);
+extern "C" HRESULT CAAFTransition_test(testMode_t mode)
 {
 	HRESULT hr = AAFRESULT_NOT_IMPLEMENTED;
 	aafWChar * pFileName = L"AAFTransitionTest.aaf";
 
 	try
 	{
-		hr = CreateAAFFile(	pFileName );
+		if(mode == kAAFUnitTestReadWrite)
+			hr = CreateAAFFile(pFileName);
+		else
+			hr = AAFRESULT_SUCCESS;
 		if(hr == AAFRESULT_SUCCESS)
 			hr = ReadAAFFile( pFileName );
 	}
 	catch (...)
 	{
 	  cerr << "CAAFTransition_test...Caught general C++"
-		" exception!" << endl; 
+		   << " exception!" << endl; 
 	  hr = AAFRESULT_TEST_FAILED;
 	}
 
