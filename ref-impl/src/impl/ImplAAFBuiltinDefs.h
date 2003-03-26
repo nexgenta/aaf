aac1e2744f7f27b0ae4645fd76482dc316113fd2
jptrainor <jptrainor>
2003-03-26 16:37:19 +0000

    Resolve 587935 - Bring ImplAAFBuiltinDefs.h and CAAbuiltinDefs.h back in line with each other.

--- ref-impl/src/impl/ImplAAFBuiltinDefs.h
+++ ref-impl/src/impl/ImplAAFBuiltinDefs.h
@@ -1,31 +1,26 @@
 #ifndef __ImplAAFBuiltinDefs_h__
 #define __ImplAAFBuiltinDefs_h__
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- * prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+//=---------------------------------------------------------------------=
+//
+// The contents of this file are subject to the AAF SDK Public
+// Source License Agreement (the "License"); You may not use this file
+// except in compliance with the License.  The License is available in
+// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
+// Association or its successor.
+// 
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+// the License for the specific language governing rights and limitations
+// under the License.
+// 
+// The Original Code of this file is Copyright 1998-2001, Licensor of the
+// AAF Association.
+// 
+// The Initial Developer of the Original Code of this file and the
+// Licensor of the AAF Association is Avid Technology.
+// All rights reserved.
+//
+//=---------------------------------------------------------------------=
 
 
 #include "ImplAAFDictionary.h"
@@ -194,10 +189,12 @@ public:
   CLASS_DEF_METHOD(FileDescriptor);
   CLASS_DEF_METHOD(Filler);
   CLASS_DEF_METHOD(FilmDescriptor);
+  CLASS_DEF_METHOD(GPITrigger);
   CLASS_DEF_METHOD(HTMLClip);
   CLASS_DEF_METHOD(HTMLDescriptor);
   CLASS_DEF_METHOD(Identification);
   CLASS_DEF_METHOD(InterpolationDefinition);
+  CLASS_DEF_METHOD(KLVData);
   CLASS_DEF_METHOD(Locator);
   CLASS_DEF_METHOD(MasterMob);
   CLASS_DEF_METHOD(Mob);
@@ -211,6 +208,7 @@ public:
   CLASS_DEF_METHOD(PluginDef);
   CLASS_DEF_METHOD(PropertyDef);
   CLASS_DEF_METHOD(Pulldown);
+  CLASS_DEF_METHOD(RGBADescriptor);
   CLASS_DEF_METHOD(ScopeReference);
   CLASS_DEF_METHOD(Selector);
   CLASS_DEF_METHOD(Sequence);
@@ -238,6 +236,7 @@ public:
   CLASS_DEF_METHOD(TypeDefInt);
   CLASS_DEF_METHOD(TypeDefRecord);
   CLASS_DEF_METHOD(TypeDefRename);
+  CLASS_DEF_METHOD(TypeDefStream);
   CLASS_DEF_METHOD(TypeDefString);
   CLASS_DEF_METHOD(TypeDefStrongObjRef);
   CLASS_DEF_METHOD(TypeDefVariableArray);
@@ -260,10 +259,22 @@ public:
   //
   // Type Def 'get' methods
   //
-  TYPE_DEF_METHOD(Indirect);
-  TYPE_DEF_METHOD(Character);
+  TYPE_DEF_METHOD(Rational);
   TYPE_DEF_METHOD(String);
+  TYPE_DEF_METHOD(Character);
+  TYPE_DEF_METHOD(Indirect);
+  TYPE_DEF_METHOD(Opaque);
+  TYPE_DEF_METHOD(Stream);
 
+  // Built-in integer types
+  TYPE_DEF_METHOD(UInt8);
+  TYPE_DEF_METHOD(UInt16);
+  TYPE_DEF_METHOD(UInt32);
+  TYPE_DEF_METHOD(UInt64);
+  TYPE_DEF_METHOD(Int8);
+  TYPE_DEF_METHOD(Int16);
+  TYPE_DEF_METHOD(Int32);
+  TYPE_DEF_METHOD(Int64);
 
 private:
   ImplAAFDictionary * _pDict;
