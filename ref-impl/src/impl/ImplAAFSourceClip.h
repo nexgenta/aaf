3e80c12b45868ec1326421176bcf2263a86f5f02
jptrainor <jptrainor>
2003-04-25 23:47:45 +0000

    Added CloneExternal source reference dependency resolution.

--- ref-impl/src/impl/ImplAAFSourceClip.h
+++ ref-impl/src/impl/ImplAAFSourceClip.h
@@ -3,14 +3,27 @@
 #ifndef __ImplAAFSourceClip_h__
 #define __ImplAAFSourceClip_h__
 
-/******************************************\
-*                                          *
-* Advanced Authoring Format                *
-*                                          *
-* Copyright (c) 1998 Avid Technology, Inc. *
-* Copyright (c) 1998 Microsoft Corporation *
-*                                          *
-\******************************************/
+//=---------------------------------------------------------------------=
+//
+// The contents of this file are subject to the AAF SDK Public
+// Source License Agreement (the "License"); You may not use this file
+// except in compliance with the License.  The License is available in
+// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
+// Association or its successor.
+// 
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+// the License for the specific language governing rights and limitations
+// under the License.
+// 
+// The Original Code of this file is Copyright 1998-2001, Licensor of the
+// AAF Association.
+// 
+// The Initial Developer of the Original Code of this file and the
+// Licensor of the AAF Association is Avid Technology.
+// All rights reserved.
+//
+//=---------------------------------------------------------------------=
 
 
 class ImplAAFDataDef;
@@ -29,9 +42,6 @@ class ImplAAFSourceClip : public ImplAAFSourceReference
 {
 public:
 
-	// Declare class storable
-
-	OMDECLARE_STORABLE(ImplAAFSourceClip)
   //
   // Constructor/destructor
   //
@@ -46,13 +56,13 @@ public:
   virtual AAFRESULT STDMETHODCALLTYPE
     Initialize
         (// @parm [in] Data Definition object
-		 aafUID_t * pDatadef  ,
+		 ImplAAFDataDef * pDataDef,
 
 		 // @parm [in] Length property value
-		 aafLength_t *     pLength   ,
+		 const aafLength_t & length,
 
 		 // @parm [in] Source Reference
-         aafSourceRef_t   sourceRef);
+         const aafSourceRef_t & sourceRef);
 
   //****************
   // GetFade()
@@ -121,16 +131,15 @@ public:
 
 public:
 
-  // Declare the module test method. The implementation of the will be be
-  // in /test/ImplAAFSourceClipTest.cpp.
-  static AAFRESULT test();
-
 	virtual AAFRESULT TraverseToClip(aafLength_t length,
 					ImplAAFSegment **sclp,
 					 ImplAAFPulldown **pulldownObj,
 					 aafInt32 *pulldownPhase,
 					 aafLength_t *sclpLen,
 					 aafBool *isMask);
+
+	// OM deep copy notification
+	virtual void onCopy(void* clientContext) const;
 
 private:
 	OMFixedSizeProperty<aafLength_t>	_fadeInLength;
