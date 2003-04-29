136a72cd636c414f7fbf297c96950f5a983c614d
jptrainor <jptrainor>
2003-04-29 01:58:26 +0000

    Add kAAFByMobId, kAAFByName, and kAAFByClass mob search criteria support - formerly NOT_IN_CURRENT_VERSION.

--- ref-impl/src/impl/ImplAAFMob.h
+++ ref-impl/src/impl/ImplAAFMob.h
@@ -3,36 +3,34 @@
 #ifndef __ImplAAFMob_h__
 #define __ImplAAFMob_h__
 
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- * prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+//=---------------------------------------------------------------------=
+//
+// The contents of this file are subject to the AAF SDK Public
+// Source License Agreement (the "License"); You may not use this file
+// except in compliance with the License.  The License is available in
+// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
+// Association or its successor.
+// 
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+// the License for the specific language governing rights and limitations
+// under the License.
+// 
+// The Original Code of this file is Copyright 1998-2001, Licensor of the
+// AAF Association.
+// 
+// The Initial Developer of the Original Code of this file and the
+// Licensor of the AAF Association is Avid Technology.
+// All rights reserved.
+//
+//=---------------------------------------------------------------------=
 
 #ifndef __ImplAAFMobSlot_h__
 #include "ImplAAFMobSlot.h"
 #endif
+
+#include "OMStrongRefVectorProperty.h"
+#include "OMWideStringProperty.h"
 
 #ifndef __ImplAAFTimelineMobSlot_h__
 #include "ImplAAFTimelineMobSlot.h"
@@ -54,9 +52,9 @@ class ImplAAFMobSlot;
 
 class ImplAAFFile;
 
-class ImplEnumAAFMobSlots;
-
-class ImplEnumAAFTaggedValues;
+template <class T> 
+class ImplAAFEnumerator;
+typedef ImplAAFEnumerator<ImplAAFMobSlot> ImplEnumAAFMobSlots;
 
 class ImplAAFFindSourceInfo;
 
@@ -469,6 +467,11 @@ virtual AAFRESULT STDMETHODCALLTYPE
   // SDK Internal
   virtual AAFRESULT ReconcileMobLength(void);
   virtual const OMMaterialIdentification& identification(void) const;
+
+  // Internal methods used to support some Mob enumerator search criterion.
+  bool IsMobIDEqual( const aafMobID_t* mobID ) const;
+  bool IsNameEqual( const aafString_t name ) const;
+  HRESULT IsClassIDEqual( const aafClassID_t* id, bool& result ) const;
 
 	protected:
 	OMFixedSizeProperty<aafMobID_t>		_mobID;
--- ref-impl/src/impl/ImplEnumAAFMobs.cpp
+++ ref-impl/src/impl/ImplEnumAAFMobs.cpp
@@ -1,29 +1,24 @@
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- *  prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+//=---------------------------------------------------------------------=
+//
+// The contents of this file are subject to the AAF SDK Public
+// Source License Agreement (the "License"); You may not use this file
+// except in compliance with the License.  The License is available in
+// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
+// Association or its successor.
+// 
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+// the License for the specific language governing rights and limitations
+// under the License.
+// 
+// The Original Code of this file is Copyright 1998-2001, Licensor of the
+// AAF Association.
+// 
+// The Initial Developer of the Original Code of this file and the
+// Licensor of the AAF Association is Avid Technology.
+// All rights reserved.
+//
+//=---------------------------------------------------------------------=
 
 #include "ImplEnumAAFMobs.h"
 
@@ -50,6 +45,16 @@ AAFRESULT STDMETHODCALLTYPE
 			(*ppMob)=pCandidate;
 			return(AAFRESULT_SUCCESS);
 
+		case kAAFByMobID:
+		  
+		  if ( pCandidate->IsMobIDEqual( &_criteria.tags.mobID ) ) {
+		    *ppMob = pCandidate;
+		    return(AAFRESULT_SUCCESS);
+		  }
+		  pCandidate->ReleaseReference();
+		  pCandidate = NULL;
+		  break;
+
 		case kAAFByMobKind:
 			aafMobKind_t	kind;
 				
@@ -69,6 +74,36 @@ AAFRESULT STDMETHODCALLTYPE
 			pCandidate = NULL;
 			break;
 
+		case kAAFByName:
+
+		  if ( pCandidate->IsNameEqual( _criteria.tags.name ) ) {
+		    *ppMob = pCandidate;
+		    return(AAFRESULT_SUCCESS);
+		  }
+		  pCandidate->ReleaseReference();
+		  pCandidate = NULL;
+		  break;
+
+		case kAAFByClass:
+
+		  bool result;
+		  ar = pCandidate->IsClassIDEqual( &_criteria.tags.objClass, result ); 
+		  if ( AAFRESULT_SUCCESS != ar ) {
+		    pCandidate->ReleaseReference();
+		    return ar;
+		  }
+
+		  if ( result ) {
+		    *ppMob = pCandidate;
+		    return(AAFRESULT_SUCCESS);
+		  }
+
+		  pCandidate->ReleaseReference();
+		  pCandidate = NULL;
+		  break;
+
+		case kAAFByDataDef:
+		case kAAFByMediaCrit:
 		default:
 			pCandidate->ReleaseReference();
 			return(AAFRESULT_NOT_IN_CURRENT_VERSION);
@@ -163,172 +198,3 @@ AAFRESULT ImplEnumAAFMobs::SetCriteria(aafSearchCrit_t *pCriteria)
 
 	return AAFRESULT_SUCCESS;
 }
-
-#if(0)
-
-#include "AAFResult.h"
-#include "ImplAAFObjectCreation.h"
-#include "aafUtils.h"
-
-ImplEnumAAFMobs::ImplEnumAAFMobs () : _enumObj(0), _iterator(0)
-{
-	_criteria.searchTag = kAAFNoSearch;
-}
-
-
-ImplEnumAAFMobs::~ImplEnumAAFMobs ()
-{
-  if (0 != _enumObj)
-  {
-    _enumObj->ReleaseReference();
-	  _enumObj = 0;
-  }
-	delete _iterator;
-	_iterator = 0;
-}
-
-
-AAFRESULT STDMETHODCALLTYPE
-    ImplEnumAAFMobs::NextOne (ImplAAFMob **ppMob)
-{
-	AAFRESULT ar = AAFRESULT_NO_MORE_OBJECTS;
-	aafBool	found = kAAFFalse;
-
-	if(ppMob == NULL)
-		return AAFRESULT_NULL_PARAM;
-
-	if (_iterator->before() || _iterator->valid())
-	{
-		while(!found && ++(*_iterator))
-		{
-			*ppMob = _iterator->value();
-				switch(_criteria.searchTag)
-				{
-				case kAAFNoSearch:
-					found = kAAFTrue;
-					break;
-
-				case kAAFByMobKind:
-					aafMobKind_t	kind;
-				
-					ar = (*ppMob)->GetMobKind (&kind);
-					if(ar != AAFRESULT_SUCCESS)
-						return ar;
-					if((kind == _criteria.tags.mobKind) || (kAAFAllMob == _criteria.tags.mobKind))
-						found = kAAFTrue;
-					break;
-				default:
-					return AAFRESULT_NOT_IN_CURRENT_VERSION;
-				}
-		}
-		if(found)
-		{
-			(*ppMob)->AcquireReference();
-			ar = AAFRESULT_SUCCESS;
-		}
-		else
-		{
-			*ppMob = NULL;
-			ar = AAFRESULT_NO_MORE_OBJECTS;
-		}
-	}
-	return ar;
-}
-
-
-AAFRESULT STDMETHODCALLTYPE
-    ImplEnumAAFMobs::Next (aafUInt32 count,
-                           ImplAAFMob **ppMobs,
-                           aafUInt32 *pFetched)
-{
-	ImplAAFMob		**	ppOneMob;
-	aafUInt32			numDefs;
-	HRESULT				hr;
-
-	if(ppMobs == NULL)
-		return AAFRESULT_NULL_PARAM;
-	
-//!!!	if ((pFetched == NULL && count != 1) || (pFetched != NULL && count == 1))
-//		return E_INVALIDARG;
-
-	// Point at the first component in the array.
-	ppOneMob = ppMobs;
-	for (numDefs = 0; numDefs < count; numDefs++)
-	{
-		hr = NextOne(&ppOneMob[numDefs]);
-		if (FAILED(hr))
-			break;
-	}
-	
-	if (pFetched)
-		*pFetched = numDefs;
-
-	return AAFRESULT_SUCCESS;
-}
-
-
-AAFRESULT STDMETHODCALLTYPE
-    ImplEnumAAFMobs::Skip (aafUInt32 count)
-{
-	AAFRESULT	ar = AAFRESULT_SUCCESS;
-	aafUInt32	n;
-	
-	for(n = 1; n <= count; n++)
-	{
-		//!!! JeffB: Handle skip of subset of mobs correctly
-		// Defined behavior of skip is to NOT advance at all if it would push us off of the end
-		if(!++(*_iterator))
-		{
-			// Off the end, decrement n and iterator back to the starting position
-			while(n >= 1)
-			{
-				--(*_iterator);
-				n--;
-			}
-			ar = AAFRESULT_NO_MORE_OBJECTS;
-			break;
-		}
-	}
-
-	return ar;
-}
-
-AAFRESULT STDMETHODCALLTYPE
-    ImplEnumAAFMobs::Clone (ImplEnumAAFMobs **ppEnum)
-{
-	AAFRESULT			ar = AAFRESULT_SUCCESS;
-	ImplEnumAAFMobs		*result;
-
-	result = (ImplEnumAAFMobs *)CreateImpl(CLSID_EnumAAFMobs);
-	if (result == NULL)
-		return E_FAIL;
-
-    ar = result->SetIterator(_enumObj,_iterator->copy());
-	if (SUCCEEDED(ar))
-	{
-		*ppEnum = result;
-	}
-	else
-	{
-	  result->ReleaseReference();
-	  result = 0;
-	  *ppEnum = NULL;
-	}
-	
-	return ar;
-}
-
-//Internal
-
-AAFRESULT
-	ImplEnumAAFMobs::SetCriteria(aafSearchCrit_t *pCriteria)
-{
-	if(NULL == pCriteria)
-		_criteria.searchTag = kAAFNoSearch;
-	else
-		_criteria = *pCriteria;
-
-	return AAFRESULT_SUCCESS;
-}
-
-#endif
