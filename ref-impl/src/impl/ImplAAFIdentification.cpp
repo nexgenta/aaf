e3453703107e083070e71ad89a834883e51862c3
jptrainor <jptrainor>
2003-06-06 19:13:14 +0000

    ImplAAFFile::SaveCopyAs() support

--- ref-impl/src/impl/ImplAAFIdentification.cpp
+++ ref-impl/src/impl/ImplAAFIdentification.cpp
@@ -1,30 +1,25 @@
 
-/***********************************************************************
- *
- *              Copyright (c) 1998-1999 Avid Technology, Inc.
- *
- * Permission to use, copy and modify this software and accompanying 
- * documentation, and to distribute and sublicense application software
- * incorporating this software for any purpose is hereby granted, 
- * provided that (i) the above copyright notice and this permission
- * notice appear in all copies of the software and related documentation,
- * and (ii) the name Avid Technology, Inc. may not be used in any
- * advertising or publicity relating to the software without the specific,
- * prior written permission of Avid Technology, Inc.
- *
- * THE SOFTWARE IS PROVIDED AS-IS AND WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
- * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- * IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
- * OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
- * ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
- * ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
- * LIABILITY.
- *
- ************************************************************************/
+//=---------------------------------------------------------------------=
+//
+// The contents of this file are subject to the AAF SDK Public
+// Source License Agreement (the "License"); You may not use this file
+// except in compliance with the License.  The License is available in
+// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
+// Association or its successor.
+// 
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+// the License for the specific language governing rights and limitations
+// under the License.
+// 
+// The Original Code of this file is Copyright 1998-2001, Licensor of the
+// AAF Association.
+// 
+// The Initial Developer of the Original Code of this file and the
+// Licensor of the AAF Association is Avid Technology.
+// All rights reserved.
+//
+//=---------------------------------------------------------------------=
 
 
 #ifndef __ImplAAFIdentification_h__
@@ -68,10 +63,12 @@ ImplAAFIdentification::ImplAAFIdentification():
   _persistentProperties.put(_generation.address());
 
   
-#if defined(_WIN32)
+#if defined( OS_WINDOWS )
   _platform = L"Win32";
-#elif defined(macintosh) || defined(_MAC)
+#elif defined( OS_MACOS )
   _platform = L"MacOS";
+#elif defined( OS_UNIX )
+  _platform = L"Unix";
 #else
   _platform = L"Unknown";
 #endif
@@ -343,7 +340,7 @@ AAFRESULT STDMETHODCALLTYPE
 
 
 AAFRESULT STDMETHODCALLTYPE
-    ImplAAFIdentification::GetGeneration (aafUID_t *  pGen)
+    ImplAAFIdentification::GetGenerationID (aafUID_t *  pGen)
 {
   if (! isInitialized ()) return AAFRESULT_NOT_INITIALIZED;
 
@@ -415,4 +412,9 @@ AAFRESULT STDMETHODCALLTYPE
 {
   _productId = PID;
   return AAFRESULT_SUCCESS;
+}
+
+void ImplAAFIdentification::onCopy(void* clientContext) const
+{
+  const_cast<ImplAAFIdentification*>(this)->setInitialized();
 }
