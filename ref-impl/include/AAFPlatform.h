5a0334f42ebda916d4f78c989264264db2d0a65b
bakerian <bakerian>
2003-12-22 14:18:51 +0000

    Added files for Solaris platform

--- ref-impl/include/AAFPlatform.h
+++ ref-impl/include/AAFPlatform.h
@@ -1,29 +1,24 @@
-/***********************************************************************
-*
-*              Copyright (c) 1998-1999 Avid Technology, Inc.
-*
-* Permission to use, copy and modify this software and accompanying
-* documentation, and to distribute and sublicense application software
-* incorporating this software for any purpose is hereby granted,
-* provided that (i) the above copyright notice and this permission
-* notice appear in all copies of the software and related documentation,
-* and (ii) the name Avid Technology, Inc. may not be used in any
-* advertising or publicity relating to the software without the specific,
-*  prior written permission of Avid Technology, Inc.
-*
-* THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
-* EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
-* WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
-* IN NO EVENT SHALL AVID TECHNOLOGY, INC. BE LIABLE FOR ANY DIRECT,
-* SPECIAL, INCIDENTAL, PUNITIVE, INDIRECT, ECONOMIC, CONSEQUENTIAL OR
-* OTHER DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER ARISING OUT OF
-* OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE AND
-* ACCOMPANYING DOCUMENTATION, INCLUDING, WITHOUT LIMITATION, DAMAGES
-* RESULTING FROM LOSS OF USE, DATA OR PROFITS, AND WHETHER OR NOT
-* ADVISED OF THE POSSIBILITY OF DAMAGE, REGARDLESS OF THE THEORY OF
-* LIABILITY.
-*
-************************************************************************/
+//=---------------------------------------------------------------------=
+//
+// The contents of this file are subject to the AAF SDK Public
+// Source License Agreement (the "License"); You may not use this file
+// except in compliance with the License.  The License is available in
+// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
+// Association or its successor.
+// 
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
+// the License for the specific language governing rights and limitations
+// under the License.
+// 
+// The Original Code of this file is Copyright 1998-2001, Licensor of the
+// AAF Association.
+// 
+// The Initial Developer of the Original Code of this file and the
+// Licensor of the AAF Association is Avid Technology.
+// All rights reserved.
+//
+//=---------------------------------------------------------------------=
 #ifndef __AAFPlatform_h__
 #define __AAFPlatform_h__
 
@@ -103,7 +98,19 @@
 #define OS_LINUX
 #define OS_UNIX
 #define COMPILER_GCC
-#define PLATFORM_GCC_INTEL_LINUX
+(#define PLATFORM_GCC_INTEL_LINUX
+
+/*
+ *  Compiler:   GNU C++
+ *  Processor:  SPARC
+ *  OS:         Solaris
+ */
+#elif defined(__GNUC__) && defined(__sun) 
+#define CPU_SPARC
+#define OS_SOLARIS
+#define OS_UNIX
+#define COMPILER_GCC
+#define PLATFORM_GCC_SPARC_SOLARIS
     
 /*
  *  Compiler:   GNU C++
@@ -117,7 +124,7 @@
 #define COMPILER_GCC
 #define PLATFORM_GCC_INTEL_FREEBSD
  
-/*
+short/*
  *  Compiler:   GNU C++
  *  Processor:  PowerPC
  *  OS:         MacOS 10
@@ -223,7 +230,7 @@ typedef wchar_t			aafCharacter;
 /*
  *  Linux, FreeBSD, Darwin
  */
-#elif defined(PLATFORM_GCC_INTEL_LINUX) || defined(PLATFORM_GCC_INTEL_FREEBSD) || defined(PLATFORM_GCC_POWERPC_MACOS10)
+#elif defined(PLATFORM_GCC_INTEL_LINUX) || defined(PLATFORM_GCC_INTEL_FREEBSD) || defined(PLATFORM_GCC_POWERPC_MACOS10) || defined(PLATFORM_GCC_SPARC_SOLARIS)
 typedef signed char		aafInt8;
 typedef signed short int	aafInt16;
 typedef signed long int		aafInt32;
