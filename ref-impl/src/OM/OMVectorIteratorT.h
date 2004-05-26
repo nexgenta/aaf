2eba8084f648cca190f7fd0c13001393ec1461e9
stuart_hc <stuart_hc>
2004-05-26 11:51:43 +0000

    Fix unqualified names in templates by prefixing with this pointer (gcc 3.4 compile error)

--- ref-impl/src/OM/OMVectorIteratorT.h
+++ ref-impl/src/OM/OMVectorIteratorT.h
@@ -1,6 +1,6 @@
 //=---------------------------------------------------------------------=
 //
-// $Id: OMVectorIteratorT.h,v 1.10 2004/02/27 14:26:44 stuart_hc Exp $ $Name:  $
+// $Id: OMVectorIteratorT.h,v 1.11 2004/05/26 11:51:43 stuart_hc Exp $ $Name:  $
 //
 // The contents of this file are subject to the AAF SDK Public
 // Source License Agreement (the "License"); You may not use this file
@@ -166,7 +166,7 @@ bool OMVectorIterator<Element>::operator++()
 {
   TRACE("OMVectorIterator<Element>::operator++()");
 
-  PRECONDITION("Valid iterator", before() || valid());
+  PRECONDITION("Valid iterator", before() || this->valid());
 
   bool result;
   if (_state == OMVectorIteratorBefore) {
@@ -194,9 +194,9 @@ bool OMVectorIterator<Element>::operator++()
   }
   POSTCONDITION("Consistent result", IMPLIES( result, !after()));
   POSTCONDITION("Consistent result", IMPLIES(!result,  after()));
-  POSTCONDITION("Consistent result", IMPLIES( result,  valid()));
-  POSTCONDITION("Consistent result", IMPLIES(!result, !valid()));
-  POSTCONDITION("Valid index", IMPLIES(valid(), _index < _vector->count()));
+  POSTCONDITION("Consistent result", IMPLIES( result,  this->valid()));
+  POSTCONDITION("Consistent result", IMPLIES(!result, !this->valid()));
+  POSTCONDITION("Valid index", IMPLIES(this->valid(), _index < _vector->count()));
   return result;
 }
 
@@ -218,7 +218,7 @@ bool OMVectorIterator<Element>::operator--()
 {
   TRACE("OMVectorIterator<Element>::operator--()");
 
-  PRECONDITION("Valid iterator", after() || valid());
+  PRECONDITION("Valid iterator", after() || this->valid());
 
   bool result;
   if (_state == OMVectorIteratorAfter) {
@@ -246,9 +246,9 @@ bool OMVectorIterator<Element>::operator--()
   }
   POSTCONDITION("Consistent result", IMPLIES( result, !before()));
   POSTCONDITION("Consistent result", IMPLIES(!result,  before()));
-  POSTCONDITION("Consistent result", IMPLIES( result,  valid()));
-  POSTCONDITION("Consistent result", IMPLIES(!result, !valid()));
-  POSTCONDITION("Valid index", IMPLIES(valid(), _index < _vector->count()));
+  POSTCONDITION("Consistent result", IMPLIES( result,  this->valid()));
+  POSTCONDITION("Consistent result", IMPLIES(!result, !this->valid()));
+  POSTCONDITION("Valid index", IMPLIES(this->valid(), _index < _vector->count()));
   return result;
 }
 
@@ -263,7 +263,7 @@ Element& OMVectorIterator<Element>::value(void) const
 {
   TRACE("OMVectorIterator<Element>::value");
 
-  PRECONDITION("Valid iterator", valid());
+  PRECONDITION("Valid iterator", this->valid());
 
   return _vector->getAt(_index);
 }
@@ -280,7 +280,7 @@ Element OMVectorIterator<Element>::setValue(Element newElement)
 {
   TRACE("OMVectorIterator<Element>::setValue");
 
-  PRECONDITION("Valid iterator", valid());
+  PRECONDITION("Valid iterator", this->valid());
 
   OMVector<Element>* vector = const_cast<OMVector<Element>*>(_vector);
 
@@ -301,7 +301,7 @@ size_t OMVectorIterator<Element>::index(void) const
 {
   TRACE("OMVectorIterator<Element>::index");
 
-  PRECONDITION("Valid iterator", valid());
+  PRECONDITION("Valid iterator", this->valid());
 
   size_t result = _index;
 
