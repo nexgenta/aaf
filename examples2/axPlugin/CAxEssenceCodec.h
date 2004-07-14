b12b9073983cfdc69d223773002d2bf0898d37f8
stuart_hc <stuart_hc>
2004-07-14 14:44:05 +0000

    Fix compiler error under gcc-3.4.1

--- examples2/axPlugin/CAxEssenceCodec.h
+++ examples2/axPlugin/CAxEssenceCodec.h
@@ -3,7 +3,7 @@
 
 //=---------------------------------------------------------------------=
 //
-// $Id: CAxEssenceCodec.h,v 1.7 2004/02/27 14:26:39 stuart_hc Exp $ $Name:  $
+// $Id: CAxEssenceCodec.h,v 1.7.2.1 2004/07/14 14:44:05 stuart_hc Exp $ $Name:  $
 //
 // The contents of this file are subject to the AAF SDK Public
 // Source License Agreement (the "License"); You may not use this file
@@ -20,6 +20,8 @@
 // AAF Association.
 //
 //=---------------------------------------------------------------------=
+
+#include "AxPluginUtil.h"
 
 #include <AAFPlugin.h>
 #include <AAFResult.h>
