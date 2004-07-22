e9c901762afc4d7b887190fd5f51e9c87ba4c223
tbingham <tbingham>
2004-07-22 18:05:51 +0000

    Make ::clear() restore the list to a proper empty state.

--- ref-impl/src/OM/OMListT.h
+++ ref-impl/src/OM/OMListT.h
@@ -1,6 +1,6 @@
 //=---------------------------------------------------------------------=
 //
-// $Id: OMListT.h,v 1.6 2004/02/27 14:26:43 stuart_hc Exp $ $Name:  $
+// $Id: OMListT.h,v 1.6.2.1 2004/07/22 18:05:51 tbingham Exp $ $Name:  $
 //
 // The contents of this file are subject to the AAF SDK Public
 // Source License Agreement (the "License"); You may not use this file
@@ -287,6 +287,9 @@ void OMList<Element>::clear(void)
     p = p->_next;
     delete q;
   }
+  _nil->_next = _nil;
+  _nil->_previous = _nil;
+  _count = 0;
 }
 
   // @mfunc The number of elements with value <p value>.
