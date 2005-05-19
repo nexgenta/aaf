55212febbbf8f0918a680f2e3753e128e214e5ec
philipn <philipn>
2005-05-19 13:04:08 +0000

    Added function to register extendible enumeration extensions; changed assertion when getting element name or value to returning a null value or name when the lookup fails

--- ref-impl/src/impl/ImplAAFTypeDefExtEnum.h
+++ ref-impl/src/impl/ImplAAFTypeDefExtEnum.h
@@ -6,7 +6,7 @@
 
 //=---------------------------------------------------------------------=
 //
-// $Id: ImplAAFTypeDefExtEnum.h,v 1.23.2.2 2005/05/03 10:33:30 philipn Exp $ $Name:  $
+// $Id: ImplAAFTypeDefExtEnum.h,v 1.23.2.3 2005/05/19 13:04:08 philipn Exp $ $Name:  $
 //
 // The contents of this file are subject to the AAF SDK Public
 // Source License Agreement (the "License"); You may not use this file
@@ -260,6 +260,8 @@ public:
 
     virtual bool isValidValue(OMUniqueObjectIdentification value) const;
     
+    virtual bool registerExtensions(OMVector<const wchar_t*>& names, 
+        OMVector<OMUniqueObjectIdentification>& values);
         
 private:
 	// names of elements in this record; stored as single wchar_t array
