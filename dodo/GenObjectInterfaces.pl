53817ee6c6b129342c094d7089d468e7a88b6be4
montrowe <montrowe>
2005-06-08 15:34:36 +0000

    Doxygen formatting fixes and copyright year increment

--- dodo/GenObjectInterfaces.pl
+++ dodo/GenObjectInterfaces.pl
@@ -1,6 +1,6 @@
 ###############################################################################
 #
-# $Id: GenObjectInterfaces.pl,v 1.5 2005/02/10 17:47:27 heydowns Exp $ $Name:  $
+# $Id: GenObjectInterfaces.pl,v 1.6 2005/06/08 15:37:06 montrowe Exp $ $Name:  $
 #
 # The contents of this file are subject to the AAF SDK Public
 # Source License Agreement (the "License"); You may not use this file
@@ -13,7 +13,7 @@
 # the License for the specific language governing rights and limitations
 # under the License.
 #
-# The Original Code of this file is Copyright 1998-2004, Licensor of the
+# The Original Code of this file is Copyright 1998-2005, Licensor of the
 # AAF Association.
 #
 # The Initial Developer of the Original Code of this file and the
@@ -232,9 +232,6 @@ sub printSupportedInterfaces {
           print "///  - ", $obj->ex, "\n";
         }
       }
-      
-      # JD XXX
-      #print "\n";
     }
   }
 }
