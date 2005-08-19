2dc9909cec32998d704f03c9f441283254b096ef
tbingham <tbingham>
2005-08-19 18:03:56 +0000

    @Assert that we don't reach the next partition while searching for essence and/or index in thuis partition (akharkev).@ [199 - tbingham]

--- ref-impl/src/OM/OMMXFStorage.cpp
+++ ref-impl/src/OM/OMMXFStorage.cpp
@@ -1,6 +1,6 @@
 //=---------------------------------------------------------------------=
 //
-// $Id: OMMXFStorage.cpp,v 1.198 2005/08/19 18:03:51 tbingham Exp $ $Name:  $
+// $Id: OMMXFStorage.cpp,v 1.199 2005/08/19 18:03:56 tbingham Exp $ $Name:  $
 //
 // The contents of this file are subject to the AAF SDK Public
 // Source License Agreement (the "License"); You may not use this file
@@ -2136,6 +2136,7 @@ void OMMXFStorage::restoreStreams(void)
       keyPosition = position();
       bool b = readOuterKLVKey(k);
       ASSERT("Read key", b);
+      ASSERT("Key is not a partition key", !isPartition(k));
       length = readKLVLength();
       if (k == primerKey) {
         markMetadataStart(keyPosition);
