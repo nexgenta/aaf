14158e14825def9733f9e615c38d0e64e472b67e
tbingham <tbingham>
2005-08-19 19:33:22 +0000

    @Fix UDEV163790 - call IAAFStreamAccess::writeStream() for KLV encoded files (akharkev).@ [182 - tbingham]

--- ref-impl/src/OM/OMKLVStoredObject.cpp
+++ ref-impl/src/OM/OMKLVStoredObject.cpp
@@ -1,6 +1,6 @@
 //=---------------------------------------------------------------------=
 //
-// $Id: OMKLVStoredObject.cpp,v 1.202 2005/08/19 19:33:17 tbingham Exp $ $Name:  $
+// $Id: OMKLVStoredObject.cpp,v 1.203 2005/08/19 19:33:22 tbingham Exp $ $Name:  $
 //
 // The contents of this file are subject to the AAF SDK Public
 // Source License Agreement (the "License"); You may not use this file
@@ -42,6 +42,7 @@
 #include "OMObjectReference.h"
 #include "OMContainerElement.h"
 #include "OMDataStream.h"
+#include "OMDataStreamAccess.h"  // !!! tjb
 #include "OMIntegerType.h"
 #include "OMCharacterType.h"
 #include "OMStringType.h"
@@ -1499,6 +1500,22 @@ void OMKLVStoredObject::flatSave(const OMPropertySet& properties) const
         ASSERT("Supported stream byte order",
                                   (stream->storedByteOrder() == unspecified) ||
                                   (stream->storedByteOrder() == bigEndian));
+        // !!! tjb bug fix repeated code from OMDataStreamProperty::save()
+        if (stream->hasStreamAccess()) {
+          OMUInt64 savedPosition = _storage->position();
+          // Set the current position to the end of the stream
+          //
+          OMUInt64 lastPosition = stream->size();
+          OMUInt64 currentPosition = stream->position();
+          if (currentPosition != lastPosition) {
+            stream->setPosition(lastPosition);
+          }
+          // Allow clients to write to the stream
+          //
+          stream->streamAccess()->save(*stream);
+          _storage->setPosition(savedPosition);
+        }
+        // !!! tjb
         break;
       }
       default:
