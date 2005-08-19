b315e4f7424c2c2efbfebdc3c38900bd05496970
tbingham <tbingham>
2005-08-19 19:34:49 +0000

    @Add $Id$ and $Name$ CVS keywords to aid identification of version and branch.@ [14 - akharkev]

--- ref-impl/src/OM/OMKLVStoredStream.cpp
+++ ref-impl/src/OM/OMKLVStoredStream.cpp
@@ -1,5 +1,7 @@
 //=---------------------------------------------------------------------=
 //
+// $Id: OMKLVStoredStream.cpp,v 1.20 2005/08/19 19:34:49 tbingham Exp $ $Name:  $
+//
 // The contents of this file are subject to the AAF SDK Public
 // Source License Agreement (the "License"); You may not use this file
 // except in compliance with the License.  The License is available in
