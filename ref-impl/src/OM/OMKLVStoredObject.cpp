87c1030a3b1e02523d46279a90efd189c286e8f1
tbingham <tbingham>
2005-08-19 19:29:10 +0000

    @Add $Id$ and $Name$ CVS keywords to aid identification of version and branch.@ [135 - akharkev]

--- ref-impl/src/OM/OMKLVStoredObject.cpp
+++ ref-impl/src/OM/OMKLVStoredObject.cpp
@@ -1,5 +1,7 @@
 //=---------------------------------------------------------------------=
 //
+// $Id: OMKLVStoredObject.cpp,v 1.160 2005/08/19 19:29:10 tbingham Exp $ $Name:  $
+//
 // The contents of this file are subject to the AAF SDK Public
 // Source License Agreement (the "License"); You may not use this file
 // except in compliance with the License.  The License is available in
