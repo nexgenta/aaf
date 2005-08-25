e01f6902e12dd0a218e25b55aba6e3718f97981c
jptrainor <jptrainor>
2005-08-25 00:28:14 +0000

    Improve the AxExBadImpl error message because I got one (which is rare) due to a missing AxAUID implementation for a new interface, and the error was too cryptic.

--- examples2/axLib/AxEx.cpp
+++ examples2/axLib/AxEx.cpp
@@ -1,6 +1,6 @@
 //=---------------------------------------------------------------------=
 //
-// $Id: AxEx.cpp,v 1.8 2004/11/26 17:20:28 stuart_hc Exp $ $Name:  $
+// $Id: AxEx.cpp,v 1.9 2005/08/25 00:28:14 jptrainor Exp $ $Name:  $
 //
 // The contents of this file are subject to the AAF SDK Public
 // Source License Agreement (the "License"); You may not use this file
@@ -159,7 +159,7 @@ AxExSmartPointer::~AxExSmartPointer() throw()
 //=---------------------------------------------------------------------=
 
 AxExBadImp::AxExBadImp( const wchar_t* what )
-:	AxEx( what )
+  :	AxEx( AxString(AxString(L"Bad implementation: ") + AxString(what)).c_str() )
 {}
 
 AxExBadImp::~AxExBadImp() throw()
