ea618805c3de5943a0b0725e38d087ab0332c8a7
ajakowpa <ajakowpa>
2005-12-14 17:25:59 +0000

    Improved/Fixed component reference resolution

--- Utilities/AAFAnalyzer/BaseTestImpl/ResolveRefVisitor.h
+++ Utilities/AAFAnalyzer/BaseTestImpl/ResolveRefVisitor.h
@@ -1,6 +1,6 @@
 //=---------------------------------------------------------------------=
 //
-// $Id: ResolveRefVisitor.h,v 1.4 2005/11/14 19:46:50 ajakowpa Exp $
+// $Id: ResolveRefVisitor.h,v 1.5 2005/12/14 17:25:59 ajakowpa Exp $
 //
 // The contents of this file are subject to the AAF SDK Public
 // Source License Agreement (the "License"); You may not use this file
@@ -24,6 +24,20 @@
 //AAF Analyzer Base files
 #include <TypedVisitor.h>
 
+//AAF files
+#include <AAFSmartPointer2.h>
+
+//STL files
+#include <set>
+#include <stack>
+
+class AxSourceClip;
+class AxSegment;
+class AxComponent;
+class AxTimelineMobSlot;
+class AxEventMobSlot;
+class AxMobSlot;
+
 namespace aafanalyzer {
 
 using namespace std;
@@ -39,14 +53,30 @@ class ResolveRefVisitor : public TypedVisitor
   ResolveRefVisitor(wostream& os, shared_ptr<EdgeMap> spEdgeMap);
   virtual ~ResolveRefVisitor();
 
+  virtual bool PreOrderVisit(AAFTypedObjNode<IAAFTimelineMobSlot>& node);
+  virtual bool PreOrderVisit(AAFTypedObjNode<IAAFEventMobSlot>& node);
+  virtual bool PreOrderVisit(AAFTypedObjNode<IAAFStaticMobSlot>& node);
+  virtual bool PreOrderVisit(AAFTypedObjNode<IAAFMobSlot>& node);
+
   virtual bool PostOrderVisit(AAFTypedObjNode<IAAFSourceClip>& node);
-  
+  virtual bool PostOrderVisit(AAFTypedObjNode<IAAFTimelineMobSlot>& node);
+  virtual bool PostOrderVisit(AAFTypedObjNode<IAAFEventMobSlot>& node);
+  virtual bool PostOrderVisit(AAFTypedObjNode<IAAFStaticMobSlot>& node);
+  virtual bool PostOrderVisit(AAFTypedObjNode<IAAFMobSlot>& node);
+
   shared_ptr<const DetailLevelTestResult> GetTestResult() const;
 
  private:
+
+  shared_ptr<set<IAAFSmartPointer2<IAAFComponent> > > GetComponentsInRange( shared_ptr<Node> spMobSlotNode, AxSourceClip& axSrcClp );
+  shared_ptr<set<IAAFSmartPointer2<IAAFComponent> > > GetComponentsToTimelineSlot( AxTimelineMobSlot& axMobSlot, AxSourceClip& axSrcClp, AxSegment& asSegment, double startTime );
+  shared_ptr<set<IAAFSmartPointer2<IAAFComponent> > > GetComponentsToEventSlot( AxEventMobSlot& axMobSlot, AxSourceClip& axSrcClp );
+  shared_ptr<set<IAAFSmartPointer2<IAAFComponent> > > GetComponentsToFromGenericSlot( AxSegment& axSegment );
+
   wostream& _os;
   shared_ptr<EdgeMap> _spEdgeMap;
   shared_ptr< DetailLevelTestResult > _spResult;
+  stack<pair<aafUID_t, aafRational_t> > _parentSlots;
 
   // prohibited
   ResolveRefVisitor();
