bed36d07191bb6dd88cbb2d6f475795cba257de7
tbingham <tbingham>
2006-07-25 23:06:33 +0000

    Add AAFSetDiagnosticOutput() and IAAFDiagnosticOutput to support diagnostic output from the library to a client implemented destination.

--- dodo/aafobjects.mk
+++ dodo/aafobjects.mk
@@ -1,6 +1,6 @@
 ###############################################################################
 #
-# $Id: aafobjects.mk,v 1.162 2006/07/14 22:57:46 heydowns Exp $ $Name:  $
+# $Id: aafobjects.mk,v 1.163 2006/07/25 23:06:34 tbingham Exp $ $Name:  $
 #
 # The contents of this file are subject to the AAF SDK Public
 # Source License Agreement (the "License"); You may not use this file
@@ -13,7 +13,7 @@
 # the License for the specific language governing rights and limitations
 # under the License.
 #
-# The Original Code of this file is Copyright 1998-2005, Licensor of the
+# The Original Code of this file is Copyright 1998-2006, Licensor of the
 # AAF Association.
 #
 # The Initial Developer of the Original Code of this file and the
@@ -35,6 +35,7 @@ AAFCOMINTERFACESONLY="\
 	AAFCompositionMob2 \
 	AAFDataDef2 \
 	AAFDataDef3 \
+	AAFDiagnosticOutput \
 	AAFDictionary2 \
 	AAFDigitalImageDescriptor2 \
 	AAFEndian \
