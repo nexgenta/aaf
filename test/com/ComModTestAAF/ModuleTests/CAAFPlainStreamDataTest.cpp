e076d2ad897f2a920f553b2a71af08c36fd35811
stuart_hc <stuart_hc>
2007-04-17 12:47:02 +0000

    Add missing $Id$ CVS keyword.

--- test/com/ComModTestAAF/ModuleTests/CAAFPlainStreamDataTest.cpp
+++ test/com/ComModTestAAF/ModuleTests/CAAFPlainStreamDataTest.cpp
@@ -2,7 +2,7 @@
 // @com This file implements the module test for CAAFPlainStreamData
 //=---------------------------------------------------------------------=
 //
-// $$ $Name:  $
+// $Id: CAAFPlainStreamDataTest.cpp,v 1.2 2007/04/17 12:47:02 stuart_hc Exp $ $Name:  $
 //
 // The contents of this file are subject to the AAF SDK Public
 // Source License Agreement (the "License"); You may not use this file
