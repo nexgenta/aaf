ad04d18f7898b34abf2df4eb5f983f40b8155bbf
stuart_hc <stuart_hc>
2007-05-15 16:30:25 +0000

    Fix MSVC compiler warning.

--- Utilities/eli2aaf/eli2aaf.cpp
+++ Utilities/eli2aaf/eli2aaf.cpp
@@ -1,5 +1,5 @@
 //
-// $Id: eli2aaf.cpp,v 1.20 2005/08/30 15:10:48 stuart_hc Exp $ $Name:  $
+// $Id: eli2aaf.cpp,v 1.21 2007/05/15 16:30:25 stuart_hc Exp $ $Name:  $
 //
 // eli2aaf.cpp -	Create an AAF file with embedded media from a lavtools
 //					(mjpegtools) ELI format file.  Such a format is created
@@ -550,7 +550,7 @@ static bool addMasterMobForDVFile(
 	SET_PROPERTY_VALUE( CDCIOffsetToFrameIndexes, aafInt32, 0 );
 	SET_PROPERTY_VALUE( DIDFrameIndexByteOrder, aafUInt16, 0x4949 );
 	SET_PROPERTY_VALUE( DIDFirstFrameOffset, aafInt32, 0x0 );
-	SET_PROPERTY_VALUE( DIDImageSize, aafInt32, eslength );
+	SET_PROPERTY_VALUE( DIDImageSize, aafInt32, static_cast<aafInt32>(eslength) );
 	if (formatPAL)
 	{
 		SET_PROPERTY_VALUE( DIDResolutionID, aafUInt32, 0x8d );
