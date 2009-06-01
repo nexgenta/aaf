62e7bcc5b6530903f0bd5c5d0a681518df69636f
stuart_hc <stuart_hc>
2009-06-01 11:46:43 +0000

    Update AAF SDK license boilerplate text to Version 2.0

--- ref-impl/include/AAFCompressionDefs.h
+++ ref-impl/include/AAFCompressionDefs.h
@@ -5,24 +5,33 @@
 //
 // This file was GENERATED for the AAF SDK
 //
-// $Id: AAFCompressionDefs.h,v 1.13 2009/05/14 11:27:40 phil_tudor Exp $ $Name:  $
+// $Id: AAFCompressionDefs.h,v 1.14 2009/06/01 11:46:57 stuart_hc Exp $ $Name:  $
 //
-// The contents of this file are subject to the AAF SDK Public
-// Source License Agreement (the "License"); You may not use this file
-// except in compliance with the License.  The License is available in
-// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
-// Association or its successor.
+// The contents of this file are subject to the AAF SDK Public Source
+// License Agreement Version 2.0 (the "License"); You may not use this
+// file except in compliance with the License.  The License is available
+// in AAFSDKPSL.TXT, or you may obtain a copy of the License from the
+// Advanced Media Workflow Association, Inc., or its successor.
 //
 // Software distributed under the License is distributed on an "AS IS"
 // basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
 // the License for the specific language governing rights and limitations
-// under the License.
+// under the License.  Refer to Section 3.3 of the License for proper use
+// of this Exhibit.
 //
-// The Original Code of this file is Copyright 1998-2006, Licensor of the
-// AAF Association.
+// WARNING:  Please contact the Advanced Media Workflow Association,
+// Inc., for more information about any additional licenses to
+// intellectual property covering the AAF Standard that may be required
+// to create and distribute AAF compliant products.
+// (http://www.amwa.tv/policies).
+//
+// Copyright Notices:
+// The Original Code of this file is Copyright 1998-2009, licensor of the
+// Advanced Media Workflow Association.  All rights reserved.
 //
 // The Initial Developer of the Original Code of this file and the
-// Licensor of the AAF Association is Metaglue Corporation.
+// licensor of the Advanced Media Workflow Association is
+// Metaglue Corporation.
 // All rights reserved.
 //
 //=---------------------------------------------------------------------=
--- ref-impl/src/OM/OMSSSStorageRWInputSource.h
+++ ref-impl/src/OM/OMSSSStorageRWInputSource.h
@@ -1,23 +1,32 @@
 //=---------------------------------------------------------------------=
 //
-// $Id: OMSSSStorageRWInputSource.h,v 1.3 2005/10/13 16:45:14 rodrigc Exp $ $Name:  $
+// $Id: OMSSSStorageRWInputSource.h,v 1.4 2009/06/01 11:47:01 stuart_hc Exp $ $Name:  $
 //
-// The contents of this file are subject to the AAF SDK Public
-// Source License Agreement (the "License"); You may not use this file
-// except in compliance with the License.  The License is available in
-// AAFSDKPSL.TXT, or you may obtain a copy of the License from the AAF
-// Association or its successor.
+// The contents of this file are subject to the AAF SDK Public Source
+// License Agreement Version 2.0 (the "License"); You may not use this
+// file except in compliance with the License.  The License is available
+// in AAFSDKPSL.TXT, or you may obtain a copy of the License from the
+// Advanced Media Workflow Association, Inc., or its successor.
 //
 // Software distributed under the License is distributed on an "AS IS"
 // basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.  See
 // the License for the specific language governing rights and limitations
-// under the License.
+// under the License.  Refer to Section 3.3 of the License for proper use
+// of this Exhibit.
 //
-// The Original Code of this file is Copyright 1998-2004, Licensor of the
-// AAF Association.
+// WARNING:  Please contact the Advanced Media Workflow Association,
+// Inc., for more information about any additional licenses to
+// intellectual property covering the AAF Standard that may be required
+// to create and distribute AAF compliant products.
+// (http://www.amwa.tv/policies).
+//
+// Copyright Notices:
+// The Original Code of this file is Copyright 1998-2009, licensor of the
+// Advanced Media Workflow Association.  All rights reserved.
 //
 // The Initial Developer of the Original Code of this file and the
-// Licensor of the AAF Association is Schema Software Inc.
+// licensor of the Advanced Media Workflow Association is
+// Schema Software Inc.
 // All rights reserved.
 //
 //=---------------------------------------------------------------------=
@@ -31,8 +40,8 @@
 * Authors: Bob Sabiston, Yuri Khramov, Mark Ambachtsheer
 ************************************************************************
 
-$Revision: 1.3 $
-$Date: 2005/10/13 16:45:14 $
+$Revision: 1.4 $
+$Date: 2009/06/01 11:47:01 $
 */
 #ifndef OMSSSSTORAGERWINPUTSOURCE_H
 #define OMSSSSTORAGERWINPUTSOURCE_H
